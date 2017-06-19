53ecdcf39cda55b12564d4c1d6b7faf6b2c3278f
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-06-19 16:46:03 +0000

    39f22c2 build: remove duplicate licenses in bundles (#5195)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -11,20 +11,6 @@
 	(factory((global.ng = global.ng || {}, global.ng.cdk = global.ng.cdk || {})));
 }(this, (function (exports) { 'use strict';
 
-/**
- * @license
- * Copyright Google Inc. All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
- */
-/**
- * @license
- * Copyright Google Inc. All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
- */
 var __TEMP__ = -1;
 
 exports.__TEMP__ = __TEMP__;
