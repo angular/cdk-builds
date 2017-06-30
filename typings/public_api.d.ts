d9ab7d7d03954cae5e1aa7d2b0c5d7ad574c758b
Jeremy Elbourn <jelbourn@gmail.com>
2017-06-30 18:31:12 +0000

    944aa39 chore(paginator): correct example name in overview (#5442)

--- typings/public_api.d.ts
+++ typings/public_api.d.ts
@@ -5,5 +5,11 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+export * from './a11y/index';
+export * from './bidi/index';
 export * from './coercion/index';
 export * from './table/index';
+export * from './platform/index';
+export * from './portal/index';
+export * from './rxjs/index';
+export * from './keyboard/keycodes';
