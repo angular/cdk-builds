800c3681f9cb327a6929de0cd976eb1b737d0420
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-08-01 21:38:35 +0000

    235f664 fix(progress-spinner): prevent users from tabbing into underlying SVG on IE (#6142)

--- @angular/cdk.es5.js
+++ @angular/cdk.es5.js
@@ -9,7 +9,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('2.0.0-beta.8-f5dc120');
+var VERSION = new Version('2.0.0-beta.8-235f664');
 /**
  * Generated bundle index. Do not edit.
  */
--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-f5dc120');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-235f664');
 
 exports.VERSION = VERSION;
 
