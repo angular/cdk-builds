0b054ce981a91b53a3f716300fcb6da640fd1ce5
mmalerba <mmalerba@google.com>
2017-08-01 21:36:33 +0000

    21e9768 fix(datepicker): mark input tocuhed on calendar date selected (#6007)

--- @angular/cdk.es5.js
+++ @angular/cdk.es5.js
@@ -9,7 +9,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('2.0.0-beta.8-3c19234');
+var VERSION = new Version('2.0.0-beta.8-21e9768');
 /**
  * Generated bundle index. Do not edit.
  */
