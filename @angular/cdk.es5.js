1a1a121f45c201bb5f782fa726dd1785861b9cb1
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-08-01 20:17:28 +0000

    20cd8a1 build: dynamic path mappings for cdk entry points (#6138)

--- @angular/cdk.es5.js
+++ @angular/cdk.es5.js
@@ -9,7 +9,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('2.0.0-beta.8-2320532');
+var VERSION = new Version('2.0.0-beta.8-20cd8a1');
 /**
  * Generated bundle index. Do not edit.
  */
--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-2320532');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-20cd8a1');
 
 exports.VERSION = VERSION;
 
