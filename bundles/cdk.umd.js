3d430f8ca120a7ddffde76299a7c0b3b7f319fef
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-08-01 22:25:04 +0000

    0d80a77 fix(sidenav): container not reacting to changes to sidenavs added after init (#6161)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-a185581');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-0d80a77');
 
 exports.VERSION = VERSION;
 
