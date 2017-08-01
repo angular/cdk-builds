ddf07e9cd7ff18da08e7c956ba19d2b69a425336
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-08-01 20:13:12 +0000

    2320532 build: no longer add pure annotations to es5 bundles (#6107)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-f78726d');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-2320532');
 
 exports.VERSION = VERSION;
 
