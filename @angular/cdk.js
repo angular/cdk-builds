8aa36f7e624d3dbbc09549e42ad3e7cb2e2201f5
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-08-03 00:25:30 +0000

    916d1f3 fix(autocomplete,datepicker,menu): closing parent dialog by pressing escape (#6226)

--- @angular/cdk.js
+++ @angular/cdk.js
@@ -10,7 +10,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('2.0.0-beta.8-a043bec');
+const VERSION = new Version('2.0.0-beta.8-916d1f3');
 
 /**
  * Generated bundle index. Do not edit.
--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-a043bec');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-916d1f3');
 
 exports.VERSION = VERSION;
 
