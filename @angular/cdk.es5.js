b688ddee81cbd31be7e2a42a8c0125051b63282f
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-08-03 00:28:51 +0000

    5967f6e fix(dialog): better handling of custom ViewContainerRef with OnPush change detection (#6164)

--- @angular/cdk.es5.js
+++ @angular/cdk.es5.js
@@ -9,7 +9,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('2.0.0-beta.8-916d1f3');
+var VERSION = new Version('2.0.0-beta.8-5967f6e');
 /**
  * Generated bundle index. Do not edit.
  */
--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-916d1f3');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-5967f6e');
 
 exports.VERSION = VERSION;
 
