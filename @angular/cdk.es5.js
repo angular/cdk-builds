fc7be5253e3cf83be008238de905f8a8477eeec9
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-08-04 21:20:04 +0000

    ea6e829 build: retry attemps for tunnel connections (#6246)

--- @angular/cdk.es5.js
+++ @angular/cdk.es5.js
@@ -9,7 +9,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('2.0.0-beta.8-991c1e0');
+var VERSION = new Version('2.0.0-beta.8-ea6e829');
 /**
  * Generated bundle index. Do not edit.
  */
--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-991c1e0');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-ea6e829');
 
 exports.VERSION = VERSION;
 
