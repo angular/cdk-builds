277e5b66b9a38dcbce3389a6535adff7d3ba3fd9
Jeremy Elbourn <jelbourn@gmail.com>
2017-08-04 21:22:03 +0000

    b0db912 chore: block use of `public` access modifier outside constructors (#6261)

--- @angular/cdk.es5.js
+++ @angular/cdk.es5.js
@@ -9,7 +9,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('2.0.0-beta.8-e86b964');
+var VERSION = new Version('2.0.0-beta.8-b0db912');
 /**
  * Generated bundle index. Do not edit.
  */
--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-e86b964');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-b0db912');
 
 exports.VERSION = VERSION;
 
