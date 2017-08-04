8c5d9d687873d8a388fd7bbcd587214a989d2d3c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-08-04 21:19:55 +0000

    991c1e0 chore(tslint): use globs for custom rule source file filtering (#6235)

--- @angular/cdk.js
+++ @angular/cdk.js
@@ -10,7 +10,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('2.0.0-beta.8-1af2ce6');
+const VERSION = new Version('2.0.0-beta.8-991c1e0');
 
 /**
  * Generated bundle index. Do not edit.
--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-1af2ce6');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-991c1e0');
 
 exports.VERSION = VERSION;
 
