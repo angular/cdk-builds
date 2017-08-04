dabf088f6b133496bd40ce9a19a87b94dd6d2a26
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-08-04 21:21:26 +0000

    e86b964 a11y: add autocomplete accessibility demo (#6254)

--- @angular/cdk.js
+++ @angular/cdk.js
@@ -10,7 +10,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('2.0.0-beta.8-ea6e829');
+const VERSION = new Version('2.0.0-beta.8-e86b964');
 
 /**
  * Generated bundle index. Do not edit.
--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-ea6e829');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-e86b964');
 
 exports.VERSION = VERSION;
 
