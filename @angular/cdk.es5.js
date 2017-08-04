e63235aa10529d92748cf0a8a83ef04bdaaa38c2
Cyrille Tuzi <web@cyrilletuzi.com>
2017-08-04 21:13:11 +0000

    1af2ce6 docs(getting-started): mention importing hammer in same file as bootstrap (#6225)

--- @angular/cdk.es5.js
+++ @angular/cdk.es5.js
@@ -9,7 +9,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('2.0.0-beta.8-5967f6e');
+var VERSION = new Version('2.0.0-beta.8-1af2ce6');
 /**
  * Generated bundle index. Do not edit.
  */
--- @angular/cdk.js
+++ @angular/cdk.js
@@ -10,7 +10,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('2.0.0-beta.8-5967f6e');
+const VERSION = new Version('2.0.0-beta.8-1af2ce6');
 
 /**
  * Generated bundle index. Do not edit.
