f52e41367d2a5b18e211d444495a17b55102f72d
mmalerba <mmalerba@google.com>
2017-08-04 22:49:44 +0000

    2af284c fix(input): update aria-describedby to also include errors (#6239)

--- @angular/cdk.js
+++ @angular/cdk.js
@@ -10,7 +10,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('2.0.0-beta.8-73c6d8d');
+const VERSION = new Version('2.0.0-beta.8-2af284c');
 
 /**
  * Generated bundle index. Do not edit.
--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-73c6d8d');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-2af284c');
 
 exports.VERSION = VERSION;
 
