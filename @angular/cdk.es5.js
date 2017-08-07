6a2736fffb3f2a601802f49db0a05bfc6277d921
Will Howell <will.s.howell@gmail.com>
2017-08-07 21:43:09 +0000

    667a4e4 fix(autocomplete): attach overlay to a more accurate input element (#6282)

--- @angular/cdk.es5.js
+++ @angular/cdk.es5.js
@@ -9,7 +9,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('2.0.0-beta.8-faa7601');
+var VERSION = new Version('2.0.0-beta.8-667a4e4');
 /**
  * Generated bundle index. Do not edit.
  */
