fd67fda3ef0eeaad664cf46c8a850c57e1f63ec1
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-08-07 21:40:38 +0000

    faa7601 fix(overlay): error when removing empty string theme (#6306)

--- @angular/cdk.js
+++ @angular/cdk.js
@@ -10,7 +10,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('2.0.0-beta.8-18a85f3');
+const VERSION = new Version('2.0.0-beta.8-faa7601');
 
 /**
  * Generated bundle index. Do not edit.
--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-18a85f3');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-faa7601');
 
 exports.VERSION = VERSION;
 
