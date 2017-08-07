3c4b40f94c291858403bad215db05f87a209cacc
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-08-07 21:48:40 +0000

    18c6689 build: disallow usage of HostBinding and HostListener (#6307)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-a190de7');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-18c6689');
 
 exports.VERSION = VERSION;
 
