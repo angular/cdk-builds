c4bab27265ea262271d88c443b8adf194b84090f
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-08-07 17:39:42 +0000

    509e3e9 build: fix error when running devapp on windows (#6288)

--- @angular/cdk.es5.js
+++ @angular/cdk.es5.js
@@ -9,7 +9,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('2.0.0-beta.8-5ecc509');
+var VERSION = new Version('2.0.0-beta.8-509e3e9');
 /**
  * Generated bundle index. Do not edit.
  */
