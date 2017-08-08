fa6af7c1ebfda7ab852f07740e55a3c3b1c985d9
Scott <saintscott119@gmail.com>
2017-08-08 20:48:02 +0000

    7bc648b feat(datepicker): export unexported components for potential extension (#6314)

--- @angular/cdk.es5.js
+++ @angular/cdk.es5.js
@@ -9,7 +9,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('2.0.0-beta.8-4ae1b0f');
+var VERSION = new Version('2.0.0-beta.8-7bc648b');
 /**
  * Generated bundle index. Do not edit.
  */
--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-4ae1b0f');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-7bc648b');
 
 exports.VERSION = VERSION;
 
