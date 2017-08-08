7cbc515ac8b4d9da02b2f1b4c41ac50a70a186ad
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-08-08 20:50:17 +0000

    333ec80 build: fix screenshot tool deployment (#6337)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-7bc648b');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-333ec80');
 
 exports.VERSION = VERSION;
 
