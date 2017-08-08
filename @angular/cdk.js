1d5309835b596429c3f347e2f4a9d325aeae32de
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-08-08 20:43:08 +0000

    4ae1b0f feat(tab-nav-bar): allow disabling ripples for links (#6273)

--- @angular/cdk.js
+++ @angular/cdk.js
@@ -10,7 +10,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('2.0.0-beta.8-c20bcf9');
+const VERSION = new Version('2.0.0-beta.8-4ae1b0f');
 
 /**
  * Generated bundle index. Do not edit.
--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-c20bcf9');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-4ae1b0f');
 
 exports.VERSION = VERSION;
 
