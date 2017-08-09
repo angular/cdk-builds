0605127f9c559de5e131c81c85b1908b811f7333
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-08-09 22:22:34 +0000

    d66d735 chore: add md-table to universal app (#6356)

--- @angular/cdk.js
+++ @angular/cdk.js
@@ -10,7 +10,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('2.0.0-beta.8-2286ad5');
+const VERSION = new Version('2.0.0-beta.8-d66d735');
 
 /**
  * Generated bundle index. Do not edit.
--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-2286ad5');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-d66d735');
 
 exports.VERSION = VERSION;
 
