ca1977a31d2101491efbfea88c76e1a98965b75a
tinayuangao <tinagao@google.com>
2017-08-09 22:18:50 +0000

    d48b957 demo(icon): Add an accessibility demo page for icon (#6348)

--- @angular/cdk.js
+++ @angular/cdk.js
@@ -10,7 +10,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('2.0.0-beta.8-0e4680f');
+const VERSION = new Version('2.0.0-beta.8-d48b957');
 
 /**
  * Generated bundle index. Do not edit.
--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-0e4680f');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-d48b957');
 
 exports.VERSION = VERSION;
 
