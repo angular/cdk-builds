8d7b01a1dc247f0e044853efc42c303dd0650d04
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-08-09 22:26:17 +0000

    72c5d39 feat(select): add ability to customize the select trigger (#3341)

--- @angular/cdk.js
+++ @angular/cdk.js
@@ -10,7 +10,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('2.0.0-beta.8-d66d735');
+const VERSION = new Version('2.0.0-beta.8-72c5d39');
 
 /**
  * Generated bundle index. Do not edit.
--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-d66d735');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-72c5d39');
 
 exports.VERSION = VERSION;
 
