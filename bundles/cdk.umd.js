c7ce1324935a5fce735c2217ceae9bd91923b344
Carlos Galan Cladera <cgcladera@gmail.com>
2017-08-09 06:44:07 +0000

    b4e2488 fix(theming): Light green 700 contrast should be white (#6346)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-f96ffeb');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-b4e2488');
 
 exports.VERSION = VERSION;
 
