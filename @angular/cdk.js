a0c7d256ac25a2a90a905104e9e0a2bc6d4219e4
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-08-09 06:48:22 +0000

    fd5e5ff fix: remove ripples in high contrast mode (#6355)

--- @angular/cdk.js
+++ @angular/cdk.js
@@ -10,7 +10,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('2.0.0-beta.8-b4e2488');
+const VERSION = new Version('2.0.0-beta.8-fd5e5ff');
 
 /**
  * Generated bundle index. Do not edit.
--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-b4e2488');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-fd5e5ff');
 
 exports.VERSION = VERSION;
 
