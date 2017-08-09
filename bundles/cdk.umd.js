a1a4aee1357ef5e2b334fc76c148fa87226f4157
tinayuangao <tinagao@google.com>
2017-08-09 22:16:38 +0000

    0e4680f demo(progress-spinner): Add progress spinner accessibility demo page (#6347)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-fd5e5ff');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-0e4680f');
 
 exports.VERSION = VERSION;
 
