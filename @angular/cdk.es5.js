9c92ca531cb2ec1e388dc6be3953a4d998a1b4a5
mmalerba <mmalerba@google.com>
2017-08-12 22:24:36 +0000

    85a6fff fix demo app (#6437)

--- @angular/cdk.es5.js
+++ @angular/cdk.es5.js
@@ -9,7 +9,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('2.0.0-beta.8-4d82f83');
+var VERSION = new Version('2.0.0-beta.8-85a6fff');
 /**
  * Generated bundle index. Do not edit.
  */
--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-4d82f83');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-85a6fff');
 
 exports.VERSION = VERSION;
 
