474f3df524a4d76162931e9814a5e8c9df21e2ac
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-08-15 22:15:34 +0000

    1df79e9 perf(tabs): avoid extra resize handler and reflow (#6434)

--- @angular/cdk.js
+++ @angular/cdk.js
@@ -10,7 +10,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('2.0.0-beta.8-85a6fff');
+const VERSION = new Version('2.0.0-beta.8-1df79e9');
 
 /**
  * Generated bundle index. Do not edit.
