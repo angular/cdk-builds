db54066610ce37914c2527b0288901d4ed84bf0c
mmalerba <mmalerba@google.com>
2017-08-15 22:25:10 +0000

    c668075 cleanup(input): rename md-input-container to md-form-field throughout material (#6421)

--- @angular/cdk.es5.js
+++ @angular/cdk.es5.js
@@ -9,7 +9,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('2.0.0-beta.8-54158fd');
+var VERSION = new Version('2.0.0-beta.8-c668075');
 /**
  * Generated bundle index. Do not edit.
  */
