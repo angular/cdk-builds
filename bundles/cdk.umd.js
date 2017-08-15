009e5ecfc847fbd47112fb3d728220280cc5bef3
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-08-15 22:28:09 +0000

    ea17d3d fix(datepicker): remove toggle icon from tab order on IE (#6405)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-c668075');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-ea17d3d');
 
 exports.VERSION = VERSION;
 
