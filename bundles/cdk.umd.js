5a2a604d7e346c30e31cceb79165827580b0cba8
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-08-15 22:16:47 +0000

    5d437ea refactor: remove usages of `if in` checks (#6436)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-1df79e9');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-5d437ea');
 
 exports.VERSION = VERSION;
 
