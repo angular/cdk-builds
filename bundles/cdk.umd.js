90371c24897e81444dea82b22ab9c6593c9b0dea
tinayuangao <tinagao@google.com>
2017-08-15 22:24:56 +0000

    54158fd docs(a11y): Add a11y docs to button, button toggle, checkbox, progress spinner/ba… (#6402)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-b5f4caf');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-54158fd');
 
 exports.VERSION = VERSION;
 
