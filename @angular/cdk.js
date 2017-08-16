ce1323ee8509b5d7afb26d1821cccdc6177e2ea6
Andrew Seguin <andrewseguin@users.noreply.github.com>
2017-08-16 17:52:36 +0000

    1807c5b fix(select): lint issue (#6497)

--- @angular/cdk.js
+++ @angular/cdk.js
@@ -10,7 +10,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('2.0.0-beta.8-9ba5d84');
+const VERSION = new Version('2.0.0-beta.8-1807c5b');
 
 /**
  * Generated bundle index. Do not edit.
