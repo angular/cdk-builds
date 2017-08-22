f179606d2c8f14f2fd4b0eadbe8963dd1ea42015
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-08-22 21:37:49 +0000

    7f0e58e fix(expansion-panel): remove margin from top and bottom panels in accordion (#6546)

--- @angular/cdk.js
+++ @angular/cdk.js
@@ -10,7 +10,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('2.0.0-beta.8-9eb9ddf');
+const VERSION = new Version('2.0.0-beta.8-7f0e58e');
 
 /**
  * Generated bundle index. Do not edit.
--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-9eb9ddf');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-7f0e58e');
 
 exports.VERSION = VERSION;
 
