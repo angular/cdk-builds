6d6329507d94cca73e91025032e1e14ed47a330c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-08-23 18:27:24 +0000

    4cff5ce build: update saucelabs connector (#6609)

--- @angular/cdk.es5.js
+++ @angular/cdk.es5.js
@@ -9,7 +9,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('2.0.0-beta.8-595cffd');
+var VERSION = new Version('2.0.0-beta.8-4cff5ce');
 /**
  * Generated bundle index. Do not edit.
  */
--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-595cffd');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-4cff5ce');
 
 exports.VERSION = VERSION;
 
