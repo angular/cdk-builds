173422513170e06003433d790244cbffda97bbad
Jeremy Elbourn <jelbourn@gmail.com>
2017-08-23 21:01:10 +0000

    3b46360 fix(theming): remove non-standard grey palette hues (#6605)

--- @angular/cdk.es5.js
+++ @angular/cdk.es5.js
@@ -9,7 +9,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('2.0.0-beta.8-32f42d6');
+var VERSION = new Version('2.0.0-beta.8-3b46360');
 /**
  * Generated bundle index. Do not edit.
  */
--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-32f42d6');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-3b46360');
 
 exports.VERSION = VERSION;
 
