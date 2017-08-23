e89b00d1064bb9ceb55e1d5b241df24d693243ce
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-08-23 01:03:44 +0000

    0526689 fix(select): error if triggerValue is accessed from an empty select (#6575)

--- @angular/cdk.js
+++ @angular/cdk.js
@@ -10,7 +10,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('2.0.0-beta.8-7f0e58e');
+const VERSION = new Version('2.0.0-beta.8-0526689');
 
 /**
  * Generated bundle index. Do not edit.
