fdb999abd75857341680eac9b97bae69a9d77e4b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-08-24 17:31:40 +0000

    dde6e19 build: better caching of node modules in travis (#6619)

--- @angular/cdk.js
+++ @angular/cdk.js
@@ -10,7 +10,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('2.0.0-beta.8-eef28c0');
+const VERSION = new Version('2.0.0-beta.8-dde6e19');
 
 /**
  * Generated bundle index. Do not edit.
