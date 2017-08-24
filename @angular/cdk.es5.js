e89c35c3c5922b34b1f84b7c869bf7d24f7191d8
Will Howell <will.s.howell@gmail.com>
2017-08-24 17:24:54 +0000

    f104101 docs(input): remove extra parens and fix nits (#6614)

--- @angular/cdk.es5.js
+++ @angular/cdk.es5.js
@@ -9,7 +9,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('2.0.0-beta.8-3b46360');
+var VERSION = new Version('2.0.0-beta.8-f104101');
 /**
  * Generated bundle index. Do not edit.
  */
--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-3b46360');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-f104101');
 
 exports.VERSION = VERSION;
 
