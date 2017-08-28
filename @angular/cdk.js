8d8d5d9854374fbd24bee709fba8a37a02f40813
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-08-28 23:58:13 +0000

    9644840 build: write custom tslint rules in typescript (#6650)

--- @angular/cdk.js
+++ @angular/cdk.js
@@ -10,7 +10,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('2.0.0-beta.8-5d1ce9f');
+const VERSION = new Version('2.0.0-beta.8-9644840');
 
 /**
  * Generated bundle index. Do not edit.
--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-5d1ce9f');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-9644840');
 
 exports.VERSION = VERSION;
 
