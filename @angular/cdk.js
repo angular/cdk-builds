004fbde50a0e48ae8b250733fc3ca95fcea15573
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-08-30 02:59:31 +0000

    950edcc build: invalid paths when building examples module (#6673)

--- @angular/cdk.js
+++ @angular/cdk.js
@@ -10,7 +10,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('2.0.0-beta.10-43b7c34');
+const VERSION = new Version('2.0.0-beta.10-950edcc');
 
 /**
  * Generated bundle index. Do not edit.
--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.10-43b7c34');
+var VERSION = new _angular_core.Version('2.0.0-beta.10-950edcc');
 
 exports.VERSION = VERSION;
 
