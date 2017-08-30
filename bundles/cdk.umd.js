adfe8285962980539f2b9bba1873d887455fff9e
Yevhen Horbachov <killabee1985@gmail.com>
2017-08-30 03:01:19 +0000

    70bd5fc feat(sort): add sorting indicator animation (#5831)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.10-2b74755');
+var VERSION = new _angular_core.Version('2.0.0-beta.10-70bd5fc');
 
 exports.VERSION = VERSION;
 
