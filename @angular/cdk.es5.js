7401a42655e388b15c5add24481a4d7fde0de4c8
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-09-01 15:24:24 +0000

    89fea50 fix(autosize): not resizing on programmatic changes (#6654)

--- @angular/cdk.es5.js
+++ @angular/cdk.es5.js
@@ -9,7 +9,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('2.0.0-beta.10-b678119');
+var VERSION = new Version('2.0.0-beta.10-89fea50');
 /**
  * Generated bundle index. Do not edit.
  */
--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.10-b678119');
+var VERSION = new _angular_core.Version('2.0.0-beta.10-89fea50');
 
 exports.VERSION = VERSION;
 
