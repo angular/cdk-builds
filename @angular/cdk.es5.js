a75a86dd01c91bdc19e049ac7a7454845a72f5f9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-09-06 17:55:35 +0000

    f1b78d4 compat: selection list not working in compatibility mode (#6801)

--- @angular/cdk.es5.js
+++ @angular/cdk.es5.js
@@ -9,7 +9,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('2.0.0-beta.10-87d607e');
+var VERSION = new Version('2.0.0-beta.10-f1b78d4');
 /**
  * Generated bundle index. Do not edit.
  */
