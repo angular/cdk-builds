48b884921997f734c566a4267188c3aff5178161
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-09-06 17:35:21 +0000

    001001c build: custom tslint rules not executing in IDEs (#6817)

--- @angular/cdk.es5.js
+++ @angular/cdk.es5.js
@@ -9,7 +9,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('2.0.0-beta.10-3989153');
+var VERSION = new Version('2.0.0-beta.10-001001c');
 /**
  * Generated bundle index. Do not edit.
  */
--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.10-3989153');
+var VERSION = new _angular_core.Version('2.0.0-beta.10-001001c');
 
 exports.VERSION = VERSION;
 
