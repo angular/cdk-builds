9eb70db66e4287f0465a5dd237a76270a6ee1e02
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-09-06 17:30:14 +0000

    0786518 chore: wrong link to stepper in demo app (#6790)

--- @angular/cdk.es5.js
+++ @angular/cdk.es5.js
@@ -9,7 +9,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('2.0.0-beta.10-5527e22');
+var VERSION = new Version('2.0.0-beta.10-0786518');
 /**
  * Generated bundle index. Do not edit.
  */
--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.10-5527e22');
+var VERSION = new _angular_core.Version('2.0.0-beta.10-0786518');
 
 exports.VERSION = VERSION;
 
