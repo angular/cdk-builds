fc26750fc0865e2257c69e44ead383b3a3dfb9a7
Jeremy Elbourn <jelbourn@gmail.com>
2017-09-12 18:05:12 +0000

    acbb1d4 chore: remove accidental premature changelog (#7019)

--- @angular/cdk.es5.js
+++ @angular/cdk.es5.js
@@ -9,7 +9,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('2.0.0-beta.11-50a6bb7');
+var VERSION = new Version('2.0.0-beta.11-acbb1d4');
 /**
  * Generated bundle index. Do not edit.
  */
--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.11-50a6bb7');
+var VERSION = new _angular_core.Version('2.0.0-beta.11-acbb1d4');
 
 exports.VERSION = VERSION;
 
