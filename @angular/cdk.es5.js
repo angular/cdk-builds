345b8f7f7a3d026a9fe13919a2f953340b489830
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-09-12 20:42:12 +0000

    04bf3d1 fix(menu): nested trigger staying highlighted after click (#6853)

--- @angular/cdk.es5.js
+++ @angular/cdk.es5.js
@@ -9,7 +9,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('2.0.0-beta.11-881630f');
+var VERSION = new Version('2.0.0-beta.11-04bf3d1');
 /**
  * Generated bundle index. Do not edit.
  */
--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.11-881630f');
+var VERSION = new _angular_core.Version('2.0.0-beta.11-04bf3d1');
 
 exports.VERSION = VERSION;
 
