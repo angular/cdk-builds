5142c734bf3b06d84cc03ed463a04e4966ef97a1
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-09-12 00:42:14 +0000

    85993d3 fix(datepicker): toggle not reacting to disabled state changes in datepicker or input (#6964)

--- @angular/cdk.js
+++ @angular/cdk.js
@@ -10,7 +10,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('2.0.0-beta.11-2cefe67');
+const VERSION = new Version('2.0.0-beta.11-85993d3');
 
 /**
  * Generated bundle index. Do not edit.
