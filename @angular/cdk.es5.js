ee7de344e3d1c41df29dcce9a65395cd12c10efc
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-09-12 20:36:39 +0000

    bcd026f fix(tooltip): closing immediately when triggered on click (#6590)

--- @angular/cdk.es5.js
+++ @angular/cdk.es5.js
@@ -9,7 +9,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('2.0.0-beta.11-acbb1d4');
+var VERSION = new Version('2.0.0-beta.11-bcd026f');
 /**
  * Generated bundle index. Do not edit.
  */
