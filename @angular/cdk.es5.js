a9691744d5c734dc6161e76acae65a724aee0121
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-09-12 00:47:47 +0000

    a1ec81a fix(slide-toggle): remove side-margin if slide-toggle label is empty (#6881)

--- @angular/cdk.es5.js
+++ @angular/cdk.es5.js
@@ -9,7 +9,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('2.0.0-beta.11-2239e7d');
+var VERSION = new Version('2.0.0-beta.11-a1ec81a');
 /**
  * Generated bundle index. Do not edit.
  */
