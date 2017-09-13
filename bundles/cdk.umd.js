b140c4e36aa5d0952b06639d4d83b722ae74a1ab
mmalerba <mmalerba@google.com>
2017-09-13 03:27:29 +0000

    dcfe515 Revert "fix(menu): multiple close events for a single close" (#7036)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.11-9545427');
+var VERSION = new _angular_core.Version('2.0.0-beta.11-dcfe515');
 
 exports.VERSION = VERSION;
 
