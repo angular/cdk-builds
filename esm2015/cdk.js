0932a33c13146cc1a18a556fcab29df0d789f879
Jeremy Elbourn <jelbourn@gmail.com>
2017-09-15 00:08:14 +0000

    5daf7ff docs(overlay): expand overview for overlay (#6976)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -10,7 +10,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('2.0.0-beta.11-1cb8907');
+const VERSION = new Version('2.0.0-beta.11-5daf7ff');
 
 /**
  * Generated bundle index. Do not edit.
