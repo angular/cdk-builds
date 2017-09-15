1754c4c23f220e5314a43789a1221beb18f1ac6d
Joey Perrott <josephperrott@gmail.com>
2017-09-15 00:16:31 +0000

    ed74da7 Bump to gulp-highlight-files@0.0.5 (#7085)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -9,7 +9,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('2.0.0-beta.11-5daf7ff');
+var VERSION = new Version('2.0.0-beta.11-ed74da7');
 /**
  * Generated bundle index. Do not edit.
  */
