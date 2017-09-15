e7327d574fee4611b98e3b17425ad06cda7d95b9
Jeremy Elbourn <jelbourn@gmail.com>
2017-09-15 00:28:25 +0000

    b8218e1 build: fix regex for matching imports for umd generation (#7093)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.11-34b5620');
+var VERSION = new _angular_core.Version('2.0.0-beta.11-b8218e1');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -10,7 +10,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('2.0.0-beta.11-34b5620');
+const VERSION = new Version('2.0.0-beta.11-b8218e1');
 
 /**
  * Generated bundle index. Do not edit.
