86ce8a7f420e4e77d38869edf02fc4b611877b25
Jeremy Elbourn <jelbourn@gmail.com>
2017-09-15 22:45:12 +0000

    df808b8 build: fix material unit tests not running (#7117)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.11-548eca0');
+var VERSION = new _angular_core.Version('2.0.0-beta.11-df808b8');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -9,7 +9,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('2.0.0-beta.11-548eca0');
+var VERSION = new Version('2.0.0-beta.11-df808b8');
 /**
  * Generated bundle index. Do not edit.
  */
