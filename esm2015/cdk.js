6c4176007349c95d3c881aa674b15c815bf6b5ab
Jeremy Elbourn <jelbourn@gmail.com>
2017-09-15 21:05:57 +0000

    b296e2b build: compile entry-points in parallel (#7094)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -10,7 +10,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('2.0.0-beta.11-b8218e1');
+const VERSION = new Version('2.0.0-beta.11-b296e2b');
 
 /**
  * Generated bundle index. Do not edit.
