91b4933a54e2ca018139cd753c129650edba4059
Jeremy Elbourn <jelbourn@gmail.com>
2017-09-15 21:12:12 +0000

    a96b545 fix: add `mat` exportAs and class aliases (#7106)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.11-b296e2b');
+var VERSION = new _angular_core.Version('2.0.0-beta.11-a96b545');
 
 exports.VERSION = VERSION;
 
