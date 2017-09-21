c7c080264db136bd38ba3b85c3e5b3a655aaa841
mmalerba <mmalerba@google.com>
2017-09-21 03:38:53 +0000

    8436f8c fix(datepicker): allow date or datetime strings in fromIso8601 (#7220)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -9,7 +9,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('2.0.0-beta.11-3025b30');
+var VERSION = new Version('2.0.0-beta.11-8436f8c');
 /**
  * Generated bundle index. Do not edit.
  */
