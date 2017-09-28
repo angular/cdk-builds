eddfe9537769f1a4fa2a16e3aea1f0a99904f7c3
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-09-28 22:38:48 +0000

    3bbbb8a chore: stepper e2e failures (#7237)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -10,7 +10,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('2.0.0-beta.11-6572879');
+const VERSION = new Version('2.0.0-beta.11-3bbbb8a');
 
 /**
  * Generated bundle index. Do not edit.
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -9,7 +9,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('2.0.0-beta.11-6572879');
+var VERSION = new Version('2.0.0-beta.11-3bbbb8a');
 /**
  * Generated bundle index. Do not edit.
  */
