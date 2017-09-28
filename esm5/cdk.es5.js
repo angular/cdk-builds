247767e1ffdd2b20c4c2af6fb86c992aaeb89d78
Will Howell <will.s.howell@gmail.com>
2017-09-28 22:49:56 +0000

    7cb7f38 docs(tabs): fix inconsistency in tab-nav snippet (#7295)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -9,7 +9,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('2.0.0-beta.11-128442d');
+var VERSION = new Version('0.0.0-PLACEHOLDER');
 /**
  * Generated bundle index. Do not edit.
  */
