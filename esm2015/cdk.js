e6132e1906a128f80907ed13345d9c3b49cf4e83
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-10-06 20:48:15 +0000

    746799f chore(tabs): remove unused initialized check (#7476)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -10,7 +10,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('2.0.0-beta.12-766faa1');
+const VERSION = new Version('2.0.0-beta.12-746799f');
 
 /**
  * Generated bundle index. Do not edit.
