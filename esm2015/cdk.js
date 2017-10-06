305bcd73cfbb0ca0720828eb7527eeac8bcff395
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-10-06 20:56:19 +0000

    fa441bc fix: missing exportAs names (#7554)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -10,7 +10,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('2.0.0-beta.12-66e71c8');
+const VERSION = new Version('2.0.0-beta.12-fa441bc');
 
 /**
  * Generated bundle index. Do not edit.
