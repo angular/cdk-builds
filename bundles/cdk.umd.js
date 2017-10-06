67ff54e23ed6b23775f4a4a5f79c2b4e3107793e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-10-06 20:48:01 +0000

    766faa1 refactor(scroll-dispatcher): remove the need to pass in a callback to the scrolled method (#6679)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.12-4bb696e');
+var VERSION = new _angular_core.Version('2.0.0-beta.12-766faa1');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -10,7 +10,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('2.0.0-beta.12-4bb696e');
+var VERSION = new Version('2.0.0-beta.12-766faa1');
 
 /**
  * Generated bundle index. Do not edit.
