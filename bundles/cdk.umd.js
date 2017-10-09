f8f6729ffc28da07f08286199bbaf8df5b8831e4
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-10-09 23:11:27 +0000

    d55aa0c fix(select): unable to preselect array value in single selection mode (#7603)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.12-717f252');
+var VERSION = new _angular_core.Version('2.0.0-beta.12-d55aa0c');
 
 exports.VERSION = VERSION;
 
