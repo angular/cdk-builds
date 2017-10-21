57c855a2821f7edb5d079c3be1ea370c18fb0f29
Austin <amcdaniel2@gmail.com>
2017-10-21 00:35:41 +0000

    74bc623 docs(demos): add more progress bar examples (#7859)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.12-bf6a2c3');
+var VERSION = new _angular_core.Version('2.0.0-beta.12-74bc623');
 
 exports.VERSION = VERSION;
 
