bf5415f76fcf31461935530f787891170734e9ff
Austin <amcdaniel2@gmail.com>
2017-10-27 17:35:21 +0000

    8993451 chore(docs): adds types column and null checks for description (#7713)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.12-38d1ec2');
+var VERSION = new _angular_core.Version('2.0.0-beta.12-8993451');
 
 exports.VERSION = VERSION;
 
