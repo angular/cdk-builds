1b09457cbc8d366d1a7eb3e4a9e8ac35616ce141
Gabriel <32056062+TheRedD0t@users.noreply.github.com>
2017-10-30 21:43:12 +0000

    fd52782 chore: add link to tracking issue for bottom-sheet component (#8119)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.12-c5cbe18');
+var VERSION = new _angular_core.Version('2.0.0-beta.12-fd52782');
 
 exports.VERSION = VERSION;
 
