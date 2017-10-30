2e0a9ec1ce2e5c1744d122cda256e2a051e6065b
Jeremy Elbourn <jelbourn@gmail.com>
2017-10-30 22:02:01 +0000

    1e67629 chore(table): fix missing import in tests (#8135)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.12-fd52782');
+var VERSION = new _angular_core.Version('2.0.0-beta.12-1e67629');
 
 exports.VERSION = VERSION;
 
