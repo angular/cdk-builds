4dbad834ea4c7b261f6f9c5e8480b10bb1dfc4b7
Austin <amcdaniel2@gmail.com>
2017-10-30 21:12:50 +0000

    b2dd17a fix(menu): make @Output names consistent #6677 (#8053)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -14,7 +14,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.12-00de3f6');
+var VERSION = new _angular_core.Version('2.0.0-beta.12-b2dd17a');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -10,7 +10,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('2.0.0-beta.12-00de3f6');
+var VERSION = new Version('2.0.0-beta.12-b2dd17a');
 
 /**
  * Generated bundle index. Do not edit.
