55d4551c0011e3c7f34fec726afb65961ec71fec
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-11-02 03:28:28 +0000

    02d3eb6 fix(tabs): incorrect ripple color for tabs with background (#8123)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.12-c4e12b5');
+var VERSION = new _angular_core.Version('2.0.0-beta.12-02d3eb6');
 
 exports.VERSION = VERSION;
 
