b0d05709735e08ad1563cc2ca495cee4f01f031b
Andrew Scott <atscott01@gmail.com>
2017-11-06 22:20:10 +0000

    2667c01 chore(docs): update autocomplete-filter-example.ts (#8167)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('5.0.0-rc0-b65c642');
+var VERSION = new _angular_core.Version('5.0.0-rc0-2667c01');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('5.0.0-rc0-b65c642');
+const VERSION = new Version('5.0.0-rc0-2667c01');
 
 /**
  * @fileoverview added by tsickle
