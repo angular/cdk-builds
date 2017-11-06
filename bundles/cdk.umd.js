b98ccbe532f09fdc7a86afc8d501d555000bed71
Andrew Seguin <andrewseguin@users.noreply.github.com>
2017-11-06 22:20:09 +0000

    020ed61 docs(table): add selection example (#8161)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('5.0.0-rc0-b65c642');
+var VERSION = new _angular_core.Version('5.0.0-rc0-020ed61');
 
 exports.VERSION = VERSION;
 
