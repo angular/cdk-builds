1b768c8ad0413a14b2062d97f1f83c19e65b20f6
Joey Perrott <josephperrott.github@gmail.com>
2017-11-06 22:19:22 +0000

    b65c642 Fix changelog bullet indentation (#8260)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('5.0.0-rc0-0c25770');
+const VERSION = new Version('5.0.0-rc0-b65c642');
 
 /**
  * @fileoverview added by tsickle
