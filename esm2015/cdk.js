772dc756b2ff094501ef092133cdb2d11a99c5ba
mmalerba <mmalerba@google.com>
2017-11-10 19:16:49 +0000

    4b59ca1 fix(datepicker): prevent `matInput` from clobbering date value (#7831)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('5.0.0-rc0-b6712f8');
+const VERSION = new Version('5.0.0-rc0-4b59ca1');
 
 /**
  * @fileoverview added by tsickle
