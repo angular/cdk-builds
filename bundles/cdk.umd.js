8e8922f18c882233496acaf3a09585f4dc2e9b4e
Jeremy Elbourn <jelbourn@gmail.com>
2017-11-10 19:11:50 +0000

    76806e3 fix(icon): remove IDs from source icon set from rendered output (#8266)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('5.0.0-rc0-ba36d3a');
+var VERSION = new _angular_core.Version('5.0.0-rc0-76806e3');
 
 exports.VERSION = VERSION;
 
