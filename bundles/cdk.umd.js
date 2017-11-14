4a557e51ebb63c3d0e2a169e25c357ed70841913
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-11-14 03:00:05 +0000

    33edff5 build: update jasmine for improved type safety (#8350)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('5.0.0-rc0-8dfe470');
+var VERSION = new _angular_core.Version('5.0.0-rc0-33edff5');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('5.0.0-rc0-8dfe470');
+const VERSION = new Version('5.0.0-rc0-33edff5');
 
 /**
  * @fileoverview added by tsickle
