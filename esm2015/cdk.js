768a9732522757f26a703af021749d2d402903b2
Jeremy Elbourn <jelbourn@gmail.com>
2017-11-14 18:16:21 +0000

    cd12561 chore(select): organize up tests to better scope compilation (#8342)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('5.0.0-rc0-3510064');
+const VERSION = new Version('5.0.0-rc0-cd12561');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('5.0.0-rc0-3510064');
+var VERSION = new Version('5.0.0-rc0-cd12561');
 
 /**
  * @fileoverview added by tsickle
