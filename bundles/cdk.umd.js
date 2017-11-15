9c54ea096c20ab6a9518a8ae987d6a5aa81bbd83
Jeremy Elbourn <jelbourn@gmail.com>
2017-11-15 22:01:44 +0000

    ebbbcbb build(circleci): npm install on circleci (#8461)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('5.0.0-rc0-7f34a35');
+var VERSION = new _angular_core.Version('5.0.0-rc0-ebbbcbb');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('5.0.0-rc0-7f34a35');
+var VERSION = new Version('5.0.0-rc0-ebbbcbb');
 
 /**
  * @fileoverview added by tsickle
