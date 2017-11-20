3d76392bf6bcea6adf6108b1792511d8a01acb4a
mmalerba <mmalerba@google.com>
2017-11-20 19:26:07 +0000

    a5b0872 chore(prerender): output prerendered index.html for debugging (#8483)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('5.0.0-rc0-2a3baa1');
+var VERSION = new _angular_core.Version('5.0.0-rc0-a5b0872');
 
 exports.VERSION = VERSION;
 
