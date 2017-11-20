2af33bf436c2af0ad4807cccce0a135afa07dd59
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-11-20 19:01:47 +0000

    e5bf7df chore(checkbox): remove fake viewport ruler tests (#8537)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('5.0.0-rc0-58598c4');
+var VERSION = new _angular_core.Version('5.0.0-rc0-e5bf7df');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('5.0.0-rc0-58598c4');
+const VERSION = new Version('5.0.0-rc0-e5bf7df');
 
 /**
  * @fileoverview added by tsickle
