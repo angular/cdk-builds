1dbda2980f60cb253f604d3a6485816655096bba
tinayuangao <tinagao@google.com>
2017-11-28 19:52:06 +0000

    d2c11ca fix(chip-list): fix error state changes in chip list (#8425)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.2-55a9f9a');
+var VERSION = new _angular_core.Version('5.0.0-rc.2-d2c11ca');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('5.0.0-rc.2-55a9f9a');
+var VERSION = new Version('5.0.0-rc.2-d2c11ca');
 
 /**
  * @fileoverview added by tsickle
