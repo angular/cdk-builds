2d867955d2cd92e22f9bef73a28b8fb0b1480579
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-11-29 19:03:16 +0000

    75c665a fix(tooltip): unable to type in input with tooltip on iOS (#8534)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.2-d6fec35');
+var VERSION = new _angular_core.Version('5.0.0-rc.2-75c665a');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('5.0.0-rc.2-d6fec35');
+var VERSION = new Version('5.0.0-rc.2-75c665a');
 
 /**
  * @fileoverview added by tsickle
