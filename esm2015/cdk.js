c111e5a04fa8f40e0e513017c7ed8cabd0e14ff9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-12-01 18:26:55 +0000

    dd3094f fix(menu): not closing when overlay is detached externally (#8654)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('5.0.0-rc.2-74c1d01');
+const VERSION = new Version('5.0.0-rc.2-dd3094f');
 
 /**
  * @fileoverview added by tsickle
