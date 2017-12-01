b0aa19a670d19dcb901bc5cc7141e2712d7f196c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-12-01 18:42:59 +0000

    847a60c chore: fix tooltip failing tests (#8737)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.2-841f753');
+var VERSION = new _angular_core.Version('5.0.0-rc.2-847a60c');
 
 exports.VERSION = VERSION;
 
