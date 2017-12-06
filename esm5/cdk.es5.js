e57135ec84795bb5da9ef26399a62906f84830ab
Joey Perrott <josephperrott.github@gmail.com>
2017-12-06 21:25:13 +0000

    ab83f8e chore: add on_* to travis.yaml for webhooks (#8850)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('5.0.0-cb2eac2');
+var VERSION = new Version('5.0.0-ab83f8e');
 
 /**
  * @fileoverview added by tsickle
