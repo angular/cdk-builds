91bd4b350510bd5a85a18f76ae527ef260cdcea9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-12-06 21:30:32 +0000

    cb2eac2 chore: re-bump rxjs version (#8849)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('5.0.0-ab83f8e');
+var VERSION = new _angular_core.Version('5.0.0-cb2eac2');
 
 exports.VERSION = VERSION;
 
