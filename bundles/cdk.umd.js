4fbf6b0437271e6dc308b6fe3b414251de8ee316
Rafael Santana <rafaelsantana3095@gmail.com>
2017-12-14 23:34:51 +0000

    6794722 docs(changelog): fix wrong description (#8945)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('5.0.1-f95f832');
+var VERSION = new _angular_core.Version('5.0.1-6794722');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('5.0.1-f95f832');
+const VERSION = new Version('5.0.1-6794722');
 
 /**
  * @fileoverview added by tsickle
