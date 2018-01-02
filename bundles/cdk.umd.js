986f26674363d281a346e3f987aeeaf206434170
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-01-02 20:07:52 +0000

    77b45f5 docs(dialog): add note about setting initial focus (#9110)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('5.0.3-e18292c');
+var VERSION = new _angular_core.Version('5.0.3-77b45f5');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('5.0.3-e18292c');
+const VERSION = new Version('5.0.3-77b45f5');
 
 /**
  * @fileoverview added by tsickle
