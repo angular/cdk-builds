79b90580480d7fcaeff09fa104052cf0addf1793
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-01-02 20:06:54 +0000

    e18292c chore(docs): minify markdown files (#9151)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('5.0.3-abc7143');
+var VERSION = new _angular_core.Version('5.0.3-e18292c');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('5.0.3-abc7143');
+const VERSION = new Version('5.0.3-e18292c');
 
 /**
  * @fileoverview added by tsickle
