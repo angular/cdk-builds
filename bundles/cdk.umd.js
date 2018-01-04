07c5cefd481e07b0969042cc7de5a93c6bf771c0
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-01-04 21:54:16 +0000

    984dece fix(select): not marked as touched when clicking away (#8784)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('5.0.3-ff31ac8');
+var VERSION = new _angular_core.Version('5.0.3-984dece');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('5.0.3-ff31ac8');
+const VERSION = new Version('5.0.3-984dece');
 
 /**
  * @fileoverview added by tsickle
