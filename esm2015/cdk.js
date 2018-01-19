7c8e8c65aa261863c8772c3b0542a44d5c362192
Joey Perrott <josephperrott.github@gmail.com>
2018-01-19 15:45:53 +0000

    4523556 chore: add rootDirs to cdk tsconfig-build.json (#9481)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('5.1.0-7af132d');
+const VERSION = new Version('5.1.0-4523556');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('5.1.0-7af132d');
+var VERSION = new Version('5.1.0-4523556');
 
 /**
  * @fileoverview added by tsickle
