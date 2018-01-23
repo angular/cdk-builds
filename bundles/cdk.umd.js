002bdc24ec0e82e7cfd93e0c0eeabc869e4564fc
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-01-23 21:15:55 +0000

    ef102aa chore: fix menu e2e test failure (#9549)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('5.1.0-e5c8880');
+var VERSION = new _angular_core.Version('5.1.0-ef102aa');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('5.1.0-e5c8880');
+var VERSION = new Version('5.1.0-ef102aa');
 
 /**
  * @fileoverview added by tsickle
