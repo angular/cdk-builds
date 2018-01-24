16adc6d5ea973ba0d96cd27e8fd5fc9ed74bfc0c
Andrew Seguin <andrewseguin@users.noreply.github.com>
2018-01-24 04:33:11 +0000

    83ca9a8 docs(tooltip): minor improvements; more examples (#9544)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('5.1.0-dcad946');
+var VERSION = new Version('5.1.0-83ca9a8');
 
 /**
  * @fileoverview added by tsickle
