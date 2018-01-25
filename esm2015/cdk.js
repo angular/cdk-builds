1fa9501ef453e92ff793c0f95a4787d54c8874f5
Luke Harries <lharries@users.noreply.github.com>
2018-01-25 20:21:16 +0000

    fac08f3 test(paginator): fix off-by-one error (#9604)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('5.1.0-e7af740');
+const VERSION = new Version('5.1.0-fac08f3');
 
 /**
  * @fileoverview added by tsickle
