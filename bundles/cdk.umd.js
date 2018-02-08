7871fc113288bd94f9a0ce05e21fd35f56edc940
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-02-08 21:41:59 +0000

    e4dd5d7 update(radio, slide-toggle, checkbox): faster ripple enter animations (#9754)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('5.2.0-e4bca63');
+var VERSION = new _angular_core.Version('5.2.0-e4dd5d7');
 
 exports.VERSION = VERSION;
 
