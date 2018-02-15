5fda2ac3f3398981079db1ee54c30164a9748c21
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-02-15 21:09:25 +0000

    master - 73bc948 build: fix select test failure on smaller devices (#9958)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('6.0.0-beta-0-d84612e');
+var VERSION = new _angular_core.Version('6.0.0-beta-0-73bc948');
 
 exports.VERSION = VERSION;
 
