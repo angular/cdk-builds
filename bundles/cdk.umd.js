caddb075394608b56dca49cbb92a03f5e006885e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-02-18 01:18:01 +0000

    master - 9d19291 fix(button): inconsistent overflow value between browsers (#9933)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('6.0.0-beta-0-8e3ab8d');
+var VERSION = new _angular_core.Version('6.0.0-beta-0-9d19291');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const VERSION = new Version('6.0.0-beta-0-8e3ab8d');
+const VERSION = new Version('6.0.0-beta-0-9d19291');
 
 /**
  * @fileoverview added by tsickle
