50e291e68bc7cf51bcc6953c14e1b89a384e5caa
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-02-18 00:26:37 +0000

    master - bd7a408 fix(badge): remove top-level ampersand selectors from theme mixin (#9991)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new Version('6.0.0-beta-0-f6537a1');
+var VERSION = new Version('6.0.0-beta-0-bd7a408');
 
 /**
  * @fileoverview added by tsickle
