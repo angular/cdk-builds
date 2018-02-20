5199b4ce89c2ab1ca34c22bd02a6a4c0f68cb359
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-02-20 15:49:08 +0000

    master - 959804b chore(menu): fix incorrect docs example (#9954)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.1-2abdb65');
+var VERSION = new _angular_core.Version('6.0.0-beta.1-959804b');
 
 exports.VERSION = VERSION;
 
