7c71f03a1d756d72bc08343cc807f10ee4ea2aae
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-03-06 16:21:20 +0000

    master - 7ff086a build: add stylelint rule to avoid top-level ampersands in themes (#10280)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-beta.4-140c94e');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-beta.4-7ff086a');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.0.0-beta.4-140c94e');
+const /** @type {?} */ VERSION = new Version('6.0.0-beta.4-7ff086a');
 
 /**
  * @fileoverview added by tsickle
