ec8f6dbc2b857b64cce968ae1ef28d443936ed41
Joey Perrott <josephperrott.github@gmail.com>
2018-03-08 22:33:26 +0000

    master - c224300 chore: update to angular 6.0.0-beta.7 (#10318)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.0.0-beta.4-f64cef4');
+const /** @type {?} */ VERSION = new Version('6.0.0-beta.4-c224300');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new Version('6.0.0-beta.4-f64cef4');
+var /** @type {?} */ VERSION = new Version('6.0.0-beta.4-c224300');
 
 /**
  * @fileoverview added by tsickle
