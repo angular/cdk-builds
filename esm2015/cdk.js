490510adc286a742b01ac2b6c1db3b748c0d5f51
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-03-09 15:29:26 +0000

    master - c42549e feat(datepicker): add animation to calendar popup (#8999)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.0.0-beta.4-c224300');
+const /** @type {?} */ VERSION = new Version('6.0.0-beta.4-c42549e');
 
 /**
  * @fileoverview added by tsickle
