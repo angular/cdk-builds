bdc4e54dbbeab52439c7c2712833fa0f2ae7310e
Jeremy Elbourn <jelbourn@gmail.com>
2018-03-12 18:15:50 +0000

    master - fcc0908 build: create sass_bundle bazel rule (#10055)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-beta.4-6273d6a');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-beta.4-fcc0908');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new Version('6.0.0-beta.4-6273d6a');
+var /** @type {?} */ VERSION = new Version('6.0.0-beta.4-fcc0908');
 
 /**
  * @fileoverview added by tsickle
