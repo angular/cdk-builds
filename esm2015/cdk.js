6b6df92e23f7a2e630ee67bdc25c2e47d1967f89
tinayuangao <tinagao@google.com>
2018-03-12 22:40:10 +0000

    master - d065aea fix(tree): fix mat-tree paddings (#10349)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.0.0-beta.4-6f63fd2');
+const /** @type {?} */ VERSION = new Version('6.0.0-beta.4-d065aea');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new Version('6.0.0-beta.4-6f63fd2');
+var /** @type {?} */ VERSION = new Version('6.0.0-beta.4-d065aea');
 
 /**
  * @fileoverview added by tsickle
