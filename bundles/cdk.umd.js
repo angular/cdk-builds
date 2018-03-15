a4e1ec1f113c1a88c6b23024f881dbae08fa7e46
tinayuangao <tinagao@google.com>
2018-03-15 21:27:41 +0000

    master - 5e5066a docs(tree): add cdk tree examples to material-examples (#10426)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-beta.4-997cae5');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-beta.4-5e5066a');
 
 exports.VERSION = VERSION;
 
