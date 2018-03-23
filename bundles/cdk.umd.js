10f3623bf7800cc4572ce1fd9f288a4b027c7f92
Jeremy Elbourn <jelbourn@gmail.com>
2018-03-23 17:56:49 +0000

    master - 3eb71c2 fix(select): scroll strategy token cannot inject overlay (#10535)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-beta.4-c05005a');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-beta.4-3eb71c2');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.0.0-beta.4-c05005a');
+const /** @type {?} */ VERSION = new Version('6.0.0-beta.4-3eb71c2');
 
 /**
  * @fileoverview added by tsickle
