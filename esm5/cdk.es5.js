891322e9e21ddc9a5dfc9339326ed8918c1ca189
Joey Perrott <josephperrott.github@gmail.com>
2018-03-29 21:58:52 +0000

    master - b47bfaf Use mat-button in simple snack bar. (#7506)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new Version('6.0.0-rc.0-6729b6d');
+var /** @type {?} */ VERSION = new Version('6.0.0-rc.0-b47bfaf');
 
 /**
  * @fileoverview added by tsickle
