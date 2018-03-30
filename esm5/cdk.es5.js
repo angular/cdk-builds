706bf7405664723df631b5c52563695431acbd51
tinayuangao <tinagao@google.com>
2018-03-30 20:21:16 +0000

    master - 7857b92 fix(tree): fix nested node is not working (#10635)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new Version('6.0.0-rc.0-a73819d');
+var /** @type {?} */ VERSION = new Version('6.0.0-rc.0-7857b92');
 
 /**
  * @fileoverview added by tsickle
