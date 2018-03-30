dbbdf707b87b5f17c2a4f4677d179d23df44095b
Jeremy Elbourn <jelbourn@gmail.com>
2018-03-30 18:49:58 +0000

    master - 872952e build: add ng_package rule for @angular/material (#10625)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.0-c64cb7e');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.0-872952e');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.0.0-rc.0-c64cb7e');
+const /** @type {?} */ VERSION = new Version('6.0.0-rc.0-872952e');
 
 /**
  * @fileoverview added by tsickle
