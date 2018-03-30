77fa51a5f846dfe6d7206b773dd0db8fde01ef88
Joey Perrott <josephperrott.github@gmail.com>
2018-03-30 18:43:17 +0000

    master - c64cb7e fix(progress-spinner): set spinner width to match diameter. (#10314)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.0-b3308de');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.0-c64cb7e');
 
 exports.VERSION = VERSION;
 
