e9175a192c3785d4300f248ded388c23ca0fa6cc
Jeremy Elbourn <jelbourn@gmail.com>
2018-04-05 19:00:50 +0000

    master - 1616f2f chore: bump version to 6.0.0-rc.1 w/ changelog (#10725)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.0-7fb2f89');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.1-1616f2f');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new Version('6.0.0-rc.0-7fb2f89');
+var /** @type {?} */ VERSION = new Version('6.0.0-rc.1-1616f2f');
 
 /**
  * @fileoverview added by tsickle
