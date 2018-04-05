20f9da1a6d46994a8c0b35592b7f2973318f1b21
mmalerba <mmalerba@google.com>
2018-04-05 15:23:36 +0000

    master - 515cc06 fix(form-field): run change detection when prefix or suffix changes (#10670)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.0-95dba59');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.0-515cc06');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.0.0-rc.0-95dba59');
+const /** @type {?} */ VERSION = new Version('6.0.0-rc.0-515cc06');
 
 /**
  * @fileoverview added by tsickle
