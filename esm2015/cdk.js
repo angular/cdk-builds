154e1d1560146f16725736b4c53a79b7a8faf1b8
Joey Perrott <josephperrott.github@gmail.com>
2018-04-05 18:03:12 +0000

    master - 7fb2f89 chore: bind devapp to 0.0.0.0 (#10714)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.0.0-rc.0-515cc06');
+const /** @type {?} */ VERSION = new Version('6.0.0-rc.0-7fb2f89');
 
 /**
  * @fileoverview added by tsickle
