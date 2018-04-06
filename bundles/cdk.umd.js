066f490f5aaf5b248d783738fb59f72e41aa806e
Joey Perrott <josephperrott.github@gmail.com>
2018-04-06 05:23:44 +0000

    master - c059982 chore: suppress passed tests logs during travis usage (#10722)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.1-c407cc8');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.1-c059982');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.0.0-rc.1-c407cc8');
+const /** @type {?} */ VERSION = new Version('6.0.0-rc.1-c059982');
 
 /**
  * @fileoverview added by tsickle
