c8f52f866dde2a3ce27682bcf0c90e84f6f0904b
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-04-06 05:32:16 +0000

    master - fddb40d refactor(input): improve test speed (#10565)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.1-d6748a9');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.1-fddb40d');
 
 exports.VERSION = VERSION;
 
