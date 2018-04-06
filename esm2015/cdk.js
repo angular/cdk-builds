587d00f5ff042fc3842401d5fdb6db5b25e9b913
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-04-06 22:52:32 +0000

    master - bcb5697 fix(table): MatTableDataSource incorrectly sorting zero (#10561)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.0.0-rc.1-6575d9c');
+const /** @type {?} */ VERSION = new Version('6.0.0-rc.1-bcb5697');
 
 /**
  * @fileoverview added by tsickle
