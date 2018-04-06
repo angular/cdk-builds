bd52d130c9749a408593302046be357ba514a309
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-04-06 22:54:06 +0000

    master - edb57f9 fix(datepicker): avoid accidental form submissions through calendar buttons (#10515)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.1-bcb5697');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.1-edb57f9');
 
 exports.VERSION = VERSION;
 
