0b920113aed7de7b8e6c4f02aa1301af19a8205d
mmalerba <mmalerba@google.com>
2018-04-10 17:58:56 +0000

    master - b472330 docs(datepicker): add docs for custom calendar header (#10543)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.1-0a50158');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.1-b472330');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.0.0-rc.1-0a50158');
+const /** @type {?} */ VERSION = new Version('6.0.0-rc.1-b472330');
 
 /**
  * @fileoverview added by tsickle
