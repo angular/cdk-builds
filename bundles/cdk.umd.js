89ab7f26bc637c0b07a9cfec85d23afac132b61c
Jeremy Elbourn <jelbourn@gmail.com>
2018-04-10 00:22:47 +0000

    master - 4211b39 build(bazel): create script to run on top of ng_package output (#10776)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.1-09a60bb');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.1-4211b39');
 
 exports.VERSION = VERSION;
 
