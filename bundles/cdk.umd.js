d8bd4e0d0c31cc286510b96e94e2e2ac25a2f1dc
mmalerba <mmalerba@google.com>
2018-04-11 20:37:01 +0000

    master - 3cc21cd cleanup: migrate migration tool to schematics (#10736)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.1-23cedc1');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.1-3cc21cd');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.0.0-rc.1-23cedc1');
+const /** @type {?} */ VERSION = new Version('6.0.0-rc.1-3cc21cd');
 
 /**
  * @fileoverview added by tsickle
