9c718386f4a6b0058d540f59f91a0b0ec3fa3339
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-04-13 22:43:30 +0000

    master - c9651bb fix(button-toggle): focus monitoring not being cleaned up on destroy (#10580)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.4-6a5a6fb');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.4-c9651bb');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new Version('6.0.0-rc.4-6a5a6fb');
+var /** @type {?} */ VERSION = new Version('6.0.0-rc.4-c9651bb');
 
 /**
  * @fileoverview added by tsickle
