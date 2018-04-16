554ffca7523b681e583fc3b723727d88cd82cdf2
mmalerba <mmalerba@google.com>
2018-04-16 21:25:15 +0000

    master - 7484321 fix(form-field): incorrect label position for outline style in prod mode (#10803)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.11-e5e7896');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.11-7484321');
 
 exports.VERSION = VERSION;
 
