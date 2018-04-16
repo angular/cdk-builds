abd592287665b4ea9c8bf2038d9055a195dfff1e
Rafael Santana <rafaelsantana3095@gmail.com>
2018-04-16 21:24:29 +0000

    master - e5e7896 chore: remove class re-export workaround (#10863)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.11-4cc72b8');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.11-e5e7896');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.0.0-rc.11-4cc72b8');
+const /** @type {?} */ VERSION = new Version('6.0.0-rc.11-e5e7896');
 
 /**
  * @fileoverview added by tsickle
