46906556099302c7dae304ca561ff9662ab9a5d7
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-04-16 21:34:45 +0000

    master - 358a12d feat(stepper): add animationDone event (#10752)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.0.0-rc.11-41be069');
+const /** @type {?} */ VERSION = new Version('6.0.0-rc.11-358a12d');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new Version('6.0.0-rc.11-41be069');
+var /** @type {?} */ VERSION = new Version('6.0.0-rc.11-358a12d');
 
 /**
  * @fileoverview added by tsickle
