35231a72e9dfb9b5921e26783b3439b1de85f022
mmalerba <mmalerba@google.com>
2018-04-17 23:11:19 +0000

    master - 9b11470 chore: update changelog for 6.0.0-rc.12 (#10902)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.0.0-rc.11-b45960d');
+const /** @type {?} */ VERSION = new Version('6.0.0-rc.12-9b11470');
 
 /**
  * @fileoverview added by tsickle
