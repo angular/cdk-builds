d080d526ed5c136ccf4e64b107df0b8bc112555a
tinayuangao <tinagao@google.com>
2018-04-18 02:14:23 +0000

    master - 4e48cf4 fix(radio): radios aren't checkable when the value is falsy (#10315)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.12-c367e66');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.12-4e48cf4');
 
 exports.VERSION = VERSION;
 
