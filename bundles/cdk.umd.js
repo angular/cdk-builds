bf752c8a451e40fefa901f914d6bc5f7747056c7
Jeremy Elbourn <jelbourn@gmail.com>
2018-04-26 23:17:57 +0000

    master - 31df21c docs(snack-bar): expand a11y guidance (#11012)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.13-fd44381');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.13-31df21c');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new Version('6.0.0-rc.13-fd44381');
+var /** @type {?} */ VERSION = new Version('6.0.0-rc.13-31df21c');
 
 /**
  * @fileoverview added by tsickle
