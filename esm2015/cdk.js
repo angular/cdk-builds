75ef4b38fe6687475777be93e9fc13193bb1e7a5
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-04-26 05:16:38 +0000

    master - 2e54f13 build: bump to rxjs 6.0 final (#11002)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.0.0-rc.12-05207e1');
+const /** @type {?} */ VERSION = new Version('6.0.0-rc.12-2e54f13');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new Version('6.0.0-rc.12-05207e1');
+var /** @type {?} */ VERSION = new Version('6.0.0-rc.12-2e54f13');
 
 /**
  * @fileoverview added by tsickle
