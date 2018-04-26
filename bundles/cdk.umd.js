4b0def714738f6d58bef80c24a643a72da18adfa
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-04-26 23:18:48 +0000

    master - b67813e fix(card): flat card selector not working (#11028)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.13-e7b5bd4');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.13-b67813e');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.0.0-rc.13-e7b5bd4');
+const /** @type {?} */ VERSION = new Version('6.0.0-rc.13-b67813e');
 
 /**
  * @fileoverview added by tsickle
