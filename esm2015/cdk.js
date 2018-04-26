612569e6765cdb4109200f30705d0f67324f784a
Joey Perrott <josephperrott.github@gmail.com>
2018-04-26 17:09:33 +0000

    master - 2ad13bf feat(accordion): allow intermediate elements between accordion and expansion panel pt2 (#11031)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.0.0-rc.12-2e54f13');
+const /** @type {?} */ VERSION = new Version('6.0.0-rc.12-2ad13bf');
 
 /**
  * @fileoverview added by tsickle
