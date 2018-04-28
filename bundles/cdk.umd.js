0a4a1c29c8b385e058ecfa6a4e04418ad19bc9df
Andrew Seguin <andrewseguin@users.noreply.github.com>
2018-04-28 01:46:44 +0000

    master - b24308c docs(table): fix typo (#11034)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.13-d96fa07');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.13-b24308c');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new Version('6.0.0-rc.13-d96fa07');
+var /** @type {?} */ VERSION = new Version('6.0.0-rc.13-b24308c');
 
 /**
  * @fileoverview added by tsickle
