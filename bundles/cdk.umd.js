5801f8cf29c87b3819df6deb01f0cb17e49db8db
Andrew Seguin <andrewseguin@users.noreply.github.com>
2018-04-28 00:49:34 +0000

    master - 45cacf7 build: bump to angular 6.0.0-rc.6 (#11057)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.13-a647579');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.13-45cacf7');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new Version('6.0.0-rc.13-a647579');
+var /** @type {?} */ VERSION = new Version('6.0.0-rc.13-45cacf7');
 
 /**
  * @fileoverview added by tsickle
