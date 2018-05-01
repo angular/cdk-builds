fc0b6d272c699078c7eb13db947308df4f1f6789
mmalerba <mmalerba@google.com>
2018-05-01 21:42:57 +0000

    master - 0f7d503 fix(form-field): inconsistent underline height at different DPIs (#11062)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.0.0-rc.14-340b0b2');
+const /** @type {?} */ VERSION = new Version('6.0.0-rc.14-0f7d503');
 
 /**
  * @fileoverview added by tsickle
