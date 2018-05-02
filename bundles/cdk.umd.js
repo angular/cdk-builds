07e31892a727509dfa64060233f9a9ed60eda577
Andrew Seguin <andrewseguin@users.noreply.github.com>
2018-05-02 18:05:17 +0000

    master - dab5eba docs(expansion): fix directive typo (#11105)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.14-1071680');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.14-dab5eba');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.0.0-rc.14-1071680');
+const /** @type {?} */ VERSION = new Version('6.0.0-rc.14-dab5eba');
 
 /**
  * @fileoverview added by tsickle
