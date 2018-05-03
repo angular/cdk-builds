c3331baa1f294364836bb4060d1616f4347338da
Lizette <lizettejoan@gmail.com>
2018-05-03 21:17:48 +0000

    master - 32111e0 change OnInit to AfterViewInit to load MatSort before http table request (#11122)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-e59d0fb');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-32111e0');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.0.0-e59d0fb');
+const /** @type {?} */ VERSION = new Version('6.0.0-32111e0');
 
 /**
  * @fileoverview added by tsickle
