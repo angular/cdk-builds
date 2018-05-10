9b230ec297e9663dba3e911033661440f1a7f83a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-05-10 16:29:44 +0000

    master - a62cdb6 fix(datepicker): dateInput event being fired if the value hasn't changed (#10952)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.0.1-fbf5648');
+const /** @type {?} */ VERSION = new Version('6.0.1-a62cdb6');
 
 /**
  * @fileoverview added by tsickle
