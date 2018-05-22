322af6208e8ebba5db66ed98b6eeb8240d3197e1
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-05-22 16:24:24 +0000

    master - 31fd6a2 chore: fix typo in form field docs (#11420)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.1.0-f59658d');
+var /** @type {?} */ VERSION = new core.Version('6.1.0-31fd6a2');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.1.0-f59658d');
+const /** @type {?} */ VERSION = new Version('6.1.0-31fd6a2');
 
 /**
  * @fileoverview added by tsickle
