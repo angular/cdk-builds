f4b9994468f6b61dd87e5c2d3da6f2cfd7ada42b
mmalerba <mmalerba@google.com>
2018-05-29 17:03:51 +0000

    master - 315c9f8 chore: add misisng branches to run on CI (#11562)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.1.0-9062640');
+var /** @type {?} */ VERSION = new core.Version('6.1.0-315c9f8');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.1.0-9062640');
+const /** @type {?} */ VERSION = new Version('6.1.0-315c9f8');
 
 /**
  * @fileoverview added by tsickle
