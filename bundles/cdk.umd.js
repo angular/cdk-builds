f7368504a4afa0101457f32c10dc9bffcec3ecba
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-05-31 18:14:13 +0000

    master - 55aaa03 fix(overlay): flexible overlay with push not handling scroll offset and position locking (#11421)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.2.0-c977763');
+var /** @type {?} */ VERSION = new core.Version('6.2.0-55aaa03');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new Version('6.2.0-c977763');
+var /** @type {?} */ VERSION = new Version('6.2.0-55aaa03');
 
 /**
  * @fileoverview added by tsickle
