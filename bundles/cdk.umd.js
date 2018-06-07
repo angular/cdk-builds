c53f37bdfbd3f95150b43e205ba1764049ebf846
Austin <amcdaniel2@gmail.com>
2018-06-07 06:03:07 +0000

    master - b349de3 fix(schematics): remove temp path #11198 (#11424)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.2.1-6bd90e8');
+var /** @type {?} */ VERSION = new core.Version('6.2.1-b349de3');
 
 exports.VERSION = VERSION;
 
