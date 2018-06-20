51c9ff6ea9d53cd2e26649513dc8495ea308aa1d
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-06-20 17:58:28 +0000

    master - 4b13555 fix(table): use solid color for sorting arrow (#11533)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.3.0-1ff9cf0');
+const /** @type {?} */ VERSION = new Version('6.3.0-4b13555');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new Version('6.3.0-1ff9cf0');
+var /** @type {?} */ VERSION = new Version('6.3.0-4b13555');
 
 /**
  * @fileoverview added by tsickle
