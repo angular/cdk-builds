110d5e2fe61715c50d41887a44c66754a5bb76f3
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-06-28 15:50:30 +0000

    master - 0ca08b7 refactor(checkbox): improve unit test speed (#11967)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.3.1-aa09628');
+var /** @type {?} */ VERSION = new core.Version('6.3.1-0ca08b7');
 
 exports.VERSION = VERSION;
 
