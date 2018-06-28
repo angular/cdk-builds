d4f6f2f2cbe4488dc633dc714525abb541865553
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-06-28 15:50:42 +0000

    master - 27811a3 feat: add experimental drag-drop package (#11864)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.3.1-0ca08b7');
+const /** @type {?} */ VERSION = new Version('6.3.1-27811a3');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new Version('6.3.1-0ca08b7');
+var /** @type {?} */ VERSION = new Version('6.3.1-27811a3');
 
 /**
  * @fileoverview added by tsickle
