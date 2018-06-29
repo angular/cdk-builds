2fed18f7fceef6987cc17b9a246521e3a7cdf7af
Leibale Eidelman <leibale1998@gmail.com>
2018-06-29 16:37:41 +0000

    master - 415c956 fix(GlobalPositionStrategy): justifyContent center ignored when direction is RTL (#11877)

--- bundles/cdk-overlay.umd.js
+++ bundles/cdk-overlay.umd.js
@@ -3230,6 +3230,9 @@ GlobalPositionStrategy = /** @class */ (function () {
         if (config.width === '100%') {
             parentStyles.justifyContent = 'flex-start';
         }
+        else if (this._justifyContent === 'center') {
+            parentStyles.justifyContent = 'center';
+        }
         else if (this._overlayRef.getConfig().direction === 'rtl') {
             // In RTL the browser will invert `flex-start` and `flex-end` automatically, but we
             // don't want that because our positioning is explicitly `left` and `right`, hence
