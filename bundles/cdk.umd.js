437036eeed2b2301715b9680d2d986435aa031a7
Joey Perrott <josephperrott.github@gmail.com>
2018-07-02 18:02:21 +0000

    master - c40fef4 chore: update changelog for 6.3.2 (#12019)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.3.1-444fb38');
+var /** @type {?} */ VERSION = new core.Version('6.3.2-c40fef4');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new Version('6.3.1-444fb38');
+var /** @type {?} */ VERSION = new Version('6.3.2-c40fef4');
 
 /**
  * @fileoverview added by tsickle
