de8a41c95170189f645d718e64b0fc56a9938bb6
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-07-05 21:36:37 +0000

    master - 3e530e7 chore: fix duplicate dependency in bazel build (#12054)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.3.2-89cad3f');
+var /** @type {?} */ VERSION = new core.Version('6.3.2-3e530e7');
 
 exports.VERSION = VERSION;
 
