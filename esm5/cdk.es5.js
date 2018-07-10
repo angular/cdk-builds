ef5484b12276c32e55252421118b10eb2d25da9e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-07-10 14:31:51 +0000

    master - 3bf10f9 fix(button-toggle): parent margin and padding being propagated to underlying button (#11993)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new Version('6.3.3-eed6110');
+var /** @type {?} */ VERSION = new Version('6.3.3-3bf10f9');
 
 /**
  * @fileoverview added by tsickle
