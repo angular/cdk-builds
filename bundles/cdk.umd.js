f023cfe7239b5cd47717a010be85c36edc1bc2e9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-07-18 19:48:20 +0000

    master - b53b66a fix(menu): changed after checked error when toggling quickly between triggers for same submenu (#12209)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.4.0-7301fa0');
+var /** @type {?} */ VERSION = new core.Version('6.4.0-b53b66a');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.4.0-7301fa0');
+const /** @type {?} */ VERSION = new Version('6.4.0-b53b66a');
 
 /**
  * @fileoverview added by tsickle
