500744ff143f4ee131ae58d6139ff29e75eaac49
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-07-18 22:18:43 +0000

    master - f35a314 fix(tabs): ripple overflow in internet explorer (#12036)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.4.0-777b3e7');
+var /** @type {?} */ VERSION = new core.Version('6.4.0-f35a314');
 
 exports.VERSION = VERSION;
 
