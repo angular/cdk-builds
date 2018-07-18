fb6cdaf0bdc6f512261cc9e6413d89fd0dfba6c6
Tobias Schweizer <tobiasschweizer@users.noreply.github.com>
2018-07-18 19:53:53 +0000

    master - 37228e5 Add public method to MatCalendar to update today's date (#11821)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.4.0-d54a75a');
+const /** @type {?} */ VERSION = new Version('6.4.0-37228e5');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new Version('6.4.0-d54a75a');
+var /** @type {?} */ VERSION = new Version('6.4.0-37228e5');
 
 /**
  * @fileoverview added by tsickle
