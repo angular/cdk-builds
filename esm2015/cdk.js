0cbe162e324c97ded63eb4f50433339be9fdc658
mmalerba <mmalerba@google.com>
2018-07-18 19:47:15 +0000

    master - 7301fa0  virtual-scroll: add support for user-provided content wrapper (#12183)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.4.0-c2fc3f4');
+const /** @type {?} */ VERSION = new Version('6.4.0-7301fa0');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new Version('6.4.0-c2fc3f4');
+var /** @type {?} */ VERSION = new Version('6.4.0-7301fa0');
 
 /**
  * @fileoverview added by tsickle
