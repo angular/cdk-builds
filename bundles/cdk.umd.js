ab0bd2ce00b197ff9595805246b66aab3344d7a5
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-07-19 16:58:29 +0000

    master - adda21f fix(table): unable to sort large numbers in strings (#12052)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.4.0-5b72ec7');
+var /** @type {?} */ VERSION = new core.Version('6.4.0-adda21f');
 
 exports.VERSION = VERSION;
 
