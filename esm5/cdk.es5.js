5453430160db196da754bf904c239aafab30f538
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-07-20 15:30:21 +0000

    master - f9b9e25 build: fix dashboard deployment (#12293)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new Version('6.4.0-e13bfe0');
+var /** @type {?} */ VERSION = new Version('6.4.0-f9b9e25');
 
 /**
  * @fileoverview added by tsickle
