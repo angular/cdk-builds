66ebc2dd2c6ba451e3a6fb0328d73b3cc48539d3
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-07-24 17:21:58 +0000

    master - e23709f build: remove unused dependencies (#12346)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.4.1-ed4e082');
+var /** @type {?} */ VERSION = new core.Version('6.4.1-e23709f');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.4.1-ed4e082');
+const /** @type {?} */ VERSION = new Version('6.4.1-e23709f');
 
 /**
  * @fileoverview added by tsickle
