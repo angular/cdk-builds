a52bbb2c2abc1cf1a298e22b231f7e622f3825c7
mmalerba <mmalerba@google.com>
2018-07-24 17:29:14 +0000

    master - e67fd7b virtual-scroll: switch back to `markForCheck` since `detectChanges` (#12329)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.4.1-e23709f');
+var /** @type {?} */ VERSION = new core.Version('6.4.1-e67fd7b');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.4.1-e23709f');
+const /** @type {?} */ VERSION = new Version('6.4.1-e67fd7b');
 
 /**
  * @fileoverview added by tsickle
