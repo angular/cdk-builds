08e230005c4a5fb81fd0aa534430eb4f7c66fbe9
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-07-24 17:15:45 +0000

    master - ed4e082 fix(schematics): properly indent inline files (#12317)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.4.1-c5b87da');
+var /** @type {?} */ VERSION = new core.Version('6.4.1-ed4e082');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.4.1-c5b87da');
+const /** @type {?} */ VERSION = new Version('6.4.1-ed4e082');
 
 /**
  * @fileoverview added by tsickle
