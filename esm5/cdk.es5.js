e5a702b459e7728c88c6c4902ad389fe1319678c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-07-29 04:55:28 +0000

    master - 4e358c3 feat(tooltip): align with 2018 material design spec (#12310)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new Version('6.4.1-1f53841');
+var /** @type {?} */ VERSION = new Version('6.4.1-4e358c3');
 
 /**
  * @fileoverview added by tsickle
