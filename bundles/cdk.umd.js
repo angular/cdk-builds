2ad5e18be303f9cc6cbb5e1288e763da6aefe47a
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-07-31 16:52:45 +0000

    master - f5f7f1c build: add version stamp to release output (#12418)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.4.2-e4ac04d');
+var /** @type {?} */ VERSION = new core.Version('6.4.2-f5f7f1c');
 
 exports.VERSION = VERSION;
 
