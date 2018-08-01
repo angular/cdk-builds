cea440ec19e7dcc2a09d93dd6544f18c50eb7595
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-08-01 14:18:21 +0000

    master - df2b371 fix(form-field): unable to distinguish disabled form field in high contrast mode (#12445)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.4.2-db8f83d');
+var /** @type {?} */ VERSION = new core.Version('6.4.2-df2b371');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new Version('6.4.2-db8f83d');
+var /** @type {?} */ VERSION = new Version('6.4.2-df2b371');
 
 /**
  * @fileoverview added by tsickle
