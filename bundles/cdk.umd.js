f90b06816c4315bc44f5da5515bd3214b9987469
Joey Perrott <josephperrott.github@gmail.com>
2018-08-07 20:56:08 +0000

    master - 58c1c95 fix(slide-toggle): fix font sizing for slide toggle to match other input methods (#10688)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.4.2-4ecde45');
+var /** @type {?} */ VERSION = new core.Version('6.4.2-58c1c95');
 
 exports.VERSION = VERSION;
 
