795cc8bb8210d8eeb39859311967765d8c7360a4
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-08-09 14:50:51 +0000

    master - 7756882 refactor(schematics): run tests for template files (#12562)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.4.2-1effb16');
+var /** @type {?} */ VERSION = new core.Version('6.4.2-7756882');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.4.2-1effb16');
+const /** @type {?} */ VERSION = new Version('6.4.2-7756882');
 
 /**
  * @fileoverview added by tsickle
