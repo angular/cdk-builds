3beb29d64dd2a75e4b139809f8f8d29b0f196b07
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-08-15 21:46:23 +0000

    master - 91b171c feat(drag-drop): add function to determine whether an item is allowed to be transferred (#12673)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.4.5-13c141e');
+var /** @type {?} */ VERSION = new core.Version('6.4.5-91b171c');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.4.5-13c141e');
+const /** @type {?} */ VERSION = new Version('6.4.5-91b171c');
 
 /**
  * @fileoverview added by tsickle
