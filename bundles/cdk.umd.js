bf1b96ba1b022e420d65af8cba7344eb0ac737dc
Erick Xavier <648239+ErickXavier@users.noreply.github.com>
2018-08-15 00:59:37 +0000

    master - 7e74b5d Fixes MAT_MOMENT_DATE_ADAPTER_OPTIONS (#12679)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.4.5-b40967f');
+var /** @type {?} */ VERSION = new core.Version('6.4.5-7e74b5d');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new Version('6.4.5-b40967f');
+var /** @type {?} */ VERSION = new Version('6.4.5-7e74b5d');
 
 /**
  * @fileoverview added by tsickle
