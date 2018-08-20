739dfc073384cc9bc3f4fbfa8cd8b1399be5ba4a
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-08-20 20:43:45 +0000

    master - 7d140a2 build(dgeni): do not warn if @breaking-change is used without @deprecated (#12750)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.4.5-a027ae5');
+var /** @type {?} */ VERSION = new core.Version('6.4.5-7d140a2');
 
 exports.VERSION = VERSION;
 
