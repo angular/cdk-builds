524e924feee872742912b2be1701582ca901c094
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-08-21 18:55:58 +0000

    master - ecf8b0d fix(button-toggle): clickable area not stretching when custom width is set (#12642)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new Version('6.4.6-136f5dd');
+var /** @type {?} */ VERSION = new Version('6.4.6-ecf8b0d');
 
 /**
  * @fileoverview added by tsickle
