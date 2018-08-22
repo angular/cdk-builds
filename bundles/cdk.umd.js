afba23da3479bf7cc33a1f49d2c99fb318d7c3b8
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-08-22 14:31:21 +0000

    master - ea9a88d refactor(text-field): allow ElementRef to be passed to autofill monitor (#12783)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.4.6-ab72e61');
+var /** @type {?} */ VERSION = new core.Version('6.4.6-ea9a88d');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new Version('6.4.6-ab72e61');
+var /** @type {?} */ VERSION = new Version('6.4.6-ea9a88d');
 
 /**
  * @fileoverview added by tsickle
