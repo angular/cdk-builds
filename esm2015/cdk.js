f0a90026e4221d6daffd1501ea9e7ca03b248b79
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-08-23 15:01:19 +0000

    master - 89d16b2 fix(card): images in title-group overlapping content (#12205)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.4.6-22953a9');
+const /** @type {?} */ VERSION = new Version('6.4.6-89d16b2');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new Version('6.4.6-22953a9');
+var /** @type {?} */ VERSION = new Version('6.4.6-89d16b2');
 
 /**
  * @fileoverview added by tsickle
