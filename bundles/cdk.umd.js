530a8a16cd528e7f05b4021329662cc7115f73f3
Jeremy Elbourn <jelbourn@gmail.com>
2018-08-23 20:01:33 +0000

    master - f3af763 fix(progress-bar): avoid error on SSR if pathname is undefined (#12807)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.4.6-89d16b2');
+var /** @type {?} */ VERSION = new core.Version('6.4.6-f3af763');
 
 exports.VERSION = VERSION;
 
