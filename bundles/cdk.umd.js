7e5dfe91bc67a9970ac5e1cf8a0fdc6454a934b4
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-08-27 16:54:33 +0000

    master - 1f88bd7 feat(sidenav): align with 2018 material design spec (#12569)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of the Angular Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('6.4.6-5278f72');
+var /** @type {?} */ VERSION = new core.Version('6.4.6-1f88bd7');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Angular Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('6.4.6-5278f72');
+const /** @type {?} */ VERSION = new Version('6.4.6-1f88bd7');
 
 /**
  * @fileoverview added by tsickle
