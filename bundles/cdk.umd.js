57a8edb82fc41790eb7ad5a4f62e8f959146f02f
Jeremy Elbourn <jelbourn@gmail.com>
2018-08-29 21:27:18 +0000

    master - f7b130c chore: temporarily disable circle ci doing anything (#12897)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('6.4.6-74a3179');
+var VERSION = new core.Version('6.4.6-f7b130c');
 
 exports.VERSION = VERSION;
 
