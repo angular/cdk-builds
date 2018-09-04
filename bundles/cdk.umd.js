0b8a3878d65d23d780e44ca1361802f143939757
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-09-04 22:16:19 +0000

    master - dc6688d build: skip duplicate commit entries in changelog (#12943)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.0-cfb4f58');
+var VERSION = new core.Version('7.0.0-beta.0-dc6688d');
 
 exports.VERSION = VERSION;
 
