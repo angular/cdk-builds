1bf85d3b1d06d4b0333cc849d2cffaa007d4dad6
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-09-04 18:51:23 +0000

    master - 0ab720a build: rework breaking changes tooling (#12950)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.0-c618f03');
+var VERSION = new core.Version('7.0.0-beta.0-0ab720a');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-const VERSION = new Version('7.0.0-beta.0-c618f03');
+const VERSION = new Version('7.0.0-beta.0-0ab720a');
 
 /**
  * @fileoverview added by tsickle
