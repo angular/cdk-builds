66d7ad2a81948046129bf6c11a2906aece1cccfc
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-09-04 19:01:45 +0000

    master - cfb4f58 refactor(schematics): split up upgrade data for target version (#12951)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-const VERSION = new Version('7.0.0-beta.0-da95e34');
+const VERSION = new Version('7.0.0-beta.0-cfb4f58');
 
 /**
  * @fileoverview added by tsickle
