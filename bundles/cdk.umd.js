a814e7c363d7fb9e6a7ffba50e8e733204d95730
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-09-04 18:48:09 +0000

    master - c618f03 build: upgrade dashboard dependencies (#12960)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.0-193c2d0');
+var VERSION = new core.Version('7.0.0-beta.0-c618f03');
 
 exports.VERSION = VERSION;
 
