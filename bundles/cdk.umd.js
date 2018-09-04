301cf04778bc1096d41f8789dbce4a83f6589479
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-09-04 18:53:42 +0000

    master - be691cb build: suppress bazel schematic tsconfig warnings (#12957)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.0-b884295');
+var VERSION = new core.Version('7.0.0-beta.0-be691cb');
 
 exports.VERSION = VERSION;
 
