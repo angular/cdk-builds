22436b442c910feff2d3b54fa7201016ab68d650
Ben Lesh <ben@benlesh.com>
2018-09-10 20:28:54 +0000

    master - 0a25fca fix(typings): update typings to support more strict typings in RxJS 6.3.2 (#12979)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.0-9886b1b');
+var VERSION = new core.Version('7.0.0-beta.0-0a25fca');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-const VERSION = new Version('7.0.0-beta.0-9886b1b');
+const VERSION = new Version('7.0.0-beta.0-0a25fca');
 
 /**
  * @fileoverview added by tsickle
