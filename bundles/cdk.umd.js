96b4fabb53253a545038cfd05d8721919a0ecde4
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-09-11 18:04:00 +0000

    master - 948e563 fix(ripple): ripples not being cleared if touch sequence is canceled (#12936)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.0-e24f24a');
+var VERSION = new core.Version('7.0.0-beta.0-948e563');
 
 exports.VERSION = VERSION;
 
