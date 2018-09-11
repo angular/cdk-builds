c850a30f911c4038797154ed5a0860960c8da65e
Jeremy Elbourn <jelbourn@gmail.com>
2018-09-11 17:37:08 +0000

    master - 5718975 Revert "feat(button): align with 2018 material design spec (#12537)" (#13080)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-const VERSION = new Version('7.0.0-beta.0-1908b3e');
+const VERSION = new Version('7.0.0-beta.0-5718975');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new Version('7.0.0-beta.0-1908b3e');
+var VERSION = new Version('7.0.0-beta.0-5718975');
 
 /**
  * @fileoverview added by tsickle
