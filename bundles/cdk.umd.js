16a98d46b154c80c8c24912824d80d0ef41bf2d0
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-09-13 18:41:15 +0000

    master - 90af397 refactor(schematics): constructor check rule does not check all diagnostics (#13103)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.0-239c781');
+var VERSION = new core.Version('7.0.0-beta.0-90af397');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-const VERSION = new Version('7.0.0-beta.0-239c781');
+const VERSION = new Version('7.0.0-beta.0-90af397');
 
 /**
  * @fileoverview added by tsickle
