5b0ad65c10fc70af71892316a0ab087f444b6d8e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-09-14 13:42:12 +0000

    master - ae3ce4a fix(chips): default click action on chip being prevented (#12856)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-const VERSION = new Version('7.0.0-beta.0-20beff4');
+const VERSION = new Version('7.0.0-beta.0-ae3ce4a');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new Version('7.0.0-beta.0-20beff4');
+var VERSION = new Version('7.0.0-beta.0-ae3ce4a');
 
 /**
  * @fileoverview added by tsickle
