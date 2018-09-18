6c3a239249d4ea4b59bb5c1f457ddd3da7388953
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-09-18 21:13:18 +0000

    master - 25f6c1a chore(schematics): create update schematic workflow doc (#13147)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.1-39d2311');
+var VERSION = new core.Version('7.0.0-beta.1-25f6c1a');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new Version('7.0.0-beta.1-39d2311');
+var VERSION = new Version('7.0.0-beta.1-25f6c1a');
 
 /**
  * @fileoverview added by tsickle
