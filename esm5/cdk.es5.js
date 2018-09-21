90c9860f6a7616dde3623cfc9cea91a76e04fb2f
Joey Perrott <josephperrott.github@gmail.com>
2018-09-21 06:26:15 +0000

    master - bfeb540 fix(button): fix stroked button border color (#13219)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new Version('7.0.0-beta.2-67a4f12');
+var VERSION = new Version('7.0.0-beta.2-bfeb540');
 
 /**
  * @fileoverview added by tsickle
