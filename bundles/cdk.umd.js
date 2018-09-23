b9ab28cd7cd475efd58618ebb9582e800c8ce161
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-09-23 16:17:31 +0000

    master - 0c5598c feat(tabs): align with 2018 material design spec (#12605)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.2-3bc52df');
+var VERSION = new core.Version('7.0.0-beta.2-0c5598c');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-const VERSION = new Version('7.0.0-beta.2-3bc52df');
+const VERSION = new Version('7.0.0-beta.2-0c5598c');
 
 /**
  * @fileoverview added by tsickle
