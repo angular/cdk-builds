2a26156ccde88c7fc39f4b37875f464253ce27fa
Michael Prentice <splaktar@gmail.com>
2018-09-24 20:08:10 +0000

    master - 8558878 fix(schematics): remove unnecessary semicolon in tree component (#13284)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.2-0ccdfc8');
+var VERSION = new core.Version('7.0.0-beta.2-8558878');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new Version('7.0.0-beta.2-0ccdfc8');
+var VERSION = new Version('7.0.0-beta.2-8558878');
 
 /**
  * @fileoverview added by tsickle
