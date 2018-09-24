e77534cdeedb43ed251f48ef72a2347f127160af
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-09-24 18:04:59 +0000

    master - f418f78 refactor(schematics): do not rerun migrations for each test-case (#13256)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.2-884762f');
+var VERSION = new core.Version('7.0.0-beta.2-f418f78');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new Version('7.0.0-beta.2-884762f');
+var VERSION = new Version('7.0.0-beta.2-f418f78');
 
 /**
  * @fileoverview added by tsickle
