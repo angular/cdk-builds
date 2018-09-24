85a140b2bc63614939c0066fbe900aa24aa97361
Michael Prentice <splaktar@gmail.com>
2018-09-24 20:38:32 +0000

    master - 0f818fd docs(schematics): clarify and refine the install schematic docs (#13267)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.2-8558878');
+var VERSION = new core.Version('7.0.0-beta.2-0f818fd');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-const VERSION = new Version('7.0.0-beta.2-8558878');
+const VERSION = new Version('7.0.0-beta.2-0f818fd');
 
 /**
  * @fileoverview added by tsickle
