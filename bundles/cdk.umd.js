8beb5b6956d28be26e1c90d42bf1e6608f41806e
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-09-24 18:17:19 +0000

    master - 21bc9b1 refactor(schematics): external resource nodes have incorrect offsets (#13289)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.2-f418f78');
+var VERSION = new core.Version('7.0.0-beta.2-21bc9b1');
 
 exports.VERSION = VERSION;
 
