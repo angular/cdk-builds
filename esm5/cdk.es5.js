b78a53d5b1fb8370793b3b91b2758e203c0aa867
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-09-24 18:00:46 +0000

    master - 884762f fix(schematics): table schematic not expanding full width (#13234)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new Version('7.0.0-beta.2-267dd65');
+var VERSION = new Version('7.0.0-beta.2-884762f');
 
 /**
  * @fileoverview added by tsickle
