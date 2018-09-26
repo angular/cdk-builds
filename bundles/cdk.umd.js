14c6f21eafd2776b0d614a1f2307f23451b99165
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-09-26 18:31:32 +0000

    master - 74fdedb build: ts compilation not reporting failures (#13312)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.2-efd2993');
+var VERSION = new core.Version('7.0.0-beta.2-74fdedb');
 
 exports.VERSION = VERSION;
 
