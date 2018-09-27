339cd721825582624170c3b67b3dac6cd55d48c8
mmalerba <mmalerba@google.com>
2018-09-27 20:56:07 +0000

    master - 5321ab3 docs(virtual-scroll): add live examples (#13327)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.2-fd3eb6a');
+var VERSION = new core.Version('7.0.0-beta.2-5321ab3');
 
 exports.VERSION = VERSION;
 
