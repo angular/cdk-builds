156c86f459759e91faae6a534ae7372e79243297
Jeremy Elbourn <jelbourn@gmail.com>
2018-09-27 14:12:49 +0000

    master - 39b5577 docs(icon): fix the bidi section interupting the a11y section (#13331)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.2-74fdedb');
+var VERSION = new core.Version('7.0.0-beta.2-39b5577');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-const VERSION = new Version('7.0.0-beta.2-74fdedb');
+const VERSION = new Version('7.0.0-beta.2-39b5577');
 
 /**
  * @fileoverview added by tsickle
