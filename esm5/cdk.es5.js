0ab59864ea4aead69826feced0f5915e54713a4b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-09-27 14:48:51 +0000

    master - ac3f8c4 feat(schematics): support for `ng add` (#13319)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new Version('7.0.0-beta.2-4e475f9');
+var VERSION = new Version('7.0.0-beta.2-ac3f8c4');
 
 /**
  * @fileoverview added by tsickle
