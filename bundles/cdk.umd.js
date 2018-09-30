17b5a32fcc75dac0ca3f0d66c31a311a33f0bbef
Jeremy Elbourn <jelbourn@gmail.com>
2018-09-30 14:51:10 +0000

    master - 8ed0129 chore: bump versions of Angular and devkit betas (#13359)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.2-04234f0');
+var VERSION = new core.Version('7.0.0-beta.2-8ed0129');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new Version('7.0.0-beta.2-04234f0');
+var VERSION = new Version('7.0.0-beta.2-8ed0129');
 
 /**
  * @fileoverview added by tsickle
