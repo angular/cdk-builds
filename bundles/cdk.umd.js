5adb97693d8bbc47c72b22cf86906054fb37d165
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-10-02 17:38:10 +0000

    master - 311d786 fix(chips): set aria-invalid on chip input (#13249)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.2-119fafd');
+var VERSION = new core.Version('7.0.0-beta.2-311d786');
 
 exports.VERSION = VERSION;
 
