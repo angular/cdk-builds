fd2888632937500556f9e2bc0d18c09b1a91d0be
vivian-hu <41269630+vivian-hu@users.noreply.github.com>
2018-10-02 17:39:07 +0000

    master - 6da7d23 fix(select): give native select a bigger clickable area (#13228)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.2-311d786');
+var VERSION = new core.Version('7.0.0-beta.2-6da7d23');
 
 exports.VERSION = VERSION;
 
