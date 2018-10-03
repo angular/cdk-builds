08fd8928c41990f6f8dee6c2082ff95ffb918973
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-10-03 21:56:08 +0000

    master - f91c0b7 docs(schematics): add drawing for CDK shared logic (#13394)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-rc.0-342c749');
+var VERSION = new core.Version('7.0.0-rc.0-f91c0b7');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-const VERSION = new Version('7.0.0-rc.0-342c749');
+const VERSION = new Version('7.0.0-rc.0-f91c0b7');
 
 /**
  * @fileoverview added by tsickle
