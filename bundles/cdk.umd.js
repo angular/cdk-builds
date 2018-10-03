287cf21dba2034172301f09e1c4dde0a70f1bded
vivian-hu <41269630+vivian-hu@users.noreply.github.com>
2018-10-03 18:55:43 +0000

    master - c07184d use pointer cursor for native select when not disabled (#13184)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-rc.0-ffa4a06');
+var VERSION = new core.Version('7.0.0-rc.0-c07184d');
 
 exports.VERSION = VERSION;
 
