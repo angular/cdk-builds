e2e5b156163379096d3d6c575c1518ad056440a3
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-10-03 18:53:49 +0000

    master - ffa4a06 fix(bottom-sheet): dismiss bottom sheet on destroy (#13120)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-rc.0-bef9a17');
+var VERSION = new core.Version('7.0.0-rc.0-ffa4a06');
 
 exports.VERSION = VERSION;
 
