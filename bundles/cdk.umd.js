a827bd6d707613db5a3175038e57b886ebc10f4f
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-10-03 18:18:00 +0000

    master - bef9a17 fix(list): selection list not marking options as selected correctly when setting value with duplicates (#13363)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-rc.0-56f2e50');
+var VERSION = new core.Version('7.0.0-rc.0-bef9a17');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-const VERSION = new Version('7.0.0-rc.0-56f2e50');
+const VERSION = new Version('7.0.0-rc.0-bef9a17');
 
 /**
  * @fileoverview added by tsickle
