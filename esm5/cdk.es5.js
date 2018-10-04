f10da2c7ce348591587f5299cc5afb05101d0f60
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-10-04 19:04:17 +0000

    master - 72ccd8b feat(schematics): create drag-drop schematic (#13368)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new Version('7.0.0-rc.0-3254992');
+var VERSION = new Version('7.0.0-rc.0-72ccd8b');
 
 /**
  * @fileoverview added by tsickle
