c6b40bc337a0de847c82ef44eeb4a8b6a016a654
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-10-04 16:53:33 +0000

    master - 8dfd2ee fix(autocomplete): closing parent overlay when pressing escpe (#13413)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-rc.0-a620fca');
+var VERSION = new core.Version('7.0.0-rc.0-8dfd2ee');
 
 exports.VERSION = VERSION;
 
