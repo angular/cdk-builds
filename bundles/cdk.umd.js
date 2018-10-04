99347f60480d3932f806ba31d14b1a1d1d2fe728
Joey Perrott <josephperrott.github@gmail.com>
2018-10-04 21:44:06 +0000

    master - 7b3de83 chore: remove orphaned duplicate comment (#13433)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-rc.0-72ccd8b');
+var VERSION = new core.Version('7.0.0-rc.0-7b3de83');
 
 exports.VERSION = VERSION;
 
