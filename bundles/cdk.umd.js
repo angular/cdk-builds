2e15f4ec932853f0e7cb5da19c8b94807688c1d6
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-10-04 17:01:50 +0000

    master - cb5b15e fix(select): allow extra content to be projected after label in mat-optgroup (#13396)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-rc.0-d58db5d');
+var VERSION = new core.Version('7.0.0-rc.0-cb5b15e');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new Version('7.0.0-rc.0-d58db5d');
+var VERSION = new Version('7.0.0-rc.0-cb5b15e');
 
 /**
  * @fileoverview added by tsickle
