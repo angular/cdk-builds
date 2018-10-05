123c0cf44aaccaa932aa290b9d40edf920cd66be
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-10-05 23:42:34 +0000

    master - 4b15b78 build(docs): ensure const types are not truncated (#13457)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-rc.0-4a9bbcb');
+var VERSION = new core.Version('7.0.0-rc.0-4b15b78');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new Version('7.0.0-rc.0-4a9bbcb');
+var VERSION = new Version('7.0.0-rc.0-4b15b78');
 
 /**
  * @fileoverview added by tsickle
