f2791a8fbdf05556b4174caeb108279d0cc84011
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-10-05 23:38:08 +0000

    master - 4a9bbcb fix(schematics): name is required when generating the drag-drop schematic (#13452)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-rc.0-da63c6f');
+var VERSION = new core.Version('7.0.0-rc.0-4a9bbcb');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new Version('7.0.0-rc.0-da63c6f');
+var VERSION = new Version('7.0.0-rc.0-4a9bbcb');
 
 /**
  * @fileoverview added by tsickle
