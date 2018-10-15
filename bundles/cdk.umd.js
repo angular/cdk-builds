f597c40658700090528d03ac9ae6b3d48d96211c
Andrew Seguin <andrewseguin@users.noreply.github.com>
2018-10-15 22:21:48 +0000

    master - 3f98306 chore: bump package.json to 7.0.0-rc.2 (#13623)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-rc.1-8cfaeea');
+var VERSION = new core.Version('7.0.0-rc.2-3f98306');
 
 exports.VERSION = VERSION;
 
