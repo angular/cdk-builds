085ac519cc880fe8dd4896e5a8f47cd8601e0cdf
Benedikt Roßgardt <benedikt@rossgardt.de>
2018-10-18 13:52:55 +0000

    master - e48a6a9 docs(table component): Improve instructions on sorting (#13658)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-rc.2-f456e85');
+var VERSION = new core.Version('7.0.0-rc.2-e48a6a9');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new Version('7.0.0-rc.2-f456e85');
+var VERSION = new Version('7.0.0-rc.2-e48a6a9');
 
 /**
  * @fileoverview added by tsickle
