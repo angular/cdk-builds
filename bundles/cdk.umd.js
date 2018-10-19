03092064c1b505161a680a859727af287706906c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-10-19 15:02:08 +0000

    7.x - a47810a fix(stepper): emitting the animationDone event twice on some browsers (#13608)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-34812fa');
+var VERSION = new core.Version('7.0.0-a47810a');
 
 exports.VERSION = VERSION;
 
