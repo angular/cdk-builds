d7b2bc0fa0d0b55418e8b871595fec7da2120798
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-10-19 14:33:41 +0000

    master - 16098a7 build: exported scss files missing in CDK (#13679)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-d177d6f');
+var VERSION = new core.Version('7.0.0-16098a7');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-const VERSION = new Version('7.0.0-d177d6f');
+const VERSION = new Version('7.0.0-16098a7');
 
 /**
  * @fileoverview added by tsickle
