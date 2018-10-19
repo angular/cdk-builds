6ab05363c01d67b5b8a2669db8281c66d086becf
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-10-19 14:34:48 +0000

    master - 34812fa build: run e2e tests with chrome headless (#13671)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new Version('7.0.0-16098a7');
+var VERSION = new Version('7.0.0-34812fa');
 
 /**
  * @fileoverview added by tsickle
