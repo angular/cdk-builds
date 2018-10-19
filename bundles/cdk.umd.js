6f72fde48350fc2df74dfc044dc520b701ea97d6
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-10-19 16:25:14 +0000

    7.0.x - cbf6037 refactor(list): convert MatLineSetter into a function (#13369)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.0-8e331a7');
+var VERSION = new core.Version('7.0.0-cbf6037');
 
 exports.VERSION = VERSION;
 
