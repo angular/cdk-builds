093b8dd7d0a70ab4ce043b352491e057b23d40d6
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-10-23 17:57:54 +0000

    7.0.x - 7877404 fix(stroked-button): ripples and focus overlay have incorrect radius (#13745)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.1-92e693e');
+var VERSION = new core.Version('7.0.1-7877404');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new Version('7.0.1-92e693e');
+var VERSION = new Version('7.0.1-7877404');
 
 /**
  * @fileoverview added by tsickle
