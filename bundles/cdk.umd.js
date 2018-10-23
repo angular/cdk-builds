c765a68242c8e0f6b8b220bff106ff8068290562
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-10-23 18:08:10 +0000

    master - cb4f5a0 feat(paginator): allow paginator to be disabled (#13146)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.1-83fc823');
+var VERSION = new core.Version('7.0.1-cb4f5a0');
 
 exports.VERSION = VERSION;
 
