c31b935042d6c791aaba49b074a97f5a2ce7fa82
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-10-24 22:39:45 +0000

    master - bec5823 build: test material with bazel (#13670)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new Version('7.0.1-3cabf78');
+var VERSION = new Version('7.0.1-bec5823');
 
 /**
  * @fileoverview added by tsickle
