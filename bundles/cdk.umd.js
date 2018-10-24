1e52054ffde523a9dcd3544ca1861542a731f1d9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-10-24 22:40:25 +0000

    master - 530c0eb docs(drag-drop): add doc section and live example for customizing the placeholder (#13777)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.1-b30ec56');
+var VERSION = new core.Version('7.0.1-530c0eb');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-const VERSION = new Version('7.0.1-b30ec56');
+const VERSION = new Version('7.0.1-530c0eb');
 
 /**
  * @fileoverview added by tsickle
