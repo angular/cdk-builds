c8c8e6c2ce660061926015563dc292960e2080ad
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-10-25 19:04:12 +0000

    master - ea9354e build: suppress warnings when building tests with bazel (#13816)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.1-fbbe463');
+var VERSION = new core.Version('7.0.1-ea9354e');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new Version('7.0.1-fbbe463');
+var VERSION = new Version('7.0.1-ea9354e');
 
 /**
  * @fileoverview added by tsickle
