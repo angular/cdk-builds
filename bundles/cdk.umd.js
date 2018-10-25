fa9160c892b6dcc87a74510460e5f9948f88d335
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-10-25 19:04:28 +0000

    master - f230633 fix(radio): don't show hover ripples on touch devices (#13701)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.1-ea9354e');
+var VERSION = new core.Version('7.0.1-f230633');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new Version('7.0.1-ea9354e');
+var VERSION = new Version('7.0.1-f230633');
 
 /**
  * @fileoverview added by tsickle
