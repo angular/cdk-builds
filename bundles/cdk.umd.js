880aba3b194c673ca776cada9b6664452e61b6dd
Greg Magolan <gmagolan@gmail.com>
2018-10-29 15:03:36 +0000

    master - b3a0037 build: update to Angular 7.0.1 http_archive & @angular/bazel package (#13831)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.2-a479366');
+var VERSION = new core.Version('7.0.2-b3a0037');
 
 exports.VERSION = VERSION;
 
