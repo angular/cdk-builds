162fc3de02e429a00b4d2d1883d3451581f43b57
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-10-30 17:41:35 +0000

    master - 30a8490 build: update dgeni-packages (#13881)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.2-c433104');
+var VERSION = new core.Version('7.0.2-30a8490');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-const VERSION = new Version('7.0.2-c433104');
+const VERSION = new Version('7.0.2-30a8490');
 
 /**
  * @fileoverview added by tsickle
