7da7fcaaebe18eba2c0b14a1a541992b9094180a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-11-01 23:25:43 +0000

    master - a619ddc build(drag-drop): clean up bazel config (#13934)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.2-4b431fe');
+var VERSION = new core.Version('7.0.2-a619ddc');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new Version('7.0.2-4b431fe');
+var VERSION = new Version('7.0.2-a619ddc');
 
 /**
  * @fileoverview added by tsickle
