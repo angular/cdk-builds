a43090c1af8708d8dfbdf1d118e214533842e2ea
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-11-03 00:32:20 +0000

    master - 235add9 fix(dialog,bottom-sheet): enter animation blocking child animations (#13888)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.2-28e3d36');
+var VERSION = new core.Version('7.0.2-235add9');
 
 exports.VERSION = VERSION;
 
