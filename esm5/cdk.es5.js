366126f22b136f97f9403d0a94c4fb3135e7f1be
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-11-03 00:30:10 +0000

    master - 1fb1c55 fix(menu): unable to swap menu panel after first open (#13819)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new Version('7.0.2-8fad5d1');
+var VERSION = new Version('7.0.2-1fb1c55');
 
 /**
  * @fileoverview added by tsickle
