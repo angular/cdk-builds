bf14ff9342b3bbf7d68b21760684447fb02b6a30
Joey Perrott <josephperrott.github@gmail.com>
2018-11-03 00:28:06 +0000

    master - 6703605 fix(snackbar): override min-width of snackbar on mobile (#13880)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.2-436809c');
+var VERSION = new core.Version('7.0.2-6703605');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-const VERSION = new Version('7.0.2-436809c');
+const VERSION = new Version('7.0.2-6703605');
 
 /**
  * @fileoverview added by tsickle
