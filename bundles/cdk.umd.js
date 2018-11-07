38b4aa1f51559023cb33eeb886616a26774c7e2f
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-11-07 22:01:58 +0000

    master - f6bfcff build: enable size plugin from angular robot (#14025)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.2-4ee6636');
+var VERSION = new core.Version('7.0.2-f6bfcff');
 
 exports.VERSION = VERSION;
 
