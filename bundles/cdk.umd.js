1a06293aa4ffc1ec85dd2887443682555035015a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-11-07 18:44:38 +0000

    master - 31bffb3 fix(datepicker): unable to disable ripple on datepicker toggle (#13997)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.2-d94d176');
+var VERSION = new core.Version('7.0.2-31bffb3');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-const VERSION = new Version('7.0.2-d94d176');
+const VERSION = new Version('7.0.2-31bffb3');
 
 /**
  * @fileoverview added by tsickle
