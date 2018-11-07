97b9aa91901e5a2683c3ae52086405e9407852c9
Jeremy Elbourn <jelbourn@gmail.com>
2018-11-07 01:10:41 +0000

    master - 4508fa1 Revert "Revert "fix(tabs): duplicate animation events on some browsers (#13674)" (#14015)" (#14016)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.2-11c96d6');
+var VERSION = new core.Version('7.0.2-4508fa1');
 
 exports.VERSION = VERSION;
 
