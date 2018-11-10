3812b05fbada99e43318c6260a1ab89f2e8057fc
Justin Tappert <justin@theflybook.com>
2018-11-10 00:14:47 +0000

    master - 10cb578 Docs: Simple grammatical error fix (#14073)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.2-06fb408');
+var VERSION = new core.Version('7.0.2-10cb578');
 
 exports.VERSION = VERSION;
 
