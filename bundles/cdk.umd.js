98229498d8cee5aaccdf1516f0ca7063ef6322ae
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-11-10 00:11:20 +0000

    master - 6136ee7 feat(experimental/dialog): add the ability to control the animation duration (#13694)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.2-5851c91');
+var VERSION = new core.Version('7.0.2-6136ee7');
 
 exports.VERSION = VERSION;
 
