619e5595a26c786bab5f9422b8db859648e52afd
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-11-12 22:32:08 +0000

    master - f8c7069 build: invalid browser name in karma config (#14081)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.2-26c73e0');
+var VERSION = new core.Version('7.0.2-f8c7069');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-const VERSION = new Version('7.0.2-26c73e0');
+const VERSION = new Version('7.0.2-f8c7069');
 
 /**
  * @fileoverview added by tsickle
