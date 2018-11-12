5f9ce54b81d29eb22dcb9fecec49d3329c4016ca
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-11-12 23:45:01 +0000

    master - 7730f45 build: increase max size limit of angular robot (#14082)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.2-f8c7069');
+var VERSION = new core.Version('7.0.2-7730f45');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new Version('7.0.2-f8c7069');
+var VERSION = new Version('7.0.2-7730f45');
 
 /**
  * @fileoverview added by tsickle
