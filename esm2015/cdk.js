46f80aa5439e87476d4ba2f9ae29028dba8e252b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-11-14 22:44:15 +0000

    master - dfcba2b build: add tslib to ts_library rules by default (#14128)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-const VERSION = new Version('7.0.3-09f7578');
+const VERSION = new Version('7.0.3-dfcba2b');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new Version('7.0.3-09f7578');
+var VERSION = new Version('7.0.3-dfcba2b');
 
 /**
  * @fileoverview added by tsickle
