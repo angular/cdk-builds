c03bad945cd68bea4c1deb649974d95ae25fa7d6
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-11-15 15:54:19 +0000

    master - 6d7f417 build: restructure demo-app in favor of bazel (#13965)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new core.Version('7.0.3-39888f3');
+var VERSION = new core.Version('7.0.3-6d7f417');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-const VERSION = new Version('7.0.3-39888f3');
+const VERSION = new Version('7.0.3-6d7f417');
 
 /**
  * @fileoverview added by tsickle
