6e4cf8dad2703f28d6a14d704aaad8bad6e7f588
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-11-19 21:57:20 +0000

    master - c618dde build: dev-app with bazel (#14162)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-const VERSION = new Version('7.0.4-3deae4b');
+const VERSION = new Version('7.0.4-c618dde');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /** *
  * Current version of the Angular Component Development Kit.
   @type {?} */
-var VERSION = new Version('7.0.4-3deae4b');
+var VERSION = new Version('7.0.4-c618dde');
 
 /**
  * @fileoverview added by tsickle
