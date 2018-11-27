184e61c9b2e7161f1436bdc48da811d4d64298c1
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-11-27 15:53:19 +0000

    master - a08ae9a build: better sharing of material test utilities (#13885)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.1.0-0e029ed');
+var VERSION = new core.Version('7.1.0-a08ae9a');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('7.1.0-0e029ed');
+var VERSION = new Version('7.1.0-a08ae9a');
 
 /**
  * @fileoverview added by tsickle
