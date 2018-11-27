27ea71898f965af51c49b87a39762fbda8b37c8f
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-11-27 21:10:19 +0000

    master - 2540b82 build: no longer load rxjs files individually (#14237)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.1.0-9ccda94');
+var VERSION = new core.Version('7.1.0-2540b82');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('7.1.0-9ccda94');
+var VERSION = new Version('7.1.0-2540b82');
 
 /**
  * @fileoverview added by tsickle
