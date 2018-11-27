ccbeffebcc44967fc2423e1ed0f84b9ab70f25ae
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-11-27 01:10:52 +0000

    master - 42681ee build: pretty print example module (#14264)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.1.0-fccbfb5');
+var VERSION = new core.Version('7.1.0-42681ee');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.1.0-fccbfb5');
+const VERSION = new Version('7.1.0-42681ee');
 
 /**
  * @fileoverview added by tsickle
