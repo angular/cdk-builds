d9da4114217d76ae051c5757ef537e1b28c22be8
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-11-27 21:06:21 +0000

    master - 2633c7c build: setup cronjob to run tests against angular snapshots (#14217)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.1.0-50bf6f1');
+var VERSION = new core.Version('7.1.0-2633c7c');
 
 exports.VERSION = VERSION;
 
