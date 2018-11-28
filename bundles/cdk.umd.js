9d7e846beb3746a2930f34416f56d1c90ce67c61
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-11-28 15:57:20 +0000

    master - acaed95 fix(tabs): avoid hitting change detection if text content hasn't changed (#14251)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.1.0-50b9da6');
+var VERSION = new core.Version('7.1.0-acaed95');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.1.0-50b9da6');
+const VERSION = new Version('7.1.0-acaed95');
 
 /**
  * @fileoverview added by tsickle
