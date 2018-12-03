c2421fca414ebc8dc21e79e2b337238c8e238098
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-12-03 23:36:30 +0000

    master - 166279b fix(form-field): long labels and option values going under select arrow (#14268)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('7.1.1-b584888');
+var VERSION = new Version('7.1.1-166279b');
 
 /**
  * @fileoverview added by tsickle
