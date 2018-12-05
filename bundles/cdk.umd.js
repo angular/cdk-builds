a5090392890ab0f10d816b10aedccc9a59bc0c11
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-12-05 18:28:55 +0000

    master - 74e945a fix(menu): reduce specificity of icon selector (#14389)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.1.1-31f0e6d');
+var VERSION = new core.Version('7.1.1-74e945a');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('7.1.1-31f0e6d');
+var VERSION = new Version('7.1.1-74e945a');
 
 /**
  * @fileoverview added by tsickle
