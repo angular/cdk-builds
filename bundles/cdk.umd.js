43d7903500fdcb73a9b060aad3349af54c3efa25
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-12-06 20:45:08 +0000

    master - 9067fd3 build: no longer require updating multiple package.json files for bazel (#14391)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.1.1-63be232');
+var VERSION = new core.Version('7.1.1-9067fd3');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.1.1-63be232');
+const VERSION = new Version('7.1.1-9067fd3');
 
 /**
  * @fileoverview added by tsickle
