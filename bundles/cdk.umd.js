75b5f60211539e4714f44f9a4a6b99deffd76818
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-12-07 01:02:22 +0000

    master - 41eb27e fix(radio): host element unable to receive focus events (#13956)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.1.1-ab78183');
+var VERSION = new core.Version('7.1.1-41eb27e');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('7.1.1-ab78183');
+var VERSION = new Version('7.1.1-41eb27e');
 
 /**
  * @fileoverview added by tsickle
