834ae9fe98ed72c0368374235f8a4c3071976b59
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-12-11 22:22:08 +0000

    master - 399019b build: re-add firebase app types (#14476)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.1.1-1124dc2');
+var VERSION = new core.Version('7.1.1-399019b');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.1.1-1124dc2');
+const VERSION = new Version('7.1.1-399019b');
 
 /**
  * @fileoverview added by tsickle
