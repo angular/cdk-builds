34f185f57b0d0c54b2d204ba587d6186dfbffc22
mmalerba <mmalerba@google.com>
2018-12-11 18:03:23 +0000

    7.1.x - 0ce6bda Revert "fix(checkbox): redirect focus to underlying input element" (#14469)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.1.1-0b9337b');
+var VERSION = new core.Version('7.1.1-0ce6bda');
 
 exports.VERSION = VERSION;
 
