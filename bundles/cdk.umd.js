e6e54083877f8d1af65ad072568ed455deee3d96
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-12-11 23:06:19 +0000

    master - 058f5ef build: generate api docs for cdk using bazel (#14427)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.1.1-cefff17');
+var VERSION = new core.Version('7.1.1-058f5ef');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.1.1-cefff17');
+const VERSION = new Version('7.1.1-058f5ef');
 
 /**
  * @fileoverview added by tsickle
