c1f47e8bd877b91cc6506b3df6df58fac8d522db
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-12-12 21:46:05 +0000

    master - a3fef46 build: update nodejs bazel rules (#14466)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.1.1-97f52c0');
+var VERSION = new core.Version('7.1.1-a3fef46');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('7.1.1-97f52c0');
+var VERSION = new Version('7.1.1-a3fef46');
 
 /**
  * @fileoverview added by tsickle
