c9d2c3cdce73f8b93a1f00c30e47a7b899180f88
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-12-12 18:52:29 +0000

    master - 97f52c0 docs(form-field): native select reset values not working in examples (#14236)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.1.1-94e76de');
+var VERSION = new core.Version('7.1.1-97f52c0');
 
 exports.VERSION = VERSION;
 
