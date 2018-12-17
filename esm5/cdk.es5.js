f0901313c815ceeb5aaed0153a69bf9eb5f618d4
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-12-17 22:45:16 +0000

    master - 04c3d6e build: update circleci to latest nodejs version (#14550)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('7.1.1-bc8fc75');
+var VERSION = new Version('7.1.1-04c3d6e');
 
 /**
  * @fileoverview added by tsickle
