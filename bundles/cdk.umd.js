b5a4b4d42e0814d585b69017127835de7a351ae3
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-12-18 15:24:27 +0000

    master - 9485aff fix(bottom-sheet): bottom-sheet content not being read out by screen readers (#14534)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.1.1-bfacbb5');
+var VERSION = new core.Version('7.1.1-9485aff');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.1.1-bfacbb5');
+const VERSION = new Version('7.1.1-9485aff');
 
 /**
  * @fileoverview added by tsickle
