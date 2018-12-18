6ba3b92341b044fb1f6d6c32f618b691a347dba1
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-12-18 04:23:35 +0000

    master - 88523c3 build: lazily compile examples in dev app (#14522)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.1.1-48d6907');
+const VERSION = new Version('7.1.1-88523c3');
 
 /**
  * @fileoverview added by tsickle
