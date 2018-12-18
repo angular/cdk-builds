54e5bec489bf5a1b5aa29f0d237f270ad70a6882
vivian-hu <41269630+vivian-hu@users.noreply.github.com>
2018-12-18 15:33:44 +0000

    master - eeda91d fix(tree): allow tree node to have undefined child or null child (#14482)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.1.1-fbb2a13');
+var VERSION = new core.Version('7.1.1-eeda91d');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.1.1-fbb2a13');
+const VERSION = new Version('7.1.1-eeda91d');
 
 /**
  * @fileoverview added by tsickle
