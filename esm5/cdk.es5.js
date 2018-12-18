6caf14f704781a8925db50829f2d89e603099f0e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-12-18 04:25:11 +0000

    master - c5bdab8 build: add 404 page to dev app (#14551)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('7.1.1-7ade500');
+var VERSION = new Version('7.1.1-c5bdab8');
 
 /**
  * @fileoverview added by tsickle
