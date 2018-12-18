e9f42bdb29521930d600c6fc2acd1df8778a3fe2
swseverance <swseverance@gmail.com>
2018-12-18 04:21:16 +0000

    master - 2853575 chore: fix duplicate "should" in unit test names (#14528)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.1.1-d304b41');
+var VERSION = new core.Version('7.1.1-2853575');
 
 exports.VERSION = VERSION;
 
