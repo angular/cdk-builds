448153a07f92373cc120dc861e02829206fa28c8
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-12-18 04:21:54 +0000

    master - 48d6907 build: temporarily disable robot size plugin (#14525)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.1.1-2853575');
+var VERSION = new core.Version('7.1.1-48d6907');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.1.1-2853575');
+const VERSION = new Version('7.1.1-48d6907');
 
 /**
  * @fileoverview added by tsickle
