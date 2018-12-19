79980cf37e74fcfa7a82164447610d153354a47c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-12-19 15:11:09 +0000

    master - 7ce7797 refactor(card): remove duplicate style declaration (#13358)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.2.0-14b90db');
+const VERSION = new Version('7.2.0-7ce7797');
 
 /**
  * @fileoverview added by tsickle
