7e05caf1c0d0cb34f551a915b64f44cb4682f249
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-12-21 01:33:10 +0000

    master - 9914970 build: temporary disable branch manager (#14600)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.2.0-12bf619');
+const VERSION = new Version('7.2.0-9914970');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('7.2.0-12bf619');
+var VERSION = new Version('7.2.0-9914970');
 
 /**
  * @fileoverview added by tsickle
