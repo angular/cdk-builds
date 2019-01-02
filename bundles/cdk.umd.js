d82bf61df960d5fed32d126c59ff06c86b4b43d9
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-01-02 23:50:52 +0000

    7.2.x - d22f48c build: package material api docs into npm package (#14508)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.2.0-1b6e7b1');
+var VERSION = new core.Version('7.2.0-d22f48c');
 
 exports.VERSION = VERSION;
 
