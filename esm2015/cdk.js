b4f6c093be8ee5c0d65b75da9576f18ea299a388
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-01-03 23:20:56 +0000

    7.2.x - 7d2748e fix(slider): don't handle keyboard events with modifier keys (#14675)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.2.0-65d615b');
+const VERSION = new Version('7.2.0-7d2748e');
 
 /**
  * @fileoverview added by tsickle
