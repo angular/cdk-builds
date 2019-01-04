dc530ef293655a19abaebf3ee3631a6a686c1eff
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-01-04 18:59:49 +0000

    master - 65ecb08 fix(typography): deprecation warning in latest sass version (#14673)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.2.0-3c71348');
+var VERSION = new core.Version('7.2.0-65ecb08');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.2.0-3c71348');
+const VERSION = new Version('7.2.0-65ecb08');
 
 /**
  * @fileoverview added by tsickle
