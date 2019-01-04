6e5b55471f6ca53e10c35e81cd7d20f5e6c5774e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-01-04 22:11:42 +0000

    master - 391a9fd fix(select): form field state not updated if options are reset (#14720)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.2.0-7188719');
+var VERSION = new core.Version('7.2.0-391a9fd');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('7.2.0-7188719');
+var VERSION = new Version('7.2.0-391a9fd');
 
 /**
  * @fileoverview added by tsickle
