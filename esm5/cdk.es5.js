748f513cf6e134d055249cafa2e3153c696f6a8c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-01-15 22:29:11 +0000

    master - 73d0fb9 fix(list): action list button outline not being reset on firefox (#14693)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('7.2.1-1d4bb18');
+var VERSION = new Version('7.2.1-73d0fb9');
 
 /**
  * @fileoverview added by tsickle
