15a8ba8372905ad60bad62472c9d9e7eb5c89a38
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-01-16 03:46:20 +0000

    master - 5773a5b chore(tabs): update api goldens (#14844)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.2.1-f66302d');
+const VERSION = new Version('7.2.1-5773a5b');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('7.2.1-f66302d');
+var VERSION = new Version('7.2.1-5773a5b');
 
 /**
  * @fileoverview added by tsickle
