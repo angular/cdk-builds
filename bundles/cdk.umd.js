90b143e9ed77c54fe4884fb19deb7e5510108838
swseverance <swseverance@gmail.com>
2019-01-29 00:21:39 +0000

    master - 17fcb37 fix(examples): fix form-field custom control `disabled` input (#14957)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.0-ddd347d');
+var VERSION = new core.Version('7.3.0-17fcb37');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.3.0-ddd347d');
+const VERSION = new Version('7.3.0-17fcb37');
 
 /**
  * @fileoverview added by tsickle
