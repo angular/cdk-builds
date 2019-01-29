f3e05b6a3c8084e4cc32cb054aac4ced96e3cead
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-01-29 00:21:16 +0000

    master - 65730d3 chore: fix dead link in docs (#14962)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.0-b51f143');
+var VERSION = new core.Version('7.3.0-65730d3');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.3.0-b51f143');
+const VERSION = new Version('7.3.0-65730d3');
 
 /**
  * @fileoverview added by tsickle
