836d2f3dc09818443ea1b84c16092c0fec593ff1
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-01-30 22:10:38 +0000

    master - d72eb46 fix(radio): not updating DOM node name if group name changes (#14950)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.0-fda0c2d');
+var VERSION = new core.Version('7.3.0-d72eb46');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('7.3.0-fda0c2d');
+var VERSION = new Version('7.3.0-d72eb46');
 
 /**
  * @fileoverview added by tsickle
