24ba317acab1d8797c865b6a733e3aa665163fa7
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-01-30 20:22:50 +0000

    master - b774e81 build: fix syntax error within codeowners file (#15014)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.0-0ab8113');
+var VERSION = new core.Version('7.3.0-b774e81');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.3.0-0ab8113');
+const VERSION = new Version('7.3.0-b774e81');
 
 /**
  * @fileoverview added by tsickle
