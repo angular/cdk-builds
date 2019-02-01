85732b344f6ae572bbb8fd7ffae5cc54de7da768
Abderrahmane Hamila <46672128+s2-abdo@users.noreply.github.com>
2019-02-01 06:20:07 +0000

    master - e039d63 fix(cdk-stepper): coercing selectedIndex value to a Number (#14817)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.0-e136fc8');
+var VERSION = new core.Version('7.3.0-e039d63');
 
 exports.VERSION = VERSION;
 
--- esm5/stepper.es5.js
+++ esm5/stepper.es5.js
@@ -8,7 +8,7 @@
 import { Directive, TemplateRef, ElementRef, ChangeDetectionStrategy, ChangeDetectorRef, Component, ContentChild, ContentChildren, EventEmitter, forwardRef, Inject, Input, Optional, Output, ViewChild, ViewEncapsulation, InjectionToken, NgModule } from '@angular/core';
 import { FocusKeyManager } from '@angular/cdk/a11y';
 import { Directionality, BidiModule } from '@angular/cdk/bidi';
-import { coerceBooleanProperty } from '@angular/cdk/coercion';
+import { coerceBooleanProperty, coerceNumberProperty } from '@angular/cdk/coercion';
 import { END, ENTER, HOME, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
 import { DOCUMENT, CommonModule } from '@angular/common';
 import '@angular/forms';
@@ -353,19 +353,21 @@ var CdkStepper = /** @class */ (function () {
          * @return {?}
          */
         function (index) {
+            /** @type {?} */
+            var newIndex = coerceNumberProperty(index);
             if (this.steps) {
                 // Ensure that the index can't be out of bounds.
-                if (index < 0 || index > this.steps.length - 1) {
+                if (newIndex < 0 || newIndex > this.steps.length - 1) {
                     throw Error('cdkStepper: Cannot assign out-of-bounds value to `selectedIndex`.');
                 }
-                if (this._selectedIndex != index &&
-                    !this._anyControlsInvalidOrPending(index) &&
-                    (index >= this._selectedIndex || this.steps.toArray()[index].editable)) {
+                if (this._selectedIndex != newIndex &&
+                    !this._anyControlsInvalidOrPending(newIndex) &&
+                    (newIndex >= this._selectedIndex || this.steps.toArray()[newIndex].editable)) {
                     this._updateSelectedItemIndex(index);
                 }
             }
             else {
-                this._selectedIndex = index;
+                this._selectedIndex = newIndex;
             }
         },
         enumerable: true,
