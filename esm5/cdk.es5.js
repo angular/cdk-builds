5f7e57faa57a06a04965fdb9d838305b044feb96
Andrew Seguin <andrewseguin@users.noreply.github.com>
2019-02-07 02:25:47 +0000

    master - a4c235a test: remove div ngFor in chip list tests (#15101)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('7.3.1-a3ab5e3');
+var VERSION = new Version('7.3.1-a4c235a');
 
 /**
  * @fileoverview added by tsickle
