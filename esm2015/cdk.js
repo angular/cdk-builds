ad22c8083ed0c8f0c60f311e719f767dacc51dc5
Pavel Agarkov <pavel.agarkov@gmail.com>
2019-02-07 19:54:33 +0000

    master - b10dd04 fix(icon): add notranslate class (#14889)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.3.1-ed11370');
+const VERSION = new Version('7.3.1-b10dd04');
 
 /**
  * @fileoverview added by tsickle
