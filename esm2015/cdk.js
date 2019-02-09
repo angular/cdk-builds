81e595eb235a9541a850a6ba831912caa742e654
mmalerba <mmalerba@google.com>
2019-02-09 00:08:31 +0000

    master - 57aadc2 chore: add `yarn format` command and pre-commit hook to ensure code is formatted (#15132)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.3.1-d69bc45');
+const VERSION = new Version('7.3.1-57aadc2');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('7.3.1-d69bc45');
+var VERSION = new Version('7.3.1-57aadc2');
 
 /**
  * @fileoverview added by tsickle
