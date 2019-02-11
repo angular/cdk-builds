a56deda3f42fb9ed7228800e38e2dfa7817a23c3
Suresh918 <suresh918.uputhula@gmail.com>
2019-02-11 23:59:00 +0000

    master - 123976f Button icons aria labels changes #14998 (#15112)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.2-cb7d3b9');
+var VERSION = new core.Version('7.3.2-123976f');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.3.2-cb7d3b9');
+const VERSION = new Version('7.3.2-123976f');
 
 /**
  * @fileoverview added by tsickle
