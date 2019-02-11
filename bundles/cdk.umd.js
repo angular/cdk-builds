ea29eebe126e39e5a5f1032e11968a50f3500392
Suresh918 <suresh918.uputhula@gmail.com>
2019-02-11 23:58:53 +0000

    master - cb7d3b9 docs(button-toggle): add aria-labels to examples (#15114)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.2-d410044');
+var VERSION = new core.Version('7.3.2-cb7d3b9');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('7.3.2-d410044');
+var VERSION = new Version('7.3.2-cb7d3b9');
 
 /**
  * @fileoverview added by tsickle
