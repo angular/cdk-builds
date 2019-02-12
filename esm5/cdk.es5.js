e37644a27c653ebb3e624a4cdc75cda702085510
Esteban Marin <estebanmarin@gmx.ch>
2019-02-12 15:34:28 +0000

    master - 2652532 fix(input): no focus on click in IE if clicked outside native control (#15108)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('7.3.2-3787695');
+var VERSION = new Version('7.3.2-2652532');
 
 /**
  * @fileoverview added by tsickle
