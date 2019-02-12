d12490a4e3e635e4169b689448002a278bb2bcf3
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-02-12 15:28:23 +0000

    master - 3787695 fix(bottom-sheet): focus trap not being attached when autoFocus is disabled (#15125)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('7.3.2-123976f');
+var VERSION = new Version('7.3.2-3787695');
 
 /**
  * @fileoverview added by tsickle
