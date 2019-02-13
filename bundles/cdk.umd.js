1a0fd74af6d388d329f72575eb47e4d67c9c64dc
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-02-13 15:38:11 +0000

    master - 35e89e5 fix: errors when building theme using CSS variables (#15140)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.2-3163cc6');
+var VERSION = new core.Version('7.3.2-35e89e5');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('7.3.2-3163cc6');
+var VERSION = new Version('7.3.2-35e89e5');
 
 /**
  * @fileoverview added by tsickle
