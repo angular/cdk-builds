06d32b12e3401293d99d360c302f89deef539e74
Jeremy Elbourn <jelbourn@gmail.com>
2019-02-14 18:03:58 +0000

    master - 0db9044 fix(table): add missing rowgroup roles (#15131)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.3.2-9ca5722');
+const VERSION = new Version('7.3.2-0db9044');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('7.3.2-9ca5722');
+var VERSION = new Version('7.3.2-0db9044');
 
 /**
  * @fileoverview added by tsickle
