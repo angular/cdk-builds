0ff2b71b1da5517cbd2cae86cea35bab4df06dab
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-02-20 00:48:45 +0000

    master - 8fc97ac fix: add peer dependency on @angular/forms (#15133)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('7.3.2-5216d38');
+var VERSION = new Version('7.3.2-8fc97ac');
 
 /**
  * @fileoverview added by tsickle
