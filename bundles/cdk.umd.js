1617d2b599f06c9d439516564d4cade0edc240be
Suresh918 <suresh918.uputhula@gmail.com>
2019-02-22 18:39:47 +0000

    master - edb7835 docs(menu): add aria-label to example (#15146)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.3-83135e1');
+var VERSION = new core.Version('7.3.3-edb7835');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.3.3-83135e1');
+const VERSION = new Version('7.3.3-edb7835');
 
 /**
  * @fileoverview added by tsickle
