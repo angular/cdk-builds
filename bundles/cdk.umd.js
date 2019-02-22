cc548af0ff101cacd5c8e659723c2ba78e96421d
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-02-22 15:12:58 +0000

    master - 78f55ea fix(select): update panel width if the viewport size changes (#14932)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.3-bcf2781');
+var VERSION = new core.Version('7.3.3-78f55ea');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.3.3-bcf2781');
+const VERSION = new Version('7.3.3-78f55ea');
 
 /**
  * @fileoverview added by tsickle
