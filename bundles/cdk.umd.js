64cdd0200ead81e2b5622113a0d947fc3c443514
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-02-22 15:18:57 +0000

    master - 66d38fc feat(selection-list): support specifying theme color (#15237)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.3-413fe33');
+var VERSION = new core.Version('7.3.3-66d38fc');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('7.3.3-413fe33');
+var VERSION = new Version('7.3.3-66d38fc');
 
 /**
  * @fileoverview added by tsickle
