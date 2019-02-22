f3a565a36604dd55387e333d1fb30131cb1bbb9a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-02-22 15:13:00 +0000

    master - 677db8c fix(radio): underlying label not expanding to width of radio button (#14895)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('7.3.3-bcf2781');
+var VERSION = new Version('7.3.3-677db8c');
 
 /**
  * @fileoverview added by tsickle
