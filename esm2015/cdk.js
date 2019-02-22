d683805d38c7b1e578a02c79cd81d42b840bc818
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-02-22 15:14:05 +0000

    master - a88d053 fix(progress-bar): clear aria-valuenow in indeterminate or query mode (#15019)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.3.3-dd4257e');
+const VERSION = new Version('7.3.3-a88d053');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('7.3.3-dd4257e');
+var VERSION = new Version('7.3.3-a88d053');
 
 /**
  * @fileoverview added by tsickle
