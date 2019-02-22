00fbb816b48f482fbe1e88390341c9b25beed1eb
Amadou Sall <ahasall.dev@gmail.com>
2019-02-22 05:57:07 +0000

    master - 5d3f515 docs: clarify docs on creating a custom material module (#15229)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.3-fe2881d');
+var VERSION = new core.Version('7.3.3-5d3f515');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('7.3.3-fe2881d');
+var VERSION = new Version('7.3.3-5d3f515');
 
 /**
  * @fileoverview added by tsickle
