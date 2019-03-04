d27817720c8e21c681b054ae5daf7012fded2a78
Thomas <4159106+dtslvr@users.noreply.github.com>
2019-03-04 22:13:07 +0000

    7.3.x - 3930da2 Fix typo in datepicker example (#15327)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.3-3f876e2');
+var VERSION = new core.Version('7.3.3-3930da2');
 
 exports.VERSION = VERSION;
 
