e31d35789e11c88e2f8151183b69ff5807edad49
Thomas <4159106+dtslvr@users.noreply.github.com>
2019-03-04 22:16:35 +0000

    master - e130c5c Fix typo in datepicker example (#15327)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.3-3f8d02c');
+var VERSION = new core.Version('7.3.3-e130c5c');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('7.3.3-3f8d02c');
+var VERSION = new Version('7.3.3-e130c5c');
 
 /**
  * @fileoverview added by tsickle
