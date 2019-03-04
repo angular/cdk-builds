c5c3e4c2bf704e039829307ce55539df12dfd2ab
swseverance <swseverance@gmail.com>
2019-03-04 19:21:44 +0000

    master - 0747228 test(list-key-manager): fix test description (#15352)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.3.3-4dab2f6');
+const VERSION = new Version('7.3.3-0747228');
 
 /**
  * @fileoverview added by tsickle
