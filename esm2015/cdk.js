1c2ae5a422d64622a8ac433d395f0d80b653d01a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-03-04 19:20:09 +0000

    master - 4dab2f6 test(expansion): rework tests that depend on static queries (#15333)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.3.3-4cffc20');
+const VERSION = new Version('7.3.3-4dab2f6');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('7.3.3-4cffc20');
+var VERSION = new Version('7.3.3-4dab2f6');
 
 /**
  * @fileoverview added by tsickle
