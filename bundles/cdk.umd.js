06ea1c1c83a4625fa050b332669cea622a548581
Andrew Seguin <andrewseguin@users.noreply.github.com>
2019-03-04 19:22:01 +0000

    7.3.x - ad03e52 docs(chips): remove comment regarding disabled style (#15360)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.3-00e4d7a');
+var VERSION = new core.Version('7.3.3-ad03e52');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.3.3-00e4d7a');
+const VERSION = new Version('7.3.3-ad03e52');
 
 /**
  * @fileoverview added by tsickle
