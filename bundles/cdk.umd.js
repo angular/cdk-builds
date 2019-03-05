79a8c95f0ffccb17ff7ce0c50d0635cfe628670b
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-03-05 15:13:23 +0000

    7.3.x - 78e7207 fix(scrolling): virtual scroll viewport error during server-side rendering (#15299)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.3-3c5e671');
+var VERSION = new core.Version('7.3.3-78e7207');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('7.3.3-3c5e671');
+var VERSION = new Version('7.3.3-78e7207');
 
 /**
  * @fileoverview added by tsickle
