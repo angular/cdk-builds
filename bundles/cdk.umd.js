d65dee43f851de71c372d4ea9619f0538354049e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-03-05 16:39:19 +0000

    7.3.x - 4ad4fd2 chore(select): fix api golden failure (#15384)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.3-81a73c6');
+var VERSION = new core.Version('7.3.3-4ad4fd2');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.3.3-81a73c6');
+const VERSION = new Version('7.3.3-4ad4fd2');
 
 /**
  * @fileoverview added by tsickle
