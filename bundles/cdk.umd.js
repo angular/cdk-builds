754119ee1f8a1cb6d4dcd0eee4717f0f9de4888f
Esteban Marin <estebanmarin@gmx.ch>
2019-03-05 15:56:13 +0000

    master - bc6b5fb fix(select): Fixes width-issue of select option panel in IE (#11801)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.3-4ff1c95');
+var VERSION = new core.Version('7.3.3-bc6b5fb');
 
 exports.VERSION = VERSION;
 
