a6409c1e128da9487230b18318d46f6e77e3f762
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-03-11 17:42:40 +0000

    master - ac7b3cd test(chip-list): rework tests to work under Ivy (#15428)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.3.4-d0cd9c0');
+const VERSION = new Version('7.3.4-ac7b3cd');
 
 /**
  * @fileoverview added by tsickle
