e17871ed2b5626c5ca6ff95906274f55334c4a99
Matias Niemelä <matias@yearofmoo.com>
2019-03-14 04:23:56 +0000

    master - 59818d1 fix(tooltip): render style values in ngOnInit instead of the constructor (#15469)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.3.4-a146499');
+const VERSION = new Version('7.3.4-59818d1');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('7.3.4-a146499');
+var VERSION = new Version('7.3.4-59818d1');
 
 /**
  * @fileoverview added by tsickle
