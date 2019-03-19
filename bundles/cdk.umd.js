b13dbadf96757c9521bccdea96e15c7fb6d8c971
mmalerba <mmalerba@google.com>
2019-03-19 22:06:56 +0000

    master - 1dce080 chore(dev-app): add lazy loading (#15522)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.5-7baeaa7');
+var VERSION = new core.Version('7.3.5-1dce080');
 
 exports.VERSION = VERSION;
 
