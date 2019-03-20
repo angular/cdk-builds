143fb2393398737a649ac04bd9ffca62da8914b5
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-03-20 20:58:30 +0000

    master - d646266 fix(icon): clearing user content when svgIcon is bound to falsy value (#15290)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.5-2b14952');
+var VERSION = new core.Version('7.3.5-d646266');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.3.5-2b14952');
+const VERSION = new Version('7.3.5-d646266');
 
 /**
  * @fileoverview added by tsickle
