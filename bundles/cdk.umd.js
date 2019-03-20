999d0f19a05ef9d60159ce9fc30f556dfd020c6f
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-03-20 16:21:30 +0000

    master - fce7205 refactor(snack-bar): rework to account for ivy (#15536)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.5-1dce080');
+var VERSION = new core.Version('7.3.5-fce7205');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.3.5-1dce080');
+const VERSION = new Version('7.3.5-fce7205');
 
 /**
  * @fileoverview added by tsickle
