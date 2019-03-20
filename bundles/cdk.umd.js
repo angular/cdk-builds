b5e5480948d1444c902407dcaf1cf4f11705beba
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-03-20 20:57:20 +0000

    master - 2b14952 fix(badge): increase font size of small badge (#15280)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.5-fce7205');
+var VERSION = new core.Version('7.3.5-2b14952');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.3.5-fce7205');
+const VERSION = new Version('7.3.5-2b14952');
 
 /**
  * @fileoverview added by tsickle
