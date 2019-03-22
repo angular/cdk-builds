079fb47bbd183be330a209b43a8a62ad45edca0b
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-03-22 22:19:06 +0000

    master - f889547 refactor(badge): rework to account for ivy changes (#15555)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.3.5-177a433');
+const VERSION = new Version('7.3.5-f889547');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('7.3.5-177a433');
+var VERSION = new Version('7.3.5-f889547');
 
 /**
  * @fileoverview added by tsickle
