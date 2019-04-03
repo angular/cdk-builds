216f9bd42c805fc091ac55d299aec8e30b4299f6
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-04-03 22:53:23 +0000

    master - c3ead65 fix(dialog): remove default aria-label from mat-dialog-close (#15654)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.6-61b29bf');
+var VERSION = new core.Version('7.3.6-c3ead65');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('7.3.6-61b29bf');
+var VERSION = new Version('7.3.6-c3ead65');
 
 /**
  * @fileoverview added by tsickle
