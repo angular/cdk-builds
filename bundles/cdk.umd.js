76caaf41ffd8743256378e45f0cc34a46199e1d0
Jeremy Elbourn <jelbourn@gmail.com>
2019-04-05 21:03:14 +0000

    master - 4816ecd chore: add cdk-experimental to deps for material-examples (#15737)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.6-533eca6');
+var VERSION = new core.Version('7.3.6-4816ecd');
 
 exports.VERSION = VERSION;
 
