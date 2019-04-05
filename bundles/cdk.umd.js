8e54b310f2e75990064f92f9e273bcbe2fd09329
Rudar Daman Singla <damanrudar@gmail.com>
2019-04-05 21:22:05 +0000

    master - 578790b fix: missing tag in footer-row of material table (#15711)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.6-a258400');
+var VERSION = new core.Version('7.3.6-578790b');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('7.3.6-a258400');
+const VERSION = new Version('7.3.6-578790b');
 
 /**
  * @fileoverview added by tsickle
