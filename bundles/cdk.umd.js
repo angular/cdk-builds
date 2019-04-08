bde88fc71975f8fb7aaf2dab1d69c437729df5dd
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-04-08 18:05:10 +0000

    master - bb5d544 feat: run v8 migration schematics for v8 beta and rc releases (#15753)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.0-beta.0-881b114');
+var VERSION = new core.Version('8.0.0-beta.0-bb5d544');
 
 exports.VERSION = VERSION;
 
