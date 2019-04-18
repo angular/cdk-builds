c2d605db8e7730a69047412b3963de0aaa11f743
mmalerba <mmalerba@google.com>
2019-04-18 01:20:58 +0000

    master - 85f95f0 build: make cdk-experimental and material-experimental build settings (#15843)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.0-beta.2-7529d86');
+var VERSION = new core.Version('8.0.0-beta.2-85f95f0');
 
 exports.VERSION = VERSION;
 
