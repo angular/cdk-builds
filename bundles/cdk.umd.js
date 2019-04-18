da25e1fec8c3dcf62c260ea558f584150b6d9443
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-04-18 22:26:57 +0000

    master - a88d883 refactor(autocomplete): explicitly set static flag on all queries (#15721)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.0-beta.2-79f8542');
+var VERSION = new core.Version('8.0.0-beta.2-a88d883');
 
 exports.VERSION = VERSION;
 
