96f956cf0959a88b9e1655a4242fb220b20ce345
mmalerba <mmalerba@google.com>
2019-04-19 22:30:11 +0000

    master - b7f064b chore: fix typo in MDC skeleton code (#15863)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.0-beta.2-427c1e7');
+var VERSION = new core.Version('8.0.0-beta.2-b7f064b');
 
 exports.VERSION = VERSION;
 
