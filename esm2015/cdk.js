5df2db1ffd54090102d0abc70580c6307e6e6294
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-04-19 15:43:35 +0000

    master - 6358ab1 build: fix race condition when building styles (#15860)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.2-fd4bf62');
+const VERSION = new Version('8.0.0-beta.2-6358ab1');
 
 /**
  * @fileoverview added by tsickle
