641d72221bc44a01d36193e605b44c9d199865cf
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-04-25 19:18:50 +0000

    master - 8f23917 prototype(menu): create prototype menu based on MDC Web (#15857)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.0-beta.2-d1366ba');
+var VERSION = new core.Version('8.0.0-beta.2-8f23917');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.2-d1366ba');
+const VERSION = new Version('8.0.0-beta.2-8f23917');
 
 /**
  * @fileoverview added by tsickle
