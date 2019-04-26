a42b46d6ab25de7e8d738a052af2f41e7bc054b1
Artur Araslanov <wizeek80@gmail.com>
2019-04-26 17:07:29 +0000

    master - 98aa699 Fixed double quotes in table.md (#15910)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.0-beta.2-35fece8');
+var VERSION = new core.Version('8.0.0-beta.2-98aa699');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('8.0.0-beta.2-35fece8');
+var VERSION = new Version('8.0.0-beta.2-98aa699');
 
 /**
  * @fileoverview added by tsickle
