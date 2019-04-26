cf63564652ef669452ce2a4dbcc85e54202bc80c
Karl Seamon <bannanafiend@gmail.com>
2019-04-26 18:43:15 +0000

    master - 058ed0f feat(popover-edit): Material version of popover edit (#15873)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.0-beta.2-98aa699');
+var VERSION = new core.Version('8.0.0-beta.2-058ed0f');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.2-98aa699');
+const VERSION = new Version('8.0.0-beta.2-058ed0f');
 
 /**
  * @fileoverview added by tsickle
