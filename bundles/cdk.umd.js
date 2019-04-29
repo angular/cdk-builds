f131b416051c48bb8c2f91fbfab140c7f6d3003e
mmalerba <mmalerba@google.com>
2019-04-29 20:16:24 +0000

    master - f21d1ae chore: bump version to 8.0.0-rc.0 w/ changelog (#15927)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.0-beta.2-84928d2');
+var VERSION = new core.Version('8.0.0-rc.0-f21d1ae');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('8.0.0-beta.2-84928d2');
+var VERSION = new Version('8.0.0-rc.0-f21d1ae');
 
 /**
  * @fileoverview added by tsickle
