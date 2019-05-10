b65bfdcbebccbe8a5981968a0f6ef77e573d6077
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-05-10 20:56:16 +0000

    master - 3968918 refactor(popover-edit): remove unnecessary components (#15954)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.0-rc.0-7774ae1');
+var VERSION = new core.Version('8.0.0-rc.0-3968918');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('8.0.0-rc.0-7774ae1');
+var VERSION = new Version('8.0.0-rc.0-3968918');
 
 /**
  * @fileoverview added by tsickle
