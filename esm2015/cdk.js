fbbe4dfdc709f14b01f8c69e70b0e3bab5149ed0
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-05-10 20:39:40 +0000

    master - 78bbc92 docs(drag-drop): add docs for cdkDragFreeDragPosition (#15978)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.0-10c546c');
+const VERSION = new Version('8.0.0-rc.0-78bbc92');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('8.0.0-rc.0-10c546c');
+var VERSION = new Version('8.0.0-rc.0-78bbc92');
 
 /**
  * @fileoverview added by tsickle
