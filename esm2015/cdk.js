63669bd370f4280d919590b3cf4fbc68a588e297
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-05-10 20:55:35 +0000

    master - 7774ae1 fix(popover-edit): direction not being passed to overlay (#15951)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.0-cbad345');
+const VERSION = new Version('8.0.0-rc.0-7774ae1');
 
 /**
  * @fileoverview added by tsickle
