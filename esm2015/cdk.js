b3f8431a488f7550f850e579d8ab1fb00b747266
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-05-10 20:41:12 +0000

    master - 133a0f9 chore: switch dev-app to secondary entry-points  (#15983)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.0-f8a2a9d');
+const VERSION = new Version('8.0.0-rc.0-133a0f9');
 
 /**
  * @fileoverview added by tsickle
