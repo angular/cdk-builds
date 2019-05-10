1ac4d6c3c2016b906e2dece58657c0898ac9e66a
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-05-10 21:39:13 +0000

    master - fb15e89 build: add rule to warn for undecorated base classes (#15976)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.0-3968918');
+const VERSION = new Version('8.0.0-rc.0-fb15e89');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('8.0.0-rc.0-3968918');
+var VERSION = new Version('8.0.0-rc.0-fb15e89');
 
 /**
  * @fileoverview added by tsickle
