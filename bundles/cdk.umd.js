7bf40cd350f83016219a594a5fb94652369c76ee
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-05-13 04:54:33 +0000

    master - 43b4fec fix(checkbox): don't show hover ripples on touch devices (#13700)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.0-rc.0-f6cd86e');
+var VERSION = new core.Version('8.0.0-rc.0-43b4fec');
 
 exports.VERSION = VERSION;
 
