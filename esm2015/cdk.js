c90e5fe576ac1a767281012877c19580fc5850ba
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-05-13 17:13:39 +0000

    master - cf03621 fix(icon): handle unescaped characters in names (#15678)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.0-7794304');
+const VERSION = new Version('8.0.0-rc.0-cf03621');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('8.0.0-rc.0-7794304');
+var VERSION = new Version('8.0.0-rc.0-cf03621');
 
 /**
  * @fileoverview added by tsickle
