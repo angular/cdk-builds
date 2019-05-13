cdfae8dc16ae178cc40854088ddf5cdc53fd0c6a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-05-13 17:14:35 +0000

    master - 58db9db fix(menu): trigger not unsubscribing from panel close stream (#15667)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.0-rc.0-623cd06');
+var VERSION = new core.Version('8.0.0-rc.0-58db9db');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.0-623cd06');
+const VERSION = new Version('8.0.0-rc.0-58db9db');
 
 /**
  * @fileoverview added by tsickle
