e1583e8d8dc1198cf4c6ce1790550b039e6afc6f
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-05-13 17:13:43 +0000

    master - 623cd06 fix(button-toggle): clear name from host node (#15668)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.0-rc.0-7794304');
+var VERSION = new core.Version('8.0.0-rc.0-623cd06');
 
 exports.VERSION = VERSION;
 
