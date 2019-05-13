7074429a9214be458608d3fd3a10730b348d0525
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-05-13 04:40:37 +0000

    master - fc0b971 refactor(drag-drop): add current index to entered event (#15932)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.0-rc.0-2e18570');
+var VERSION = new core.Version('8.0.0-rc.0-fc0b971');
 
 exports.VERSION = VERSION;
 
