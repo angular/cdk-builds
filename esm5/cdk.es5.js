35701dc2f4f382a5f182d0d3bfc9b70dbee4b0f7
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-05-13 04:43:47 +0000

    master - 6c03e2f feat(radio): support theme color on mat-radio-group (#15741)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('8.0.0-rc.0-31e72a7');
+var VERSION = new Version('8.0.0-rc.0-6c03e2f');
 
 /**
  * @fileoverview added by tsickle
