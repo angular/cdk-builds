b04d958a3cb86972f99ccf2cda7de5b1cc2f8669
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-05-14 17:15:00 +0000

    master - 0c62798 fix(datepicker): use narrower value for aria-haspopup (#15666)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.0-rc.1-20a0930');
+var VERSION = new core.Version('8.0.0-rc.1-0c62798');
 
 exports.VERSION = VERSION;
 
