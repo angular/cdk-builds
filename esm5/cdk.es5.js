ab087ec2fbb986c47d35b1ef64aa63f1a2287af7
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-05-14 15:17:54 +0000

    master - b5889ca refactor(sidenav): expose api to update content margins (#15833)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('8.0.0-rc.1-94c27b5');
+var VERSION = new Version('8.0.0-rc.1-b5889ca');
 
 /**
  * @fileoverview added by tsickle
