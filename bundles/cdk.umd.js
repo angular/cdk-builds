f8a7d604dca4e365b16e2e6f4d83099dbea28b66
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-05-20 23:49:38 +0000

    master - 558295b fix(popover-edit): rework host listeners to account for changes in Ivy (#16060)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.0-rc.1-59fc0cb');
+var VERSION = new core.Version('8.0.0-rc.1-558295b');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('8.0.0-rc.1-59fc0cb');
+var VERSION = new Version('8.0.0-rc.1-558295b');
 
 /**
  * @fileoverview added by tsickle
