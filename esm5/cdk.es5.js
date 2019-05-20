52d30adce9e21c965c3d588da458e9989c4d30ef
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-05-20 23:48:15 +0000

    master - 59fc0cb build: update bazel nodejs rules to v0.29.0 (#16063)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('8.0.0-rc.1-b66a9a2');
+var VERSION = new Version('8.0.0-rc.1-59fc0cb');
 
 /**
  * @fileoverview added by tsickle
