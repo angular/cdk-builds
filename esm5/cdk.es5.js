d14f0bef2eb51631963a65e4eb7ce12a4271bc3c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-05-23 17:02:03 +0000

    master - 92448bc build: fix tslint being unable to type check cross-package references

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('8.0.0-rc.1-6c7fd30');
+var VERSION = new Version('8.0.0-rc.1-92448bc');
 
 /**
  * @fileoverview added by tsickle
