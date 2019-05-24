a64993b8a1934d702bf1f12c343142fae8f55389
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-05-24 16:32:51 +0000

    master - dc51691 fix(ng-update): type imports not migrated to secondary entry-points (#16108)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.1-86da318');
+const VERSION = new Version('8.0.0-rc.1-dc51691');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('8.0.0-rc.1-86da318');
+var VERSION = new Version('8.0.0-rc.1-dc51691');
 
 /**
  * @fileoverview added by tsickle
