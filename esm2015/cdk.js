f32eb420796a4157aebb6ca8d10eb28546aa8597
mmalerba <mmalerba@google.com>
2019-05-24 21:47:01 +0000

    master - 755fd56 build: fill in missing MATERIAL_EXPERIMENTAL_PACKAGES (#16113)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.1-f1bd252');
+const VERSION = new Version('8.0.0-rc.1-755fd56');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('8.0.0-rc.1-f1bd252');
+var VERSION = new Version('8.0.0-rc.1-755fd56');
 
 /**
  * @fileoverview added by tsickle
