7b754e6578af402506780f8c81bd24d817fdbec5
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-05-24 03:42:36 +0000

    master - 86da318 test: fix flaky slide-toggle test with pending timer task

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.0-rc.1-254fb49');
+var VERSION = new core.Version('8.0.0-rc.1-86da318');
 
 exports.VERSION = VERSION;
 
