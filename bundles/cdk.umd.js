5639efee8988d7cab037720794323b67bd067c50
Jeremy Elbourn <jelbourn@google.com>
2019-05-24 23:41:07 +0000

    master - eef132b chore: bump version to 8.0.0-rc.2 w/ changelog (#16116)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.0-rc.1-755fd56');
+var VERSION = new core.Version('8.0.0-rc.2-eef132b');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('8.0.0-rc.1-755fd56');
+var VERSION = new Version('8.0.0-rc.2-eef132b');
 
 /**
  * @fileoverview added by tsickle
