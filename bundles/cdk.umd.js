54aae305aab176fc81a6199d0a58c8423316d539
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-06-10 20:46:30 +0000

    master - 0bfcb32 chore: bazel lint error message printing out wrong fix command (#16258)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.1-4560306');
+var VERSION = new core.Version('8.0.1-0bfcb32');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('8.0.1-4560306');
+const VERSION = new Version('8.0.1-0bfcb32');
 
 /**
  * @fileoverview added by tsickle
