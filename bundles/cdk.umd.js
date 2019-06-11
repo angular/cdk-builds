233434b27fda54931ba45341784f56d50ca14e62
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-06-11 22:05:56 +0000

    master - cfc3373 chore: add scaffolding for mdc-based tabs (#16270)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.1-c3eac17');
+var VERSION = new core.Version('8.0.1-cfc3373');
 
 exports.VERSION = VERSION;
 
