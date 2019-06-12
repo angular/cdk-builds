ce814417b639b1040415663d0969e3b924b3bfea
mmalerba <mmalerba@google.com>
2019-06-12 20:24:41 +0000

    master - c3ff175 chore: update marked (#16277)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.1-57b507b');
+var VERSION = new core.Version('8.0.1-c3ff175');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('8.0.1-57b507b');
+const VERSION = new Version('8.0.1-c3ff175');
 
 /**
  * @fileoverview added by tsickle
