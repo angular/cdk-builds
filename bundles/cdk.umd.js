88b4a2273d536de1d87e691eb3e3070402013dc1
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-06-19 05:36:22 +0000

    master - 7a04788 fix(experimental/dialog): set aria-modal on dialog container (#16323)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.1-9744b31');
+var VERSION = new core.Version('8.0.1-7a04788');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('8.0.1-9744b31');
+var VERSION = new Version('8.0.1-7a04788');
 
 /**
  * @fileoverview added by tsickle
