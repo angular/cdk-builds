d486dcd9d099eda97507283d6a6b5853a0e7d093
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-06-19 17:48:12 +0000

    master - b0d1bad fix(expansion-panel): header animating on init when using non-default height (#16122)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('8.0.1-b15a7ec');
+const VERSION = new Version('8.0.1-b0d1bad');
 
 /**
  * @fileoverview added by tsickle
