d0211b94515bc8d192f8647b6b7cb1572144c46f
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-06-21 22:17:10 +0000

    master - 10370ef65 build: use bazel from node modules (#16361)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.1-6a7fc81');
+var VERSION = new core.Version('8.0.1-10370ef65');
 
 exports.VERSION = VERSION;
 
