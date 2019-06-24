b61dfc5887e6b645f1fccae3cd6b98707a98ac58
Vanessa Schmitt <vschmitt@google.com>
2019-06-24 21:26:36 +0000

    master - 86aad590f feat(material-experimental/chips): MDC chip APIs match existing APIs (#16145)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('8.0.1-d511fbea3');
+const VERSION = new Version('8.0.1-86aad590f');
 
 /**
  * @fileoverview added by tsickle
