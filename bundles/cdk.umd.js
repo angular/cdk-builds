18d208bd9e03459910c20ad9063c6bf8dee988c0
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-06-26 20:59:47 +0000

    master - aa2236820 feat(tabs): support pagination in nav bar (#16055)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.1-a461f32e9');
+var VERSION = new core.Version('8.0.1-aa2236820');
 
 exports.VERSION = VERSION;
 
