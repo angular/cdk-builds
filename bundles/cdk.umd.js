fab84b5db62070a4c499cc3b5d135235c1b3db12
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-06-26 16:05:40 +0000

    master - 0ed9416ab fix(dialog): don't move focus to dialog container if focus is already inside the dialog (#16297)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.1-93f4b584a');
+var VERSION = new core.Version('8.0.1-0ed9416ab');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('8.0.1-93f4b584a');
+var VERSION = new Version('8.0.1-0ed9416ab');
 
 /**
  * @fileoverview added by tsickle
