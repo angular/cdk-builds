f239c856408d5dbcec76b0bd4170622f82b9f1e4
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-06-26 16:02:37 +0000

    master - 760058a5c fix(ng-update): do not rely on node-glob for finding rule directories (#16381)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.1-2bf06125a');
+var VERSION = new core.Version('8.0.1-760058a5c');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('8.0.1-2bf06125a');
+var VERSION = new Version('8.0.1-760058a5c');
 
 /**
  * @fileoverview added by tsickle
