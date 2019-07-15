28c48bc910a1e652f4638aa2bef6270017ccb689
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-07-15 18:03:14 +0000

    master - dde12c2b9 chore: clean up top-level package imports (#16514)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.2-dcde11598');
+var VERSION = new core.Version('8.0.2-dde12c2b9');
 
 exports.VERSION = VERSION;
 
