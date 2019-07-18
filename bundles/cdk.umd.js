934dd1a33fda40eb4b05c0716a99dd52d6f2a387
Jeremy Elbourn <jelbourn@google.com>
2019-07-18 23:22:09 +0000

    master - 0d4269b7e docs(tabs): describe interaction difference between tab-group & nav-tab-bar (#16563)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.1.1-036729dc1');
+var VERSION = new core.Version('8.1.1-0d4269b7e');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('8.1.1-036729dc1');
+var VERSION = new Version('8.1.1-0d4269b7e');
 
 /**
  * @fileoverview added by tsickle
