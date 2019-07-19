f297e00922d37989979ecc2227c917b5ef2d1517
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-07-19 14:38:31 +0000

    master - fba65fc98 refactor(checkbox): remove unused host binding for required validator (#16548)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('8.1.1-e4f710a41');
+const VERSION = new Version('8.1.1-fba65fc98');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('8.1.1-e4f710a41');
+var VERSION = new Version('8.1.1-fba65fc98');
 
 /**
  * @fileoverview added by tsickle
