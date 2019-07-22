514f99e095b740d332ef67610ef8790e8f4f54e9
Vanessa Schmitt <vschmitt@google.com>
2019-07-22 18:51:53 +0000

    master - 701302dc4 style(mdc chips) Fix clashing input and remove classes (#16561)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.1.1-d9977bdb1');
+var VERSION = new core.Version('8.1.1-701302dc4');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('8.1.1-d9977bdb1');
+var VERSION = new Version('8.1.1-701302dc4');
 
 /**
  * @fileoverview added by tsickle
