612218b92d6e0061224b8b6dc3fc96ef42107260
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-07-25 18:50:22 +0000

    master - 615070acb fix(popover-edit): unable to close by pressing on enter on some screen readers (#16466)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.1.1-bd24369d3');
+var VERSION = new core.Version('8.1.1-615070acb');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('8.1.1-bd24369d3');
+var VERSION = new Version('8.1.1-615070acb');
 
 /**
  * @fileoverview added by tsickle
