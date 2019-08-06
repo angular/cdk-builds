856e631fc2274117a9d8aa4bb198daa01120e031
Andrew Seguin <andrewseguin@users.noreply.github.com>
2019-08-06 19:01:11 +0000

    master - 808370e0a feat(material-experimental): add progress bar test harness skeleton (#16619)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('8.1.2-0e1e33ba9');
+const VERSION = new Version('8.1.2-808370e0a');
 
 /**
  * @fileoverview added by tsickle
