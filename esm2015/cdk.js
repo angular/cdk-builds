e8e226eeb1e218e35836d46055efd8ba0ffca944
mmalerba <mmalerba@google.com>
2019-08-12 17:19:04 +0000

    master - 286038c4a feat(cdk-experimental/testing): Improve keyboard event support in harnesses (#16645)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('8.1.2-6416babbb');
+const VERSION = new Version('8.1.2-286038c4a');
 
 /**
  * @fileoverview added by tsickle
