e1ecf9a3d3ce53923e4f9c3d48ba7835f40ac3da
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-08-13 17:30:16 +0000

    master - 417adf865 build: fix bazel not available if cache could not be restored (#16764)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.1.2-1d6682cb8');
+var VERSION = new core.Version('8.1.2-417adf865');
 
 exports.VERSION = VERSION;
 
