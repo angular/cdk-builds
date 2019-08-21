60b578bf31f5b55fac521c2f17623e430e2005f6
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-08-21 17:41:56 +0000

    master - 7c406791f build: update jasmine types dependency (#16834)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('8.1.3-3aa5ca417');
+const VERSION = new Version('8.1.3-7c406791f');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('8.1.3-3aa5ca417');
+var VERSION = new Version('8.1.3-7c406791f');
 
 /**
  * @fileoverview added by tsickle
