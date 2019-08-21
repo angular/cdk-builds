c5ec033346a56a4b8b15b5d8694d6c078fe5b438
Steven Xu <steven@stevenxu.ca>
2019-08-21 21:03:18 +0000

    master - f2cbadedb fix(radio): show ripple on programmatic focus (#16512)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('8.1.3-b0495f644');
+const VERSION = new Version('8.1.3-f2cbadedb');
 
 /**
  * @fileoverview added by tsickle
