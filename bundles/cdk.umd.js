dbc70db23fcd4578ee97a260691e7414062ec96b
Kern Zhao <siyangkernzhao@gmail.com>
2019-08-21 20:59:51 +0000

    master - 9ca0f0c78 fix(button): fix opacity is not added to ripple when using CSS variables to define custom theme

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.1.3-7039b25f1');
+var VERSION = new core.Version('8.1.3-9ca0f0c78');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('8.1.3-7039b25f1');
+var VERSION = new Version('8.1.3-9ca0f0c78');
 
 /**
  * @fileoverview added by tsickle
