ed1754c4ac9edafc3623452305532cc4daf14102
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-08-23 21:50:37 +0000

    master - 21228c7dd build: yarn dev-app script should run with ibazel by default (#16856)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('8.1.3-ab9a847da');
+const VERSION = new Version('8.1.3-21228c7dd');
 
 /**
  * @fileoverview added by tsickle
