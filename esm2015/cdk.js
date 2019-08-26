2e1378a4966dee60467f43e2a1d62dafe3c3c023
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-08-26 20:50:06 +0000

    master - ba7cbe726 fix(material-experimental/menu): missing outline and focus indication in high contrast mode (#16871)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('8.1.3-bae65b38f');
+const VERSION = new Version('8.1.3-ba7cbe726');
 
 /**
  * @fileoverview added by tsickle
