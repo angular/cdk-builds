78b0c82a0f96d6602232db390f01ec344886ac7a
Andrew Seguin <andrewseguin@users.noreply.github.com>
2019-08-29 23:18:25 +0000

    master - 52c33c76f feat(material-experimental/mdc-chips): add harness skeleton (#16737)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.1.4-9027c6f37');
+var VERSION = new core.Version('8.1.4-52c33c76f');
 
 exports.VERSION = VERSION;
 
