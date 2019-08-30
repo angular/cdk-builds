b5f12c89bec24f3b224191e768ab27724aa6375c
mmalerba <mmalerba@google.com>
2019-08-30 17:39:48 +0000

    master - 64932e25d build: fix rebase conflict between autocomplete harness and selector filter (#16918)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('8.1.4-181ce5912');
+const VERSION = new Version('8.1.4-64932e25d');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('8.1.4-181ce5912');
+var VERSION = new Version('8.1.4-64932e25d');
 
 /**
  * @fileoverview added by tsickle
