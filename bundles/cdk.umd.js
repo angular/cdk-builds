eb6bc5f4fb49f4b3a4d20706c478500141a48d8f
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-09-03 22:02:45 +0000

    master - 1c74518e8 build: create custom bazel dev-server rule (#16937)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.1.4-ce71a4544');
+var VERSION = new core.Version('8.1.4-1c74518e8');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('8.1.4-ce71a4544');
+const VERSION = new Version('8.1.4-1c74518e8');
 
 /**
  * @fileoverview added by tsickle
