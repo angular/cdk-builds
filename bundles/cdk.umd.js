ae45b64e74eac36a52c72c9f6e08a650aafaa672
Joey Perrott <josephperrott.github@gmail.com>
2019-09-05 21:11:55 +0000

    master - c1057547e ci: move to latest lock-closed commit for github action (#16984)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.1.4-cd0611d71');
+var VERSION = new core.Version('8.1.4-c1057547e');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('8.1.4-cd0611d71');
+var VERSION = new Version('8.1.4-c1057547e');
 
 /**
  * @fileoverview added by tsickle
