546e24e686229b051f31154c797c53659fb2518b
Andrew Seguin <andrewseguin@users.noreply.github.com>
2019-09-05 23:41:43 +0000

    master - a4fc77973 docs(material-experimental/button): update class JsDoc (#16988)

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('8.1.4-680ed0085');
+const VERSION = new Version('8.1.4-a4fc77973');
 
 /**
  * @fileoverview added by tsickle
