430ce0715976998e7de1bb0d84d153862e37bd23
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-09-06 22:08:57 +0000

    master - 44b8a47b4 fix(drag-drop): handle the element going out of the boundary after a viewport resize (#16874)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -19,7 +19,7 @@
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.1.4-f02fb47f3');
+var VERSION = new core.Version('8.1.4-44b8a47b4');
 
 exports.VERSION = VERSION;
 
--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('8.1.4-f02fb47f3');
+const VERSION = new Version('8.1.4-44b8a47b4');
 
 /**
  * @fileoverview added by tsickle
