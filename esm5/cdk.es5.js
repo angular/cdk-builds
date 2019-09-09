411cdedbc5902785a460b36e5b80a90d2d3d2e4e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-09-09 14:22:02 +0000

    master - 8d1290238 fix(snack-bar): don't stretch to fullscreen in landscape orientation (#16940)

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -15,7 +15,7 @@ import { Version } from '@angular/core';
  * Current version of the Angular Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('8.1.4-7594ca121');
+var VERSION = new Version('8.1.4-8d1290238');
 
 /**
  * @fileoverview added by tsickle
