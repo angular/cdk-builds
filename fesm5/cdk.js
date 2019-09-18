a10caf243b2b4fce8e445e589d31f5377f1ae3ce
mmalerba <mmalerba@google.com>
2019-09-18 19:20:25 +0000

    master - 8229353f6 feat(slider): move harness out of experimental (#17136)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('8.2.0-9d6f4c2c0-9d6f4c2');
+var VERSION = new Version('8.2.0-8229353f6-8229353');
 
 /**
  * @license
