9c808d7e3292e4c5f34db728e54f899af64c7c1a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-09-30 16:35:39 +0000

    master - cc9d12774 refactor(google-maps): remove unnecessary modules (#17165)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('8.2.0-8e141efee-8e141ef');
+	var VERSION = new core.Version('8.2.0-cc9d12774-cc9d127');
 
 	/**
 	 * @license
