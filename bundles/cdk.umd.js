55fe207f10db31fa8521ca0c9fda6319f98e7654
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-10-01 20:13:46 +0000

    master - 9586b5c06 docs(datepicker): add missing provider (#17259)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('8.2.0-a306e93d0-a306e93');
+	var VERSION = new core.Version('8.2.0-9586b5c06-9586b5c');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('8.2.0-a306e93d0-a306e93');
+const VERSION = new Version('8.2.0-9586b5c06-9586b5c');
 
 /**
  * @license
