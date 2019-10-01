8eb999fe592f31be8ec23813cc55a8b0e775d710
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-10-01 20:42:02 +0000

    master - 412602970 feat(tooltip): remove Hammer.js dependency (#17003)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('8.2.0-7893e8a79-7893e8a');
+var VERSION = new Version('8.2.0-412602970-4126029');
 
 /**
  * @license
