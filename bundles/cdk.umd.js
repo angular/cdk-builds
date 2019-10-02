e3a4afa32f752de30ba3629d05e6b66df20ddc94
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-10-02 16:20:46 +0000

    master - 94cec9f02 build: do generate invalid semver versions for snapshot builds (#17267)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('8.2.0-8c9801398-8c98013');
+	var VERSION = new core.Version('8.2.0-sha-94cec9f02-94cec9f');
 
 	/**
 	 * @license
--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('8.2.0-8c9801398-8c98013');
+var VERSION = new Version('8.2.0-sha-94cec9f02-94cec9f');
 
 /**
  * @license
