8c4d35dee182782b3115f417e25532399c77bfdb
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-10-03 00:43:37 +0000

    master - d453d0662 fix(select): highlighted option not updated if value is reset while closed (#17213)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('8.2.2-sha-ae28fce1f');
+	var VERSION = new core.Version('8.2.2-sha-d453d0662');
 
 	/**
 	 * @license
--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('8.2.2-sha-ae28fce1f');
+var VERSION = new Version('8.2.2-sha-d453d0662');
 
 /**
  * @license
