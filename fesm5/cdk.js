3c8757b19bc64ad0ec8180a758ac41aa328a589b
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-10-03 18:47:05 +0000

    master - 396154413 refactor: clean up deprecated uses of combineLatest (#17168)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('8.2.2-sha-3673f3d30');
+var VERSION = new Version('8.2.2-sha-396154413');
 
 /**
  * @license
