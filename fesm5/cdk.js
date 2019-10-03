c745499e160c1e8e0912c1e4d60a70e461d9d3b9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-10-03 18:58:03 +0000

    master - ef5d9cbf2 fix(tabs): avoid Ivy template type checking errors in tab link (#17282)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('8.2.2-sha-5a65a63ab');
+var VERSION = new Version('8.2.2-sha-ef5d9cbf2');
 
 /**
  * @license
