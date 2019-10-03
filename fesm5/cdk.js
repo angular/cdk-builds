afaffcd5f8d35cbbd86ec2a6912f14cc78560f78
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-10-03 18:16:46 +0000

    master - c4f2174e2 refactor: clean up remaining hammer.js usages (#17268)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('8.2.2-sha-361d1cf80');
+var VERSION = new Version('8.2.2-sha-c4f2174e2');
 
 /**
  * @license
