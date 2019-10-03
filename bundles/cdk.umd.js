36d01ef8577b2d0e316cbb61739a397a216f6b8c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-10-03 19:11:02 +0000

    master - 29be7686e build: re-add theming scss bundle to material package (#17278)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('8.2.2-sha-8669606b0');
+	var VERSION = new core.Version('8.2.2-sha-29be7686e');
 
 	/**
 	 * @license
