a7ab5dab8a7098edc1acee5a040638ac3f14afcf
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-10-03 00:40:35 +0000

    master - ae28fce1f fix(datepicker): year not formatted in multi-year view button (#17202)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('8.2.2-sha-fc6cb1069');
+const VERSION = new Version('8.2.2-sha-ae28fce1f');
 
 /**
  * @license
