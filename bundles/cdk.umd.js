9386e762153bd9f104f29509aa6e14262a21011e
Jeremy Elbourn <jelbourn@google.com>
2019-10-03 22:30:59 +0000

    master - 569c149fc chore: bump version to 9.0.0-next.0 w/ changelog (#17288)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('8.2.2-sha-29be7686e');
+	var VERSION = new core.Version('9.0.0-next.0-sha-569c149fc');
 
 	/**
 	 * @license
--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('8.2.2-sha-29be7686e');
+var VERSION = new Version('9.0.0-next.0-sha-569c149fc');
 
 /**
  * @license
