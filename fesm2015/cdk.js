363b4f2712247fce4799e08b97d63c3075972543
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-10-03 23:51:05 +0000

    master - a78edf86f build: include favicon in dev-app to avoid error (#17244)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-next.0-sha-1f9ce4469');
+const VERSION = new Version('9.0.0-next.0-sha-a78edf86f');
 
 /**
  * @license
