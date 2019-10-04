de20280fbdc266fa7fc3482f827f21c7a0e30cc5
mmalerba <mmalerba@google.com>
2019-10-04 01:19:59 +0000

    master - ab3b6d2e1 docs(cdk/testing): add guide for using harnesses (#17195)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-next.0-sha-a78edf86f');
+	var VERSION = new core.Version('9.0.0-next.0-sha-ab3b6d2e1');
 
 	/**
 	 * @license
