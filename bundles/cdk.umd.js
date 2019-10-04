d67ff2d22c3b103b9c3224cbfa2d9dcbbdd1a3ba
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-10-04 18:04:44 +0000

    master - b228f079e refactor: annotate remaining base classes with @directive for… (#17279)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-next.0-sha-230fd2f6a');
+	var VERSION = new core.Version('9.0.0-next.0-sha-b228f079e');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-next.0-sha-230fd2f6a');
+const VERSION = new Version('9.0.0-next.0-sha-b228f079e');
 
 /**
  * @license
