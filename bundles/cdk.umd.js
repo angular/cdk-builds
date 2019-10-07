c90c0c1051a3be81606c5dd75ba1931968520aca
mbehrlich <mattehrlich86@gmail.com>
2019-10-07 23:51:53 +0000

    master - f50755031 refactor(google-maps): remove duplicate module (#17322)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-next.0-sha-45a43008d');
+	var VERSION = new core.Version('9.0.0-next.0-sha-f50755031');
 
 	/**
 	 * @license
