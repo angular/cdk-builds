ae2f8c798ac5d14559eec46e23081901b1d6d510
mmalerba <mmalerba@google.com>
2019-10-09 15:34:10 +0000

    master - a1f59b262 chore: copy 8.2.3 changelog to master (#17335)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-next.0-sha-1a589ce00');
+	var VERSION = new core.Version('9.0.0-next.0-sha-a1f59b262');
 
 	/**
 	 * @license
