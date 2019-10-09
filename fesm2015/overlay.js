4651541f49b6591d3b6e9ceb4080ffcc37f5f7da
NothingEverHappens <kirjs@google.com>
2019-10-09 15:57:39 +0000

    master - 5352c035c docs(overlay): Drop extra `the` in a comment (#17341)

--- fesm2015/overlay.js
+++ fesm2015/overlay.js
@@ -4046,7 +4046,7 @@ if (false) {
  * @type {?}
  */
 let nextUniqueId = 0;
-// Note that Overlay is *not* scoped to the app root because the of the ComponentFactoryResolver
+// Note that Overlay is *not* scoped to the app root because of the ComponentFactoryResolver
 // which needs to be different depending on where OverlayModule is imported.
 /**
  * Service to create Overlays. Overlays are dynamically added pieces of floating UI, meant to be
