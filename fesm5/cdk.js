593d26e4370d877f21e099f2939df8e189bd00a7
Joey Perrott <josephperrott.github@gmail.com>
2019-10-09 17:50:40 +0000

    master - d3fe81186 build: set up remote-http-caching (#17342)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-next.0-sha-664a60647');
+var VERSION = new Version('9.0.0-next.0-sha-d3fe81186');
 
 /**
  * @license
