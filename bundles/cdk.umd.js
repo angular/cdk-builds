ff42f1bff5e5f456ebfc1e18c5d0aeb93dfee6bc
Greg Magolan <gmagolan@gmail.com>
2019-10-10 22:18:15 +0000

    master - 43af4c298 build: update to nodejs rules 0.38.2 (#17361)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-next.0-sha-885d06a50');
+	var VERSION = new core.Version('9.0.0-next.0-sha-43af4c298');
 
 	/**
 	 * @license
