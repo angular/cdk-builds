4d37a89ae366a1e71e1665ceb99a305a8acef81c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-10-10 19:29:52 +0000

    master - 49e72a9ac build: ensure to never output files next to sources (#17362)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-next.0-sha-b836050fd');
+const VERSION = new Version('9.0.0-next.0-sha-49e72a9ac');
 
 /**
  * @license
