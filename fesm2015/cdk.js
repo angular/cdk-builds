a80d98d2102c9cdc14cfb28b8f327a554ec35395
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-10-11 20:42:30 +0000

    master - 14c4dba1b build: do not generate type definitions without reference to needed global types (#17373)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-next.0-sha-d4bf7be64');
+const VERSION = new Version('9.0.0-next.0-sha-14c4dba1b');
 
 /**
  * @license
