6f1088e2f76749c7dc53b30265d413668dc83902
mmalerba <mmalerba@google.com>
2019-10-15 18:17:14 +0000

    master - 405f5d02c fix(menu): trigger should aria-controls panel (#17365)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-next.0-sha-aa826e9d4');
+	var VERSION = new core.Version('9.0.0-next.0-sha-405f5d02c');
 
 	/**
 	 * @license
