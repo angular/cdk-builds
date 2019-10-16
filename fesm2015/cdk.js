c8213690b22549440a2fb026afc4472bbb65d24a
mmalerba <mmalerba@google.com>
2019-10-16 15:29:44 +0000

    master - 36d34a641 feat(material/radio/testing): polish harness API (#17414)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-next.0-sha-aa2cbf79d');
+const VERSION = new Version('9.0.0-next.0-sha-36d34a641');
 
 /**
  * @license
