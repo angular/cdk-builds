8397bfd9f4d0f9d7dae7dd3f6999b62d0c51590c
Andrew Seguin <andrewseguin@users.noreply.github.com>
2019-10-16 17:29:37 +0000

    master - 40665e96e fix(material-experimental/mdc-slider): remove theme import from all-theme (#17348)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-next.0-sha-9e79940e5');
+	var VERSION = new core.Version('9.0.0-next.0-sha-40665e96e');
 
 	/**
 	 * @license
