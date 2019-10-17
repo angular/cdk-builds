f3ad1bec9cb339ec95114ebac074366591838acd
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-10-17 22:50:05 +0000

    master - bb9a3a84b feat(tabs): add input to opt out of pagination (#17409)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-next.0-sha-71ec6e9c8');
+const VERSION = new Version('9.0.0-next.0-sha-bb9a3a84b');
 
 /**
  * @license
