f74df9eefdd7784bfdb23514544b4f0b7d34cbc5
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-10-21 23:01:03 +0000

    master - 4663d8f8d fix(form-field): annotate base class with Directive for Ivy (#17457)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-next.0-sha-67b009f04');
+const VERSION = new Version('9.0.0-next.0-sha-4663d8f8d');
 
 /**
  * @license
