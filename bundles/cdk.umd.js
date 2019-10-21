26d6ea3546df0256fd54d2c2488fb40d4d46a4cb
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-10-21 16:22:16 +0000

    master - 5e108336f fix(schematics): secondary entry point migration not working against v9 (#17452)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-next.0-sha-1ab4b77ad');
+	var VERSION = new core.Version('9.0.0-next.0-sha-5e108336f');
 
 	/**
 	 * @license
