dbba57cb6ecf1f801e2247320bf23722eccd3d53
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-10-22 18:15:44 +0000

    master - af68f57b2 build: avoid bazel analysis cache discarding (#17461)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-next.0-sha-db283f11e');
+const VERSION = new Version('9.0.0-next.0-sha-af68f57b2');
 
 /**
  * @license
