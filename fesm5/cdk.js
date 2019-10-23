410e4c9731e74e6d7fa3586efd1f12093eb1d4c4
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-10-23 22:11:34 +0000

    master - d3235c98b build: ensure angular version placeholder is up-to-date when staging release (#17465)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-next.0-sha-fc7288f00');
+var VERSION = new Version('9.0.0-next.0-sha-d3235c98b');
 
 /**
  * @license
