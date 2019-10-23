f0f173ec439f0f45b8172d3f2bcf9ef05952827c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-10-23 22:16:09 +0000

    master - cd0a47e20 chore: clean up leftover references to private/testing (#17486)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-next.0-sha-d3235c98b');
+	var VERSION = new core.Version('9.0.0-next.0-sha-cd0a47e20');
 
 	/**
 	 * @license
