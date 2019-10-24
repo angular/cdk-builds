6bc154e52331d6ba622af5d44c67bc895577e821
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-10-24 22:12:02 +0000

    master - 3de0b0802 fix(material-experimental/mdc-slide-toggle): missing focus indication in high contrast mode (#17471)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-next.0-sha-ece800463');
+const VERSION = new Version('9.0.0-next.0-sha-3de0b0802');
 
 /**
  * @license
