3c4e9275f4135cd5561c548c8f77bdce274dc621
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-10-24 18:04:39 +0000

    master - c44aee6c3 build: update mdc version placeholder range (#17496)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-next.0-sha-cd0a47e20');
+const VERSION = new Version('9.0.0-next.0-sha-c44aee6c3');
 
 /**
  * @license
