c722e09964731ec508c0f0cd3a73b07c909d3ca6
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-10-24 18:27:06 +0000

    master - 4fab30ff4 build: update node and yarn to latest stable versions (#17474)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-next.0-sha-71b99a07f');
+var VERSION = new Version('9.0.0-next.0-sha-4fab30ff4');
 
 /**
  * @license
