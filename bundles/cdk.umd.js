c788b5014a2ac500151b4776d6021e60c6e4a538
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-10-26 02:22:00 +0000

    master - f0df3088a fix(autocomplete): not picking up indirect descendant option g… (#17510)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-next.1-sha-4301fb0fa');
+	var VERSION = new core.Version('9.0.0-next.1-sha-f0df3088a');
 
 	/**
 	 * @license
