745f4f2407a9e4ff67c7d0b10ae284244558b547
Andrew Seguin <andrewseguin@users.noreply.github.com>
2019-10-28 19:25:07 +0000

    master - 3fdab10bd feat(checkbox): add options defaults config (#17473)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-next.1-sha-e2de5e6d3');
+	var VERSION = new core.Version('9.0.0-next.1-sha-3fdab10bd');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-next.1-sha-e2de5e6d3');
+const VERSION = new Version('9.0.0-next.1-sha-3fdab10bd');
 
 /**
  * @license
