350f2a53ed8c00f56253fc8dac858b689ac6aebd
Andrew Seguin <andrewseguin@users.noreply.github.com>
2019-10-29 20:40:43 +0000

    master - 77d51ca42 feat(material-experimental/mdc-tabs): add option to fit ink bar to content (#17507)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-next.1-sha-29e2548b5');
+var VERSION = new Version('9.0.0-next.1-sha-77d51ca42');
 
 /**
  * @license
