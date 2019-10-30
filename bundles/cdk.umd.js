c1f4d2c0508938e7d17aee4a23298557e2a4077c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-10-30 15:54:11 +0000

    master - aadfcb537 build: no longer depend on gulp for lint tasks (#17516)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-next.1-sha-08d78f9fb');
+	var VERSION = new core.Version('9.0.0-next.1-sha-aadfcb537');
 
 	/**
 	 * @license
