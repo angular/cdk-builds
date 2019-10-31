e42a0a5986dfb388494943ad03d68b193e6a841c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-10-31 14:56:38 +0000

    master - 87dfbc796 chore: bump to Angular 9.0.0-next.15 (#17551)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-next.1-sha-6085f3998');
+	var VERSION = new core.Version('9.0.0-next.1-sha-87dfbc796');
 
 	/**
 	 * @license
--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-next.1-sha-6085f3998');
+var VERSION = new Version('9.0.0-next.1-sha-87dfbc796');
 
 /**
  * @license
