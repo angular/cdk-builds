8679a19b5fa830cf0c28f53e7a6fa672c0d73c36
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-11-01 17:40:43 +0000

    master - f55461047 build: generate changelog for google-maps package (#17563)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.0-sha-50514b235');
+	var VERSION = new core.Version('9.0.0-rc.0-sha-f55461047');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.0-sha-50514b235');
+const VERSION = new Version('9.0.0-rc.0-sha-f55461047');
 
 /**
  * @license
