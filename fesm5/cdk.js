69951cfc2af3c164692660edd926c3132accba19
Andrew Seguin <andrewseguin@users.noreply.github.com>
2019-11-01 18:42:46 +0000

    master - 2f843891c feat(material-experimental/mdc-tabs): add default fitInkBarToContent option (#17556)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-rc.0-sha-031e15f68');
+var VERSION = new Version('9.0.0-rc.0-sha-2f843891c');
 
 /**
  * @license
