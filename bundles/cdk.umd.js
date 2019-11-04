ae6cc309a825af43914b3130778970ed13a93e2e
Andrew Seguin <andrewseguin@users.noreply.github.com>
2019-11-04 19:27:28 +0000

    master - 9f9b017b9 fix(material-experimental/mdc-button): rename state class (#17575)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.0-sha-cd7d8c86b');
+	var VERSION = new core.Version('9.0.0-rc.0-sha-9f9b017b9');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.0-sha-cd7d8c86b');
+const VERSION = new Version('9.0.0-rc.0-sha-9f9b017b9');
 
 /**
  * @license
