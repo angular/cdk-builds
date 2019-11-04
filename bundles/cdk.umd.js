ac602a8c237ff996e41aff632793ed62f4451ac0
Craig Spence <craigspence0@gmail.com>
2019-11-04 18:54:38 +0000

    master - e22407d0b Fix typo? (#17590)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.0-sha-8422580fe');
+	var VERSION = new core.Version('9.0.0-rc.0-sha-e22407d0b');
 
 	/**
 	 * @license
