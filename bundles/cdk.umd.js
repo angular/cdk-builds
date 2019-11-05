3828b3704ebae740039a633b96115fc7e6d697bd
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-11-05 23:41:11 +0000

    master - 69ccd34d5 test: fix a couple of shared harness functions ignoring module parameter (#17617)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.0-sha-7cd314c53');
+	var VERSION = new core.Version('9.0.0-rc.0-sha-69ccd34d5');
 
 	/**
 	 * @license
