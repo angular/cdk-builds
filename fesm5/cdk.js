dd8eff8884ad29cd7f8d4e83c635194c8f592596
Victor <victoraugusto16@gmail.com>
2019-11-05 22:55:45 +0000

    master - 4adb2fd40 docs(table): use correct directives for flex table (#17612)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-rc.0-sha-47fa62f9d');
+var VERSION = new Version('9.0.0-rc.0-sha-4adb2fd40');
 
 /**
  * @license
