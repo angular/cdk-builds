0f56e42c193f040fdb0b7332a5bf1e932f174067
Sriram Jayarman <sriramemailsyou@gmail.com>
2019-11-06 20:35:36 +0000

    master - 9ae3b635b Fix docs api section (#17622)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.0-sha-1255139a3');
+const VERSION = new Version('9.0.0-rc.0-sha-9ae3b635b');
 
 /**
  * @license
