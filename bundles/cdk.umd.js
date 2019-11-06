b3adc9ec2979bf3a7839b91c4d98486e654012b1
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-11-06 20:32:06 +0000

    master - ab0095e5b build: member naming rule not catching private readonly (#17515)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.0-sha-049da9541');
+	var VERSION = new core.Version('9.0.0-rc.0-sha-ab0095e5b');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.0-sha-049da9541');
+const VERSION = new Version('9.0.0-rc.0-sha-ab0095e5b');
 
 /**
  * @license
