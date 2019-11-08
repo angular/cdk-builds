d7da6f67d2792ab5683bb53210419f9d67f41d36
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-11-08 14:42:35 +0000

    master - b8b0f57c3 build: switch mdc snapshot test to run against master branch (#17652)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.0-sha-d3d885965');
+	var VERSION = new core.Version('9.0.0-rc.0-sha-b8b0f57c3');
 
 	/**
 	 * @license
