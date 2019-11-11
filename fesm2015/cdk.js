136de0c5f5fdaa83e04c33b25e84ac18ec3b88fc
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-11-11 19:23:20 +0000

    master - 1fcea5015 build: remove release packaging gulp code (#17668)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.1-sha-fa18ce172');
+const VERSION = new Version('9.0.0-rc.1-sha-1fcea5015');
 
 /**
  * @license
