946d51bb93adb5d26b2e059b003cb99a49122d63
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-11-11 20:42:52 +0000

    master - 903777fae build: fix failing api golden for clipboard (#17671)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.1-sha-a96f0a5e5');
+	var VERSION = new core.Version('9.0.0-rc.1-sha-903777fae');
 
 	/**
 	 * @license
