a87ebca4584460fb687d114b4ece38c3335227d7
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-11-11 19:35:02 +0000

    master - 39c007413 fix(sidenav): not destroying custom QueryList (#17619)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-rc.1-sha-e8ee64109');
+var VERSION = new Version('9.0.0-rc.1-sha-39c007413');
 
 /**
  * @license
