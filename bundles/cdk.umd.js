0a4356b095466383d78e89d8dda39ffac55600ef
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-11-11 19:23:33 +0000

    master - e8ee64109 build: remove rollup windows postinstall workaround (#17669)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.1-sha-1fcea5015');
+	var VERSION = new core.Version('9.0.0-rc.1-sha-e8ee64109');
 
 	/**
 	 * @license
