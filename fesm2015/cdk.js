df7c6d9e9f22fe2a96cddfe1a982373e6bdd1b93
Joey Perrott <josephperrott.github@gmail.com>
2019-11-12 21:27:23 +0000

    master - 3a10c2cfe build: set up all packages to publish via wombot proxy (#17679)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.1-sha-5822051dd');
+const VERSION = new Version('9.0.0-rc.1-sha-3a10c2cfe');
 
 /**
  * @license
