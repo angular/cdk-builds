c5b9e00db05207bd27d6b4203fc55de5ba76f2da
mmalerba <mmalerba@google.com>
2019-11-13 18:39:58 +0000

    master - 2b02cc39d chore: mark all packages as `sideEffects: false` (#17686)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.1-sha-5f447c1bf');
+	var VERSION = new core.Version('9.0.0-rc.1-sha-2b02cc39d');
 
 	/**
 	 * @license
