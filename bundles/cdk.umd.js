bd31972036cd44842a121be20a129f9874255f84
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-11-14 21:39:51 +0000

    master - 227c49040 fix(checkbox): unknown property warning with Ivy during server-side rendering (#17485)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.1-sha-bfa88fdae');
+	var VERSION = new core.Version('9.0.0-rc.1-sha-227c49040');
 
 	/**
 	 * @license
