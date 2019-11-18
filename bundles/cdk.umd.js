deda0d37f108392345582e05e22d89d5bdd7881e
Robert Coie <rapropos@users.noreply.github.com>
2019-11-18 16:18:44 +0000

    master - 03c8bb25e docs(list): remove spurious "the" (#17724)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.2-sha-3e8bf9eaf');
+	var VERSION = new core.Version('9.0.0-rc.2-sha-03c8bb25e');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.2-sha-3e8bf9eaf');
+const VERSION = new Version('9.0.0-rc.2-sha-03c8bb25e');
 
 /**
  * @license
