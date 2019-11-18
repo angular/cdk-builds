f414ea55ff91c476f1c4d3ac2c4fdc4e6e09844b
Mitchell Skaggs <skaggsm333@gmail.com>
2019-11-18 17:08:26 +0000

    master - 36ab4d2e0 docs(guides/typography): update recommended font url (#17698)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.2-sha-52ce0d4dc');
+	var VERSION = new core.Version('9.0.0-rc.2-sha-36ab4d2e0');
 
 	/**
 	 * @license
--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-rc.2-sha-52ce0d4dc');
+var VERSION = new Version('9.0.0-rc.2-sha-36ab4d2e0');
 
 /**
  * @license
