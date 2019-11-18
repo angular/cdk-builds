f7d422fd7ed900fc120612e7b124146f97fb6c4e
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-11-18 15:48:35 +0000

    master - 37015ace1 refactor(ng-update): follow-up cleanup of hammer v9 migration (#17739)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-rc.2-sha-f9ab98294');
+var VERSION = new Version('9.0.0-rc.2-sha-37015ace1');
 
 /**
  * @license
