26ea26371e8655deb23938199a758d4a55371ef6
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-11-18 16:11:16 +0000

    master - d2ea0b8e5 build: Object.assign linting not working (#17733)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.2-sha-37015ace1');
+	var VERSION = new core.Version('9.0.0-rc.2-sha-d2ea0b8e5');
 
 	/**
 	 * @license
