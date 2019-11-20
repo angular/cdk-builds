8889e9ce5cf4952ca27b495cb1685ff4d2e47e3d
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-11-20 03:29:56 +0000

    master - 5d19e0530 build: update to bazel v1.1.0 (#17700)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.2-sha-0c78c320d');
+	var VERSION = new core.Version('9.0.0-rc.2-sha-5d19e0530');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.2-sha-0c78c320d');
+const VERSION = new Version('9.0.0-rc.2-sha-5d19e0530');
 
 /**
  * @license
