5148d3480e36e784f98608c6768196431a1c414f
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-11-20 18:44:54 +0000

    master - 5428e9370 build: rework package-builder script to node script (#17758)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-rc.2-sha-1512721ad');
+var VERSION = new Version('9.0.0-rc.2-sha-5428e9370');
 
 /**
  * @license
