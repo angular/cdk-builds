f75e9d9179a50de61b4927df36764984a9e26c0c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-11-20 03:21:59 +0000

    master - 0c78c320d build: server-side rendering check not failing for async errors (#17745)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.2-sha-aaa5783a1');
+const VERSION = new Version('9.0.0-rc.2-sha-0c78c320d');
 
 /**
  * @license
