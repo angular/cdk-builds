60e9d0daf320dda40183cd1ad94be4f80a426f40
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-11-20 14:36:25 +0000

    master - 49977f71f build: fix missing amd module name for primary entry-point (#17757)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-rc.2-sha-e2fd0995b');
+var VERSION = new Version('9.0.0-rc.2-sha-49977f71f');
 
 /**
  * @license
