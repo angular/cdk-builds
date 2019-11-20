d8f2bc5ebeec1b3e5f6fb5cb5d5f897659f0aeda
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-11-20 14:37:19 +0000

    master - 712438209 build: remove schematic tsconfig project references (#17756)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.2-sha-49977f71f');
+	var VERSION = new core.Version('9.0.0-rc.2-sha-712438209');
 
 	/**
 	 * @license
