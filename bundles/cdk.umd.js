592d3b521610e1ea5118d79d8d240b7087a5f457
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-11-20 14:40:42 +0000

    master - b53e092b6 fix(ng-update): do not copy gesture config if only standard HammerJS events are used (#17753)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.2-sha-712438209');
+	var VERSION = new core.Version('9.0.0-rc.2-sha-b53e092b6');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.2-sha-712438209');
+const VERSION = new Version('9.0.0-rc.2-sha-b53e092b6');
 
 /**
  * @license
