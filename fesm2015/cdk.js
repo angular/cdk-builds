990cdb0d2e3ce8912f0556a8a3c2af283a152f33
Jeremy Elbourn <jelbourn@google.com>
2019-11-21 16:56:15 +0000

    master - 3f737a53d Revert "build: fix missing amd module name for primary entry-point (#17757)" (#17772)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.3-sha-5325e965e');
+const VERSION = new Version('9.0.0-rc.3-sha-3f737a53d');
 
 /**
  * @license
