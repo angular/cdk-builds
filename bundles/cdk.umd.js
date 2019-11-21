cf7804db2e4391613b6b5fc7206a775964cf385c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-11-21 17:05:54 +0000

    master - 8466ed2b7 build: update to angular v9.0.0-rc.3 (#17768)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.3-sha-3f737a53d');
+	var VERSION = new core.Version('9.0.0-rc.3-sha-8466ed2b7');
 
 	/**
 	 * @license
--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-rc.3-sha-3f737a53d');
+var VERSION = new Version('9.0.0-rc.3-sha-8466ed2b7');
 
 /**
  * @license
