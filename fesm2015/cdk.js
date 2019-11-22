f0b3f89c197b89b1fba23f51792907818b604aea
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-11-22 00:26:28 +0000

    master - 3f6a1fda9 build: enable bazel node modules symlinking [resubmit w/ fixes] (#17776)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.3-sha-ef85134c0');
+const VERSION = new Version('9.0.0-rc.3-sha-3f6a1fda9');
 
 /**
  * @license
