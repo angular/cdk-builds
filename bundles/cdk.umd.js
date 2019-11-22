46b40dd11c7fab9b957281efda2e083a2251f91a
mmalerba <mmalerba@google.com>
2019-11-22 00:26:13 +0000

    master - ef85134c0 docs(harnesses): ensure all harnesses have complete and correct docs (#17723)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.3-sha-74dde10c5');
+	var VERSION = new core.Version('9.0.0-rc.3-sha-ef85134c0');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.3-sha-74dde10c5');
+const VERSION = new Version('9.0.0-rc.3-sha-ef85134c0');
 
 /**
  * @license
