4b356bd4c9cf621215b2e32345de9b802bfcecde
Jeremy Elbourn <jelbourn@google.com>
2019-11-23 00:05:55 +0000

    master - 9583feee6 chore: bump version to 9.0.0-rc.4 w/ changelog (#17793)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-rc.3-sha-ddb29606b');
+var VERSION = new Version('9.0.0-rc.4-sha-9583feee6');
 
 /**
  * @license
