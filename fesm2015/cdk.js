bb72041957997580ed0e6dea52cd3ea15fa643a6
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-11-25 19:03:17 +0000

    master - 9e0699389 build: update to rules_nodejs v0.41.0 (#17811)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.4-sha-036a40a99');
+const VERSION = new Version('9.0.0-rc.4-sha-9e0699389');
 
 /**
  * @license
