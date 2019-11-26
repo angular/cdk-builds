cc6c1e1ad0c690c30b0f3f07b362b38b7ad5059f
Andrew Seguin <andrewseguin@users.noreply.github.com>
2019-11-26 04:24:47 +0000

    master - 71165f103 fix(material-experimental/mdc-tabs): markForCheck on ink bar content input setter (#17561)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.4-sha-ec7751343');
+const VERSION = new Version('9.0.0-rc.4-sha-71165f103');
 
 /**
  * @license
