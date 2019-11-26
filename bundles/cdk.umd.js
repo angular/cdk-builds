ac3ce2e025185a3d132517453960c4575a459489
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-11-26 18:57:38 +0000

    master - 61b423a40 perf(youtube-player): triggering change detection for unused events (#17665)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.4-sha-7aa522aa9');
+	var VERSION = new core.Version('9.0.0-rc.4-sha-61b423a40');
 
 	/**
 	 * @license
