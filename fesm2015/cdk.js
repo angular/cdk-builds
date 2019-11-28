6c2787b0603a3ef217ca0bb747cfae2906786952
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-11-28 15:53:31 +0000

    master - ab0f30df7 fix(youtube-player): clean up internal observables (#17835)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.4-sha-d4a2a0c10');
+const VERSION = new Version('9.0.0-rc.4-sha-ab0f30df7');
 
 /**
  * @license
