16d15f35937e0a4d5816c708a27093274cd55f33
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-12-03 16:10:07 +0000

    master - a6f235d5e fix(form-field): incorrect assumptions about page direction (#17395)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-rc.4-sha-c69a727ac');
+var VERSION = new Version('9.0.0-rc.4-sha-a6f235d5e');
 
 /**
  * @license
