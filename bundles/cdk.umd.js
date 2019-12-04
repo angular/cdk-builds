96f8cb36f68ea2b020838d9742d4e1c8be4a223c
Vanessa Schmitt <vschmitt@google.com>
2019-12-04 00:07:49 +0000

    master - 9db133363 chore: add FocusTrap demo page (#17858)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.4-sha-03a9c1da7');
+	var VERSION = new core.Version('9.0.0-rc.4-sha-9db133363');
 
 	/**
 	 * @license
