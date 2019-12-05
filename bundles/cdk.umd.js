e7519dd7bc05b7fa256c2bfd3a77602dabf15e9c
Joey Perrott <josephperrott.github@gmail.com>
2019-12-05 20:03:43 +0000

    master - c1d898661 ci: update saucelabs to use angular-components account (#17873)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.4-sha-de560b9dc');
+	var VERSION = new core.Version('9.0.0-rc.4-sha-c1d898661');
 
 	/**
 	 * @license
