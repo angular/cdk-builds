d46838224fb37c3aaaaf85bab2b944731170da16
Younes Jaaidi <yjaaidi@gmail.com>
2019-12-05 20:04:23 +0000

    master - ed53a9b07 docs(cdk/testing): fix typo in harness guide (#17883)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-rc.4-sha-c1d898661');
+var VERSION = new Version('9.0.0-rc.4-sha-ed53a9b07');
 
 /**
  * @license
