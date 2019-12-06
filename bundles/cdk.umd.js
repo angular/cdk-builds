b1c723b289e8693a74f0296118ceb4e230aa8182
Jeremy Elbourn <jelbourn@google.com>
2019-12-06 17:48:47 +0000

    master - 81a204e42 chore: bump version to 9.0.0-rc.5 w/ changelog (#17892)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.4-sha-ed53a9b07');
+	var VERSION = new core.Version('9.0.0-rc.5-sha-81a204e42');
 
 	/**
 	 * @license
