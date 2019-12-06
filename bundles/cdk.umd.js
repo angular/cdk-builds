27a69e1fe79c61852d35e9d383baa9ed339fb790
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-12-06 22:27:04 +0000

    master - 27fae2927 fix(youtube-player): avoid setInterval change detection when player is created (#17894)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.5-sha-81a204e42');
+	var VERSION = new core.Version('9.0.0-rc.5-sha-27fae2927');
 
 	/**
 	 * @license
--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-rc.5-sha-81a204e42');
+var VERSION = new Version('9.0.0-rc.5-sha-27fae2927');
 
 /**
  * @license
