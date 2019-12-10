f0479035642903823d088335228314c5a25afffb
Joakim Zebic <joakim.zebic@gmail.com>
2019-12-10 00:55:37 +0000

    master - e7508adfb fix(material/chips): fix ripple element opacity when using CSS variable theming (#17576)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-rc.5-sha-a3e953ee7');
+var VERSION = new Version('9.0.0-rc.5-sha-e7508adfb');
 
 /**
  * @license
