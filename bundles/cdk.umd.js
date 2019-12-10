1de611dce2c0c38efd899c0bb97217ac72dd4d51
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-12-10 18:43:47 +0000

    master - 6c12ea889 build: exit with non-zero status code if rollup globals lint fails (#17910)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.5-sha-e7508adfb');
+	var VERSION = new core.Version('9.0.0-rc.5-sha-6c12ea889');
 
 	/**
 	 * @license
