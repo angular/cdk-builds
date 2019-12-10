f059cfd134d73c57b014891bbd4190662632c507
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-12-10 00:28:26 +0000

    master - 1c1af58ce feat(select,input,form-field): move test harnesses out of expe… (#17874)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.5-sha-27fae2927');
+const VERSION = new Version('9.0.0-rc.5-sha-1c1af58ce');
 
 /**
  * @license
