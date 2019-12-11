d7228d3c775cf031a4377df5647f673e0703eaf5
mmalerba <mmalerba@google.com>
2019-12-11 17:16:41 +0000

    master - 498b8848e Revert "chore: update MDC to latest canary" (#17932)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.5-sha-e6b638403');
+	var VERSION = new core.Version('9.0.0-rc.5-sha-498b8848e');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.5-sha-e6b638403');
+const VERSION = new Version('9.0.0-rc.5-sha-498b8848e');
 
 /**
  * @license
