7a774f1589db9dd4e6a74e660cc2688fc56971c5
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-12-11 20:27:27 +0000

    master - 2a2d8378c fix: google-maps and youtube-player package missing tslib dependency (#17939)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.5-sha-6ae74a0eb');
+	var VERSION = new core.Version('9.0.0-rc.5-sha-2a2d8378c');
 
 	/**
 	 * @license
