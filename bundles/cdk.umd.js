735611c4c0196d4c2b325b290d4761674d344272
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-12-12 21:46:31 +0000

    master - 1cab7df96 build: add ci jobs to ensure ngcc compatibility (#17930)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.5-sha-45d34ad4f');
+	var VERSION = new core.Version('9.0.0-rc.5-sha-1cab7df96');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.5-sha-45d34ad4f');
+const VERSION = new Version('9.0.0-rc.5-sha-1cab7df96');
 
 /**
  * @license
