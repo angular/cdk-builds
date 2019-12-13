37403886c47e4ae233251523f8a8822cab8267cd
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-12-13 23:34:52 +0000

    master - 0ec499d7d feat(core): add test harnesses for mat-option and mat-optgroup (#17940)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.5-sha-887d06305');
+const VERSION = new Version('9.0.0-rc.5-sha-0ec499d7d');
 
 /**
  * @license
