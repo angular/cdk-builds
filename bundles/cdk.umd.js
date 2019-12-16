4cb925ed3cbae3a0437859b8652f467d55205bf2
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-12-16 19:17:07 +0000

    master - c734deb14 build: update scss-bundle to avoid node-sass dependency (#17978)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.5-sha-527f1b596');
+	var VERSION = new core.Version('9.0.0-rc.5-sha-c734deb14');
 
 	/**
 	 * @license
