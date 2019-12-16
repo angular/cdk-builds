da6df301aec134361ee8b01fa155c45c8a930ca0
Michael Prentice <splaktar@gmail.com>
2019-12-16 19:33:51 +0000

    master - 6ab8e3632 docs(readme): fix link to FAQ (#17972)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.5-sha-c734deb14');
+	var VERSION = new core.Version('9.0.0-rc.5-sha-6ab8e3632');
 
 	/**
 	 * @license
