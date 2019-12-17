060935373785217f6f9ac0b8a65b0b0400e3703b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-12-17 18:36:27 +0000

    master - 27fc3b127 build: add script to deploy dev-app web package to firebase (#17989)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.5-sha-49a1324ac');
+	var VERSION = new core.Version('9.0.0-rc.5-sha-27fc3b127');
 
 	/**
 	 * @license
