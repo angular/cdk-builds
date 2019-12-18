9268d29eee1859bd7b99324180a791188ea13d3b
Andrew Seguin <andrewseguin@users.noreply.github.com>
2019-12-18 21:07:18 +0000

    master - bb68c7b4a feat(material-experimental/mdc-table): add MDC styles/classes (#18000)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.5-sha-093d00773');
+	var VERSION = new core.Version('9.0.0-rc.5-sha-bb68c7b4a');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.5-sha-093d00773');
+const VERSION = new Version('9.0.0-rc.5-sha-bb68c7b4a');
 
 /**
  * @license
