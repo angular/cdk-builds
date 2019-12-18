74be7a4803e23b6a2d96505af39b24c0ba58d69b
Andrew Seguin <andrewseguin@users.noreply.github.com>
2019-12-18 22:16:53 +0000

    master - 8796d7cc6 feat(material-experimental/mdc-table): add tests (#18003)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-rc.5-sha-bb68c7b4a');
+var VERSION = new Version('9.0.0-rc.5-sha-8796d7cc6');
 
 /**
  * @license
