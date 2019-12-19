bf9083257f66b5dfb3d2c0d2860a5bc45e5a35f5
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-12-19 21:25:42 +0000

    master - 9a6be0d24 feat(button-toggle): add test harness (#17996)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.5-sha-8cd0b78cc');
+	var VERSION = new core.Version('9.0.0-rc.5-sha-9a6be0d24');
 
 	/**
 	 * @license
