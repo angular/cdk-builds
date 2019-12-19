e41a0a4bc065877dce5b7a79af9e4cb4631075a2
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-12-19 21:24:10 +0000

    master - 3a3a1be91 build: update to latest Angular version (#18006)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-rc.5-sha-5a6cdaf26');
+var VERSION = new Version('9.0.0-rc.5-sha-3a3a1be91');
 
 /**
  * @license
