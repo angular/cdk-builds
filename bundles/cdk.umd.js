3872d298f16aaeaf8dd86064c307a61a817a9683
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-12-19 21:24:18 +0000

    master - 8cd0b78cc build: remove entryComponents from development apps (#17999)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.5-sha-3a3a1be91');
+	var VERSION = new core.Version('9.0.0-rc.5-sha-8cd0b78cc');
 
 	/**
 	 * @license
