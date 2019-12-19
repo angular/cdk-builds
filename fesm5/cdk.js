06aebba2ca181e82bf58c1dd377f1c92b270992c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-12-19 22:07:02 +0000

    master - 3299d8c6f fix(menu): forward aria attribute to menu panel (#17957)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-rc.5-sha-9a6be0d24');
+var VERSION = new Version('9.0.0-rc.5-sha-3299d8c6f');
 
 /**
  * @license
