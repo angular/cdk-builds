0647dd4dcbacef19695cd64552aa18b25d87b9df
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-12-29 22:48:06 +0000

    master - 5e9ca2893 feat(material/grid-list): add test harness (#18001)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.6-sha-8a42b9890');
+	var VERSION = new core.Version('9.0.0-rc.6-sha-5e9ca2893');
 
 	/**
 	 * @license
