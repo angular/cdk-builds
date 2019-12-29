e6afcc13aaa41287f529d5c7e5726a0fecd85852
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-12-29 22:48:13 +0000

    master - 0b0d779c1 refactor: remove ts 3.7 compatibility type cast in ripple-rend… (#17934)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.6-sha-5e9ca2893');
+const VERSION = new Version('9.0.0-rc.6-sha-0b0d779c1');
 
 /**
  * @license
