574a0aff903f7077aa1cd8122981c1e85f129497
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-12-29 22:39:53 +0000

    master - 99af8e9c3 fix(material-experimental/mdc-chips): checkmark not visible in… (#18061)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.6-sha-61029c827');
+	var VERSION = new core.Version('9.0.0-rc.6-sha-99af8e9c3');
 
 	/**
 	 * @license
