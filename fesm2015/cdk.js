9a35b3c121800607e2508028742515b643942625
Michael Prentice <splaktar@gmail.com>
2019-12-29 22:47:56 +0000

    master - 8a42b9890 docs(material/autocomplete): fix exception on page load (#17956)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.6-sha-c9856eeb5');
+const VERSION = new Version('9.0.0-rc.6-sha-8a42b9890');
 
 /**
  * @license
