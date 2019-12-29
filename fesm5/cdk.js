6b3c1750e9a4ad4809661ecd865e550e53217dc4
Vanessa Schmitt <vschmitt@google.com>
2019-12-29 22:43:16 +0000

    master - 22d7f77e8 fix(mdc-chips): Set aria-required on input instead of grid (#18049)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-rc.6-sha-c36466c2f');
+var VERSION = new Version('9.0.0-rc.6-sha-22d7f77e8');
 
 /**
  * @license
