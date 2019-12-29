cf3b435fc21ae9a561ee4d637453f5e40edbd4bb
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-12-29 22:39:13 +0000

    master - 61029c827 fix(datepicker): not respecting form control updateOn: blur fo… (#18063)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.6-sha-a30094b47');
+	var VERSION = new core.Version('9.0.0-rc.6-sha-61029c827');
 
 	/**
 	 * @license
