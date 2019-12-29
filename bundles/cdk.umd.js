ec6819e1b6157f9d23fb2d207b2b673c07b4dd48
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-12-29 22:43:55 +0000

    master - 17a7bcb8d fix(material-experimental/mdc-chips): avoid potential server-s… (#18044)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.6-sha-22d7f77e8');
+	var VERSION = new core.Version('9.0.0-rc.6-sha-17a7bcb8d');
 
 	/**
 	 * @license
