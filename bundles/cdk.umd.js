cd6797b556068945fd8292264670d03c33239921
Raz Luvaton <rluvaton@gmail.com>
2020-01-06 23:11:35 +0000

    master - 0429dfc56 Fix part of #13195 (Size not enforced) (#18028)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.6-sha-f2cbc066a');
+	var VERSION = new core.Version('9.0.0-rc.6-sha-0429dfc56');
 
 	/**
 	 * @license
