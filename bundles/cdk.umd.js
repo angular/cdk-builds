3da7b615d9b0125449a055b20a7fab5d2b9ebeff
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-06 23:10:46 +0000

    master - f2cbc066a fix(google-maps): incorrect event name (#18027)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.6-sha-ad689cdbb');
+	var VERSION = new core.Version('9.0.0-rc.6-sha-f2cbc066a');
 
 	/**
 	 * @license
--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-rc.6-sha-ad689cdbb');
+var VERSION = new Version('9.0.0-rc.6-sha-f2cbc066a');
 
 /**
  * @license
