b98062bde5bc818fcc16c754c552ca71d36135a8
Greg Magolan <gmagolan@gmail.com>
2020-01-06 23:10:16 +0000

    master - ad689cdbb build: update to rules_nodejs 1.0.0 (#18064)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.6-sha-551154816');
+const VERSION = new Version('9.0.0-rc.6-sha-ad689cdbb');
 
 /**
  * @license
