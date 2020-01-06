c7ea3d4efde6c7df7ad5c237c552d8e9f23b2b3d
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-01-06 22:17:30 +0000

    master - 43184107c docs(cdk/testing): correct test harnesses typo (#18115)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.6-sha-6c5c08e62');
+const VERSION = new Version('9.0.0-rc.6-sha-43184107c');
 
 /**
  * @license
