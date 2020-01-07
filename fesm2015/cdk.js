9f72eb7022bd56750755d848ce2a5ab897de6601
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-01-07 21:35:44 +0000

    master - 697c3a0c4 feat(material-experimental): initial prototype of mdc-form-fie… (#17903)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.6-sha-935bb3948');
+const VERSION = new Version('9.0.0-rc.6-sha-697c3a0c4');
 
 /**
  * @license
