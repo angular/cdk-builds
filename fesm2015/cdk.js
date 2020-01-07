8a106107c22a5ba1cd6eb26f4d3d2ea3961f34b6
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-01-07 21:03:46 +0000

    master - 13ec3413d build: remove unused gulp test watch code (#18077)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.6-sha-9e7ba2512');
+const VERSION = new Version('9.0.0-rc.6-sha-13ec3413d');
 
 /**
  * @license
