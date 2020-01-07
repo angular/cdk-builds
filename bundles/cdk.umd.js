5f3c473bcaa999339db53f4996fd971a7520fbce
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-07 21:04:46 +0000

    master - 86a4ba790 fix(material-experimental/mdc-tabs): incorrect tab text color in dark theme (#18068)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.6-sha-13ec3413d');
+	var VERSION = new core.Version('9.0.0-rc.6-sha-86a4ba790');
 
 	/**
 	 * @license
