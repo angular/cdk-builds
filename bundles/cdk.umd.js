4c7143788c7d3916b5b0c2b157204888cc274777
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-08 20:26:42 +0000

    master - 2fd862d68 fix(slide-toggle): fix and simplify high contrast styles (#18104)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.6-sha-d1de6fa26');
+	var VERSION = new core.Version('9.0.0-rc.6-sha-2fd862d68');
 
 	/**
 	 * @license
