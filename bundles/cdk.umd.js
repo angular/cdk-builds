97a1444ce8a123133bdbebbdf27cbc8e94133dd4
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-10 00:24:43 +0000

    master - 02c24ec85 fix(google-maps): support numbers for width/height of the map (#18105)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.7-sha-efebc9412');
+	var VERSION = new core.Version('9.0.0-rc.7-sha-02c24ec85');
 
 	/**
 	 * @license
