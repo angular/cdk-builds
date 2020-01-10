642dccfc7dc5a9f92f5e4e696908d374e72e2d9a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-10 19:18:35 +0000

    master - 5fe233add fix(datepicker): memory leak in popup mode (#18102)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.7-sha-fba22bc9d');
+	var VERSION = new core.Version('9.0.0-rc.7-sha-5fe233add');
 
 	/**
 	 * @license
