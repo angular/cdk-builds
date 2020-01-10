457e610a488edb4a3290c787e78e214ece221ede
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-01-10 19:22:01 +0000

    master - bfdf3239a refactor(material): deprecate MAT_LABEL_GLOBAL_OPTIONS in favo… (#18017)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.7-sha-dbdc1a8c5');
+	var VERSION = new core.Version('9.0.0-rc.7-sha-bfdf3239a');
 
 	/**
 	 * @license
--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-rc.7-sha-dbdc1a8c5');
+var VERSION = new Version('9.0.0-rc.7-sha-bfdf3239a');
 
 /**
  * @license
