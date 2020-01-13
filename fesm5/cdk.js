133793ece9ed5abef8d75c2f533c62901541827a
Andrius <17968027+andrius-pra@users.noreply.github.com>
2020-01-13 23:01:53 +0000

    master - 7cced9c3f refactor: fix typescript 3.7 compilation errors.Fixes `Import declaration conflicts with local declaration` error. (#18141)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-rc.7-sha-8aa38aca6');
+var VERSION = new Version('9.0.0-rc.7-sha-7cced9c3f');
 
 /**
  * @license
