cb4d6695455e18ac02073b2561ddf72a033b8b82
Olivier Suritz <olivier.suritz@gmail.com>
2020-01-13 23:37:30 +0000

    master - 71955d2e1 feat(material/icon): New FakeMatIconRegitry for unit tests (#18151)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.7-sha-7cced9c3f');
+	var VERSION = new core.Version('9.0.0-rc.7-sha-71955d2e1');
 
 	/**
 	 * @license
