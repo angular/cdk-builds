b8f169afee5c34bad0d6de9d36a36ba1226ce902
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-14 20:57:13 +0000

    9.0.x - a7f7e9b82 fix(google-maps): sub-components throwing during server-side rendering (#18059)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.7-sha-b2da5375d');
+	var VERSION = new core.Version('9.0.0-rc.7-sha-a7f7e9b82');
 
 	/**
 	 * @license
