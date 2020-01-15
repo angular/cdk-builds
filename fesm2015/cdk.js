bb22ea9a17356aea1770b359d5e9eb69a9da3b7b
mmalerba <mmalerba@google.com>
2020-01-15 20:16:19 +0000

    master - d550e4ef9 fix(material/form-field): undeprecated legacy and standard appearances (#18144)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.7-sha-1d9033e97');
+const VERSION = new Version('9.0.0-rc.7-sha-d550e4ef9');
 
 /**
  * @license
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -31,7 +31,7 @@
             // In order to align the CDK version with other Angular dependencies that are setup
             // by "@schematics/angular", we use tilde instead of caret. This is default for Angular
             // dependencies in new CLI projects.
-            package_config_1.addPackageToPackageJson(host, '@angular/cdk', `~9.0.0-rc.7-sha-1d9033e97`);
+            package_config_1.addPackageToPackageJson(host, '@angular/cdk', `~9.0.0-rc.7-sha-d550e4ef9`);
             // Add a task to run the package manager. This is necessary because we updated the
             // workspace "package.json" file and we want lock files to reflect the new version range.
             context.addTask(new tasks_1.NodePackageInstallTask());
