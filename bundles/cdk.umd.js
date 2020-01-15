a0f2a88e7389e43236862182c22fe633351e0247
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-15 20:11:54 +0000

    master - 1d9033e97 build: make cross entrypoint rule an AbstractRule (#18156)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.7-sha-203c8d63c');
+	var VERSION = new core.Version('9.0.0-rc.7-sha-1d9033e97');
 
 	/**
 	 * @license
--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-rc.7-sha-203c8d63c');
+var VERSION = new Version('9.0.0-rc.7-sha-1d9033e97');
 
 /**
  * @license
