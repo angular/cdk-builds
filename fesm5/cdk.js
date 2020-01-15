476d6fdb15f5266b889c4ead2351fccf8edc71dd
Andrew Seguin <andrewseguin@users.noreply.github.com>
2020-01-15 20:05:06 +0000

    master - 500d235e2 fix(material-experimental/mdc-input): input should not inherit parent classnames (#18180)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-rc.7-sha-149669e6e');
+var VERSION = new Version('9.0.0-rc.7-sha-500d235e2');
 
 /**
  * @license
