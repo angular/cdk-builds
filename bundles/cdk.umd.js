8b43aef31fc89b649f7b25ed987677c0540ee812
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-01-16 22:31:42 +0000

    master - f46d7b33d build: enable strict attribute type checking (#17977)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.7-sha-8f62ffe73');
+	var VERSION = new core.Version('9.0.0-rc.7-sha-f46d7b33d');
 
 	/**
 	 * @license
