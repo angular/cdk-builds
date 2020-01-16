cb1b9a602bf5b764f8342558a801384b1db832bc
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-01-16 23:35:54 +0000

    master - 581d1f2f9 build: remove custom rxjs imports rule in favor of import-banlist rule (#18157)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.8-sha-f31680596');
+const VERSION = new Version('9.0.0-rc.8-sha-581d1f2f9');
 
 /**
  * @license
