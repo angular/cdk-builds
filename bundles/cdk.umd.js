751a5a5a3ff435b0668eaa189c0a64a11673b8b5
Michael Prentice <splaktar@gmail.com>
2020-01-16 18:32:37 +0000

    master - 4405429d9 docs(material/table): use a hover color that is consistent with Material Design spec (#18193)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.7-sha-6973145a5');
+	var VERSION = new core.Version('9.0.0-rc.7-sha-4405429d9');
 
 	/**
 	 * @license
