c2093a5e2c4bb2be86b1d79db16ac11c1ac95550
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-01-16 00:48:25 +0000

    master - 16159b3fb build: remove hard-coded karma systemjs config (#18025)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.7-sha-3b242f00d');
+	var VERSION = new core.Version('9.0.0-rc.7-sha-16159b3fb');
 
 	/**
 	 * @license
