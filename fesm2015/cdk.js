323b556cd80c068491a1ae028793164cf55dc7bd
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-16 21:42:13 +0000

    master - 3e2e0234e fix(google-maps): internal events run inside NgZone (#18034)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.7-sha-4405429d9');
+const VERSION = new Version('9.0.0-rc.7-sha-3e2e0234e');
 
 /**
  * @license
