3b72f61be9e80cab2b3d439e342743677a3825a5
Andrew Seguin <andrewseguin@users.noreply.github.com>
2020-01-17 19:50:32 +0000

    master - 2d866ae90 chore: move mdc-chips change to material-experimental in 9.0.0-rc.7 (#18207)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.8-sha-581d1f2f9');
+	var VERSION = new core.Version('9.0.0-rc.8-sha-2d866ae90');
 
 	/**
 	 * @license
