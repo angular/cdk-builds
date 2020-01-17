7c27d123fc70b2e4789486967571a93a7928d6b9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-17 22:27:20 +0000

    master - 97a7e2bab docs(icon): add note about HttpClientModule requirement (#18070)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.8-sha-2d866ae90');
+	var VERSION = new core.Version('9.0.0-rc.8-sha-97a7e2bab');
 
 	/**
 	 * @license
