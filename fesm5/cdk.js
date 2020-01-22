52144fe9cb650abb6201e8dda5a63e33808539e8
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-22 02:43:35 +0000

    master - 8f2007f78 fix(youtube-player): not picking up static startSeconds and endSeconds (#18214)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-rc.8-sha-a15473b6a');
+var VERSION = new Version('9.0.0-rc.8-sha-8f2007f78');
 
 /**
  * @license
