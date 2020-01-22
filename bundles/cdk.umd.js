93d8f673adf7a8e9bf37789a0da726ca9f22430d
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-22 22:18:19 +0000

    master - 8d57d3249 fix(chips): set aria-required on chip input (#18098)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.8-sha-9227c7b83');
+	var VERSION = new core.Version('9.0.0-rc.8-sha-8d57d3249');
 
 	/**
 	 * @license
