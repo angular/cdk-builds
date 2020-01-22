7d8571bb2dd04030ecde91df43492b4f43fe802d
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-22 02:46:05 +0000

    9.0.x - 8ea430f5e fix(youtube-player): not picking up static startSeconds and endSeconds (#18214)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.8-sha-4310f8e24');
+	var VERSION = new core.Version('9.0.0-rc.8-sha-8ea430f5e');
 
 	/**
 	 * @license
