03d11286ed09c88ae9589529d28d9116a4283273
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-22 22:21:07 +0000

    9.0.x - ca9b204d2 fix(tabs): don't start auto-scroll when right clicking on buttons (#18033)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.8-sha-fbbac3713');
+const VERSION = new Version('9.0.0-rc.8-sha-ca9b204d2');
 
 /**
  * @license
