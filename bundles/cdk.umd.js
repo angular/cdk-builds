f3af52d59fa64efdee9c77070b68d2dd90ff4ee4
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-22 19:36:22 +0000

    9.0.x - 01b31de47 fix(tabs): better high contrast indication on supported browsers (#18160)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.8-sha-9086a4b4e');
+	var VERSION = new core.Version('9.0.0-rc.8-sha-01b31de47');
 
 	/**
 	 * @license
