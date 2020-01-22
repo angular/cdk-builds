a2da2573cf664fbc4a037cb0f0c27ccab407a710
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-22 05:41:13 +0000

    9.0.x - 6b3a271c9 fix(youtube-player): memory leak if player is destroyed before it is done initializing (#18046)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.8-sha-8ea430f5e');
+	var VERSION = new core.Version('9.0.0-rc.8-sha-6b3a271c9');
 
 	/**
 	 * @license
