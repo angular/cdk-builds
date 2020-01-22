fb63778e704209583843040fac471b138a454241
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-22 18:57:03 +0000

    9.0.x - 9086a4b4e fix(datepicker): able to focus disabled datepicker toggle while disabled via click (#18231)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.8-sha-6b3a271c9');
+	var VERSION = new core.Version('9.0.0-rc.8-sha-9086a4b4e');
 
 	/**
 	 * @license
