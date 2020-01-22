ec69c529d2e7f3be5e8399e0a32d99469270a9fe
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-22 19:30:46 +0000

    master - 1b167019a fix(material-experimental): errors with latest MDC canary version (#18173)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.8-sha-3a6c16f0c');
+	var VERSION = new core.Version('9.0.0-rc.8-sha-1b167019a');
 
 	/**
 	 * @license
