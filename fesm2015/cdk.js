49087207f1be1714bf202efef8b3356f37d77913
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-22 19:20:58 +0000

    master - 3a6c16f0c fix(expansion): unable to toggle disabled panel via methods (#18181)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.8-sha-e70499687');
+const VERSION = new Version('9.0.0-rc.8-sha-3a6c16f0c');
 
 /**
  * @license
