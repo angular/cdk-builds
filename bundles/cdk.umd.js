6458518594859b769b420b967812e41b6b957335
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-25 00:42:24 +0000

    master - ce40ec2dc build: bump to latest MDC Canary to fix regression (#18281)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.8-sha-c6a9f15e2');
+	var VERSION = new core.Version('9.0.0-rc.8-sha-ce40ec2dc');
 
 	/**
 	 * @license
