15cf19bc1583108f26161b66ed5dbd24037e6188
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-01-25 00:53:47 +0000

    master - 27498ea35 build: run saucelabs and browserstack tests with ivy (#18276)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.8-sha-ce40ec2dc');
+	var VERSION = new core.Version('9.0.0-rc.8-sha-27498ea35');
 
 	/**
 	 * @license
