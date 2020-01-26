3e255aa67766cb8ff6077f0502ebe6c01e89f84e
Michael Prentice <splaktar@gmail.com>
2020-01-26 21:57:38 +0000

    master - 0e30214cd docs(examples): all examples should use mat-form-field's fill appearance by default (#18252)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.8-sha-27111eb04');
+const VERSION = new Version('9.0.0-rc.8-sha-0e30214cd');
 
 /**
  * @license
