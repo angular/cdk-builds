925ee6f5078d17753c3920fadbe718d6ff1d9b29
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-27 06:36:33 +0000

    master - d9cf17534 fix(checkbox): outline not visible when checked in high contrast mode (#18048)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.8-sha-d05c69569');
+	var VERSION = new core.Version('9.0.0-rc.8-sha-d9cf17534');
 
 	/**
 	 * @license
