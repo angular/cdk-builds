753ce91f772339587a999c3db295f396faa798ff
Michael Prentice <splaktar@gmail.com>
2020-01-28 18:08:02 +0000

    master - 6ef58a25d docs(datepicker): convert date module tables to be mobile-friendly (#18210)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.8-sha-b163b644d');
+	var VERSION = new core.Version('9.0.0-rc.8-sha-6ef58a25d');
 
 	/**
 	 * @license
