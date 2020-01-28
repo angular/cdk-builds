489cd9829ea60c1933f82e47b33e6087df19fa3e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-28 19:47:39 +0000

    9.0.x - 2b83a0af2 fix(schematics): global typography class always being added (#18315)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-rc.8-sha-964de7b35');
+var VERSION = new Version('9.0.0-rc.8-sha-2b83a0af2');
 
 /**
  * @license
