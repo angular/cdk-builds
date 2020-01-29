3ded80cd32a5cdf0b1e41a57d5bb51ec0ac33a5c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-29 16:08:47 +0000

    master - 79330b7fb perf(ripple): avoid page layouts from ripple elements (#17253)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.8-sha-4c08a88ad');
+	var VERSION = new core.Version('9.0.0-rc.8-sha-79330b7fb');
 
 	/**
 	 * @license
--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-rc.8-sha-4c08a88ad');
+var VERSION = new Version('9.0.0-rc.8-sha-79330b7fb');
 
 /**
  * @license
