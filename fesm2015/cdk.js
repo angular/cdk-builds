bc92ebfd2ab8b43585bf69b3575b0cb4e72acc6a
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2020-01-29 22:26:21 +0000

    master - dfb3e0540 docs(cdk/testing): fix a few grammar issues (#18293)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.0-rc.8-sha-8df035026');
+const VERSION = new Version('9.0.0-rc.8-sha-dfb3e0540');
 
 /**
  * @license
