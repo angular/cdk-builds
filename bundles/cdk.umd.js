4ad951ddf59f1b5000aa4e741829a464c73bc218
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-01-29 18:47:41 +0000

    master - 8df035026 build: update to latest ts-api-guardian version (#18330)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.8-sha-05f59b36a');
+	var VERSION = new core.Version('9.0.0-rc.8-sha-8df035026');
 
 	/**
 	 * @license
