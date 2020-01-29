f25a107bfef31cdb3d07227f58b95752062fe620
Jeremy Elbourn <jelbourn@gmail.com>
2020-01-29 06:57:13 +0000

    master - ea171358a fix(cdk/a11y): make cdk-high-contrast work w/ emulated view encapsulation (#18152)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.8-sha-96f1625f7');
+	var VERSION = new core.Version('9.0.0-rc.8-sha-ea171358a');
 
 	/**
 	 * @license
--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-rc.8-sha-96f1625f7');
+var VERSION = new Version('9.0.0-rc.8-sha-ea171358a');
 
 /**
  * @license
