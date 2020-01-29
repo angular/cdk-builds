c6f9fda1540f2a217ff8e07d0043ea5742af8bfe
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-29 15:50:02 +0000

    master - 4c08a88ad fix(material-experimental/mdc-chips): leading icon not hidden on init when preselected (#17997)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-rc.8-sha-27ef8425f');
+var VERSION = new Version('9.0.0-rc.8-sha-4c08a88ad');
 
 /**
  * @license
