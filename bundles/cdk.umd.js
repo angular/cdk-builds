30ad36d8cfbe69911bf84015cafa003cc9fec211
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-30 18:05:41 +0000

    master - 2c4e2afc3 build: enable all ngtsc strictness flags (#18339)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.8-sha-dfb3e0540');
+	var VERSION = new core.Version('9.0.0-rc.8-sha-2c4e2afc3');
 
 	/**
 	 * @license
