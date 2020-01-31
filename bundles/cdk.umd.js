575da46787b6c29768912aa5625cc1bb5268cb6e
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-01-31 00:05:35 +0000

    9.0.x - deea0cad2 build: allow force overriding of expected publish branch (#18238)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.8-sha-cd3c8ef6b');
+	var VERSION = new core.Version('9.0.0-rc.8-sha-deea0cad2');
 
 	/**
 	 * @license
