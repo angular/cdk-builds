517d6a525b6949108e78d52ed1ffcec21afdbb63
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-02-03 18:02:40 +0000

    master - 2ec7254f8 build: remove ngcc postinstall patch in favor of separate script (#18355)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.0.0-rc.9-sha-12edc0b61');
+var VERSION = new Version('9.0.0-rc.9-sha-2ec7254f8');
 
 /**
  * @license
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -31,7 +31,7 @@
             // In order to align the CDK version with other Angular dependencies that are setup
             // by "@schematics/angular", we use tilde instead of caret. This is default for Angular
             // dependencies in new CLI projects.
-            package_config_1.addPackageToPackageJson(host, '@angular/cdk', `~9.0.0-rc.9-sha-12edc0b61`);
+            package_config_1.addPackageToPackageJson(host, '@angular/cdk', `~9.0.0-rc.9-sha-2ec7254f8`);
             // Add a task to run the package manager. This is necessary because we updated the
             // workspace "package.json" file and we want lock files to reflect the new version range.
             context.addTask(new tasks_1.NodePackageInstallTask());
