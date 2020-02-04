5d4c42ebc975cd6eb0ecb1e08378a70040a10e49
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-02-04 21:21:52 +0000

    master - b219cbcc6 fix(ng-add): do not overwrite version range specified in `ng add` (#18365)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.9-sha-0d108d3c8');
+	var VERSION = new core.Version('9.0.0-rc.9-sha-b219cbcc6');
 
 	/**
 	 * @license
