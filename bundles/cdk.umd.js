1b01ffd7cf23fb38520602e24d88c807ee5fff36
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-02-04 18:57:26 +0000

    master - 2fc488cfe build: minor improvements to testing script (#18363)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.9-sha-22d7cf643');
+	var VERSION = new core.Version('9.0.0-rc.9-sha-2fc488cfe');
 
 	/**
 	 * @license
