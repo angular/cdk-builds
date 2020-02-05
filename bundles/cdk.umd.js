653cd2fa7204c4df2b6158ae4753b9e9f3105389
Miles Malerba <mmalerba@google.com>
2020-02-05 18:39:13 +0000

    9.0.x - 1001b70ec Revert "build: remove unused angular bazel postinstall patch (#18373)"

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.9-sha-a35639a16');
+	var VERSION = new core.Version('9.0.0-rc.9-sha-1001b70ec');
 
 	/**
 	 * @license
