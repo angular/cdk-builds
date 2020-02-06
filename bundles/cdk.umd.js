b638b1bab8270cb8e8d5365b156f02d3df5b22d6
Jeremy Elbourn <jelbourn@google.com>
2020-02-06 00:44:56 +0000

    9.0.x - edb5f8a66 chore: bump version to 9.0.0-rc.10 w/ changelog (#18408)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.0-rc.9-sha-351d0c4f3');
+	var VERSION = new core.Version('9.0.0-rc.10-sha-edb5f8a66');
 
 	/**
 	 * @license
