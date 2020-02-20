b7a7fed613025403b83a69f2e6afaa2d2675a069
Andrew Seguin <andrewseguin@users.noreply.github.com>
2020-02-20 23:14:03 +0000

    master - 3d351802c fix(material-experimental): rename theme files to match stable components (#18383)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.1-sha-01e4fbac8');
+	var VERSION = new core.Version('9.0.1-sha-3d351802c');
 
 	/**
 	 * @license
