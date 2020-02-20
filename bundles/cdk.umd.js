f7dfc04dbddda4f531bfa28ec97c8413045337ca
Jeremy Elbourn <jelbourn@google.com>
2020-02-20 19:12:16 +0000

    master - c246a1c3a Merge google-maps-next branch into master (#18485)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.1-sha-d1a6ea74d');
+	var VERSION = new core.Version('9.0.1-sha-c246a1c3a');
 
 	/**
 	 * @license
