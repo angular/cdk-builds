b3dbc420ac8984655a121402e96bee7eb77abc2b
Antonio Cardenas <antonioalx66@gmail.com>
2020-02-20 19:33:20 +0000

    master - 2e939d4ce docs(youtube-player component): fix incorrect syntax

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.1-sha-e9debff8d');
+	var VERSION = new core.Version('9.0.1-sha-2e939d4ce');
 
 	/**
 	 * @license
