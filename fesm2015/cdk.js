14e2ea3323776693ab48d98b1839e6af3048f88e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-02-20 19:21:51 +0000

    master - 5e34de2fd fix(datepicker): don't call date filter function if date is out of bounds (#18419)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.1-sha-a761359c8');
+const VERSION = new Version('9.0.1-sha-5e34de2fd');
 
 /**
  * @license
