f7b71cb09e3b53f697012a6b8370f171319f23c6
CaerusKaru <caerus.karu@gmail.com>
2020-02-20 19:18:41 +0000

    master - a761359c8 test(layout): remove unneeded LayoutModule (#18447)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.1-sha-4b329f089');
+	var VERSION = new core.Version('9.0.1-sha-a761359c8');
 
 	/**
 	 * @license
