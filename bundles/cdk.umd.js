543936c7c09a02c380d5c7bfabfd0c8ff97a2015
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-02-20 18:56:04 +0000

    master - 66b4171fb build: update to MDC 5.0.0 final (#18500)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.1-sha-0d7e2f00b');
+	var VERSION = new core.Version('9.0.1-sha-66b4171fb');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.1-sha-0d7e2f00b');
+const VERSION = new Version('9.0.1-sha-66b4171fb');
 
 /**
  * @license
