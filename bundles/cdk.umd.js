c00a0928edc3fa9b69c3d978f57c5decc506b8f1
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-02-20 19:35:49 +0000

    master - 01e4fbac8 fix(ng-update): do not throw if project does not have dependencies (#18470)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.1-sha-aea79f08e');
+	var VERSION = new core.Version('9.0.1-sha-01e4fbac8');
 
 	/**
 	 * @license
