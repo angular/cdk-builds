9c60710b0a5d2f065c58cbd7a72306255bdf32a5
Andrew Seguin <andrewseguin@users.noreply.github.com>
2020-02-24 19:12:10 +0000

    master - 220447eb0 chore: bump version to 9.1.0 w/ changelog (#18601)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.1-sha-e43497769');
+const VERSION = new Version('9.1.0-sha-220447eb0');
 
 /**
  * @license
