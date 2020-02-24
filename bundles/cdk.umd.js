fa514ca52c3646dfbd43c5805847d6e29be4d8db
Andrew Seguin <andrewseguin@users.noreply.github.com>
2020-02-24 17:48:51 +0000

    master - e43497769 chore: fix Angular package version number (#18600)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.0.1-sha-48d820e70');
+	var VERSION = new core.Version('9.0.1-sha-e43497769');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.0.1-sha-48d820e70');
+const VERSION = new Version('9.0.1-sha-e43497769');
 
 /**
  * @license
