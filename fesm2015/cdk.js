afccc263ad1821b69941035487967bd81ab57b54
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-02-24 19:38:01 +0000

    master - 1ba8b26b2 build: remove ibazel workaround (#18589)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.1.0-sha-220447eb0');
+const VERSION = new Version('9.1.0-sha-1ba8b26b2');
 
 /**
  * @license
