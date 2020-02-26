26924938c734e727c9a4669bbe399cee93d4a994
mmalerba <mmalerba@google.com>
2020-02-26 20:29:27 +0000

    master - 9343d0852 demo(mdc-list): Add a demo page for the MDC-based list (#18604)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.1.0-sha-d19132965');
+	var VERSION = new core.Version('9.1.0-sha-9343d0852');
 
 	/**
 	 * @license
