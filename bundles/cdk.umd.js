bb44c58c12b01e693e6b596c22d4a7a76bbfa936
Andrew Seguin <andrewseguin@users.noreply.github.com>
2020-02-26 15:42:34 +0000

    master - d19132965 fix(material-experimental/mdc-button): add base css class to all buttons (#18422)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.1.0-sha-7b3ea26af');
+	var VERSION = new core.Version('9.1.0-sha-d19132965');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.1.0-sha-7b3ea26af');
+const VERSION = new Version('9.1.0-sha-d19132965');
 
 /**
  * @license
