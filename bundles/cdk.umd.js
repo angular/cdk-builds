768ce0104b4c1dff46d7f2a766f49647db55bbf8
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-02-26 15:33:31 +0000

    master - 9badc54cc fix(chips): only add type attribute to button remove icons (#18477)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.1.0-sha-ff8d56e8f');
+	var VERSION = new core.Version('9.1.0-sha-9badc54cc');
 
 	/**
 	 * @license
