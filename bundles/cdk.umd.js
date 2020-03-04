c16753d194b24bceed9eb44680e9e8123fd3cc60
mmalerba <mmalerba@google.com>
2020-03-04 22:03:17 +0000

    master - 77a0d5f3c Update table-row-context-example.html (#18674)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.1.0-sha-400901b84');
+	var VERSION = new core.Version('9.1.0-sha-77a0d5f3c');
 
 	/**
 	 * @license
