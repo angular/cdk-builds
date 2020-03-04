036f05cb2c76d88c7b023897123bee391b8fb48c
mmalerba <mmalerba@google.com>
2020-03-04 22:04:22 +0000

    master - 27aac7240 build: include tslib helpers in umd bundles (#18672)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.1.0-sha-77a0d5f3c');
+var VERSION = new Version('9.1.0-sha-27aac7240');
 
 /**
  * @license
