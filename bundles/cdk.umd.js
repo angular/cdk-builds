dcd41a92b25c095068a17fd6a8abdebb564f1ecc
crisbeto <crisbeto@abv.bg>
2020-03-05 22:56:45 +0000

    master - e0634c9e3 docs(clipboard): example in readme not working on firefox

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.1.0-sha-18e1e48f0');
+	var VERSION = new core.Version('9.1.0-sha-e0634c9e3');
 
 	/**
 	 * @license
--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.1.0-sha-18e1e48f0');
+var VERSION = new Version('9.1.0-sha-e0634c9e3');
 
 /**
  * @license
