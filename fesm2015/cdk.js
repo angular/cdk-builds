a8332e6a0ff5b7ff341209e2f7104949fc35e524
crisbeto <crisbeto@abv.bg>
2020-03-05 22:52:37 +0000

    master - cf085b4c4 fix(material-experimental/mdc-progress-bar): account for breaking changes in latest canary

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.1.0-sha-f95af1a84');
+const VERSION = new Version('9.1.0-sha-cf085b4c4');
 
 /**
  * @license
