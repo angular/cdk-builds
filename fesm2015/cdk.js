42239616ceb68bacdbb813fd1981c98f1b841448
mmalerba <mmalerba@google.com>
2020-03-05 18:50:00 +0000

    master - 770bf8266 docs: clarify "what we're working on" in readme (#18725)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.1.0-sha-ef1673594');
+const VERSION = new Version('9.1.0-sha-770bf8266');
 
 /**
  * @license
