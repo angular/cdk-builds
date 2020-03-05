220428d41aa69100944e615de68a04ffeea195e1
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-03-05 22:52:54 +0000

    master - 18e1e48f0 build: downgrade ibazel version to v0.12.0

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.1.0-sha-cf085b4c4');
+	var VERSION = new core.Version('9.1.0-sha-18e1e48f0');
 
 	/**
 	 * @license
