33d80865247fb0452f7c5fedfd02e2745f3e9507
Jeremy Elbourn <jelbourn@google.com>
2020-03-06 21:26:57 +0000

    master - 598db096e chore: copy 9.1.1 changelog to master (#18743)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.1.0-sha-3447891cf');
+	var VERSION = new core.Version('9.1.1-sha-598db096e');
 
 	/**
 	 * @license
