179f90851530100951d6c86b1b4c62b2f4c7db2f
Andrew Seguin <andrewseguin@users.noreply.github.com>
2020-03-06 20:07:43 +0000

    master - d60c9af42 chore: split portion of CHANGELOG.md (#18731)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.1.0-sha-eae5cf886');
+const VERSION = new Version('9.1.0-sha-d60c9af42');
 
 /**
  * @license
