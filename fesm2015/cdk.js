ae38d96aa8faa0de819eac5ee2919e4b5196f6c1
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-03-09 22:00:41 +0000

    master - 00ede242c build: do not hide output of bazel yarn install (#18765)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.1.1-sha-598db096e');
+const VERSION = new Version('9.1.1-sha-00ede242c');
 
 /**
  * @license
