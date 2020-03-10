5a9dd7c5b3fff2ec85a756c685567cdbe67e3e2c
Miles Malerba <mmalerba@google.com>
2020-03-10 19:23:54 +0000

    9.1.x - ef75635bc Revert "chore: add script to resync caretaker app open PRs (#18732)"

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.1.1-sha-9a63a5659');
+	var VERSION = new core.Version('9.1.1-sha-ef75635bc');
 
 	/**
 	 * @license
