fff48292fd58adf3347064da44e86d612630bb50
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-03-10 18:24:26 +0000

    9.1.x - 9a63a5659 build: setup snapshot builds for experimental and new packages (#18736)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.1.1-sha-d318b2734');
+	var VERSION = new core.Version('9.1.1-sha-9a63a5659');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.1.1-sha-d318b2734');
+const VERSION = new Version('9.1.1-sha-9a63a5659');
 
 /**
  * @license
