843a70f1771e9a3d35a2fdba2085fc470e8cac16
mmalerba <mmalerba@google.com>
2020-03-10 19:34:21 +0000

    master - 807498dbb chore: bump version to 9.1.2 w/ changelog (#18782)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.1.1-sha-59ae34bf9');
+const VERSION = new Version('9.1.2-sha-807498dbb');
 
 /**
  * @license
