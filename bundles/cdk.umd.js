05350e35648da318b2450faec5d0d60f8243ac6a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-03-12 21:42:57 +0000

    master - b2e86918e fix(material-experimetal/mdc-form-field): account for breaking changes in MDC (#18795)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.1.2-sha-3854db02b');
+	var VERSION = new core.Version('9.1.2-sha-b2e86918e');
 
 	/**
 	 * @license
