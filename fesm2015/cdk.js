6420f125b1b5001a7f3cdfd5ef68c60e892c088c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-03-12 21:16:48 +0000

    master - b2d08df8e fix(google-maps): server-side rendering error for polygon and rectangle components (#18573)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.1.2-sha-ea03ccec5');
+const VERSION = new Version('9.1.2-sha-b2d08df8e');
 
 /**
  * @license
