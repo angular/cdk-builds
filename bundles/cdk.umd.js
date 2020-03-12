d3e8b0811e265107167009491e57c030e24e9b51
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-03-12 21:23:04 +0000

    master - ee3958d31 fix(list): incorrectly selecting items when moving focus using shift + arrow key in single selection mode (#18579)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.1.2-sha-b2d08df8e');
+	var VERSION = new core.Version('9.1.2-sha-ee3958d31');
 
 	/**
 	 * @license
