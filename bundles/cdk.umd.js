7b7c5eb3e1fa6ebfc0a6d80642bcb4275123a71d
Andrew Seguin <andrewseguin@users.noreply.github.com>
2020-03-13 18:06:50 +0000

    master - b5b3c8a63 Revert "fix(radio): clicks not landing correctly in some cases on Chrome (#18357)" (#18820)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.1.2-sha-bcb36c26d');
+	var VERSION = new core.Version('9.1.2-sha-b5b3c8a63');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.1.2-sha-bcb36c26d');
+const VERSION = new Version('9.1.2-sha-b5b3c8a63');
 
 /**
  * @license
