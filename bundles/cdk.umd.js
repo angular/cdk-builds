4c94e12a54e930ed62126d2e89b68c75ed94f943
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-03-16 04:19:53 +0000

    master - 0f37367b0 docs(table): fix incorrect class description (#16468)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.1.2-sha-b5b3c8a63');
+	var VERSION = new core.Version('9.1.2-sha-0f37367b0');
 
 	/**
 	 * @license
