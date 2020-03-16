e3047b00bbaf21d37677133993a58d69e6743598
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-03-16 04:25:14 +0000

    master - f144100f1 build: update tslint to latest version (#18823)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.1.2-sha-0f37367b0');
+const VERSION = new Version('9.1.2-sha-f144100f1');
 
 /**
  * @license
