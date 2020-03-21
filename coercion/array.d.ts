e205a0efead9609d09057bee7fd8dc96382e4b77
Klemen Oslaj <klemenoslaj0@gmail.com>
2020-03-21 05:21:33 +0000

    master - cd968863c fix(cdk/coercion): add the support for readonly array coercion (#18807)

--- coercion/array.d.ts
+++ coercion/array.d.ts
@@ -7,3 +7,4 @@
  */
 /** Wraps the provided value in an array, unless the provided value is an array. */
 export declare function coerceArray<T>(value: T | T[]): T[];
+export declare function coerceArray<T>(value: T | readonly T[]): readonly T[];
--- fesm2015/coercion.js
+++ fesm2015/coercion.js
@@ -40,7 +40,6 @@ function _isNumberValue(value) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-/** Wraps the provided value in an array, unless the provided value is an array. */
 function coerceArray(value) {
     return Array.isArray(value) ? value : [value];
 }
