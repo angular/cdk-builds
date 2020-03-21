cf3f3279d9d91d96f2dbfda94712723f0b468678
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-03-21 05:18:11 +0000

    master - 6275339f0 fix(form-field): incorrectly calculating start gap in RTL in the presence of a prefix (#18867)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.2.0-next.0-sha-32aec936b');
+	var VERSION = new core.Version('9.2.0-next.0-sha-6275339f0');
 
 	/**
 	 * @license
