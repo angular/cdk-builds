9f8f26403874e3bd1bd48122e22cdb2912930071
mmalerba <mmalerba@google.com>
2020-03-23 22:13:43 +0000

    master - 16e2cf094 Revert "refactor: change isDateInstance return type to is D" (#18897)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.2.0-next.0-sha-4d7ffaa3a');
+	var VERSION = new core.Version('9.2.0-next.0-sha-16e2cf094');
 
 	/**
 	 * @license
--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.2.0-next.0-sha-4d7ffaa3a');
+var VERSION = new Version('9.2.0-next.0-sha-16e2cf094');
 
 /**
  * @license
