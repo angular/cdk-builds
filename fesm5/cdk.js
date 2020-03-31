fc2f99281f7f423dd6711b3ba3180715fc15a011
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-03-31 16:09:01 +0000

    master - 5354fe602 build: clean up stylelint rules directory (#18919)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.2.0-sha-1c8a2c9f9');
+var VERSION = new Version('9.2.0-sha-5354fe602');
 
 /**
  * @license
