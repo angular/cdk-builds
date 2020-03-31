0b078e8731cd5024f4cf4dcaa617618072a9d5ae
Livvie Lin <livvie.lin@gmail.com>
2020-03-31 16:10:04 +0000

    master - 93f6b3020 docs(button): add more comprehensive button examples (#18878)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.2.0-sha-5354fe602');
+var VERSION = new Version('9.2.0-sha-93f6b3020');
 
 /**
  * @license
