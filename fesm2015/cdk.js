6e35a8aaff914029220ed921e6d2a2fcff76e4aa
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-03-31 16:07:27 +0000

    master - 17f8e9419 fix(popover-edit): unable to close focus content using the keyboard (#18945)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.2.0-sha-dc802800f');
+const VERSION = new Version('9.2.0-sha-17f8e9419');
 
 /**
  * @license
