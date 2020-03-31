b69c0a8a0688bf2ca24eeede350e7a0a129cdf27
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-03-31 20:24:22 +0000

    master - 274416b6c fix(bottom-sheet): allow result to be passed when dismissing through service (#18831)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.2.0-sha-124ea5aa0');
+	var VERSION = new core.Version('9.2.0-sha-274416b6c');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.2.0-sha-124ea5aa0');
+const VERSION = new Version('9.2.0-sha-274416b6c');
 
 /**
  * @license
