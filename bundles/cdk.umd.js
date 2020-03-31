1b5f79cbcb6fac41177364ed8e6b1a078b511e78
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-03-31 17:34:25 +0000

    9.2.x - f023328a8 build: resolve module names in circular deps test (#18957)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.2.0-sha-0a8aee8f3');
+	var VERSION = new core.Version('9.2.0-sha-f023328a8');
 
 	/**
 	 * @license
