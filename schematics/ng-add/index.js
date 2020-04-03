1b54c694116da92c5a703f9b531877920cb21d3f
Greg Magolan <gmagolan@gmail.com>
2020-04-03 18:51:37 +0000

    master - e4cfa2b19 build: update to rules_nodejs 1.5.0 (#18960)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -37,7 +37,7 @@
                 // In order to align the CDK version with other Angular dependencies that are setup by
                 // `@schematics/angular`, we use tilde instead of caret. This is default for Angular
                 // dependencies in new CLI projects.
-                package_config_1.addPackageToPackageJson(host, '@angular/cdk', `~9.2.0-sha-d51339c66`);
+                package_config_1.addPackageToPackageJson(host, '@angular/cdk', `~9.2.0-sha-e4cfa2b19`);
                 // Add a task to run the package manager. This is necessary because we updated the
                 // workspace "package.json" file and we want lock files to reflect the new version range.
                 context.addTask(new tasks_1.NodePackageInstallTask());
