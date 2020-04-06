3de840851e9142a526b41af330f5241eb696870b
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-04-06 22:52:17 +0000

    master - 619f7f8de fix(autocomplete): provide horizontal fallback positions (#18906)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.2.0-sha-92bbc7705');
+	var VERSION = new core.Version('9.2.0-sha-619f7f8de');
 
 	/**
 	 * @license
--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.2.0-sha-92bbc7705');
+var VERSION = new Version('9.2.0-sha-619f7f8de');
 
 /**
  * @license
