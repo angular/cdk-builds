215948fc3c09e74161aafb833ab6f7f9d3646fd7
Zack Elliott <4220717+zelliott@users.noreply.github.com>
2020-04-06 18:23:27 +0000

    master - 92bbc7705 fix(mat-button-toggle): mat-button-toggle element should not h… (#18746)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.2.0-sha-e0b579784');
+	var VERSION = new core.Version('9.2.0-sha-92bbc7705');
 
 	/**
 	 * @license
--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.2.0-sha-e0b579784');
+var VERSION = new Version('9.2.0-sha-92bbc7705');
 
 /**
  * @license
