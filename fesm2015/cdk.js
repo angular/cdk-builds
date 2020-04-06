e445c4ae1f4aba3ce98ba7f20da36f9e9b7b0e8a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-04-06 22:54:00 +0000

    master - 8c3ba3841 build: update buildifier and fix failures (#18997)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.2.0-sha-619f7f8de');
+const VERSION = new Version('9.2.0-sha-8c3ba3841');
 
 /**
  * @license
