f7c371a9c7274c5e2f576e5a87f326fe26e39642
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-04-06 22:54:12 +0000

    master - 8cfa8e84f fix(material-experimental/mdc-radio): no focus indication in h… (#18969)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.2.0-sha-8c3ba3841');
+	var VERSION = new core.Version('9.2.0-sha-8cfa8e84f');
 
 	/**
 	 * @license
