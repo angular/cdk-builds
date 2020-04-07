4cb6388e64a328bad4bbe75642749674058653a6
Vanessa Schmitt <vschmitt@google.com>
2020-04-07 05:20:16 +0000

    master - 029e36329 fix(material-experimental/mdc-chips) Replace default values (#19006)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.2.0-sha-3ede17dc9');
+var VERSION = new Version('9.2.0-sha-029e36329');
 
 /**
  * @license
