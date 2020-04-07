7f29c8c4ccb10bf13e35435a55f728bb212333e7
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-04-07 19:15:33 +0000

    master - 049a11b52 fix(datepicker): add hover indication to navigation buttons (#18995)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.2.0-sha-c7657e064');
+var VERSION = new Version('9.2.0-sha-049a11b52');
 
 /**
  * @license
