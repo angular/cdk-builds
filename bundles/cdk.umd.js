824f841793e4387fa3c63b92188688adf02b5314
Tim Paschedag <tpaschedag@gmail.com>
2020-04-09 17:58:43 +0000

    master - 1921df338 Spelling error in CDK Drag and drop placeholder (#19023)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.2.0-sha-3238fbb5a');
+	var VERSION = new core.Version('9.2.0-sha-1921df338');
 
 	/**
 	 * @license
