986a4fde7960c0b1e35ebd0e3f5ac1fe5e9a355b
mmalerba <mmalerba@google.com>
2020-04-14 21:23:34 +0000

    master - 712ce5c81 chore: bump version to 9.2.1 w/ changelog (#19073)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.2.0-sha-695dde64a');
+	var VERSION = new core.Version('9.2.1-sha-712ce5c81');
 
 	/**
 	 * @license
--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.2.0-sha-695dde64a');
+var VERSION = new Version('9.2.1-sha-712ce5c81');
 
 /**
  * @license
