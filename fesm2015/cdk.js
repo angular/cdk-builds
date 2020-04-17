a2e5b734311f9ae64264b6ae8baad41933767365
Cyrille Tuzi <web@cyrilletuzi.com>
2020-04-17 16:27:44 +0000

    master - 7d47b4592 refactor(schematics): remove comments in collection.json (#19077)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.2.1-sha-712ce5c81');
+const VERSION = new Version('9.2.1-sha-7d47b4592');
 
 /**
  * @license
