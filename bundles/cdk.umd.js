c16f13315d611c4d411f100a426096965ffb6b08
Santosh Yadav <santosh.yadav198613@gmail.com>
2020-04-20 14:44:01 +0000

    master - 1cf4048b2 docs: fix docs (#19041)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.2.1-sha-a2d041803');
+	var VERSION = new core.Version('9.2.1-sha-1cf4048b2');
 
 	/**
 	 * @license
--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.2.1-sha-a2d041803');
+var VERSION = new Version('9.2.1-sha-1cf4048b2');
 
 /**
  * @license
