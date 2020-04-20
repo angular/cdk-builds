77e0e30102fb3ccefd0768ee53c7e53167a50638
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-04-20 14:54:49 +0000

    master - c67337b71 fix(chips): chip list disabled state out of sync when swapping out form group with a disabled one (#17993)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.2.1-sha-d5a9eaa15');
+	var VERSION = new core.Version('9.2.1-sha-c67337b71');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.2.1-sha-d5a9eaa15');
+const VERSION = new Version('9.2.1-sha-c67337b71');
 
 /**
  * @license
