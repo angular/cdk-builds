28ead41fe33b7668676238d14ab333cb367b2d59
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-04-20 14:40:58 +0000

    master - a2d041803 fix(material-experimental/mdc-slide-toggle): strong focus indication not visible (#19048)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.2.1-sha-4431442d7');
+	var VERSION = new core.Version('9.2.1-sha-a2d041803');
 
 	/**
 	 * @license
