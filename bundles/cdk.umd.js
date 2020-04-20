a507d3c452fa960e78ba3260f0618fbeacaf40cd
mbehrlich <mehrlich@google.com>
2020-04-20 14:52:11 +0000

    master - 3e00f4c5f feat(google-maps): expose the underlying Google Maps objects. (#18613)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.2.1-sha-04d158816');
+	var VERSION = new core.Version('9.2.1-sha-3e00f4c5f');
 
 	/**
 	 * @license
