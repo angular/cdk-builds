2a45faadc23b5d2a59d2dff1c27881628f21e4f6
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-04-20 14:40:49 +0000

    master - 4431442d7 fix(drag-drop): expose Point interface (#19051)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.2.1-sha-7e660371d');
+	var VERSION = new core.Version('9.2.1-sha-4431442d7');
 
 	/**
 	 * @license
