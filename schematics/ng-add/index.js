e43b77509ade0afb83ddce5ee622d2de7dc917c6
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-04-20 14:52:28 +0000

    master - d5a9eaa15 fix(material-experimental/mdc-chips): not disabling all animations when animations are disabled (#18440)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -37,7 +37,7 @@
                 // In order to align the CDK version with other Angular dependencies that are setup by
                 // `@schematics/angular`, we use tilde instead of caret. This is default for Angular
                 // dependencies in new CLI projects.
-                package_config_1.addPackageToPackageJson(host, '@angular/cdk', `~9.2.1-sha-3e00f4c5f`);
+                package_config_1.addPackageToPackageJson(host, '@angular/cdk', `~9.2.1-sha-d5a9eaa15`);
                 // Add a task to run the package manager. This is necessary because we updated the
                 // workspace "package.json" file and we want lock files to reflect the new version range.
                 context.addTask(new tasks_1.NodePackageInstallTask());
