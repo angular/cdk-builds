a703ade028fbfb10a4a047f0010732c1c1cebef6
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-04-22 21:39:39 +0000

    master - 9d31d7191 build: create merge script w/ cherry-picking  (#18856)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.2.1-sha-e512581a8');
+	var VERSION = new core.Version('9.2.1-sha-9d31d7191');
 
 	/**
 	 * @license
--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.2.1-sha-e512581a8');
+var VERSION = new Version('9.2.1-sha-9d31d7191');
 
 /**
  * @license
