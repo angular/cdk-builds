d89b6742e3d68c1982c9839a1950c675a2fe3fac
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-04-24 14:40:08 +0000

    master - bac677547 fix(youtube-player): unable to bind to events after initialization (#18996)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.2.1-sha-9104a0b2e');
+const VERSION = new Version('9.2.1-sha-bac677547');
 
 /**
  * @license
