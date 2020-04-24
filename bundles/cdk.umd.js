39666ad108f4b8f417aeca3fae624e10009afe8a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-04-24 14:39:45 +0000

    master - 9104a0b2e feat(datepicker): allow for the dropdown position to be customized (#16698)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.2.1-sha-403a3d1ca');
+	var VERSION = new core.Version('9.2.1-sha-9104a0b2e');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.2.1-sha-403a3d1ca');
+const VERSION = new Version('9.2.1-sha-9104a0b2e');
 
 /**
  * @license
