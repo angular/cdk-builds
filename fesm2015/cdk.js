988c0c32e00a85359f8ced8f33a625f3fc63d14f
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-04-24 14:32:58 +0000

    master - 403a3d1ca fix(tree): complete viewChange stream on destroy (#18991)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.2.1-sha-6a64130b6');
+const VERSION = new Version('9.2.1-sha-403a3d1ca');
 
 /**
  * @license
