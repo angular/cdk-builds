5c94bafed9f6d07aa7e523a41281d45a119cc4c5
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-04-28 20:47:49 +0000

    9.2.x - 5d22b9f00 build: dev-infra-private package not updated locally (#19196)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.2.1-sha-8ac81aea4');
+	var VERSION = new core.Version('9.2.1-sha-5d22b9f00');
 
 	/**
 	 * @license
