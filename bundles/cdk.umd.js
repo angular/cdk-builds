21eb1c04a4094119533af9d65f25ca3c5a9a1b9e
Jeremy Elbourn <jelbourn@google.com>
2020-04-30 22:10:17 +0000

    9.2.x - 9e0ae2ea7 Revert "build: add server-side rendering check for ground overlay (#19188)"

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.2.1-sha-bacd18b7f');
+	var VERSION = new core.Version('9.2.1-sha-9e0ae2ea7');
 
 	/**
 	 * @license
