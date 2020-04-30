d9db1d3eb5c109a503b8bbee610663ffbf94de47
Marco Marche <marco.marche@gnucoop.com>
2020-04-30 00:52:53 +0000

    master - 15efb677a build: update to rules_nodejs 1.6.0 (#19185)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.2.1-sha-d319e5623');
+	var VERSION = new core.Version('9.2.1-sha-15efb677a');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.2.1-sha-d319e5623');
+const VERSION = new Version('9.2.1-sha-15efb677a');
 
 /**
  * @license
