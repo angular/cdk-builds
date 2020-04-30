611729a0f41ae225438458ee54770f440705ab7e
Jeremy Elbourn <jelbourn@google.com>
2020-04-30 23:29:41 +0000

    9.2.x - 8c241cb62 chore: bump version to 9.2.2 w/ changelog (#19214)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.2.1-sha-9e0ae2ea7');
+	var VERSION = new core.Version('9.2.2-sha-8c241cb62');
 
 	/**
 	 * @license
