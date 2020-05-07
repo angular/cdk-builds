d0fac712642a13e2a222f33326fb9138400be024
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-05-07 05:38:25 +0000

    master - d4d12a95d build: do not build runfile trees unnecessarily

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.2.1-sha-f02a726c7');
+const VERSION = new Version('9.2.1-sha-d4d12a95d');
 
 /**
  * @license
