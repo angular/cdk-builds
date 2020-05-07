effcbd380ba9ed2c2e66ac154632791275337f92
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-05-07 07:13:52 +0000

    master - ecf410539 build: unable to run schematics tests through test script (#19230)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.2.1-sha-63ce66584');
+	var VERSION = new core.Version('9.2.1-sha-ecf410539');
 
 	/**
 	 * @license
