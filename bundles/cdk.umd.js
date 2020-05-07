da5a52debcdbcf6f1c5a72b9661a8b87844fd65e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-05-07 20:50:35 +0000

    9.2.x - 322efa00e build: add dev app for MDC sidenav (#19282)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.2.2-sha-d270bf6af');
+	var VERSION = new core.Version('9.2.2-sha-322efa00e');
 
 	/**
 	 * @license
