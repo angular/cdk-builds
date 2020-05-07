2e357218a3dbaaf4585a297300ef1cf78e45de14
Alan Agius <alan.agius4@gmail.com>
2020-05-07 05:40:05 +0000

    master - 63ce66584 refactor: remove usage of private `buildDefaultPath` method from `@schematics/angular` (#19229)

--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.2.1-sha-d4a962461');
+var VERSION = new Version('9.2.1-sha-63ce66584');
 
 /**
  * @license
