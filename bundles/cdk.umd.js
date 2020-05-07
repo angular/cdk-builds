06e41198446291b294208f4c373e2e0ace785a1c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-05-07 22:53:51 +0000

    9.2.x - 5e9ac1c64 feat(material-experimental/mdc-form-field): expose `MAT_FORM_FIELD` injection token (#19277)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.2.2-sha-9f3bba35c');
+	var VERSION = new core.Version('9.2.2-sha-5e9ac1c64');
 
 	/**
 	 * @license
