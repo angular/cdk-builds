9ddc6f5ae884b702c415435d65759b27102ff29f
Alan Agius <alan.agius4@gmail.com>
2020-05-07 05:41:30 +0000

    9.2.x - d509ff7f2 refactor: remove usage of private `buildDefaultPath` method from `@schematics/angular` (#19229)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.2.2-sha-70ddc361a');
+	var VERSION = new core.Version('9.2.2-sha-d509ff7f2');
 
 	/**
 	 * @license
--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.2.2-sha-70ddc361a');
+var VERSION = new Version('9.2.2-sha-d509ff7f2');
 
 /**
  * @license
