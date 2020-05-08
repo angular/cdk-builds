37550914819c23716dad48f8e678289d94cf583e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-05-08 19:17:02 +0000

    master - 5bb81e84b fix(dialog): focus trap not attached if autoFocus is disabled (#19251)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.2.1-sha-703b1daa8');
+const VERSION = new Version('9.2.1-sha-5bb81e84b');
 
 /**
  * @license
