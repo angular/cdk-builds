ed42755982938e044bc21b0b3150b098bdde5e95
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-05-08 19:51:43 +0000

    master - 9be515627 fix(chips): chip list overriding chip disabled value (#19228)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.2.1-sha-5bb81e84b');
+	var VERSION = new core.Version('9.2.1-sha-9be515627');
 
 	/**
 	 * @license
