b2c79f619782bb91d378596881de92ac9ae06989
Liz Mitchell <lizmitchell@google.com>
2020-05-08 20:44:38 +0000

    master - 7b42c7e42 fix(material-experimental/form-field): add setLabelRequired adapter (#19284)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.2.1-sha-ba521d23a');
+	var VERSION = new core.Version('9.2.1-sha-7b42c7e42');
 
 	/**
 	 * @license
