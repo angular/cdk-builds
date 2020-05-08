ee869120095614cac2fe0ec157b686b2c62fc501
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-05-08 19:53:46 +0000

    master - 7fde30524 fix(list): selection list marked as touched when tabbing in (#19177)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.2.1-sha-b38c812da');
+	var VERSION = new core.Version('9.2.1-sha-7fde30524');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.2.1-sha-b38c812da');
+const VERSION = new Version('9.2.1-sha-7fde30524');
 
 /**
  * @license
