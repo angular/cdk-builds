acb40f69b8c8e88dae26b665efc1f3cef453b2ab
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-05-08 19:52:54 +0000

    master - b38c812da fix(tabs): don't prevent default space/enter action if action tab doesn't change (#19207)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.2.1-sha-9be515627');
+	var VERSION = new core.Version('9.2.1-sha-b38c812da');
 
 	/**
 	 * @license
