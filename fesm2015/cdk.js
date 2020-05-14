601b03d3e4a31c9eee767de14c957dda91954162
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-05-14 20:06:06 +0000

    master - caad0b54e refactor(material-experimental/mdc-form-field): disambiguate mixin names from non-mdc form-field

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.0.0-next.0-sha-fe87bda34');
+const VERSION = new Version('10.0.0-next.0-sha-caad0b54e');
 
 /**
  * @license
