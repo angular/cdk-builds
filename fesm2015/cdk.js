264e3bcc7d5cbbdfa5df20192a371730925aaeda
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-05-15 17:35:23 +0000

    master - eaff142d0 refactor(youtube-player): change deprecated APIs for version 10 (#19348)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.0.0-next.0-sha-caad0b54e');
+const VERSION = new Version('10.0.0-next.0-sha-eaff142d0');
 
 /**
  * @license
