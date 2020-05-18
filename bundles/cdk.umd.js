bb22b73878a6feb4bb382b2e674b5e600fda45f1
Miles Malerba <mmalerba@google.com>
2020-05-18 21:58:53 +0000

    9.2.x - fec578dbc chore: bump version to 9.2.4 w/ changelog

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.2.3-sha-bad049cda');
+	var VERSION = new core.Version('9.2.4-sha-fec578dbc');
 
 	/**
 	 * @license
