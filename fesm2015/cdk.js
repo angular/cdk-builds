91314537dbaa52042c7b17f2c5e46c32947a4bda
mmalerba <mmalerba@google.com>
2020-05-18 23:41:28 +0000

    master - e72e9aea5 chore: bump version to 10.0.0-next.1 w/ changelog (#19388)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.0.0-next.0-sha-16f5f793d');
+const VERSION = new Version('10.0.0-next.1-sha-e72e9aea5');
 
 /**
  * @license
