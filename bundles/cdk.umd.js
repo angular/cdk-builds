fe4c04f2eae25031623274f14198a2049891940b
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-05-19 21:01:53 +0000

    master - 2d64ad72b build: update to latest MDC canary and fix error (#19389)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.0-next.1-sha-e72e9aea5');
+	var VERSION = new core.Version('10.0.0-next.1-sha-2d64ad72b');
 
 	/**
 	 * @license
