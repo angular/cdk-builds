262a86ce38420f985343ff8bdbc5b1a8bab33ea5
Alan Agius <alan.agius4@gmail.com>
2020-05-19 22:10:04 +0000

    master - b6358b2e3 build: update to tslib 2.0 and move to direct dependencies (#19393)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.0-next.1-sha-2d64ad72b');
+	var VERSION = new core.Version('10.0.0-next.1-sha-b6358b2e3');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.0.0-next.1-sha-2d64ad72b');
+const VERSION = new Version('10.0.0-next.1-sha-b6358b2e3');
 
 /**
  * @license
