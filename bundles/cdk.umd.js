724f6fffb797a632ee165841597d148f61e6609f
Karl Seamon <bannanafiend@gmail.com>
2020-05-21 01:35:23 +0000

    9.2.x - 7bae4e193 fix(column-resize): Unbreak Sass build by importing duplicate styles file (#19404)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.2.4-sha-fe396c2f8');
+	var VERSION = new core.Version('9.2.4-sha-7bae4e193');
 
 	/**
 	 * @license
--- fesm5/cdk.js
+++ fesm5/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-var VERSION = new Version('9.2.4-sha-fe396c2f8');
+var VERSION = new Version('9.2.4-sha-7bae4e193');
 
 /**
  * @license
