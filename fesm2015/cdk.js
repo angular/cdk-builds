f4e94e6e455aedf1f9243b613afab66bbfbf3ab9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-05-21 04:39:17 +0000

    master - 95007e585 fix(table): incorrectly sticking multiple footer rows (#19321)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.0.0-next.1-sha-72b0219fb');
+const VERSION = new Version('10.0.0-next.1-sha-95007e585');
 
 /**
  * @license
