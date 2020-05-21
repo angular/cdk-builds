5dfd43bc3b169b4d77ba09124cae4561be36216e
Annie Wang <anniewang8697@gmail.com>
2020-05-21 02:39:11 +0000

    master - bc280f33d refactor(components-example): extract regions from component example source files (#19376)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.0-next.1-sha-0faff496f');
+	var VERSION = new core.Version('10.0.0-next.1-sha-bc280f33d');
 
 	/**
 	 * @license
