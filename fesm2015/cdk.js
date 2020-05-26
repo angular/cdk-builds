98aca4a83c83b46608217a858368750e98bd7847
Andy Chrzaszcz <andy9775@gmail.com>
2020-05-26 18:52:10 +0000

    9.2.x - b032deb62 docs(cdk/focus-trap): fix FocusTrap class documentation typo (#19431)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('9.2.4-sha-8d67249d2');
+const VERSION = new Version('9.2.4-sha-b032deb62');
 
 /**
  * @license
