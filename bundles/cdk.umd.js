209d213f1fec1b3aeb1490acfcb72306113043cd
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-05-26 17:31:19 +0000

    master - 25d2e0566 refactor(autocomplete): change deprecated APIs for version 10 (#19325)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.0-next.1-sha-268728a59');
+	var VERSION = new core.Version('10.0.0-next.1-sha-25d2e0566');
 
 	/**
 	 * @license
