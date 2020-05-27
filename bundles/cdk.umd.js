65124ca25895752e741725127c92cafe64ea3cdc
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-05-27 14:15:11 +0000

    9.2.x - 607b8571f build: disable ngcc snapshot compatibility job [patch only]

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('9.2.4-sha-b032deb62');
+	var VERSION = new core.Version('9.2.4-sha-607b8571f');
 
 	/**
 	 * @license
