815d3a6d16a76e3d66d84fb9742b71b175ee7902
Jeremy Elbourn <jelbourn@google.com>
2020-06-01 22:00:14 +0000

    master - 800821822 chore: bump version to 10.0.0-rc.0 w/ changelog (#19504)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.0-next.2-sha-3c579b68b');
+	var VERSION = new core.Version('10.0.0-rc.0-sha-800821822');
 
 	/**
 	 * @license
