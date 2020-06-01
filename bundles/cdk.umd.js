3a6f2193364734874e86cfe739982574baa6849a
Joey Perrott <josephperrott.github@gmail.com>
2020-06-01 19:56:32 +0000

    master - d55458d81 build: remove wombot proxy registry from package.jsons for release (#19500)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.0-next.2-sha-96c24f5c8');
+	var VERSION = new core.Version('10.0.0-next.2-sha-d55458d81');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.0.0-next.2-sha-96c24f5c8');
+const VERSION = new Version('10.0.0-next.2-sha-d55458d81');
 
 /**
  * @license
