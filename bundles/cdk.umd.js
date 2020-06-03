5783625637f627ede79929dc973b871523cdd923
Andy Chrzaszcz <andymchr@google.com>
2020-06-03 21:18:05 +0000

    master - 72c38f751 docs(cdk/overlay): add a simple overlay example (#19419)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.0-rc.0-sha-b365ba74c');
+	var VERSION = new core.Version('10.0.0-rc.0-sha-72c38f751');
 
 	/**
 	 * @license
