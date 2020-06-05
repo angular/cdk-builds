36ce59d568a3fbdd5f3a5965720173095c58bed5
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-06-05 02:45:45 +0000

    master - f22626444 build: avoid manual listing of MDC sass files in bazel setup (#19535)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.0.0-rc.0-sha-ba11a0bd3');
+const VERSION = new Version('10.0.0-rc.0-sha-f22626444');
 
 /**
  * @license
