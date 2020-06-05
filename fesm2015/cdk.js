75785544d3c12a4b81407fcea10795308716d379
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-05 02:59:16 +0000

    master - 2a6a63ceb fix(slider): focus ring not matching theme color (#19519)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.0.0-rc.0-sha-94571c5aa');
+const VERSION = new Version('10.0.0-rc.0-sha-2a6a63ceb');
 
 /**
  * @license
