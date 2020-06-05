600dae658f845077098e14c14c8d457c2a08afb1
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-05 03:00:19 +0000

    master - 00e2171b2 fix(datepicker): compilation errors with ViewEngine (#19516)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.0-rc.0-sha-2a6a63ceb');
+	var VERSION = new core.Version('10.0.0-rc.0-sha-00e2171b2');
 
 	/**
 	 * @license
