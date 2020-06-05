a0871da9c411688ed9e6986b0f7191afed832a8d
Annie Wang <anniewang8697@gmail.com>
2020-06-05 03:34:05 +0000

    master - a72883fae refactor: update snippets/embedded example comments in material docs md to reflect new API (chips, menu, tabs) (#19486)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.0-rc.0-sha-9716a0164');
+	var VERSION = new core.Version('10.0.0-rc.0-sha-a72883fae');
 
 	/**
 	 * @license
