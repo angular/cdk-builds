5a629120a72b7cb647dc4c93c2027ec855d7907f
Andy Chrzaszcz <andymchr@google.com>
2020-06-08 16:35:10 +0000

    master - 4346b522a build: fix bazel version (#19553)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.0.0-rc.0-sha-2b2ee1d8c');
+const VERSION = new Version('10.0.0-rc.0-sha-4346b522a');
 
 /**
  * @license
