2888256996ea0ced7269cad38e7b41cb3f16d1d1
Wagner Maciel <wagnermaciel@google.com>
2020-06-09 21:11:31 +0000

    master - 7d4fcd566 fix(benchmark): fix license header, remove a11y import, and remove @title doc (#19582)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.0.0-rc.1-sha-efed41c7d');
+const VERSION = new Version('10.0.0-rc.1-sha-7d4fcd566');
 
 /**
  * @license
