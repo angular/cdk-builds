d0092c864cb2df01a4e3421a361db11e8523339d
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-06-09 21:09:54 +0000

    master - efed41c7d build: switch to chromium provided in dev-infra package (#19565)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.0.0-rc.1-sha-227e568d3');
+const VERSION = new Version('10.0.0-rc.1-sha-efed41c7d');
 
 /**
  * @license
