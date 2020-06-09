dfc7f049a0bc5cab608217fa7399ce420ccb846c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-09 20:07:03 +0000

    master - b618a96be fix(material-experimental/mdc-menu): missing padding after latest canary release (#19548)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.0-rc.1-sha-6c212b74c');
+	var VERSION = new core.Version('10.0.0-rc.1-sha-b618a96be');
 
 	/**
 	 * @license
