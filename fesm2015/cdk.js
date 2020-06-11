94ae1dd59f9ef527a45a091d829778c92261db60
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-06-11 21:00:48 +0000

    master - f92e0df07 build: update merge script temporarily to merge into rc branch (#19617)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.0.0-rc.1-sha-a290168a3');
+const VERSION = new Version('10.0.0-rc.1-sha-f92e0df07');
 
 /**
  * @license
