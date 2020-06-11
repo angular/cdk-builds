5e005f729d13d1a61d66047afb599260180113c3
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-06-11 19:59:36 +0000

    master - a290168a3 refactor(list): use light-weight tokens for injecting parent lists  (#19568)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.0.0-rc.1-sha-d15f19edb');
+const VERSION = new Version('10.0.0-rc.1-sha-a290168a3');
 
 /**
  * @license
