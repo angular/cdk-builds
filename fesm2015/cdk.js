5afc16e303b6dc59e827b758baa2438b523ccff3
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-06-11 18:08:53 +0000

    master - d15f19edb build: fix ci failures in patch branch (#19607)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.0.0-rc.1-sha-0d266e21d');
+const VERSION = new Version('10.0.0-rc.1-sha-d15f19edb');
 
 /**
  * @license
