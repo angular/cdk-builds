a3c825a854e378366680019afe7d1879b5c35c54
David Lozano Jarque <mail@davidlj95.com>
2020-06-12 17:21:46 +0000

    master - 7700eff08 docs: fix MatSelect example (#19614)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.0-rc.1-sha-6bf7f246f');
+	var VERSION = new core.Version('10.0.0-rc.1-sha-7700eff08');
 
 	/**
 	 * @license
