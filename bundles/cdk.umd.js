21c6f49eda8bf7a1453c9e0d7bd83a81d739ddee
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-06-12 13:20:58 +0000

    master - b1e2a6d2f build: disable stale remote http cache for local development (#19603)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.0-rc.1-sha-69796c292');
+	var VERSION = new core.Version('10.0.0-rc.1-sha-b1e2a6d2f');
 
 	/**
 	 * @license
