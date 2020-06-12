316a9c6ffca0d5b90c0fe651a81caa1150a414d6
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-06-12 17:25:18 +0000

    10.0.x - e9dc45aba build: option to automatically retry postinstall patches (#19615)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.0-rc.1-sha-5f9be0567');
+	var VERSION = new core.Version('10.0.0-rc.1-sha-e9dc45aba');
 
 	/**
 	 * @license
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -28,7 +28,7 @@ function default_1() {
             // In order to align the CDK version with other Angular dependencies that are setup by
             // `@schematics/angular`, we use tilde instead of caret. This is default for Angular
             // dependencies in new CLI projects.
-            package_config_1.addPackageToPackageJson(host, '@angular/cdk', `~10.0.0-rc.1-sha-5f9be0567`);
+            package_config_1.addPackageToPackageJson(host, '@angular/cdk', `~10.0.0-rc.1-sha-e9dc45aba`);
             // Add a task to run the package manager. This is necessary because we updated the
             // workspace "package.json" file and we want lock files to reflect the new version range.
             context.addTask(new tasks_1.NodePackageInstallTask());
