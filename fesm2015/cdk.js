092da7aa35db2531d1b04af40e5990e81e63d567
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-12 18:05:49 +0000

    10.0.x - 72af3cd10 build: fix size check failure (#19625)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.0.0-rc.1-sha-e9dc45aba');
+const VERSION = new Version('10.0.0-rc.1-sha-72af3cd10');
 
 /**
  * @license
