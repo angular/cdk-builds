e686aa8554f99b69cc55da3fda1a3a4a56844a0c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-12 21:42:01 +0000

    master - 366852c82 build: fix wrong directory for notification script (#19626)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.0-rc.1-sha-76ba09735');
+	var VERSION = new core.Version('10.0.0-rc.1-sha-366852c82');
 
 	/**
 	 * @license
