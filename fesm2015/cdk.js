fe005cde3a3744bf4794d75731a8d1adb796e416
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-06-13 00:05:12 +0000

    10.0.x - a57668a38 build: switch to yaml file for size test golden  (#19621)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.0.0-rc.1-sha-3fc101c91');
+const VERSION = new Version('10.0.0-rc.1-sha-a57668a38');
 
 /**
  * @license
