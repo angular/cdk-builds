173eb903f1b253ee2cd466a3048cbe08a2b5610e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-15 14:14:11 +0000

    master - 264c20d19 build: enforce consistent array types (#19640)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.0.0-rc.1-sha-07a14522e');
+const VERSION = new Version('10.0.0-rc.1-sha-264c20d19');
 
 /**
  * @license
