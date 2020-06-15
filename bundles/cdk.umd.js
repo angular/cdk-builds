3e67cb7b4072699f51329d89242b3f1cf5845e1a
Andrew Seguin <andrewseguin@users.noreply.github.com>
2020-06-15 18:04:05 +0000

    10.0.x - 1decf34f3 chore: bump version to 10.0.0-rc.2 w/ changelog (#19651)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.0-rc.1-sha-102b65b91');
+	var VERSION = new core.Version('10.0.0-rc.2-sha-1decf34f3');
 
 	/**
 	 * @license
