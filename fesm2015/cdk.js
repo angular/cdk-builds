307dd432a31ff222adbf50c64aefbf0646bd86cd
nielsr98 <54564567+nielsr98@users.noreply.github.com>
2020-06-17 22:40:38 +0000

    master - 042823109 feat(cdk-experimental/listbox): add file skeleton (#19612)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.0.0-rc.2-sha-d8c2d6380');
+const VERSION = new Version('10.0.0-rc.2-sha-042823109');
 
 /**
  * @license
