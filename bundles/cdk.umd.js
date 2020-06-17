933611767c3d3071fd1d5bebe803ecb080901b41
Vanessa Schmitt <vschmitt@google.com>
2020-06-17 22:44:55 +0000

    master - de1a36897 fix(material-experimental/mdc-chips) adjust margins (#19223)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.0-rc.2-sha-042823109');
+	var VERSION = new core.Version('10.0.0-rc.2-sha-de1a36897');
 
 	/**
 	 * @license
