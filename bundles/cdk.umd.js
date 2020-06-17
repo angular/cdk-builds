4d8d42801d6c0dfd9c956b755e89a386d65abac5
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-17 23:02:23 +0000

    master - 1b56ab568 build: update to latest Angular RC (#19658)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.0-rc.2-sha-3e3e2c5f8');
+	var VERSION = new core.Version('10.0.0-rc.2-sha-1b56ab568');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.0.0-rc.2-sha-3e3e2c5f8');
+const VERSION = new Version('10.0.0-rc.2-sha-1b56ab568');
 
 /**
  * @license
