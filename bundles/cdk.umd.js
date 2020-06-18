4d3959b776475adcaedbf178e78baf6b3f877e65
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-18 22:13:42 +0000

    master - a73089823 docs: fix syntax error in doc region (#19683)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.0-rc.2-sha-1dd57ffc9');
+	var VERSION = new core.Version('10.0.0-rc.2-sha-a73089823');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.0.0-rc.2-sha-1dd57ffc9');
+const VERSION = new Version('10.0.0-rc.2-sha-a73089823');
 
 /**
  * @license
