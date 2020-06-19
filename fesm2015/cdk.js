4e71613896b496284c6c9fbb25cf7519884c1b3a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-19 18:48:56 +0000

    10.0.x - 10888f393 fix(datepicker): not revalidating after value is changed through the calendar (#19695)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.0.0-rc.2-sha-613606de4');
+const VERSION = new Version('10.0.0-rc.2-sha-10888f393');
 
 /**
  * @license
