d05dd1a92e8fae024e333df755dd6fe86c546b56
mmalerba <mmalerba@google.com>
2020-06-19 20:54:42 +0000

    master - 166bb5248 feat(material-experimental/mdc-list): add MDC foundation for action/nav list (#19601)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.0-rc.2-sha-33aeea9a9');
+	var VERSION = new core.Version('10.0.0-rc.2-sha-166bb5248');
 
 	/**
 	 * @license
