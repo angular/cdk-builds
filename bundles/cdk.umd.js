240e270735f0e97c3623faf6d72ef9ddf0cde02e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-22 18:30:54 +0000

    10.0.x - 87a2d99bf build: fix syntax error in docs (#19699)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.0-rc.2-sha-10888f393');
+	var VERSION = new core.Version('10.0.0-rc.2-sha-87a2d99bf');
 
 	/**
 	 * @license
