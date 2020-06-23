cce6452e2093dd568903f061e29696e6e1d05a95
Jeremy Elbourn <jelbourn@google.com>
2020-06-23 21:58:13 +0000

    10.0.x - 6df4bc7c1 chore: change dev/test app codeowners (#19734)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.0-rc.3-sha-02a642b95');
+	var VERSION = new core.Version('10.0.0-rc.3-sha-6df4bc7c1');
 
 	/**
 	 * @license
