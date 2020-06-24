7c49601d9a3e6ca95a35e86e7c06587c9cc66913
Jeremy Elbourn <jelbourn@google.com>
2020-06-24 21:49:24 +0000

    master - f04482320 build: update to Angular 10.0.0 (#19747)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.0-rc.2-sha-f128a33b3');
+	var VERSION = new core.Version('10.0.0-rc.2-sha-f04482320');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.0.0-rc.2-sha-f128a33b3');
+const VERSION = new Version('10.0.0-rc.2-sha-f04482320');
 
 /**
  * @license
