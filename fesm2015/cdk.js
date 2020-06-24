6e5e9c33e4d1938787955c610baa2cdf983e655a
Jeremy Elbourn <jelbourn@google.com>
2020-06-24 21:54:08 +0000

    10.0.x - 3cad918b4 build: update to Angular 10.0.0 (#19747)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.0.0-rc.3-sha-e11bf9149');
+const VERSION = new Version('10.0.0-rc.3-sha-3cad918b4');
 
 /**
  * @license
