893a41641d60885b74b65b922d6899616f663567
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-06-24 21:20:15 +0000

    master - f128a33b3 test: fix schematic test infrastructure failing with Angular CLI v10 (#19749)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.0-rc.2-sha-e086544b9');
+	var VERSION = new core.Version('10.0.0-rc.2-sha-f128a33b3');
 
 	/**
 	 * @license
