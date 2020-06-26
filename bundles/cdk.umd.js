8f5e65e1a4d941913ad59f1757e8c4a21e5da67b
swseverance <sam.severance@merrillcorp.com>
2020-06-26 03:18:37 +0000

    master - 5d50ec7b9 docs(list): fix single selection example selection display (#19731)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.0-rc.2-sha-b68ae0de0');
+	var VERSION = new core.Version('10.0.0-rc.2-sha-5d50ec7b9');
 
 	/**
 	 * @license
