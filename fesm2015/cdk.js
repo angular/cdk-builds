dd54853b0745ea27bc509ef0cc412f1c52b070d0
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-26 03:38:17 +0000

    master - 8147cc956 docs(select): switch examples to fill appearance (#19708)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.0.0-rc.2-sha-ab74abec7');
+const VERSION = new Version('10.0.0-rc.2-sha-8147cc956');
 
 /**
  * @license
