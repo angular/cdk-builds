2c41f91a9b6f0bc188894f9e9a527b84191b52d4
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-06-26 03:12:09 +0000

    master - b68ae0de0 build: avoid conflicts when publishing snapshot builds (#19737)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.0-rc.2-sha-cc36af7d1');
+	var VERSION = new core.Version('10.0.0-rc.2-sha-b68ae0de0');
 
 	/**
 	 * @license
