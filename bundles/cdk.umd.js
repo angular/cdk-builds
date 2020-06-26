3708cdd2b9c0e91d00f77f7818ceb5b63cc2790a
Andy Chrzaszcz <andymchr@google.com>
2020-06-26 02:43:56 +0000

    master - 23e0249bb refactor(cdk-experimental/menu): change from submenu to menu naming (#19766)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.0-rc.2-sha-b3d6d92ea');
+	var VERSION = new core.Version('10.0.0-rc.2-sha-23e0249bb');
 
 	/**
 	 * @license
