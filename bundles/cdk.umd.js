f308659689c9455dee88a2d0ff30da61892a07be
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-26 03:34:49 +0000

    master - ab74abec7 build: some scripts not being linted (#19711)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.0-rc.2-sha-e3cf243ed');
+	var VERSION = new core.Version('10.0.0-rc.2-sha-ab74abec7');
 
 	/**
 	 * @license
