cdb30b5f15bedf2207684646c2a75ce1be1983e3
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-26 03:10:33 +0000

    master - cc36af7d1 feat(youtube-player): support passing in the playerVars parameter (#19746)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.0-rc.2-sha-970d15b88');
+	var VERSION = new core.Version('10.0.0-rc.2-sha-cc36af7d1');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.0.0-rc.2-sha-970d15b88');
+const VERSION = new Version('10.0.0-rc.2-sha-cc36af7d1');
 
 /**
  * @license
