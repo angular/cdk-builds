b251312d574d92d766c3f396e136ff124bc541b9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-29 16:06:01 +0000

    master - 2785719a8 refactor(cdk/schematics): remove circular dependency (#19787)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.1.0-next.0-sha-bc88cc1bb');
+const VERSION = new Version('10.1.0-next.0-sha-2785719a8');
 
 /**
  * @license
