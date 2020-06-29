7a4631870a68729c6f12b3d20722e2240cad538d
Andrew Seguin <andrewseguin@users.noreply.github.com>
2020-06-29 16:07:17 +0000

    master - a63bfc549 feat(material-experimental/snack-bar): add MDC-based snack-bar (#19738)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.1.0-next.0-sha-2785719a8');
+	var VERSION = new core.Version('10.1.0-next.0-sha-a63bfc549');
 
 	/**
 	 * @license
