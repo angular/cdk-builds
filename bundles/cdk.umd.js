7c864b31660e967565726926f99dd4b6024dd8db
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-29 15:50:01 +0000

    master - fcfc224c8 fix(datepicker): label state not being updated in some cases (#19761)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.1.0-next.0-sha-f4e18ac01');
+	var VERSION = new core.Version('10.1.0-next.0-sha-fcfc224c8');
 
 	/**
 	 * @license
