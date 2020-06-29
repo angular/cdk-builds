e5cbb3fd3e53daf3155a3cae0bd1a961b2e3ec5c
Jeremy Elbourn <jelbourn@google.com>
2020-06-29 15:48:06 +0000

    master - f4e18ac01 chore: bump master version to 10.1.0-next.0 (#19796)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.0-rc.2-sha-081d833bd');
+	var VERSION = new core.Version('10.1.0-next.0-sha-f4e18ac01');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.0.0-rc.2-sha-081d833bd');
+const VERSION = new Version('10.1.0-next.0-sha-f4e18ac01');
 
 /**
  * @license
