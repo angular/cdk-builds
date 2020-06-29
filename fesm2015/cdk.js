a8c2813f9201e0c1a656ea9fc1cde21d0f9436f9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-29 16:34:41 +0000

    master - 187bc98ac build: fix failing size tests (#19797)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.1.0-next.0-sha-f4f0d959e');
+const VERSION = new Version('10.1.0-next.0-sha-187bc98ac');
 
 /**
  * @license
