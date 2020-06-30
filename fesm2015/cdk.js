244a48e16554627a643214cb9cbbb91a8172326d
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-30 23:11:50 +0000

    master - b15fe6d1d refactor(datepicker): remove circular dependencies (#19629)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.1.0-next.0-sha-37e21710f');
+const VERSION = new Version('10.1.0-next.0-sha-b15fe6d1d');
 
 /**
  * @license
