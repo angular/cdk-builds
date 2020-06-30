4e977483e78a987b941ed23c05301038126d804f
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-30 23:10:27 +0000

    master - 37e21710f fix(datepicker): don't set aria-labelledby if form field does not have a label (#19639)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.1.0-next.0-sha-1319c0a29');
+	var VERSION = new core.Version('10.1.0-next.0-sha-37e21710f');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.1.0-next.0-sha-1319c0a29');
+const VERSION = new Version('10.1.0-next.0-sha-37e21710f');
 
 /**
  * @license
