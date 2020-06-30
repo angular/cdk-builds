0a88be8bacb1ff4d18146bb29bc387f1bce65eed
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-30 23:09:10 +0000

    master - 1319c0a29 fix(autocomplete): don't close when clicking inside custom origin (#19784)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.1.0-next.0-sha-0265c2fbc');
+	var VERSION = new core.Version('10.1.0-next.0-sha-1319c0a29');
 
 	/**
 	 * @license
