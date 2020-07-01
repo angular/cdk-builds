88d9157ebb95bd0bc0337c3ed6357fed3297b7bd
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-07-01 17:15:21 +0000

    master - cdfec0766 build: add custom linting for symbol names (#19811)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.1.0-next.0-sha-1106cb1ea');
+const VERSION = new Version('10.1.0-next.0-sha-cdfec0766');
 
 /**
  * @license
