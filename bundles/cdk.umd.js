cbeff9f859efb4c6cef26d18e40cfff6c4a813e2
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-07-08 17:00:54 +0000

    master - 6db1134be build: remove workaround for decorator metadata in universal-app (#19840)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.1.0-next.0-sha-a69cee142');
+	var VERSION = new core.Version('10.1.0-next.0-sha-6db1134be');
 
 	/**
 	 * @license
