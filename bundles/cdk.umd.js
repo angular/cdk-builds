d843e9e1fddbabc781c5ebf5e68d95da7bc5588d
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-07-08 20:11:54 +0000

    master - 19cbf8b44 build: set up caretaker note label in merge script (#19718)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.1.0-next.0-sha-d9b223879');
+	var VERSION = new core.Version('10.1.0-next.0-sha-19cbf8b44');
 
 	/**
 	 * @license
