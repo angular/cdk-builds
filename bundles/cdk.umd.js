6802e9aac70094f60883dc570d9216b519e0dbd5
Andrew Seguin <andrewseguin@users.noreply.github.com>
2020-07-10 00:11:49 +0000

    master - 5725e2bf9 test(material-experimental/mdc-form-field): remove e2e file (#19931)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.1.0-next.0-sha-dd8a68c3c');
+	var VERSION = new core.Version('10.1.0-next.0-sha-5725e2bf9');
 
 	/**
 	 * @license
