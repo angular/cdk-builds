06798758c39c9a31dd5ed46459a535531a88522d
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-07-10 00:22:56 +0000

    master - 36d4c5d59 refactor(popover-edit): remove circular reference (#19679)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.1.0-next.0-sha-e538e1409');
+const VERSION = new Version('10.1.0-next.0-sha-36d4c5d59');
 
 /**
  * @license
