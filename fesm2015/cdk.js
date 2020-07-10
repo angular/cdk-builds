e19e93fd2094e3571ebcc897b496a0443a941eef
Andrew Seguin <andrewseguin@users.noreply.github.com>
2020-07-10 00:11:21 +0000

    master - dd8a68c3c docs(material-experimental/mdc-chips): add README (#19891)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.1.0-next.0-sha-54c2d1676');
+const VERSION = new Version('10.1.0-next.0-sha-dd8a68c3c');
 
 /**
  * @license
