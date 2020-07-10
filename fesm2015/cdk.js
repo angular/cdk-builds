f73030fbc38a607eb6a7af0522009c08742fb023
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-07-10 00:18:32 +0000

    master - 912ad8dcf fix(material-experimental/mdc-snack-bar): error during server-side rendering (#19922)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.1.0-next.0-sha-a9e0ac564');
+const VERSION = new Version('10.1.0-next.0-sha-912ad8dcf');
 
 /**
  * @license
