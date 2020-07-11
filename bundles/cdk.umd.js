44f5cbb006314a6aad2bf4b6073da9533c2fdfbd
swseverance <sam.severance@merrillcorp.com>
2020-07-11 00:22:09 +0000

    master - 998bb18ab fix(material/list): use flexbox to size mat-line elements (#19809) (#19819)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.1.0-next.0-sha-0ddad0777');
+	var VERSION = new core.Version('10.1.0-next.0-sha-998bb18ab');
 
 	/**
 	 * @license
