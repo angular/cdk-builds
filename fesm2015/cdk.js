3ea6638a6f0d6db3934f413fc5e3bd91544c2541
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-07-14 22:16:43 +0000

    master - 223caaa89 test: fix failing MDC snackbar test (#19974)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.1.0-next.0-sha-7b789d40c');
+const VERSION = new Version('10.1.0-next.0-sha-223caaa89');
 
 /**
  * @license
