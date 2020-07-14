8facbec7fe4f68e090956e47e8c94107e65f9289
Karl Seamon <bannanafiend@gmail.com>
2020-07-14 22:30:38 +0000

    10.0.x - 7e9d6a344 perf(tooltip): Defer hooking up events until there's a message and the tooltip is not disabled (#19764)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.2-sha-be5ce28f6');
+	var VERSION = new core.Version('10.0.2-sha-7e9d6a344');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.0.2-sha-be5ce28f6');
+const VERSION = new Version('10.0.2-sha-7e9d6a344');
 
 /**
  * @license
