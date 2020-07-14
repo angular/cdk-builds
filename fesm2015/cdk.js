d730dd36bd7bd3b92b90576631a0ac5bc7371bb4
mmalerba <mmalerba@google.com>
2020-07-14 18:05:27 +0000

    10.0.x - ba5c2feb0 chore: bump version to 10.0.2 w/ changelog (#19972)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.0.1-sha-3e9b77fbd');
+const VERSION = new Version('10.0.2-sha-ba5c2feb0');
 
 /**
  * @license
