079d0924fabc855251f8495dfa35d14405fd3f7b
Andrew Seguin <andrewseguin@users.noreply.github.com>
2020-07-17 19:24:42 +0000

    10.0.x - 40b0d9b8f fix(material-experimental/chips): fix empty check when no chips (#20025)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.2-sha-7e9d6a344');
+	var VERSION = new core.Version('10.0.2-sha-40b0d9b8f');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.0.2-sha-7e9d6a344');
+const VERSION = new Version('10.0.2-sha-40b0d9b8f');
 
 /**
  * @license
