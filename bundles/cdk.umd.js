70c7fc324a312a3cfc7b7c54db92f2a1bc9a4064
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-07-17 20:02:22 +0000

    master - 23170afae test(material-experimental/mdc-slider): try to reduce test flakes (#19987)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.1.0-next.0-sha-ab91e7cf8');
+	var VERSION = new core.Version('10.1.0-next.0-sha-23170afae');
 
 	/**
 	 * @license
