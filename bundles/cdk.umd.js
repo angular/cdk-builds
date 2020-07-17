3c8d2af57f97ffbb652ac55f44adc98f1013b0e9
Andy Chrzaszcz <andymchr@google.com>
2020-07-17 19:59:24 +0000

    master - 69661f900 refactor(cdk-experimental/menu) use typing  to expose a subject as observable and skip method call (#19978)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.1.0-next.0-sha-9e97a34cf');
+	var VERSION = new core.Version('10.1.0-next.0-sha-69661f900');
 
 	/**
 	 * @license
