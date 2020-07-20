a6a9084683b2ec3bf2e16b674b6dde8ed18823f6
Scott Wang <curiosity142000@gmail.com>
2020-07-20 14:34:37 +0000

    10.0.x - 8ae68190d perf(int-bar): Convert MatInkBarFoundation adapter to class object (#19986)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.0.2-sha-5615ef56e');
+	var VERSION = new core.Version('10.0.2-sha-8ae68190d');
 
 	/**
 	 * @license
