6308d4ada7c8aee970084cb96dcf45c612801462
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-07-20 22:54:18 +0000

    master - 0777c44c0 fix(material-experimental/mdc-tabs): missing focus indication in high contrast mode (#17601)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.1.0-sha-e6d9c1bd2');
+	var VERSION = new core.Version('10.1.0-sha-0777c44c0');
 
 	/**
 	 * @license
