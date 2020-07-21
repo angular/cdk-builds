2ae13cfaf4d1263bb81bd0d6509809213294dd73
Andy Chrzaszcz <andymchr@google.com>
2020-07-21 16:45:18 +0000

    master - 7303780cc fix(cdk-experimental/menu): fix bug preventing keyboard event handling if opened programmatically (#20004)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.1.0-sha-add6ad195');
+	var VERSION = new core.Version('10.1.0-sha-7303780cc');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.1.0-sha-add6ad195');
+const VERSION = new Version('10.1.0-sha-7303780cc');
 
 /**
  * @license
