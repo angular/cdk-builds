9ec4e124fc8c7586459d392cb644ea5af58998e2
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-07-21 19:07:52 +0000

    10.1.x - 48a226b84 build: update to latest MDC canary and disable slider (#20054)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.1.0-sha-d7954d67f');
+const VERSION = new Version('10.1.0-sha-48a226b84');
 
 /**
  * @license
