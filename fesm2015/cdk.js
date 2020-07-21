275d551bc39f494b826ccd585c03ffe965725edf
Wagner Maciel <wagnermaciel@google.com>
2020-07-21 19:32:33 +0000

    master - 2240b4496 test(mdc-card): add performance tests for mdc-card (#20016)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.1.0-sha-93054e465');
+const VERSION = new Version('10.1.0-sha-2240b4496');
 
 /**
  * @license
