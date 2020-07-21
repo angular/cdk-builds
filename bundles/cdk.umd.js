2a7cae8cabac2b899875144287553bf20fcbbb62
Andy Chrzaszcz <andymchr@google.com>
2020-07-21 23:31:51 +0000

    master - ba441d44f refactor(cdk-experimental/menu): refactor MenuStack to ensure submenus are popped off and closed (#20006)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.1.0-sha-bbce2de12');
+	var VERSION = new core.Version('10.1.0-sha-ba441d44f');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.1.0-sha-bbce2de12');
+const VERSION = new Version('10.1.0-sha-ba441d44f');
 
 /**
  * @license
