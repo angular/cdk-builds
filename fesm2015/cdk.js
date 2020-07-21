93c96387c70d56d6ba509a8e3e8cf3f0a3273642
Wagner Maciel <wagnermaciel@google.com>
2020-07-21 04:09:00 +0000

    10.0.x - c9aeb3086 Revert "perf(int-bar): Convert MatInkBarFoundation adapter to class object (#19986)" (#20051)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.0.2-sha-a04c660ee');
+const VERSION = new Version('10.0.2-sha-c9aeb3086');
 
 /**
  * @license
