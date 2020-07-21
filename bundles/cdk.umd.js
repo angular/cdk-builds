dca28a41c305141cf4b49536f8d0e37ec243d898
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-07-21 19:28:25 +0000

    master - dc245d206 fix(datepicker): emitting dateChange event when opposite range input changes (#19995)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.1.0-sha-0bd626d53');
+	var VERSION = new core.Version('10.1.0-sha-dc245d206');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.1.0-sha-0bd626d53');
+const VERSION = new Version('10.1.0-sha-dc245d206');
 
 /**
  * @license
