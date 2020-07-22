92ec767ab8690ca8c1b52128c245d45a7fa3b69f
Michael Prentice <splaktar@gmail.com>
2020-07-22 22:48:23 +0000

    10.1.x - 40d0b2294 docs(material/table): fix example overflowing docs site width (#20063)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.1.0-sha-310e0b0b0');
+	var VERSION = new core.Version('10.1.0-sha-40d0b2294');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.1.0-sha-310e0b0b0');
+const VERSION = new Version('10.1.0-sha-40d0b2294');
 
 /**
  * @license
