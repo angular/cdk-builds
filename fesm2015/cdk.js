8146108ee54abd785a99ca58200db6cd2feaed6f
Nikhil Walvekar <walvekar@gmail.com>
2020-07-22 22:52:21 +0000

    master - 5f76afa7a docs(stepper): Make CDK stepper example accessible in dark mode. (#20013)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.1.0-sha-e7fe772a7');
+const VERSION = new Version('10.1.0-sha-5f76afa7a');
 
 /**
  * @license
