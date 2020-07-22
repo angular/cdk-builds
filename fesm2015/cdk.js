915cd29dfdd731fa947b5e82eabee66520ed3449
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-07-22 22:49:46 +0000

    master - 33b31b149 build: snapshot builds incorrectly modify semver versions (#20053)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.1.0-sha-092b1515f');
+const VERSION = new Version('10.1.0-sha-33b31b149');
 
 /**
  * @license
