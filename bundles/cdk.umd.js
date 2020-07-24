5ca08e26a66ecf4aeca43b0e37360ca935db0089
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-07-24 02:13:44 +0000

    master - 1b7ba0bf0 fix(menu): throw error if menu contains its own trigger (#19943)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.1.0-sha-42448c7e0');
+	var VERSION = new core.Version('10.1.0-sha-1b7ba0bf0');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.1.0-sha-42448c7e0');
+const VERSION = new Version('10.1.0-sha-1b7ba0bf0');
 
 /**
  * @license
