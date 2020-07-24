3716b0204e730e956d0c9c723ec07da0f85f67aa
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-07-24 02:20:23 +0000

    master - 89b5fa89d fix(drag-drop): warn if connected container ID doesn't exist (#20057)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.1.0-sha-316141ff0');
+	var VERSION = new core.Version('10.1.0-sha-89b5fa89d');
 
 	/**
 	 * @license
