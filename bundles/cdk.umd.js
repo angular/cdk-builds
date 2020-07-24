744339e215443371cba283ab49a82ec49297f6be
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-07-24 02:12:28 +0000

    10.1.x - 16e8376bd fix(menu): throw error if menu contains its own trigger (#19943)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.1.0-sha-ae75fab6e');
+	var VERSION = new core.Version('10.1.0-sha-16e8376bd');
 
 	/**
 	 * @license
