99162788b67283cbf6434c2cd9e23beb8c88c050
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-07-27 19:27:38 +0000

    master - 90f4d0e6a fix(google-maps): avoid re-initializing info window for same marker (#19299)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.1.0-sha-2b0072582');
+	var VERSION = new core.Version('10.1.0-sha-90f4d0e6a');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.1.0-sha-2b0072582');
+const VERSION = new Version('10.1.0-sha-90f4d0e6a');
 
 /**
  * @license
