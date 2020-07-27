a247a45e768af0983f5f2132eec90ad31661a8b6
Nikhil Walvekar <walvekar@gmail.com>
2020-07-27 18:20:16 +0000

    master - e6fd32547 docs(material/progress-bar) : improve wording for buffer & query modes (#20093)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.1.0-sha-05d07c7b8');
+	var VERSION = new core.Version('10.1.0-sha-e6fd32547');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.1.0-sha-05d07c7b8');
+const VERSION = new Version('10.1.0-sha-e6fd32547');
 
 /**
  * @license
