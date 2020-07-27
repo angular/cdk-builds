62c6e92a880acebcc2e741b430c724e998cdfc0c
Nikhil Walvekar <walvekar@gmail.com>
2020-07-27 18:20:16 +0000

    10.1.x - cd2f2e00c docs(material/progress-bar) : improve wording for buffer & query modes (#20093)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.1.0-sha-4fa9eb9d2');
+const VERSION = new Version('10.1.0-sha-cd2f2e00c');
 
 /**
  * @license
