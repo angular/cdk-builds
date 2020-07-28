773e328bbde4cde48c26496458080eeb62898cab
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-07-28 14:32:46 +0000

    master - 165622ee1 feat(chips): add test harness (#20028)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-next.0-sha-a6f1a3399');
+const VERSION = new Version('10.2.0-next.0-sha-165622ee1');
 
 /**
  * @license
