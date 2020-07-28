ffadcae8c6a9762f014dd1510898759e8fda5c91
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-07-28 14:31:24 +0000

    master - a6f1a3399 fix(clipboard): scroll position changing while copying on some browsers (#20073)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.2.0-next.0-sha-3dce8410d');
+	var VERSION = new core.Version('10.2.0-next.0-sha-a6f1a3399');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-next.0-sha-3dce8410d');
+const VERSION = new Version('10.2.0-next.0-sha-a6f1a3399');
 
 /**
  * @license
