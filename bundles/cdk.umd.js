810c56312a9ebe51df1279785e2528f0ca98a927
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-07-29 15:24:27 +0000

    10.1.x - 57600a5bb build: fix lint failure in MDC option (#20124)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.1.1-sha-3d0f82775');
+	var VERSION = new core.Version('10.1.1-sha-57600a5bb');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.1.1-sha-3d0f82775');
+const VERSION = new Version('10.1.1-sha-57600a5bb');
 
 /**
  * @license
