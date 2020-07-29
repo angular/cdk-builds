9ad2b131e491396ad07d40df6a133773a8c4607a
Andy Chrzaszcz <andymchr@google.com>
2020-07-29 19:57:08 +0000

    master - cdbf2c181 feat(cdk-experimental/menu): add the ability to open/close menus on mouse click and hover (#20118)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.2.0-next.0-sha-c5ec29fd1');
+	var VERSION = new core.Version('10.2.0-next.0-sha-cdbf2c181');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-next.0-sha-c5ec29fd1');
+const VERSION = new Version('10.2.0-next.0-sha-cdbf2c181');
 
 /**
  * @license
