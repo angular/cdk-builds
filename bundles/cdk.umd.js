bf248d280335cb73bb0c399fe7f30c723d0d8370
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-07-29 14:39:06 +0000

    master - 23818f544 feat(form-field): expose label content element id for custom controls (#18528)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.2.0-next.0-sha-7b03d4861');
+	var VERSION = new core.Version('10.2.0-next.0-sha-23818f544');
 
 	/**
 	 * @license
