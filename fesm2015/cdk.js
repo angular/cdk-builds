74a74cbb396589578655d3ee2d4ae1ef1a2ec585
swseverance <sam.severance@merrillcorp.com>
2020-07-29 14:32:45 +0000

    master - f0e56f01a feat(material/datepicker): add `getValidDateOrNull` method (#19915)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-next.0-sha-77b11f47c');
+const VERSION = new Version('10.2.0-next.0-sha-f0e56f01a');
 
 /**
  * @license
