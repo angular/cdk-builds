9a53463ba5460433b8c49e17e043b24bd2e7a6a4
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-07-29 14:36:04 +0000

    master - d1607cbb8 feat(material-experimental): add MDC-based mat-option and mdc-core entry point (#19557)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.2.0-next.0-sha-f0e56f01a');
+	var VERSION = new core.Version('10.2.0-next.0-sha-d1607cbb8');
 
 	/**
 	 * @license
