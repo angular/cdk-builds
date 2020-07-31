c8cdba4f2e59fd13646b4a2d8656cad8116e4891
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-07-31 22:57:55 +0000

    10.1.x - fdcf67534 docs(tabs): fix incorrect file reference (#20142)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.1.1-sha-57600a5bb');
+	var VERSION = new core.Version('10.1.1-sha-fdcf67534');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.1.1-sha-57600a5bb');
+const VERSION = new Version('10.1.1-sha-fdcf67534');
 
 /**
  * @license
