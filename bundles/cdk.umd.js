d2b2d83fecbe5b70bb8e1e783d50969983e365c4
Annie Wang <annieyw@google.com>
2020-07-31 23:36:05 +0000

    10.1.x - f80c7d523 prototype(progress-spinner): create prototype progress-spinner based on MDC Web (#20048)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.1.1-sha-fdcf67534');
+	var VERSION = new core.Version('10.1.1-sha-f80c7d523');
 
 	/**
 	 * @license
