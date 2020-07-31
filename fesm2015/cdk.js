8672dc163476c1bca668f0c9191fc50cfab34f1b
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-07-31 23:00:25 +0000

    master - 338c02ff3 build: add server-side rendering check for kml layer (#20147)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-next.0-sha-6470ef3b3');
+const VERSION = new Version('10.2.0-next.0-sha-338c02ff3');
 
 /**
  * @license
