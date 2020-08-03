3939f4c40ad86e357bfe65bcb8411e92559b2562
Jeremy Elbourn <jelbourn@google.com>
2020-08-03 20:06:06 +0000

    10.1.x - 7b0bb2c40 chore: bump version to 10.1.2 w/ changelog (#20168)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.1.1-sha-f80c7d523');
+	var VERSION = new core.Version('10.1.2-sha-7b0bb2c40');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.1.1-sha-f80c7d523');
+const VERSION = new Version('10.1.2-sha-7b0bb2c40');
 
 /**
  * @license
