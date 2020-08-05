40c14b050ceeee9843dd61655b1ff157231b6200
Andy Chrzaszcz <andymchr@google.com>
2020-08-05 18:23:26 +0000

    master - 5412fb139 refactor(cdk-experimental/menu): move listeners from host object to host listener decorator (#20164)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-next.0-sha-0398d933a');
+const VERSION = new Version('10.2.0-next.0-sha-5412fb139');
 
 /**
  * @license
