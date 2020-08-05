f8faa782f842ade26c9e4f819be31c02ccab1387
Andy Chrzaszcz <andymchr@google.com>
2020-08-05 20:30:05 +0000

    master - 8b6808379 feat(cdk-experimental/menu): add support for inline menus (#20143)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-next.0-sha-afa29cffc');
+const VERSION = new Version('10.2.0-next.0-sha-8b6808379');
 
 /**
  * @license
