418aa72ee261f5863f59d23016df21ae9d4d72d9
Andy Chrzaszcz <andymchr@google.com>
2020-08-05 18:19:20 +0000

    master - ecb6e3845 fix(cdk-experimental/menu): re-open menu on hover after click to close on trigger (#20173)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.2.0-next.0-sha-5ef49d8a9');
+	var VERSION = new core.Version('10.2.0-next.0-sha-ecb6e3845');
 
 	/**
 	 * @license
