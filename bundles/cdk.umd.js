d3b2929f33b34a8c39559af6e30168a065130e94
Karl Seamon <bannanafiend@gmail.com>
2020-08-06 17:21:03 +0000

    master - 1b1c31e91 perf(table): Slightly improve speed of adding/remvoing sticky styles (#19823)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.2.0-next.0-sha-aeb6f8930');
+	var VERSION = new core.Version('10.2.0-next.0-sha-1b1c31e91');
 
 	/**
 	 * @license
