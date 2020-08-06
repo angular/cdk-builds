87287d4014b8cfa56e0f984fc3363ebac141464d
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-08-06 17:12:31 +0000

    10.1.x - 7900eb0be build: fix compilation error with latest MDC canary (#20208)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.1.2-sha-18e8a6389');
+const VERSION = new Version('10.1.2-sha-7900eb0be');
 
 /**
  * @license
