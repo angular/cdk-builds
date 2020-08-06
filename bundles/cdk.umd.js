b599fc5306aa187e6e59ced79ee25c285646fcd8
Wagner Maciel <wagnermaciel@google.com>
2020-08-06 17:26:35 +0000

    master - cd433a41f test(mdc-chips): add performance tests for mdc-chips (#20131)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.2.0-next.0-sha-d2b499da9');
+	var VERSION = new core.Version('10.2.0-next.0-sha-cd433a41f');
 
 	/**
 	 * @license
