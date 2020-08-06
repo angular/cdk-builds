e34115e626dfb9e45cc91eb244a1366459eb3936
Wagner Maciel <wagnermaciel@google.com>
2020-08-06 17:29:27 +0000

    master - e83dbb11c test(mdc-slide-toggle): add performance tests for mdc-slide-toggle (#20133)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.2.0-next.0-sha-1e8c31301');
+	var VERSION = new core.Version('10.2.0-next.0-sha-e83dbb11c');
 
 	/**
 	 * @license
