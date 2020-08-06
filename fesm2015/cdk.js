8d75bebe08195974ccf1797a027296c141f6b20c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-08-06 17:21:59 +0000

    master - df7156e06 fix(stepper): remove tabindex from content (#16809)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-next.0-sha-1b1c31e91');
+const VERSION = new Version('10.2.0-next.0-sha-df7156e06');
 
 /**
  * @license
