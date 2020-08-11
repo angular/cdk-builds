eb3f57e3357388c2313f6883b24b7f1b499c8fdb
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-08-11 13:38:03 +0000

    master - 098be5f0e doc(stepper): add missing doc region (#20264)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.2.0-next.0-sha-25ce323fb');
+	var VERSION = new core.Version('10.2.0-next.0-sha-098be5f0e');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-next.0-sha-25ce323fb');
+const VERSION = new Version('10.2.0-next.0-sha-098be5f0e');
 
 /**
  * @license
