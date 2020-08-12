bf9531376bfff7c16a02c0c812f41e28aa8a3c56
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-08-12 03:41:41 +0000

    master - 0433cd9e5 fix(datepicker): not showing single-day comparison ranges (#20102)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-next.0-sha-098be5f0e');
+const VERSION = new Version('10.2.0-next.0-sha-0433cd9e5');
 
 /**
  * @license
