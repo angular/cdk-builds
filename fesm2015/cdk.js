ec69100c64f0cc75221da8277986aa86a7c2f283
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-08-12 04:41:52 +0000

    master - 53c9441b8 build: create script for building docs-content (#20198)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-next.0-sha-f6e82cc37');
+const VERSION = new Version('10.2.0-next.0-sha-53c9441b8');
 
 /**
  * @license
