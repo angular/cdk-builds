9c8853fd64dcd28186cee38cab50fe086a053a11
Eric NICOLAS <ccjmne@gmail.com>
2020-08-12 04:46:50 +0000

    10.1.x - 095069b87 Fix extraneous & missing words in datepicker docs (#20244)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.1.3-sha-17fc3bdb7');
+	var VERSION = new core.Version('10.1.3-sha-095069b87');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.1.3-sha-17fc3bdb7');
+const VERSION = new Version('10.1.3-sha-095069b87');
 
 /**
  * @license
