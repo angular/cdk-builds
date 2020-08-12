5f2901ee62b2425b1703d390389c5d5064599f84
Karl Seamon <bannanafiend@gmail.com>
2020-08-12 04:25:05 +0000

    master - 3ce445264 perf(column-resize): Further defer initial min/max column size application to reduce layout thrashing (#20238)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.2.0-next.0-sha-c7a7b1e5f');
+	var VERSION = new core.Version('10.2.0-next.0-sha-3ce445264');
 
 	/**
 	 * @license
