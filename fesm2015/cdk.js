b481d14f8a5ad4128645d7ad683920dd46b59b53
Andy Chrzaszcz <andymchr@google.com>
2020-08-12 04:30:43 +0000

    master - b4309e501 refactor(cdk-experimental/menu): refactor the dev-app to include context menu and inline menu (#20228)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-next.0-sha-c79dc6fc4');
+const VERSION = new Version('10.2.0-next.0-sha-b4309e501');
 
 /**
  * @license
