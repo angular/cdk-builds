241840422a4559c364ce61368318432850959114
Wagner Maciel <wagnermaciel@google.com>
2020-08-12 04:44:05 +0000

    master - 4317a4b42 refactor(table-benchmark): move template to a separate html file (#19977)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-next.0-sha-5c988fcfa');
+const VERSION = new Version('10.2.0-next.0-sha-4317a4b42');
 
 /**
  * @license
