ca208954c1c0b0b57dba0c4600886919b0f3a7d1
crisbeto <crisbeto@abv.bg>
2020-08-12 13:59:33 +0000

    master - cb8de6195 test(cdk-experimental/menu): fix test failure

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.2.0-next.0-sha-3828b7e2e');
+	var VERSION = new core.Version('10.2.0-next.0-sha-cb8de6195');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-next.0-sha-3828b7e2e');
+const VERSION = new Version('10.2.0-next.0-sha-cb8de6195');
 
 /**
  * @license
