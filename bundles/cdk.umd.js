b4bff1fbdd723b943d195acc54088758c80d54e4
Michael Prentice <splaktar@gmail.com>
2020-08-12 04:46:57 +0000

    master - 0665d863b chore: store dev-app dark/light theme state in localStorage (#18218)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.2.0-next.0-sha-094a49cf6');
+	var VERSION = new core.Version('10.2.0-next.0-sha-0665d863b');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-next.0-sha-094a49cf6');
+const VERSION = new Version('10.2.0-next.0-sha-0665d863b');
 
 /**
  * @license
