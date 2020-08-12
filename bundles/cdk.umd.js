ad66f7c782fef9e2db0b6eb684f053a350a23eb4
Andy Chrzaszcz <andymchr@google.com>
2020-08-12 04:23:39 +0000

    master - c0499b80b fix(cdk-experimental/menu): fix double state toggle in radio and checkbox menu items (#20259)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.2.0-next.0-sha-253d78fe7');
+	var VERSION = new core.Version('10.2.0-next.0-sha-c0499b80b');
 
 	/**
 	 * @license
