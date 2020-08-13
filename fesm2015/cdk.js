6a77ace9183ad5c8bc25e87da6f8b1bdee595f3e
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-08-13 19:03:17 +0000

    master - c063fbc4a build: use new labeling and branching in merge script (#20295)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-next.0-sha-c4b760483');
+const VERSION = new Version('10.2.0-next.0-sha-c063fbc4a');
 
 /**
  * @license
