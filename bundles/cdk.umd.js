653e5df6b61c1bd04748cfce3b9e635e83953ff5
Wagner Maciel <wagnermaciel@google.com>
2020-08-13 19:10:33 +0000

    master - df516c521 test(mdc-checkbox): add performance tests for mdc-checkbox (#20129)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.2.0-next.0-sha-68b43740b');
+	var VERSION = new core.Version('10.2.0-next.0-sha-df516c521');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-next.0-sha-68b43740b');
+const VERSION = new Version('10.2.0-next.0-sha-df516c521');
 
 /**
  * @license
