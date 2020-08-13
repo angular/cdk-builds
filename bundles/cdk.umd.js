8a39503e020677073de01de6b1820e883159c5bb
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-08-13 18:58:05 +0000

    master - 199f72803 fix(dialog): backdrop not detaching if container view is destroyed (#20232)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.2.0-next.0-sha-c8f03c760');
+	var VERSION = new core.Version('10.2.0-next.0-sha-199f72803');
 
 	/**
 	 * @license
