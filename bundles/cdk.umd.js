769f36b69784c6d64ab403a21db1ab5edab8682f
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-08-13 20:11:46 +0000

    10.1.x - c4dbfb4e4 perf(icon): avoid unnecessarily parsing icon sets (#18654)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.1.3-sha-f4da874d9');
+	var VERSION = new core.Version('10.1.3-sha-c4dbfb4e4');
 
 	/**
 	 * @license
