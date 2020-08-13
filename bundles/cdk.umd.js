21bebb326f74cff926ebb666d40c51d6bb4445c8
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-08-13 19:07:47 +0000

    master - 68b43740b feat(material-experimental/mdc-radio): add test harness (#20283)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.2.0-next.0-sha-b2d11a6f2');
+	var VERSION = new core.Version('10.2.0-next.0-sha-68b43740b');
 
 	/**
 	 * @license
