c7e34fa1647e6cbc016785bd4623b3c98450391d
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-08-13 19:04:46 +0000

    master - 5f87552d1 build: do not prompt on postinstall in bazel yarn install (#20294)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.2.0-next.0-sha-c063fbc4a');
+	var VERSION = new core.Version('10.2.0-next.0-sha-5f87552d1');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-next.0-sha-c063fbc4a');
+const VERSION = new Version('10.2.0-next.0-sha-5f87552d1');
 
 /**
  * @license
