8fa2f984a80d43554245f55e5a0cf221c898f53d
Andy Chrzaszcz <andymchr@google.com>
2020-08-14 04:04:55 +0000

    master - fd2976f6a feat(material-experimental/menubar): extends components from cdk menu equivalents (#20301)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-next.0-sha-ebb39988e');
+const VERSION = new Version('10.2.0-next.0-sha-fd2976f6a');
 
 /**
  * @license
