8678baeda230a4d22bcf4833c34a7095c5ac8b05
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-08-14 04:06:13 +0000

    master - b69a75b4d feat(material-experimental/mdc-card): add test harness (#20298)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.2.0-next.0-sha-fd2976f6a');
+	var VERSION = new core.Version('10.2.0-next.0-sha-b69a75b4d');
 
 	/**
 	 * @license
