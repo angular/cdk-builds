b4add5cd1a6fadb5ae5166765eb8741cf0c9df6c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-08-14 15:24:02 +0000

    10.1.x - d79ff1dbf fix(slider): don't emit change events on mousedown (#20240)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.1.3-sha-c4dbfb4e4');
+const VERSION = new Version('10.1.3-sha-d79ff1dbf');
 
 /**
  * @license
