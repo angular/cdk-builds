0acaba7b4e512fcec52c37f5f7e6c4703d9d1511
Yifan Ge <yifange@google.com>
2020-08-14 04:03:39 +0000

    master - ebb39988e feat(cdk-experimental/selection): Merge cdk–selection to the master (#20229)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.2.0-next.0-sha-5526ab9d0');
+	var VERSION = new core.Version('10.2.0-next.0-sha-ebb39988e');
 
 	/**
 	 * @license
