2ec37965d7c05488318a88d68f5a48f80f9d9d8a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-08-18 01:01:19 +0000

    master - 3854b664f feat(material-experimental/mdc-tabs): add test harnesses (#20322)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-next.0-sha-c223fa77b');
+const VERSION = new Version('10.2.0-next.0-sha-3854b664f');
 
 /**
  * @license
