284340e3cc87b643a61a206526913a64d860ed45
Andy Chrzaszcz <andymchr@google.com>
2020-08-18 01:05:24 +0000

    master - 69481861f fix(cdk-experimental/menu): do not allow two separate triggers to open the same menu (#20300)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-next.0-sha-3396b4b42');
+const VERSION = new Version('10.2.0-next.0-sha-69481861f');
 
 /**
  * @license
