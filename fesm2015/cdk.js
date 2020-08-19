34f69e4ae5d1402fd62b44586ff5da7addae9d93
Rafael Santana <rafaelsantana3095@gmail.com>
2020-08-19 01:18:15 +0000

    master - 490a8939f fix(datepicker): add coercion for opened input (#20250)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-next.0-sha-2c47b068c');
+const VERSION = new Version('10.2.0-next.0-sha-490a8939f');
 
 /**
  * @license
