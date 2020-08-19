b8b5ddbb5d92981809194e6ed83bc1618d15cf6c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-08-19 01:22:09 +0000

    master - 2683b3dae fix(datepicker): support dateClass on year and multi-year views (#20256)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.2.0-next.0-sha-e3699dc3c');
+	var VERSION = new core.Version('10.2.0-next.0-sha-2683b3dae');
 
 	/**
 	 * @license
