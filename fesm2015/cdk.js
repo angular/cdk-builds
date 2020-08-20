0a824e36351cda4fd08e1f9c887b6335eb79427a
swseverance <sam.severance@merrillcorp.com>
2020-08-20 02:25:28 +0000

    master - f9c5ffe41 fix(list): determine if option value changed (#19828) (#19870)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-next.0-sha-134cbf3d5');
+const VERSION = new Version('10.2.0-next.0-sha-f9c5ffe41');
 
 /**
  * @license
