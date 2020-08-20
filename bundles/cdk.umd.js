42bd00c1077a83b00f616e6f08c359ab94057279
Alan Agius <alan.agius4@gmail.com>
2020-08-20 18:50:38 +0000

    master - bbf75f3c7 tests: remove unsupported TS integration tests (#20359)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.2.0-next.0-sha-06a9843ef');
+	var VERSION = new core.Version('10.2.0-next.0-sha-bbf75f3c7');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-next.0-sha-06a9843ef');
+const VERSION = new Version('10.2.0-next.0-sha-bbf75f3c7');
 
 /**
  * @license
