c21e08d065f3fc76772cbfe952166fd10ad386e8
Andrew Seguin <andrewseguin@users.noreply.github.com>
2020-08-20 18:46:39 +0000

    master - 55f788200 fix(material-experimental/mdc-table): reset table layout (#20364)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-next.0-sha-f9c5ffe41');
+const VERSION = new Version('10.2.0-next.0-sha-55f788200');
 
 /**
  * @license
