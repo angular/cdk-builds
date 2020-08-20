6e5bbbea4b9093aeb83187e827024ba83fa1a565
Duncan <info@e-ips.com>
2020-08-20 18:52:00 +0000

    master - ac2d385de docs(material/form-field): updated documentation (#20355)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.2.0-next.0-sha-bbf75f3c7');
+	var VERSION = new core.Version('10.2.0-next.0-sha-ac2d385de');
 
 	/**
 	 * @license
