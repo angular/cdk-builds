0b28ebd924a7b922a7d21756752ddd219264d991
swseverance <sam.severance@merrillcorp.com>
2020-08-20 02:19:33 +0000

    10.1.x - c65458945 fix(list): determine if option value changed (#19828) (#19870)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.1.3-sha-89b05a863');
+	var VERSION = new core.Version('10.1.3-sha-c65458945');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.1.3-sha-89b05a863');
+const VERSION = new Version('10.1.3-sha-c65458945');
 
 /**
  * @license
