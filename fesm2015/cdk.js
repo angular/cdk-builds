6e6b2136bac514192557d9fde8732d8f318d69c4
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-08-20 18:54:53 +0000

    master - e784f8f2f fix(material-experimental/mdc-menu/testing): add unimplemented methods (#20352)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-next.0-sha-9a4adb7bb');
+const VERSION = new Version('10.2.0-next.0-sha-e784f8f2f');
 
 /**
  * @license
