b8d4932a41d1c1e4a72cf4a69ec785c2b680e180
Andrew Seguin <andrewseguin@users.noreply.github.com>
2020-08-20 02:22:32 +0000

    master - 8fb560ff1 fix(material-experimental/mdc-menu): apply typography to button menu items (#20169)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.2.0-next.0-sha-75f73aedb');
+	var VERSION = new core.Version('10.2.0-next.0-sha-8fb560ff1');
 
 	/**
 	 * @license
