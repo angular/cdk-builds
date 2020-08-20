48b88872f119f785b81c1de7322e19e960c3c2a5
Rafael Santana <rafaelsantana3095@gmail.com>
2020-08-20 18:56:14 +0000

    master - f64b60d20 docs(expansion-panel): fix duplicate selectors and add missing documentation (#20286)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.2.0-next.0-sha-e784f8f2f');
+	var VERSION = new core.Version('10.2.0-next.0-sha-f64b60d20');
 
 	/**
 	 * @license
