3aa10e85527afcfa7fe56135cc2f13051cb480a7
mbehrlich <mehrlich@google.com>
2020-08-21 02:25:35 +0000

    master - 3345a9a39 feat(google-maps): Add Layer components. (#19604)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-next.0-sha-7d511ba4a');
+const VERSION = new Version('10.2.0-next.0-sha-3345a9a39');
 
 /**
  * @license
