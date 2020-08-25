15bbd72371c9359cc0e782ab76f894cf869501e3
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-08-25 17:16:14 +0000

    master - 6fbf142ac fix(select): use combobox pattern for accessibility (#20082)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-next.0-sha-f0c7a25e0');
+const VERSION = new Version('10.2.0-next.0-sha-6fbf142ac');
 
 /**
  * @license
