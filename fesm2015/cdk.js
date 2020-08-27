f90745d8452e10ef40eb33ec05552d60b7948997
Jeremy Elbourn <jelbourn@google.com>
2020-08-27 00:24:01 +0000

    master - 9094de847 chore: bump version to 10.2.0-rc.0 w/ changelog (#20430)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-next.0-sha-7a2a7327a');
+const VERSION = new Version('10.2.0-rc.0-sha-9094de847');
 
 /**
  * @license
