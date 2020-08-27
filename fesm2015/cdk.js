b88cc313eb495e949f0b5c63d26a4ca791651a4f
Jeremy Elbourn <jelbourn@google.com>
2020-08-27 19:16:53 +0000

    master - 1cc520c8c chore: bump master to 11.0.0-next.0 (#20436)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-rc.0-sha-de8fd2166');
+const VERSION = new Version('11.0.0-next.0-sha-1cc520c8c');
 
 /**
  * @license
