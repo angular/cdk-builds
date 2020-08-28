de7bb0df67be6f1a8dd03ecb7d6c6d7e55ec45dd
mmalerba <mmalerba@google.com>
2020-08-28 05:29:25 +0000

    master - abfb7a293 fix(button): don't use divs in button template (#20376)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-next.0-sha-fd34f35bd');
+	var VERSION = new core.Version('11.0.0-next.0-sha-abfb7a293');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.0-sha-fd34f35bd');
+const VERSION = new Version('11.0.0-next.0-sha-abfb7a293');
 
 /**
  * @license
