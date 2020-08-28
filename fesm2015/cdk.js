71deb8d28eb42e277bcf057f5ffcec9bdd16b583
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-08-28 05:25:00 +0000

    master - eb17f3724 build(google-maps): add server-side rendering checks for new layers (#20386)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.0-sha-f2953dd41');
+const VERSION = new Version('11.0.0-next.0-sha-eb17f3724');
 
 /**
  * @license
