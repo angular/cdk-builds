977ad8f84ecc84cf5f7ff5c015447210f0cd0ae0
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-08-28 05:18:34 +0000

    master - f795b9d8c fix(drag-drop): expose some missing injection tokens (#20410)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-next.0-sha-1d0e3c76f');
+	var VERSION = new core.Version('11.0.0-next.0-sha-f795b9d8c');
 
 	/**
 	 * @license
--- drag-drop/index.d.ts
+++ drag-drop/index.d.ts
@@ -2,5 +2,4 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { CDK_DROP_LIST as ɵangular_material_src_cdk_drag_drop_drag_drop_b, CdkDropListInternal as ɵangular_material_src_cdk_drag_drop_drag_drop_a } from './directives/drop-list';
-export { CDK_DRAG_PARENT as ɵangular_material_src_cdk_drag_drop_drag_drop_c } from './drag-parent';
+export { CdkDropListInternal as ɵangular_material_src_cdk_drag_drop_drag_drop_a } from './directives/drop-list';
