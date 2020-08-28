c114ae6183a3202be1bd9dfc4d437037ed6f5faa
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-08-28 05:22:49 +0000

    master - f2953dd41 fix(material-experimental/mdc-button): double outline for FAB in high contrast mode (#20391)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-next.0-sha-54218cd78');
+	var VERSION = new core.Version('11.0.0-next.0-sha-f2953dd41');
 
 	/**
 	 * @license
