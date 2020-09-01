a674da9f3054bf79758905ad4b9cb47b7c5e10ce
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-09-01 20:48:44 +0000

    master - 98d4799cb refactor(material): remove month constants from core (#18111)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-next.0-sha-3c2334cb3');
+	var VERSION = new core.Version('11.0.0-next.0-sha-98d4799cb');
 
 	/**
 	 * @license
