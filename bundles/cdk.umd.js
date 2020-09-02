59b4f92a3f671b54c0077b736633042f941db92b
mmalerba <mmalerba@google.com>
2020-09-02 23:35:47 +0000

    master - f00f46a15 chore: copy 10.2.0 changelog to master branch (#20486)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-next.0-sha-d9875872b');
+	var VERSION = new core.Version('11.0.0-next.0-sha-f00f46a15');
 
 	/**
 	 * @license
