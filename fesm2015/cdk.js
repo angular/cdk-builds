86a3ea2959cf0b097c7a5668b4da9aa8e99970bc
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-09-02 21:38:37 +0000

    master - 42fb35b26 build: add a script to flag missing unit tests in MDC packages (#20471)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.0-sha-17c67e5d2');
+const VERSION = new Version('11.0.0-next.0-sha-42fb35b26');
 
 /**
  * @license
