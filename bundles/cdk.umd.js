383a8e274c87a55afe148dbd8c27e07caa9bb04e
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-09-05 19:23:36 +0000

    master - 3474aaa86 mdc-list: support for strong focus indicators and fix incorrect cursor for non-interactive items (#20476)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-next.0-sha-7510f4c76');
+	var VERSION = new core.Version('11.0.0-next.0-sha-3474aaa86');
 
 	/**
 	 * @license
