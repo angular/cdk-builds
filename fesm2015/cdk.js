3d575a49990173a31127c0fa1288695c1cff6545
mbehrlich <mattehrlich86@gmail.com>
2020-09-05 19:24:55 +0000

    master - 541c94286 docs(google-maps): Improve documentation for Google Maps. (#20492)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.0-sha-3474aaa86');
+const VERSION = new Version('11.0.0-next.0-sha-541c94286');
 
 /**
  * @license
