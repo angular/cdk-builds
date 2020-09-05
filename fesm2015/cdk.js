218e9b999d8efa4fd044bb8c6e9144dc3505ec33
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-09-05 19:19:31 +0000

    master - 6296da29d fix(datepicker): don't revalidate if new date object for same date is passed through input (#20362)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.0-sha-faddbaa59');
+const VERSION = new Version('11.0.0-next.0-sha-6296da29d');
 
 /**
  * @license
