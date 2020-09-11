570fb66a1b7ebeb80f4f1db5931c07ac43f84f24
Joey Perrott <josephperrott.github@gmail.com>
2020-09-11 14:01:28 +0000

    master - e7f5e4f83 build: bump to lastest sha of angular/dev-infra's lock-closed action (#20438)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-next.0-sha-f951bf405');
+	var VERSION = new core.Version('11.0.0-next.0-sha-e7f5e4f83');
 
 	/**
 	 * @license
