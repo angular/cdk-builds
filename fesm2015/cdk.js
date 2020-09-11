e998094ad232cacc01229d0bd10b13712b87cbd4
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-09-11 14:07:53 +0000

    master - 1f5b0a8c4 docs: fix missing space in mdc-dialog readme (#20542)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.0-sha-8b1c7f294');
+const VERSION = new Version('11.0.0-next.0-sha-1f5b0a8c4');
 
 /**
  * @license
