d70292d8977d9a29904248e36231d528d1a61199
jeripeierSBB <jeremias.peier@sbb.ch>
2020-09-11 14:11:43 +0000

    10.2.x - 2ca019474 docs: fix typo in sidenav docs (#20544)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-sha-fdd87b9b0');
+const VERSION = new Version('10.2.0-sha-2ca019474');
 
 /**
  * @license
