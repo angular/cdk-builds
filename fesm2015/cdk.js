3e29bfd5d8b8d946121f84f270b9b9e7d982be42
Wagner Maciel <wagnermaciel@google.com>
2020-09-11 14:01:22 +0000

    master - 7177f3f27 test(button-harness): add performance tests for buttons using the pro… (#20222)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.0-sha-f951bf405');
+const VERSION = new Version('11.0.0-next.0-sha-7177f3f27');
 
 /**
  * @license
