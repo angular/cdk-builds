c413ccdea6fcc2d2700654a4d066f653f1f21e2d
Andrew Seguin <andrewjs@google.com>
2020-09-14 20:39:59 +0000

    10.2.x - ec14f8f3b chore: bump version to 10.2.1 w/ changelog

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.2.0-sha-2ca019474');
+	var VERSION = new core.Version('10.2.1-sha-ec14f8f3b');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.0-sha-2ca019474');
+const VERSION = new Version('10.2.1-sha-ec14f8f3b');
 
 /**
  * @license
