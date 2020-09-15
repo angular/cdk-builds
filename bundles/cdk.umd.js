431920268279c40e9db6e0e7d76eb9b834a66065
Wagner Maciel <wagnermaciel@google.com>
2020-09-15 13:31:20 +0000

    10.2.x - da8cf79a0 build: add @types/resize-observer-browser

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.2.1-sha-ec14f8f3b');
+	var VERSION = new core.Version('10.2.1-sha-da8cf79a0');
 
 	/**
 	 * @license
