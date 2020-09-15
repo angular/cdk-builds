46b0597f993d81af840b22fc5711e337744adb12
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-09-15 13:29:44 +0000

    master - 8d643a069 fix(material/list/testing): avoid element selectors that could conflict with the MDC version (#20528)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-next.0-sha-20b4bc9c5');
+	var VERSION = new core.Version('11.0.0-next.0-sha-8d643a069');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.0-sha-20b4bc9c5');
+const VERSION = new Version('11.0.0-next.0-sha-8d643a069');
 
 /**
  * @license
