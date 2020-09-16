7aa05251a1c592e450065c31c615c92af76798bf
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-09-16 02:31:18 +0000

    master - 8b670ab92 refactor(testing): add methods for accessing projected content in component container harnesses (#20556)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-next.0-sha-13284206b');
+	var VERSION = new core.Version('11.0.0-next.0-sha-8b670ab92');
 
 	/**
 	 * @license
