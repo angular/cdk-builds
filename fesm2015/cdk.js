5f566b579eda5c6dd1c11833293f6127ede9b9a6
fhuschle <testerfelix@gmail.com>
2020-09-16 02:49:18 +0000

    master - dfdf5cf75 feat(datepicker): add viewChanged output to calendar (#13759) (#20468)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.0-sha-d14d9861d');
+const VERSION = new Version('11.0.0-next.0-sha-dfdf5cf75');
 
 /**
  * @license
