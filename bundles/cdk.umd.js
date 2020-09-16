ff73a69709e8bb761c96367fa71d1ddae0f515de
Andrew Seguin <andrewseguin@users.noreply.github.com>
2020-09-16 23:55:17 +0000

    master - 862f415a7 chore(material-experimental/mdc-table): add todo for layout (#20562)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-next.0-sha-42be968d7');
+	var VERSION = new core.Version('11.0.0-next.0-sha-862f415a7');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.0-sha-42be968d7');
+const VERSION = new Version('11.0.0-next.0-sha-862f415a7');
 
 /**
  * @license
