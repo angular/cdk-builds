2c49698142ca3e8d9423909f210581bf8f15ec75
Joey Perrott <josephperrott.github@gmail.com>
2020-09-16 23:53:50 +0000

    master - 42be968d7 ci: set up commit message conformance rules using ng-dev (#20580)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-next.0-sha-100d25c3f');
+	var VERSION = new core.Version('11.0.0-next.0-sha-42be968d7');
 
 	/**
 	 * @license
