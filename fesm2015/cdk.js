9eedef0f0c64d53fa6df77951fcadb09f8442caa
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-09-16 02:33:38 +0000

    master - 6e957c1b7 refactor: remove light-weight token content-child query cast (#20433)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.0-sha-50eca15c1');
+const VERSION = new Version('11.0.0-next.0-sha-6e957c1b7');
 
 /**
  * @license
