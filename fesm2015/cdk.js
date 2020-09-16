5036c570ec26db588339c3139b5f9c78d5786278
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-09-16 23:59:16 +0000

    master - 85e8f3450 fix(button-toggle): don't use divs inside button template (#20390)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.0-sha-f8ab9ba8e');
+const VERSION = new Version('11.0.0-next.0-sha-85e8f3450');
 
 /**
  * @license
