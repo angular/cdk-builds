db8ae8a64561624493ba0ccca82f4963c2e73fdc
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-09-22 15:56:31 +0000

    master - 8c2838d25 refactor(cdk/tree): remove deprecated APIs for v11 (#20573)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-next.0-sha-c21c6f5d4');
+	var VERSION = new core.Version('11.0.0-next.0-sha-8c2838d25');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.0-sha-c21c6f5d4');
+const VERSION = new Version('11.0.0-next.0-sha-8c2838d25');
 
 /**
  * @license
