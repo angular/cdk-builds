c494ab4d761b093dfa4241d7712d304e3739debc
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-09-22 15:59:38 +0000

    master - d8def8be6 fix(material-experimental/mdc-form-field): not adding mat-primary theme class (#20605)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.0-sha-29e1f266a');
+const VERSION = new Version('11.0.0-next.0-sha-d8def8be6');
 
 /**
  * @license
