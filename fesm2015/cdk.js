570ad2e9c599872a05ba4129bdaef7c06dac75a8
Andy Chrzaszcz <andy9775@gmail.com>
2020-09-22 23:37:35 +0000

    master - 469984f20 docs(cdk-experimental/menu): add examples to menu documentation (#20482)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.0-sha-2967ad0c2');
+const VERSION = new Version('11.0.0-next.0-sha-469984f20');
 
 /**
  * @license
