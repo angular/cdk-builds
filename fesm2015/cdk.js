8ea35b58419f0a2fe0a7e9fc6e5ad95f0738b336
Wagner Maciel <wagnermaciel@google.com>
2020-09-22 16:02:22 +0000

    master - 4d4b4da3f build: update dev-infra version (#20547)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.0-sha-d8def8be6');
+const VERSION = new Version('11.0.0-next.0-sha-4d4b4da3f');
 
 /**
  * @license
