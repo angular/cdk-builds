f2a3e4641b49dccc416dcca9f2dfef51e09b9d90
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-09-22 16:08:22 +0000

    master - e8265ccaf build: setup default browser for running legacy tests locally (#20620)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.0-sha-09e77790b');
+const VERSION = new Version('11.0.0-next.0-sha-e8265ccaf');
 
 /**
  * @license
