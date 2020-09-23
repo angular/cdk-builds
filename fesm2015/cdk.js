d3ab5111f44d1009dc77d455d957832dda67a121
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-09-23 18:36:37 +0000

    master - 993767f23 refactor(material/sidenav): undeprecate constructor signature (#20589)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.0-sha-54c66bc87');
+const VERSION = new Version('11.0.0-next.0-sha-993767f23');
 
 /**
  * @license
