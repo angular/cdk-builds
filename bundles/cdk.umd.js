ec6e9ef56fd957e558bd89dfa8ead1c1cd02d3a0
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-09-29 21:59:28 +0000

    master - d87ae3c91 feat(material-experimental/mdc-paginator): implement MDC-based paginator (#20607)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-next.0-sha-4b37b98fc');
+	var VERSION = new core.Version('11.0.0-next.0-sha-d87ae3c91');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.0-sha-4b37b98fc');
+const VERSION = new Version('11.0.0-next.0-sha-d87ae3c91');
 
 /**
  * @license
