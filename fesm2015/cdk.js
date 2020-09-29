58b384ccf3e9d83ef17c247504a6724e5155627b
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-09-29 22:00:57 +0000

    master - e99ca0ac9 refactor(material/core): remove deprecated APIs for v11 (#20479)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.0-sha-d87ae3c91');
+const VERSION = new Version('11.0.0-next.0-sha-e99ca0ac9');
 
 /**
  * @license
