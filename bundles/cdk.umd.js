66013838aeafe064ba1f82abedb3f36298b2de2d
Ron Lapushner <lapushnerron@gmail.com>
2020-09-29 17:05:37 +0000

    master - b0d0388c1 fix(youtube-player): YT.Player is not a constructor (#20616)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-next.0-sha-5c42f3cea');
+	var VERSION = new core.Version('11.0.0-next.0-sha-b0d0388c1');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.0-sha-5c42f3cea');
+const VERSION = new Version('11.0.0-next.0-sha-b0d0388c1');
 
 /**
  * @license
