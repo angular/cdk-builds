83752a0093492a8c314c990aa29b6edc5fcae3d3
Joey Perrott <josephperrott.github@gmail.com>
2020-09-29 17:00:47 +0000

    master - 34485d926 refactor(material-experimental/mdc-checkbox): remove usage of term blacklist (#20655)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.0-sha-8b6aab12e');
+const VERSION = new Version('11.0.0-next.0-sha-34485d926');
 
 /**
  * @license
