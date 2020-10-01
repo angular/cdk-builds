8d32055f470ff99430a93433bbca66ca53d50a2a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-10-01 07:19:42 +0000

    master - 50d3f29b6 fix(material-experimental/mdc-snack-bar): button pushed outside container (#20696)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.0-sha-2dbbc0d72');
+const VERSION = new Version('11.0.0-next.0-sha-50d3f29b6');
 
 /**
  * @license
