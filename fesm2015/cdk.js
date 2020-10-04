35e2d2dd30ac1d373cfcdbbdd489bfe3a95f6b34
mmalerba <mmalerba@google.com>
2020-10-04 21:24:30 +0000

    10.2.x - 8b4ce730a fix(material/datepicker): fix handling of short years (#20709)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.3-sha-ce49e2f2c');
+const VERSION = new Version('10.2.3-sha-8b4ce730a');
 
 /**
  * @license
