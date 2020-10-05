ae7dcfd1847e74e85d6ec4c8e7244ed94e4b9351
mmalerba <mmalerba@google.com>
2020-10-05 22:18:53 +0000

    master - 1c83423e3 release: bump version to 11.0.0-next.1 w/ changelog (#20728)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.0-sha-b3f1fb34a');
+const VERSION = new Version('11.0.0-next.1-sha-1c83423e3');
 
 /**
  * @license
