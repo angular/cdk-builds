ed7d978c695a46bcff343c8150889c1e899caebe
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-10-07 19:27:57 +0000

    master - a087edd6c fix(material/form-field): dynamic hint end alignment not working (#20657)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-next.1-sha-628b5e9fd');
+	var VERSION = new core.Version('11.0.0-next.1-sha-a087edd6c');
 
 	/**
 	 * @license
