af4a731da23fd68f2480257907c38e1f01ad23e3
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-10-07 19:36:27 +0000

    master - 407398f6e fix(material/datepicker): add close button for screen readers (#20666)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.1-sha-33a43f7b0');
+const VERSION = new Version('11.0.0-next.1-sha-407398f6e');
 
 /**
  * @license
