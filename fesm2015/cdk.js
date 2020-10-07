fbcd9da1f7e3765955fb8812ea031c27bd8627a0
Mark Hansen <mark@markhansen.co.nz>
2020-10-07 19:25:42 +0000

    master - d9359c4f5 fix(material/tooltip): convert message to string (#20685)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.1-sha-b59e4a18c');
+const VERSION = new Version('11.0.0-next.1-sha-d9359c4f5');
 
 /**
  * @license
