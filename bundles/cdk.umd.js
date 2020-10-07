647de232d6b6bd4d0992116f8a4eab67373c0020
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-10-07 19:33:34 +0000

    master - d6e6f44c1 feat(tabs): allow for dynamicHeight to be configured through MAT_TABS_CONFIG (#19677)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-next.1-sha-861686e8a');
+	var VERSION = new core.Version('11.0.0-next.1-sha-d6e6f44c1');
 
 	/**
 	 * @license
