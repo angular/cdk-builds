b2263f6259b465faf92c32154059193802c589c0
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-10-07 19:30:51 +0000

    master - 04896057a refactor: clean up MDC menu inheritance (#20268)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.1-sha-0ee25a2a4');
+const VERSION = new Version('11.0.0-next.1-sha-04896057a');
 
 /**
  * @license
