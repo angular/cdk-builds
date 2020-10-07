25ce55852a4e0ccb9c70de2f828afc365553b654
mmalerba <mmalerba@google.com>
2020-10-07 19:24:11 +0000

    master - b59e4a18c fix(material/datepicker): lifecycle hooks not being called (#20700)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-next.1-sha-17a9ad021');
+	var VERSION = new core.Version('11.0.0-next.1-sha-b59e4a18c');
 
 	/**
 	 * @license
