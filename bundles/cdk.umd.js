a6b53e2364f8f83e0e43fb058c2a5ceaee84acbe
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-10-07 21:13:31 +0000

    master - 6201764bf test(material/datepicker): fix test failure (#20746)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-next.1-sha-54f36a3e6');
+	var VERSION = new core.Version('11.0.0-next.1-sha-6201764bf');
 
 	/**
 	 * @license
