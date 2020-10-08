3157d63d1bab39a883e8a06f959977ad772fb5b9
Joey Perrott <josephperrott.github@gmail.com>
2020-10-08 03:14:57 +0000

    master - d49a7749a build: migrate to new dependabot config (#20727)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.1-sha-6dfa28c69');
+const VERSION = new Version('11.0.0-next.1-sha-d49a7749a');
 
 /**
  * @license
