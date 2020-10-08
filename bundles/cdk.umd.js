11e864002e1a7ccbc87d415813daa31772f4d16b
Joey Perrott <josephperrott.github@gmail.com>
2020-10-08 03:17:36 +0000

    master - c9b7a1ac0 Restore commit messages on failures (#20707)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-next.1-sha-b7cfe6737');
+	var VERSION = new core.Version('11.0.0-next.1-sha-c9b7a1ac0');
 
 	/**
 	 * @license
