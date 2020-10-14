94115d8ca79200965f21d77e4be250fa3910b1a0
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-10-14 21:23:39 +0000

    master - 02d334cef fix(material-experimental/mdc-form-field): test harness referring to non-MDC harnesses (#20762)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -28,7 +28,7 @@ function default_1() {
             // In order to align the CDK version with other Angular dependencies that are setup by
             // `@schematics/angular`, we use tilde instead of caret. This is default for Angular
             // dependencies in new CLI projects.
-            package_config_1.addPackageToPackageJson(host, '@angular/cdk', `~11.0.0-next.1-sha-cc310f9cc`);
+            package_config_1.addPackageToPackageJson(host, '@angular/cdk', `~11.0.0-next.1-sha-02d334cef`);
             // Add a task to run the package manager. This is necessary because we updated the
             // workspace "package.json" file and we want lock files to reflect the new version range.
             context.addTask(new tasks_1.NodePackageInstallTask());
