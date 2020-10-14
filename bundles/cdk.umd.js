76ae983d3991fe9ade49f0a84fb43d23968dd84f
Joey Perrott <josephperrott.github@gmail.com>
2020-10-14 15:56:39 +0000

    10.2.x - 90f985144 build: add required fields to dependabot configuration (#20786)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.2.4-sha-69ac70ca2');
+	var VERSION = new core.Version('10.2.4-sha-90f985144');
 
 	/**
 	 * @license
