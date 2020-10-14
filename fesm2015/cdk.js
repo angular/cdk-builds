7e5508ac4c1b5e9f2368ddbb06c1bd387f215301
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-10-14 21:33:29 +0000

    master - 0909bc91a test(material-experimental/mdc-list): add missing test coverage (#20771)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.1-sha-bbcc5b2c6');
+const VERSION = new Version('11.0.0-next.1-sha-0909bc91a');
 
 /**
  * @license
