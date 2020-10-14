2dc3da9a3fbb7534aa48228754e65e94867ab0ee
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-10-14 21:25:02 +0000

    master - 70e9eb967 refactor(material/dialog): use type overload for open method (#11731)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-next.1-sha-02d334cef');
+	var VERSION = new core.Version('11.0.0-next.1-sha-70e9eb967');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.1-sha-02d334cef');
+const VERSION = new Version('11.0.0-next.1-sha-70e9eb967');
 
 /**
  * @license
