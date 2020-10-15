cdf3eb9af8b0b908d4ed0ccd51a206e77d518988
Annie Wang <annieyw@google.com>
2020-10-15 19:42:41 +0000

    master - e03a4d6fa release: bump version to 11.0.0-next.2 w/ changelog (#20813)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-next.1-sha-f6cd5e199');
+	var VERSION = new core.Version('11.0.0-next.2-sha-e03a4d6fa');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.1-sha-f6cd5e199');
+const VERSION = new Version('11.0.0-next.2-sha-e03a4d6fa');
 
 /**
  * @license
