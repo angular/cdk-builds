5da9a70c8c4bef3aa72d3cc60542f7d27b11755f
Annie Wang <annieyw@google.com>
2020-10-15 01:41:24 +0000

    master - 00f32746d release: bump version to 10.2.5 w/ changelog (#20798)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.1-sha-59d3840ce');
+const VERSION = new Version('11.0.0-next.1-sha-00f32746d');
 
 /**
  * @license
