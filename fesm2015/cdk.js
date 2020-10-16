91743992b4f4becaae67d96222095035584c299f
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-10-16 19:04:48 +0000

    master - f854196a8 fix(material-experimental/mdc-slide-toggle): align focus behavior with standard version (#20772)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.2-sha-5eb10359d');
+const VERSION = new Version('11.0.0-next.2-sha-f854196a8');
 
 /**
  * @license
