a6a8a329573fbaf60c435093be3dd4e7a81bff4d
Lukáš Matta <lukasmatta@users.noreply.github.com>
2020-10-19 19:26:25 +0000

    master - 298ed5c92 docs: fix a handful of typos (#20824)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-next.2-sha-ca13dc0d8');
+	var VERSION = new core.Version('11.0.0-next.2-sha-298ed5c92');
 
 	/**
 	 * @license
