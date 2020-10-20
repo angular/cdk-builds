87269334f8b1859499d6bc6ec5d31ce2e35f00b6
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-10-20 15:01:44 +0000

    master - 19f5f5ca3 fix(material/slider): some screen readers announcing long decimal values (#20760)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.2-sha-e15f82c25');
+const VERSION = new Version('11.0.0-next.2-sha-19f5f5ca3');
 
 /**
  * @license
