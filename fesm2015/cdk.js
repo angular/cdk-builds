4fe5782297f264586bf2d206f463f67c096ccede
Joey Perrott <josephperrott.github@gmail.com>
2020-10-20 19:38:02 +0000

    master - 018d6c9e6 ci: update sauceconnect verstion to 4.6.2 (#20835)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.2-sha-f81c31c6e');
+const VERSION = new Version('11.0.0-next.2-sha-018d6c9e6');
 
 /**
  * @license
