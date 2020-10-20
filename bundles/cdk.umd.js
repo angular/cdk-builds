04cefbf15e86186b42b24d6e787e9737ff1b77ed
Howard <howardjing@google.com>
2020-10-20 14:59:16 +0000

    master - e15f82c25 feat(cdk/scrolling): update CdkVirtualForOf to work with sets. (#20594)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-next.2-sha-af037ed31');
+	var VERSION = new core.Version('11.0.0-next.2-sha-e15f82c25');
 
 	/**
 	 * @license
