41bf3f027c3f5e1f4fb330fafaa2cc323b5a158a
Stefan Aleksovski <aleksovski.stefan994@gmail.com>
2020-10-20 14:49:55 +0000

    master - b0e97afb9 docs(material/input): add type="email" to input examples (#20796)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-next.2-sha-f1950362e');
+	var VERSION = new core.Version('11.0.0-next.2-sha-b0e97afb9');
 
 	/**
 	 * @license
