2f30e9892561f1d3038081d7ccfa80b0aaa2de24
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-10-20 14:51:20 +0000

    master - de542dfe9 fix(material-experimental/mdc-chips): add missing functionality and align tests (#20814)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.2-sha-b0e97afb9');
+const VERSION = new Version('11.0.0-next.2-sha-de542dfe9');
 
 /**
  * @license
