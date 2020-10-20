003a4221356e299d03e23c18d9f77f63d439052d
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-10-20 14:48:45 +0000

    10.2.x - 02f756189 refactor(material/core): remove usages of deprecated Sass functions (#20811)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.2.5-sha-d123a4dab');
+	var VERSION = new core.Version('10.2.5-sha-02f756189');
 
 	/**
 	 * @license
