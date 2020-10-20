c2eeb3acec64281dbb170a2911b7fef43a2e8329
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-10-20 14:46:35 +0000

    master - 17de49c43 fix(material-experimental/mdc-card): double outline in high contrast mode (#20823)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-next.2-sha-345ad70ef');
+	var VERSION = new core.Version('11.0.0-next.2-sha-17de49c43');
 
 	/**
 	 * @license
