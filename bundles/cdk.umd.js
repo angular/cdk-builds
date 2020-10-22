36efd0aadbee57251e757be5ea6206b575d214bd
Jeremy Elbourn <jelbourn@google.com>
2020-10-22 16:55:37 +0000

    master - cc9758d49 Revert "fix(material/slider): some screen readers announcing long decimal values (#20760)" (#20849)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-next.2-sha-018d6c9e6');
+	var VERSION = new core.Version('11.0.0-next.2-sha-cc9758d49');
 
 	/**
 	 * @license
