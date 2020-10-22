094ef3afb7b9afdec3ac805dcd15ef8377407372
Jeremy Elbourn <jelbourn@google.com>
2020-10-22 18:21:08 +0000

    master - 41479fc02 release: set master to 11.1.0-next.0 (#20860)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-next.2-sha-cc9758d49');
+	var VERSION = new core.Version('11.1.0-next.0-sha-41479fc02');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-next.2-sha-cc9758d49');
+const VERSION = new Version('11.1.0-next.0-sha-41479fc02');
 
 /**
  * @license
