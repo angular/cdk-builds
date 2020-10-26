b073a2c435303f4c3b4f3890fd2bb2ec7d78a66e
George Kalpakas <kalpakas.g@gmail.com>
2020-10-26 14:18:17 +0000

    master - 676057454 build: update `lock-closed` GitHub action to latest version (#20848)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-594b4efbb');
+const VERSION = new Version('11.1.0-next.0-sha-676057454');
 
 /**
  * @license
