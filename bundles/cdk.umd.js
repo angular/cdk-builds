d46a41aa4b6e21ef05eac20d12930dbfb5dc7e04
Annie Wang <annieyw@google.com>
2020-10-26 14:19:44 +0000

    master - 0b2d15033 docs(material/button-toggle): add button toggle harness example (#20837)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-676057454');
+	var VERSION = new core.Version('11.1.0-next.0-sha-0b2d15033');
 
 	/**
 	 * @license
