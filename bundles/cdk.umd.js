cf2281fb0147cb9148150155a68989bf3d546005
Jeremy Elbourn <jelbourn@google.com>
2020-10-27 20:07:40 +0000

    10.2.x - 0ccaef987 release: bump version to 10.2.6 w/ changelog (#20899)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('10.2.5-sha-97ab37ad0');
+	var VERSION = new core.Version('10.2.6-sha-0ccaef987');
 
 	/**
 	 * @license
