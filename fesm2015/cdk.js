70f2c822cb6bba65077a343bbdb4c404bf783afb
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-10-28 11:28:09 +0000

    10.2.x - 863845613 fix(material/list): exclude icons from harness getText result (#20845)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('10.2.6-sha-010eb0bb9');
+const VERSION = new Version('10.2.6-sha-863845613');
 
 /**
  * @license
