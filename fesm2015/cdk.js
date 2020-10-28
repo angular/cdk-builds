7c7738ee7dde7af6f6d11f51c3d3d606aa01dc92
Annie Wang <annieyw@google.com>
2020-10-28 03:13:45 +0000

    master - 435d0472c docs(material/card): add card harness example (#20834)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-3d15ecb52');
+const VERSION = new Version('11.1.0-next.0-sha-435d0472c');
 
 /**
  * @license
