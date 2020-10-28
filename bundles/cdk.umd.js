3218b4df15166e1933682751bda044fe9faf47c5
Annie Wang <annieyw@google.com>
2020-10-28 03:12:27 +0000

    master - 3d15ecb52 docs(material/badge): add badge harness example (#20832)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-e2ebfe040');
+	var VERSION = new core.Version('11.1.0-next.0-sha-3d15ecb52');
 
 	/**
 	 * @license
