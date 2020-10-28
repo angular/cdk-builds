4b8cae56dd91dbfa5e47afc0f93c1745c589d49e
Joey Perrott <josephperrott.github@gmail.com>
2020-10-28 21:25:00 +0000

    master - 086002ff0 build: add husky ignored files to .gitignore (#20856)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-51c409614');
+	var VERSION = new core.Version('11.1.0-next.0-sha-086002ff0');
 
 	/**
 	 * @license
