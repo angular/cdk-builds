bf9286867da55b4ad23c0d0ab1aecf6631a9be93
Dmitriy Stepanenko <33101123+dmitry-stepanenko@users.noreply.github.com>
2020-10-28 11:30:03 +0000

    master - f0a4e59fd fix(google-maps): rendering blank if custom options with no zoom are provided (#20882)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-f99d0dd62');
+const VERSION = new Version('11.1.0-next.0-sha-f0a4e59fd');
 
 /**
  * @license
