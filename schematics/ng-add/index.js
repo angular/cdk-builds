a3200f5dfe1a6eb891d09e8934872b30d44d3de5
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-10-28 18:10:32 +0000

    10.2.x - 38dc5113c fix(google-maps): maintain subscriptions across event targets (#20897)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -28,7 +28,7 @@ function default_1() {
             // In order to align the CDK version with other Angular dependencies that are setup by
             // `@schematics/angular`, we use tilde instead of caret. This is default for Angular
             // dependencies in new CLI projects.
-            package_config_1.addPackageToPackageJson(host, '@angular/cdk', `~10.2.6-sha-863845613`);
+            package_config_1.addPackageToPackageJson(host, '@angular/cdk', `~10.2.6-sha-38dc5113c`);
             // Add a task to run the package manager. This is necessary because we updated the
             // workspace "package.json" file and we want lock files to reflect the new version range.
             context.addTask(new tasks_1.NodePackageInstallTask());
