4f8f58cb77612e2b5d00f2f4be22caa5df2b07dc
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-10-28 11:27:16 +0000

    11.0.x - aec12b6f7 fix(material/list): exclude icons from harness getText result (#20845)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-rc.0-sha-6d805ab40');
+	var VERSION = new core.Version('11.0.0-rc.0-sha-aec12b6f7');
 
 	/**
 	 * @license
