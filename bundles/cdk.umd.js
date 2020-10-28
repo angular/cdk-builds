f2e40b4b1347232bd42dbc50970c715216afe6f9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-10-28 11:33:08 +0000

    master - 5f2407eb0 feat(material/chips): add test harnesses for selectable chips (#20875)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-dfdd42250');
+	var VERSION = new core.Version('11.1.0-next.0-sha-5f2407eb0');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-dfdd42250');
+const VERSION = new Version('11.1.0-next.0-sha-5f2407eb0');
 
 /**
  * @license
