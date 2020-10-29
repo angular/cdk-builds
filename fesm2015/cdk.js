512c3d6689d06fb2e2254c32b37f03612ca25079
Jeremy Elbourn <jelbourn@google.com>
2020-10-29 19:14:29 +0000

    master - 40b602f20 refactor(material/theming): remove underscores from non-mdc density mixins (#20915)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-0b013917a');
+const VERSION = new Version('11.1.0-next.0-sha-40b602f20');
 
 /**
  * @license
