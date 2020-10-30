78cef736482040e510b1898d8564acc37460e9d4
mmalerba <mmalerba@google.com>
2020-10-30 03:55:18 +0000

    master - cf4aa5123 fix(material-experimental/mdc-snack-bar): add missing exports (#20928)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-9b3992bcb');
+const VERSION = new Version('11.1.0-next.0-sha-cf4aa5123');
 
 /**
  * @license
