ba79daf644b8256e8f4eab53cef0574f084683c1
mmalerba <mmalerba@google.com>
2020-11-02 14:22:58 +0000

    11.0.x - f76578da0 fix(material-experimental/mdc-checkbox): fix broken dark theme (#20940)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-rc.0-sha-9011039ea');
+const VERSION = new Version('11.0.0-rc.0-sha-f76578da0');
 
 /**
  * @license
