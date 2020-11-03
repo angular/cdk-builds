f0635da9707e5fea39bd5a3cb1725025511d4cf7
Vanessa Schmitt <vschmitt@google.com>
2020-11-03 06:14:51 +0000

    master - 335fc0c05 feat(material-experimental/mdc-chips): Allow custom role for ChipSet (#20829)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-f7023c8a3');
+const VERSION = new Version('11.1.0-next.0-sha-335fc0c05');
 
 /**
  * @license
