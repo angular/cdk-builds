3b52b8933e5d587d5524b2d6ccf0d1c1042eb74d
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-11-03 06:19:29 +0000

    master - 43b463feb fix(material/dialog): incorrect action height if box-sizing is configured globally (#20889)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-ea2f29db9');
+	var VERSION = new core.Version('11.1.0-next.0-sha-43b463feb');
 
 	/**
 	 * @license
