b53beffabe9ab00132d1d6031b8d87592efd8f81
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-11-03 06:17:27 +0000

    11.0.x - cc32c5d4d fix(material/dialog): incorrect action height if box-sizing is configured globally (#20889)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-rc.1-sha-19ef64a2e');
+const VERSION = new Version('11.0.0-rc.1-sha-cc32c5d4d');
 
 /**
  * @license
