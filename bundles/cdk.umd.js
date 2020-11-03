7c01285df3433ca6f0d8e7ce6a0ae8f486681e33
tomrlq <tomrlq@foxmail.com>
2020-11-03 06:16:52 +0000

    master - 16576e428 fix(cdk/layout): decrease breakpoint upper bounds (#20866)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-335fc0c05');
+	var VERSION = new core.Version('11.1.0-next.0-sha-16576e428');
 
 	/**
 	 * @license
