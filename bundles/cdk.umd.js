44f0c6c88346adca685d2c39ff40beedb244d0b1
Jessica Xu <jessica@replicahq.com>
2020-11-03 06:20:53 +0000

    master - 23d0b80ba feat(material/button): allow focus origin to be optional input in focus method (#20898)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-43b463feb');
+	var VERSION = new core.Version('11.1.0-next.0-sha-23d0b80ba');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-43b463feb');
+const VERSION = new Version('11.1.0-next.0-sha-23d0b80ba');
 
 /**
  * @license
