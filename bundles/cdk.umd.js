414f544331a93ec4b74781030b7bbaff3e83aa57
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-11-03 05:51:49 +0000

    master - f7023c8a3 fix(google-maps): allow ground overlay bounds to be changed (#20873)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-d3a271871');
+	var VERSION = new core.Version('11.1.0-next.0-sha-f7023c8a3');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-d3a271871');
+const VERSION = new Version('11.1.0-next.0-sha-f7023c8a3');
 
 /**
  * @license
