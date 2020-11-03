4f0674b0e3967b1caf89d1f32f5e7a0921ec8c61
Jessica Xu <jessica@replicahq.com>
2020-11-03 06:18:10 +0000

    master - ea2f29db9 feat(material/expansion): allow focus origin to be optional input in focus method (#20912)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-16576e428');
+const VERSION = new Version('11.1.0-next.0-sha-ea2f29db9');
 
 /**
  * @license
