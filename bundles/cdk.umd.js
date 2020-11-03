a2d686a468ad38cc752afd660e754b906f12522c
Jessica Xu <jessica@replicahq.com>
2020-11-03 05:49:10 +0000

    master - a3b5fe3b9 feat(material/stepper): allow focus origin to be optional input in focus method (#20914)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-513bfa49b');
+	var VERSION = new core.Version('11.1.0-next.0-sha-a3b5fe3b9');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-513bfa49b');
+const VERSION = new Version('11.1.0-next.0-sha-a3b5fe3b9');
 
 /**
  * @license
