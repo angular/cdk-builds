072dd2a95c4a9101119f12700edcc78962a2dc35
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-11-04 18:19:58 +0000

    master - 7596532e2 fix(material-experimental/mdc-chips): use MDC-based form field (#20880)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-5b1660e09');
+const VERSION = new Version('11.1.0-next.0-sha-7596532e2');
 
 /**
  * @license
