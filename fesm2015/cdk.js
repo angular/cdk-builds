1e0b24e2d42cee433e1fe1ae53daf2151168258d
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-11-04 18:47:26 +0000

    11.0.x - 4eb55045c fix(progress-bar): buffer not rendering correctly on ios (#19346)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-rc.1-sha-87ccb988c');
+const VERSION = new Version('11.0.0-rc.1-sha-4eb55045c');
 
 /**
  * @license
