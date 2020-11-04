59c4be510965c13d55b7724ed05a99d0c119e4bc
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-11-04 18:34:49 +0000

    master - 3866761c9 fix(material/autocomplete): options inside groups not read out with VoiceOver (#20819)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-9f4415efa');
+	var VERSION = new core.Version('11.1.0-next.0-sha-3866761c9');
 
 	/**
 	 * @license
