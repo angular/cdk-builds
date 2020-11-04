bfed0f795126c77bacac694ca8eb3237402f5cec
Emma Twersky <pokepsi@gmail.com>
2020-11-04 18:27:50 +0000

    master - 2dcdd1b59 docs(material-experimental/mdc-theming): readmes (#20957)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-7335c6530');
+	var VERSION = new core.Version('11.1.0-next.0-sha-2dcdd1b59');
 
 	/**
 	 * @license
