0f8441aed8493bae2a1114d5f1cfaa97158bfe0c
Matteo Vesprini-Heidrich <MatteoVH@users.noreply.github.com>
2020-11-04 18:24:59 +0000

    master - d29ef7629 feat(material-experimental/mdc-chips): add `remove` method to MDC chip harness (#20831)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-57a366117');
+	var VERSION = new core.Version('11.1.0-next.0-sha-d29ef7629');
 
 	/**
 	 * @license
