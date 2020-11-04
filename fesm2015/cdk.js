14dcf8d5762e186563cd8bbbe4652f6f9e36ec51
Michael-James <2666285+MichaelJamesParsons@users.noreply.github.com>
2020-11-04 18:18:56 +0000

    master - 5b1660e09 feat(material/datepicker): allow custom pickers with date inputs (#20864)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-5575673b4');
+const VERSION = new Version('11.1.0-next.0-sha-5b1660e09');
 
 /**
  * @license
