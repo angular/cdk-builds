45ae3ef7976adb66c9fa7bc310e7ee8cae15e8c4
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-11-06 03:48:35 +0000

    master - 1ae8f6bdf build: use MDC components for MDC demos (#20946)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-4699f30f9');
+	var VERSION = new core.Version('11.1.0-next.0-sha-1ae8f6bdf');
 
 	/**
 	 * @license
