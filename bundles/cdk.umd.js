0083b2b48b9432ce1b69aefe6c7c0833f4726bdc
Joey Perrott <josephperrott.github@gmail.com>
2020-11-07 00:17:38 +0000

    master - 154757c55 ci: rebase PRs before running CI checks (#20985)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-472adb82e');
+	var VERSION = new core.Version('11.1.0-next.0-sha-154757c55');
 
 	/**
 	 * @license
