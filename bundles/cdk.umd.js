af3af29e32a97a191047b9ba388663a27c6d35c6
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-11-07 00:07:17 +0000

    master - b9756b998 fix(button-toggle): clear static aria attributes from host nodes (#17086)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-92bb1c89d');
+	var VERSION = new core.Version('11.1.0-next.0-sha-b9756b998');
 
 	/**
 	 * @license
