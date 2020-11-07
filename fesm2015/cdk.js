c2e36866d20d26f230abdea533d5e326da5f461f
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-11-07 00:05:43 +0000

    master - 92bb1c89d fix(material-experimental/mdc-button): narrow down overly-broad selector (#20969)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-d686290ff');
+const VERSION = new Version('11.1.0-next.0-sha-92bb1c89d');
 
 /**
  * @license
