6eb8ea14ab8e0d07d62001a50901ca194abcd15e
Annie Wang <annieyw@google.com>
2020-11-09 19:14:30 +0000

    11.0.x - f67e5ccd0 build: fix broken yarn.lock attributes (#20992)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-rc.1-sha-36c0afdc1');
+	var VERSION = new core.Version('11.0.0-rc.1-sha-f67e5ccd0');
 
 	/**
 	 * @license
