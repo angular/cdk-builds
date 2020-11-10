13115c34c5012237867ebdabc8d33ba3dd47e076
Annie Wang <annieyw@google.com>
2020-11-10 01:22:23 +0000

    master - 8f08b2d32 build: update to latest Angular rc version (#20993)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-324bc248f');
+	var VERSION = new core.Version('11.1.0-next.0-sha-8f08b2d32');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-324bc248f');
+const VERSION = new Version('11.1.0-next.0-sha-8f08b2d32');
 
 /**
  * @license
