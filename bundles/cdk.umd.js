c777a285a01d194ef12fef7f4326594277029af2
mmalerba <mmalerba@google.com>
2020-11-11 23:00:49 +0000

    master - d6a357bfa release: bump angular to version 11 (#21018)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-rc.4-sha-bab8d4f7a');
+	var VERSION = new core.Version('11.0.0-rc.4-sha-d6a357bfa');
 
 	/**
 	 * @license
