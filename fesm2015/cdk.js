5b03c001cc1cbc904bb1912c0242b6853f92df83
mmalerba <mmalerba@google.com>
2020-11-11 23:05:09 +0000

    11.0.x - f16918720 release: update angular version in packages.bzl (#21020)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-rc.4-sha-04ddcdc31');
+const VERSION = new Version('11.0.0-rc.4-sha-f16918720');
 
 /**
  * @license
