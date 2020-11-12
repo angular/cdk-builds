fc4439fe51c62e6f5ddf8d69c6c52b25824c832f
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-11-12 18:05:07 +0000

    master - 7ff7b136d build: add CI check to keep MDC tests in line (#20941)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-28a6d0c81');
+	var VERSION = new core.Version('11.1.0-next.0-sha-7ff7b136d');
 
 	/**
 	 * @license
