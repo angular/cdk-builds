bbe76885be22ca1c6c757d4334d58020e0cdd462
Jessica Xu <jessica@replicahq.com>
2020-11-12 19:53:01 +0000

    11.0.x - e001e7099 feat(material/radio): Avoid nested divs in labels by changing to span instead. (#20986)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-sha-a347ff601');
+const VERSION = new Version('11.0.0-sha-e001e7099');
 
 /**
  * @license
