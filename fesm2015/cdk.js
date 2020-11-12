e9e0c0b6af82fb352cbffba5003dd4dcc59acff0
mmalerba <mmalerba@google.com>
2020-11-12 02:18:29 +0000

    master - 478e5780e release: add 11.0.0 changelog and update packages.bzl (#21023)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-rc.4-sha-d6a357bfa');
+const VERSION = new Version('11.0.0-rc.4-sha-478e5780e');
 
 /**
  * @license
