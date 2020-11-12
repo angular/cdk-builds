0cd8838231959253902bc41ba0edbc7fbb59c859
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-11-12 16:48:57 +0000

    11.0.x - 22a8bea93 build: use MDC 9.0 canary when publishing to npm (#21017)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-sha-3143c38fb');
+const VERSION = new Version('11.0.0-sha-22a8bea93');
 
 /**
  * @license
