5f1338aee4491a461c963c6f4158efd804ca16b3
Annie Wang <annieyw@google.com>
2020-11-12 18:34:43 +0000

    master - 554c7755b docs(material/chips): add chips harness example (#20942)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-7ff7b136d');
+	var VERSION = new core.Version('11.1.0-next.0-sha-554c7755b');
 
 	/**
 	 * @license
