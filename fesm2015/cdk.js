753491aeb7cb47946ca900d00fcb0bb340ea0dc7
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-11-12 02:30:08 +0000

    master - 7ca2af0d8 build: update to latest scss bundler and resolve file size issue (#21016)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-rc.4-sha-478e5780e');
+const VERSION = new Version('11.0.0-rc.4-sha-7ca2af0d8');
 
 /**
  * @license
