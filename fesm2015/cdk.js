bf6c95a73be74c2c74d5e2af08384065f8e897d7
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-11-12 19:56:35 +0000

    master - 7f6375024 build: update to latest sass version (#20984)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-e3e0f228a');
+const VERSION = new Version('11.1.0-next.0-sha-7f6375024');
 
 /**
  * @license
