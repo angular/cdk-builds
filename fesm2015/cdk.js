82c4a01564c9afb06891718904a7ccc4ebe2262c
Annie Wang <annieyw@google.com>
2020-11-13 18:58:42 +0000

    master - 9f641d153 docs(material/dialog): add a11y section about manual focus restoration (#21011)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-933e1e349');
+const VERSION = new Version('11.1.0-next.0-sha-9f641d153');
 
 /**
  * @license
