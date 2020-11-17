e42483bc9379387b9cabf8081d9c54e053a540ee
Michael-James <2666285+MichaelJamesParsons@users.noreply.github.com>
2020-11-17 18:56:08 +0000

    11.0.x - cb6f05347 refactor(material/datepicker): expose month view change detector ref (#21048)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-sha-42acf116b');
+const VERSION = new Version('11.0.0-sha-cb6f05347');
 
 /**
  * @license
