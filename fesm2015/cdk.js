d996728bf7c1337c3542fedb74e75431b406cc88
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-11-17 17:23:31 +0000

    master - 44b7e1a57 fix(material/select): remove focus outline on panel (#21005)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-17c357f69');
+const VERSION = new Version('11.1.0-next.0-sha-44b7e1a57');
 
 /**
  * @license
