cfbb458274cb916b117509470fdc65721c862d1e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-11-17 17:11:59 +0000

    master - efd8ab640 fix(material-experimental/mdc-progress-spinner): prevent animation from affecting surrounding layout (#20962)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-eda397c8d');
+	var VERSION = new core.Version('11.1.0-next.0-sha-efd8ab640');
 
 	/**
 	 * @license
