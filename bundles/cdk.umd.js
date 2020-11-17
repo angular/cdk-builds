d9444c4114688bb5b8826e37c4e1fa669665409c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-11-17 23:55:09 +0000

    11.0.x - 445b1a59c refactor(google-maps): simplify marker internal setup (#20975)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-sha-7ab8a8e0c');
+	var VERSION = new core.Version('11.0.0-sha-445b1a59c');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-sha-7ab8a8e0c');
+const VERSION = new Version('11.0.0-sha-445b1a59c');
 
 /**
  * @license
