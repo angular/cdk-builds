734255bcfa0d275a7a69b159f54e0932029558f5
Wagner Maciel <wagnermaciel@google.com>
2020-11-18 12:08:21 +0000

    11.0.x - 3ee3e3085 fix(google-maps): avoid using dotted property access (#21074)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.0-sha-445b1a59c');
+	var VERSION = new core.Version('11.0.0-sha-3ee3e3085');
 
 	/**
 	 * @license
