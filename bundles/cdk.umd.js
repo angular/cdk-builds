3396fd1324cccb5a383e16c41849ecdde4b7e451
Annie Wang <annieyw@google.com>
2020-11-19 20:38:02 +0000

    master - 5ab55fc52 docs(material/tooltip): add harness example for tooltip (#21083)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-dd8a92503');
+	var VERSION = new core.Version('11.1.0-next.0-sha-5ab55fc52');
 
 	/**
 	 * @license
