488d1109ebbc9b3ef169173d2ac748e42fdbdcf1
Annie Wang <annieyw@google.com>
2020-11-19 13:19:57 +0000

    master - 2a9919a65 docs(material/tree): add harness example for tree (#21082)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-d511cd043');
+	var VERSION = new core.Version('11.1.0-next.0-sha-2a9919a65');
 
 	/**
 	 * @license
