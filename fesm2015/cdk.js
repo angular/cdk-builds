b2ab76890e582893b8bf607fb5e68cfda1cf5628
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-11-19 16:50:54 +0000

    master - d9cc05513 fix(material/datepicker): screen reader close button style specificity too low (#21058)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-e9323b9c0');
+const VERSION = new Version('11.1.0-next.0-sha-d9cc05513');
 
 /**
  * @license
