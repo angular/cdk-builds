189045f5c0cb4325c65c9b7ef19ca71f3676fcd1
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-11-24 17:07:47 +0000

    master - 5bfe312b5 build: fix exports check failure (#21136)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-a235792a5');
+	var VERSION = new core.Version('11.1.0-next.0-sha-5bfe312b5');
 
 	/**
 	 * @license
