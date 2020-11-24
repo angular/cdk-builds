8625c81dbacb6f8047b205089a89c42629035704
Wagner Maciel <wagnermaciel@google.com>
2020-11-24 18:15:02 +0000

    master - c14d92ce7 release: update changelog (#21140)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-5bfe312b5');
+const VERSION = new Version('11.1.0-next.0-sha-c14d92ce7');
 
 /**
  * @license
