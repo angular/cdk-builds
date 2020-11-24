20c44ee96927eb0b4ccb3af86b0c7399adf302b3
Wagner Maciel <wagnermaciel@google.com>
2020-11-24 01:11:05 +0000

    11.0.x - 00127c12c build: update yarn.lock (#21125)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.0-sha-5800ab99b');
+const VERSION = new Version('11.0.0-sha-00127c12c');
 
 /**
  * @license
