313578572b7c8f0c994ce2dd99f6aa2c0c5b6a31
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-11-24 04:46:01 +0000

    master - 909c1e098 refactor(material/datepicker): consistently import MatCommonModule everywhere (#19861)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-bb2bb9c30');
+	var VERSION = new core.Version('11.1.0-next.0-sha-909c1e098');
 
 	/**
 	 * @license
