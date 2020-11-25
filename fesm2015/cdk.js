02492727345a15c3816e2e65c0013a236a7ac9df
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-11-25 16:34:17 +0000

    master - 26c7e5404 build: fix errors with latest MDC canary version (#21151)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-34a43f622');
+const VERSION = new Version('11.1.0-next.0-sha-26c7e5404');
 
 /**
  * @license
