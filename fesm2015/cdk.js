1d733131ba18d23b92f68c5495e8818ac474920e
Michael Prentice <splaktar@gmail.com>
2020-11-25 05:51:15 +0000

    11.0.x - 38b80c880 docs(material/chips): add mat-labels to examples (#21126)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.1-sha-b455609a3');
+const VERSION = new Version('11.0.1-sha-38b80c880');
 
 /**
  * @license
