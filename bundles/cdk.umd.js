8de8787982f0a151d63242b66287a03770f7c5cd
Amit Gharat <amit.2006.it@gmail.com>
2020-12-01 22:15:03 +0000

    master - bc9d56c94 docs(material/checkbox): use the defined color in the template (#21163) (#21167)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-2f43fd9d3');
+	var VERSION = new core.Version('11.1.0-next.0-sha-bc9d56c94');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-2f43fd9d3');
+const VERSION = new Version('11.1.0-next.0-sha-bc9d56c94');
 
 /**
  * @license
