9473ddb10f430e8b83e3d872ef34d3e21ff4cc26
Mitchell Wills <mwills@google.com>
2020-12-04 20:07:54 +0000

    master - 5a28f0043 fix(material-experimental/mdc-radio): include form field styles in mdc-radio theme (#21168)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-e95edadfa');
+	var VERSION = new core.Version('11.1.0-next.0-sha-5a28f0043');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-e95edadfa');
+const VERSION = new Version('11.1.0-next.0-sha-5a28f0043');
 
 /**
  * @license
