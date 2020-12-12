351ebcf0489574cd514de412456a2304c27c043a
mmalerba <mmalerba@google.com>
2020-12-12 00:06:59 +0000

    11.0.x - c6231e19b refactor(material/core): make typography normalization function private (#21286)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.2-sha-75e23cb8d');
+	var VERSION = new core.Version('11.0.2-sha-c6231e19b');
 
 	/**
 	 * @license
