b35bb480fc08cb7e514324361440b22bc410af16
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-12-14 21:34:59 +0000

    11.0.x - 413b7b300 build: clean up some internal deprecated API usages (#21342)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.3-sha-2e45a8ed3');
+const VERSION = new Version('11.0.3-sha-413b7b300');
 
 /**
  * @license
