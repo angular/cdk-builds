08a5eebfb69f4a1bae06e15ceb2a5def6c0523fb
Wagner Maciel <wagnermaciel@google.com>
2020-12-14 17:59:44 +0000

    11.0.x - 2e45a8ed3 release: bump version to 11.0.3 w/ changelog (#21344)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.2-sha-c6231e19b');
+const VERSION = new Version('11.0.3-sha-2e45a8ed3');
 
 /**
  * @license
