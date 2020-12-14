8fe5bacadf12b26a11a170c6448545609fd652c0
Aristeidis Bampakos <bampakoa@gmail.com>
2020-12-14 21:35:31 +0000

    master - 272c1a33a docs: fix link in DEV_ENVIRONMENT guide (#20773)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-e70e5dd30');
+	var VERSION = new core.Version('11.1.0-next.0-sha-272c1a33a');
 
 	/**
 	 * @license
