170822af78eee39a8bfa4f53b5d7b42239b4d675
Andrew Seguin <andrewseguin@users.noreply.github.com>
2020-12-17 07:19:31 +0000

    master - c5a111d98 fix(material-experimental/mdc-button): fix ripple noop animation (#21360)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-f0f3748b7');
+	var VERSION = new core.Version('11.1.0-next.0-sha-c5a111d98');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-f0f3748b7');
+const VERSION = new Version('11.1.0-next.0-sha-c5a111d98');
 
 /**
  * @license
