df3386ffe0e272481da898fdd5491e9883e0451a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-12-17 22:48:13 +0000

    master - 6d3694251 fix(material/table): unsubscribe from source when disconnecting (#21338)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-411b1747d');
+	var VERSION = new core.Version('11.1.0-next.0-sha-6d3694251');
 
 	/**
 	 * @license
