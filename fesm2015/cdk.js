4b3501190fc2b131916c3c01fbce3ece44363192
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-12-17 07:30:35 +0000

    11.0.x - 2a6862d9e fix(cdk/scrolling): virtual scroll not picking up trackBy function when items come in after init (#21335)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.3-sha-4e6b00465');
+const VERSION = new Version('11.0.3-sha-2a6862d9e');
 
 /**
  * @license
