7f636a66233132d8f95e644e7ae574c9693d0a1e
Annie Wang <annieyw@google.com>
2020-12-17 07:19:10 +0000

    master - f0f3748b7 feat(material-experimental/mdc-table): add mat-table selector (#20994)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-3c37e4b1c');
+const VERSION = new Version('11.1.0-next.0-sha-f0f3748b7');
 
 /**
  * @license
