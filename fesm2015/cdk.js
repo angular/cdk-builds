1c444fe880e1865a4ac734fbc260136e653e2253
Annie Wang <annieyw@google.com>
2021-01-06 11:47:56 +0000

    master - 714f70fa3 docs(material/paginator): add harness example for paginator (#21454)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-47fa75cec');
+const VERSION = new Version('11.1.0-next.0-sha-714f70fa3');
 
 /**
  * @license
