1770a83cda6a9c2b8b096203454b372e1eda2fbd
Annie Wang <annieyw@google.com>
2021-01-06 11:59:43 +0000

    master - 2c73a4c19 docs(material/expansion): add harness example for expansion (#21472)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-1df54cc80');
+	var VERSION = new core.Version('11.1.0-next.0-sha-2c73a4c19');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-1df54cc80');
+const VERSION = new Version('11.1.0-next.0-sha-2c73a4c19');
 
 /**
  * @license
