ea5776d8265eee518a09195f46910426146d4588
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-01-06 17:28:05 +0000

    master - 3335a9295 fix(material-experimental/mdc-autocomplete): panel losing max-height if menu is opened after autocomplete (#21394)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-f940d344b');
+const VERSION = new Version('11.1.0-next.0-sha-3335a9295');
 
 /**
  * @license
