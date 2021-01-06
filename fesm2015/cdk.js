31e813f2b17ccdfce711597d199b9adf4773cad5
Michael-James <2666285+MichaelJamesParsons@users.noreply.github.com>
2021-01-06 17:14:39 +0000

    master - 74cd45a9b refactor(material/datepicker): expose year view change detector ref (#21499)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-3484f711b');
+const VERSION = new Version('11.1.0-next.0-sha-74cd45a9b');
 
 /**
  * @license
