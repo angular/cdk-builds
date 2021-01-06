ec4843c83ebe600cd37b7baa653ea61f457c36da
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-01-06 17:22:02 +0000

    master - 37a9c4ee4 fix(material-experimental/mdc-progress-spinner): indeterminate animation not working (#21391)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-eb318d938');
+const VERSION = new Version('11.1.0-next.0-sha-37a9c4ee4');
 
 /**
  * @license
