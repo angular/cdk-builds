bc98b7459e1036ec39d70774613247ed0b057084
Annie Wang <annieyw@google.com>
2021-01-06 02:36:59 +0000

    master - 47fa75cec Project import generated by Copybara. (#21502)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-15c41757b');
+const VERSION = new Version('11.1.0-next.0-sha-47fa75cec');
 
 /**
  * @license
