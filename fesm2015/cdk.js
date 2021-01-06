ae2f3cb21f8a3a21712401018c0160469c19d255
Annie Wang <annieyw@google.com>
2021-01-06 11:58:13 +0000

    master - 1df54cc80 docs(material/form-field): add harness example for form-field (#21471)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-988c945cc');
+const VERSION = new Version('11.1.0-next.0-sha-1df54cc80');
 
 /**
  * @license
