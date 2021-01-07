c85b610e71b8d20d3afc70e225077947efadef90
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-01-07 13:45:54 +0000

    11.0.x - 5c25fca4d build: fix compilation errors in utility scripts (#21488)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.3-sha-f9a166d5e');
+const VERSION = new Version('11.0.3-sha-5c25fca4d');
 
 /**
  * @license
