b7f857ce79eb8e472a0b899643cfb4edfa7f54e6
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-01-07 16:58:31 +0000

    master - 6c1001f5b build: update to gulp 4 (#21445)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-fb833e75c');
+	var VERSION = new core.Version('11.1.0-next.0-sha-6c1001f5b');
 
 	/**
 	 * @license
