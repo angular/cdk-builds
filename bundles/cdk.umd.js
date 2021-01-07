e5fa0a8fbdd2c4340e897f316cd9d7a66f33b424
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-01-07 13:45:43 +0000

    master - 841d9e1e2 build: bump year (#21486)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-461602d4a');
+	var VERSION = new core.Version('11.1.0-next.0-sha-841d9e1e2');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-461602d4a');
+const VERSION = new Version('11.1.0-next.0-sha-841d9e1e2');
 
 /**
  * @license
