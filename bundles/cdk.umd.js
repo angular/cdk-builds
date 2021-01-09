60daf8dd00aa899338bbcf44f1b85eeae647d029
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-01-09 06:50:29 +0000

    master - d5908473b feat(material/datepicker): add support for cancel/apply buttons (#21487)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-f8df9f86d');
+	var VERSION = new core.Version('11.1.0-next.0-sha-d5908473b');
 
 	/**
 	 * @license
