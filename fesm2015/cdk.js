063e5f890c1a2e7a920e45a76779b262969b0eee
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-01-09 07:00:37 +0000

    master - 1fb03ceaa feat(material/icon): add support for registry resolver function (#21431)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-1f1875058');
+const VERSION = new Version('11.1.0-next.0-sha-1fb03ceaa');
 
 /**
  * @license
