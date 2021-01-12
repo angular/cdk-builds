2b5709a7432e28257ad5fb22e4ad7ce8c5d22007
mmalerba <mmalerba@google.com>
2021-01-12 23:29:48 +0000

    11.0.x - 6771ca470 Revert "fix(material-experimental/mdc-table): error when flexbox-based table is initialized (#21428)" (#21568)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.3-sha-17240311c');
+	var VERSION = new core.Version('11.0.3-sha-6771ca470');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.3-sha-17240311c');
+const VERSION = new Version('11.0.3-sha-6771ca470');
 
 /**
  * @license
