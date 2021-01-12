0ee731d395912a769f3fefb438828332d951e082
Annie Wang <annieyw@google.com>
2021-01-12 18:05:13 +0000

    master - 3d92f91b3 docs(material/dialog): add harness example for dialog (#21474)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-ddc2e23e8');
+	var VERSION = new core.Version('11.1.0-next.0-sha-3d92f91b3');
 
 	/**
 	 * @license
