941354451feb89670c0d525cc0d9763248eaca2c
Michael Prentice <splaktar@gmail.com>
2021-01-12 18:11:17 +0000

    master - bd88e6411 docs(material/tree): add a description of the when predicate (#21545)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-b842891d8');
+const VERSION = new Version('11.1.0-next.0-sha-bd88e6411');
 
 /**
  * @license
