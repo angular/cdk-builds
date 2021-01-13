d81d8c6b032d96da6a604bcbae46b622de934471
mmalerba <mmalerba@google.com>
2021-01-13 21:15:02 +0000

    master - 8aebd0398 feat(material-experimental/mdc-autocomplete): add panel animation (#21525)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-d53c60f4e');
+	var VERSION = new core.Version('11.1.0-next.0-sha-8aebd0398');
 
 	/**
 	 * @license
