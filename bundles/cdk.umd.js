ed1941563cc408809cddba29129c60d1a2eaed2a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-01-14 23:59:45 +0000

    master - 4be7d155d fix(material-experimental/mdc-list): not accounting for global ripple options (#21393)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.2.0-next.0-sha-ecb5663c6');
+	var VERSION = new core.Version('11.2.0-next.0-sha-4be7d155d');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.2.0-next.0-sha-ecb5663c6');
+const VERSION = new Version('11.2.0-next.0-sha-4be7d155d');
 
 /**
  * @license
