87a61dd38ad2f5db3b8c20561006e553cd3bd229
mmalerba <mmalerba@google.com>
2021-01-14 22:58:41 +0000

    11.1.x - fbeec4241 release: bump version to 11.1.0-rc.0 w/ changelog (#21596)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-11e9f464e');
+const VERSION = new Version('11.1.0-rc.0-sha-fbeec4241');
 
 /**
  * @license
