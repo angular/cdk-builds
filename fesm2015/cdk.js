61b2b33e64168b9674c10f135e770fa22f093264
mmalerba <mmalerba@google.com>
2021-01-14 21:56:54 +0000

    master - 11e9f464e release: fix release-script (#21595)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-next.0-sha-dc33472ac');
+const VERSION = new Version('11.1.0-next.0-sha-11e9f464e');
 
 /**
  * @license
