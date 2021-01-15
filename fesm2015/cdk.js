6adae925dca3bec2c37a831d4bbbe27341317902
Joey Perrott <josephperrott.github@gmail.com>
2021-01-15 23:08:28 +0000

    master - 0911820f9 build: configure the ng-dev caretaker check command (#21611)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.2.0-next.0-sha-4ecdcb53a');
+const VERSION = new Version('11.2.0-next.0-sha-0911820f9');
 
 /**
  * @license
