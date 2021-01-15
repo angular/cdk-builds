942ead59df23f2ecce483e26d89948c5c6dbc853
Miles Malerba <mmalerba@google.com>
2021-01-15 22:03:47 +0000

    11.0.x - 02201c9ea Revert "build: bump @bazel/jasmine from 2.2.1 to 3.0.0 (#21424)"

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.4-sha-a13da618d');
+	var VERSION = new core.Version('11.0.4-sha-02201c9ea');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.0.4-sha-a13da618d');
+const VERSION = new Version('11.0.4-sha-02201c9ea');
 
 /**
  * @license
