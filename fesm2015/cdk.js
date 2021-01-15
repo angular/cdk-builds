63887c3f00912d6414ae3ff0ee79c098c1f4c181
marvinderksen <derksen.marvin@gmail.com>
2021-01-15 18:27:39 +0000

    master - 6a5816169 docs(cdk/tree): Overflowing inline-code markup in readme (#21426)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.2.0-next.0-sha-a567c6481');
+const VERSION = new Version('11.2.0-next.0-sha-6a5816169');
 
 /**
  * @license
