e8adad9b20ec140458d4a527ac08b6445e467d3d
Rafael Santana <rafaelsantana3095@gmail.com>
2021-01-15 18:36:18 +0000

    master - c7ef1a168 docs: fix incorrect changelog entries (#21600)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.2.0-next.0-sha-6a5816169');
+	var VERSION = new core.Version('11.2.0-next.0-sha-c7ef1a168');
 
 	/**
 	 * @license
