4302c83c7c1ca02c15a6810b5814cf67eb24c508
Annie Wang <annieyw@google.com>
2021-01-15 00:04:21 +0000

    master - e93edbc2f docs(material/stepper): add harness example for stepper (#21409)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.2.0-next.0-sha-4be7d155d');
+	var VERSION = new core.Version('11.2.0-next.0-sha-e93edbc2f');
 
 	/**
 	 * @license
