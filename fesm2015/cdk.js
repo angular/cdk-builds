bb02dd27573661ede559ca9e6db6e554590e441d
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-01-15 00:19:53 +0000

    master - f12f6c36f refactor(material/core): remove unnecessary styles from mat-option (#21432)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.2.0-next.0-sha-bf0dff8fb');
+const VERSION = new Version('11.2.0-next.0-sha-f12f6c36f');
 
 /**
  * @license
