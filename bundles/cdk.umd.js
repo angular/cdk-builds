39e8c3274b345a9bc60b9acd17652cb9631b867c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-01-15 18:26:04 +0000

    master - a567c6481 build: check tooling scripts for compilation errors (#21604)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.2.0-next.0-sha-0c8cfa7c0');
+	var VERSION = new core.Version('11.2.0-next.0-sha-a567c6481');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.2.0-next.0-sha-0c8cfa7c0');
+const VERSION = new Version('11.2.0-next.0-sha-a567c6481');
 
 /**
  * @license
