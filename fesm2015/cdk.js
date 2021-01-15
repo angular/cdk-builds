cc6a340ebc9e42ecea6f7779ce17a3319ad6278d
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-01-15 00:18:24 +0000

    master - bf0dff8fb fix(material/tabs): background color inherited to nested groups (#21533)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.2.0-next.0-sha-6f06f9694');
+const VERSION = new Version('11.2.0-next.0-sha-bf0dff8fb');
 
 /**
  * @license
