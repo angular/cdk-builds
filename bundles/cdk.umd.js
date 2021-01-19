db44918e371cdf3f3879c835c6df1f5d570852b4
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-01-19 17:41:59 +0000

    11.0.x - 30e351e35 refactor(material/core): clean up deprecation API usages (#21597)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.0.4-sha-2bad02f7a');
+	var VERSION = new core.Version('11.0.4-sha-30e351e35');
 
 	/**
 	 * @license
