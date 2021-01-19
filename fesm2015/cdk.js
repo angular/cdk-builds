298f15579209886fe6b88156907153de519af97e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-01-19 17:40:45 +0000

    master - ddf99cbba fix(material/tooltip): not closing when clicking outside using auxiliary button (#21467)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.2.0-next.0-sha-2d66ecbee');
+const VERSION = new Version('11.2.0-next.0-sha-ddf99cbba');
 
 /**
  * @license
