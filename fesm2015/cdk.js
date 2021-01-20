27593596e0b9eafd90f958dcda2c734d71c09ec9
mmalerba <mmalerba@google.com>
2021-01-20 22:19:03 +0000

    11.1.x - 9b1ef8569 release: bump angular deps to 11.1.0 (#21651)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-rc.0-sha-453c4b90b');
+const VERSION = new Version('11.1.0-rc.0-sha-9b1ef8569');
 
 /**
  * @license
