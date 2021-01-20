04e66a1414be9ef8bca0b96916de73656dd723c1
mmalerba <mmalerba@google.com>
2021-01-20 22:19:16 +0000

    master - c46fc8dc1 release: bump angular deps to 11.1.0 (#21651)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.2.0-next.0-sha-182aba4b5');
+	var VERSION = new core.Version('11.2.0-next.0-sha-c46fc8dc1');
 
 	/**
 	 * @license
