0361023fb3c93117fb6f39bd6570a7fddd2bbcf7
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-01-21 13:34:02 +0000

    master - acb3f3341 build: fix error from latest MDC canary (#21663)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.2.0-next.0-sha-9190785fd');
+	var VERSION = new core.Version('11.2.0-next.0-sha-acb3f3341');
 
 	/**
 	 * @license
