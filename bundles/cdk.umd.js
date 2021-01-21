3b12548863cf6823490a53ce958bd26c9a9d72e3
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-01-21 13:35:05 +0000

    11.1.x - f8c3597d8 build: fix error from latest MDC canary (#21663)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.0-sha-b270f5ac0');
+	var VERSION = new core.Version('11.1.0-sha-f8c3597d8');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-sha-b270f5ac0');
+const VERSION = new Version('11.1.0-sha-f8c3597d8');
 
 /**
  * @license
