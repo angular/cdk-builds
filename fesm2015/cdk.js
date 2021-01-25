2e0f922b3cb4862d617fad1c32cc13e12c4f5a4c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-01-25 18:20:12 +0000

    11.1.x - 9c99070be fix(material/autocomplete): prevent inert host node from affecting surrounding layout (#21649)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-sha-0134ac77c');
+const VERSION = new Version('11.1.0-sha-9c99070be');
 
 /**
  * @license
