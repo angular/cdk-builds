60383a77b640da7d81af1b94bb7bc05dc0a4baaa
Annie Wang <annieyw@google.com>
2021-01-25 20:04:59 +0000

    master - 277ad5b40 fix(material-experimental/mdc-chips): set correct ripple opacity for … (#21624)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.2.0-next.0-sha-3269daefd');
+const VERSION = new Version('11.2.0-next.0-sha-277ad5b40');
 
 /**
  * @license
