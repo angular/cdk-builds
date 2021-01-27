580130bcefa390989d39cac05fa4f76644c50405
Andrew Seguin <andrewseguin@users.noreply.github.com>
2021-01-27 15:34:23 +0000

    11.1.x - 512fadee3 release: bump version to 11.1.1 w/ changelog (#21718)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-sha-7643a3cb1');
+const VERSION = new Version('11.1.1-sha-512fadee3');
 
 /**
  * @license
