1ab781480d063163896e292713b0cb1cfa2e1ba6
mmalerba <mmalerba@google.com>
2021-01-27 13:28:05 +0000

    11.1.x - 7643a3cb1 fix(material-experimental/mdc-chips): decouple removal from animation (#21636)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.1.0-sha-2eb897245');
+const VERSION = new Version('11.1.0-sha-7643a3cb1');
 
 /**
  * @license
