74f2f9684cbf261138a8a3d5860fc873167069a4
Andrew Seguin <andrewjs@google.com>
2021-01-29 21:24:01 +0000

    master - 2e1f809f9 release: bump changelog to 11.1.1

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.2.0-next.0-sha-223215692');
+const VERSION = new Version('11.2.0-next.0-sha-2e1f809f9');
 
 /**
  * @license
