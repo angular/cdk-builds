e46f46303044f391c721c8d442d2c9d25b0afea7
Esteban Gehring <esteban.gehring@bithost.ch>
2021-01-30 16:11:32 +0000

    master - ccf2b60a2 docs(material-experimental/mdc-core): fix scss examples (#21740)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.2.0-next.0-sha-3594a5eb9');
+const VERSION = new Version('11.2.0-next.0-sha-ccf2b60a2');
 
 /**
  * @license
