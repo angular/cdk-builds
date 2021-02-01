fc31c4d13a176fbd924a59fe669a7cb80e5b5c3c
Michael-James <2666285+MichaelJamesParsons@users.noreply.github.com>
2021-02-01 04:28:06 +0000

    master - dff4a3ca2 feat(material/sort): Add default configuration options (#21648)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.2.0-next.0-sha-57c68e81e');
+	var VERSION = new core.Version('11.2.0-next.0-sha-dff4a3ca2');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.2.0-next.0-sha-57c68e81e');
+const VERSION = new Version('11.2.0-next.0-sha-dff4a3ca2');
 
 /**
  * @license
