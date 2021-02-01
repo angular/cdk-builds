f4b174dc4c233036c4134a73e2d4d12775bec7c3
Adam Kocsis <kocccs@gmail.com>
2021-02-01 04:25:23 +0000

    master - da1f7e7a5 fix(material/select): overlay offset calculation for disabled option centering (#21716)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.2.0-next.0-sha-dbb39a10d');
+	var VERSION = new core.Version('11.2.0-next.0-sha-da1f7e7a5');
 
 	/**
 	 * @license
