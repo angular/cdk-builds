2d449c155b7fdc87bde1d4da02efcad58cfdfa0e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-02-01 04:29:33 +0000

    master - 192efb80a fix(material/datepicker): reset preview range after selecting a date (#21591)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.2.0-next.0-sha-dff4a3ca2');
+	var VERSION = new core.Version('11.2.0-next.0-sha-192efb80a');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.2.0-next.0-sha-dff4a3ca2');
+const VERSION = new Version('11.2.0-next.0-sha-192efb80a');
 
 /**
  * @license
