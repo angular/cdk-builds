fe31ce7f6c86e9bce958685fe6186f6686fde163
Michael-James <2666285+MichaelJamesParsons@users.noreply.github.com>
2021-02-02 04:15:40 +0000

    master - 155c7c2da feat(cdk/table): add HTML elements to sticky styler update (#21704)

--- fesm2015/table.js
+++ fesm2015/table.js
@@ -780,10 +780,10 @@ class StickyStyler {
                 }
             }
             if (position === 'top') {
-                (_a = this._positionListener) === null || _a === void 0 ? void 0 : _a.stickyHeaderRowsUpdated({ sizes: stickyCellHeights });
+                (_a = this._positionListener) === null || _a === void 0 ? void 0 : _a.stickyHeaderRowsUpdated({ sizes: stickyCellHeights, elements: elementsToStick });
             }
             else {
-                (_b = this._positionListener) === null || _b === void 0 ? void 0 : _b.stickyFooterRowsUpdated({ sizes: stickyCellHeights });
+                (_b = this._positionListener) === null || _b === void 0 ? void 0 : _b.stickyFooterRowsUpdated({ sizes: stickyCellHeights, elements: elementsToStick });
             }
         });
     }
