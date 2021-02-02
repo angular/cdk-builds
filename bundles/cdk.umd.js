4a832976dd7298d4dd6c99d103ee2f1027bb1048
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-02-02 20:21:26 +0000

    11.1.x - fdacb22f2 fix(material/expansion): add focus indication in high contrast mode (#21549)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.1.1-sha-d0106c190');
+	var VERSION = new core.Version('11.1.1-sha-fdacb22f2');
 
 	/**
 	 * @license
