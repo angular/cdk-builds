e9ba57e59563bb33915c8be27b873b82cb85e6d6
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-02-05 10:08:08 +0000

    master - ae6e930bf feat(material/list): return selected options from selection list selectAll/deselectAll (#21358)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.0-sha-9dbf49b66');
+const VERSION = new Version('12.0.0-next.0-sha-ae6e930bf');
 
 /**
  * @license
