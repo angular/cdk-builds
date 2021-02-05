18e774aceb6dbf8b9ebb9d7298352430202c8fd7
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-02-05 10:32:49 +0000

    master - d2165529e build: a couple of minor improvements to the dev app (#21766)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.0-sha-a1776165c');
+	var VERSION = new core.Version('12.0.0-next.0-sha-d2165529e');
 
 	/**
 	 * @license
