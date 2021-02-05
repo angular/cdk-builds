0514b816dc6755f178b3e0eb230aa59046b82631
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-02-05 10:19:50 +0000

    master - ab8f62f49 refactor(material-experimental/mdc-slide-toggle): de-duplicate test harness logic (#21747)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.0-sha-da5e2eb39');
+const VERSION = new Version('12.0.0-next.0-sha-ab8f62f49');
 
 /**
  * @license
