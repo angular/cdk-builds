c62d5a3f0dc30346a5e28ca3cef178d18c06df73
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-02-05 10:17:05 +0000

    master - d3cdb345e fix(material/menu): unable to move focus from inside opened event (#20968)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.0-sha-060ab9eb5');
+const VERSION = new Version('12.0.0-next.0-sha-d3cdb345e');
 
 /**
  * @license
