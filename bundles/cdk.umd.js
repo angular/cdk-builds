07a5e3e1b05574d5533e1471e2460d981af427cf
Annie Wang <annieyw@google.com>
2021-02-05 10:12:51 +0000

    master - f7ace54fe feat(material-experimental/mdc-button): add extended fab (#21585)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.0-sha-f68662cd5');
+	var VERSION = new core.Version('12.0.0-next.0-sha-f7ace54fe');
 
 	/**
 	 * @license
