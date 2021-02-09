3a2ade8e05a48943e98859e044641355a2c3fbbb
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-02-09 16:41:27 +0000

    master - 28c36f8a0 refactor(google-maps): clean up internal setup (#21079)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.0-sha-cd3d0e946');
+	var VERSION = new core.Version('12.0.0-next.0-sha-28c36f8a0');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.0-sha-cd3d0e946');
+const VERSION = new Version('12.0.0-next.0-sha-28c36f8a0');
 
 /**
  * @license
