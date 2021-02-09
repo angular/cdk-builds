8e2faaed92b85f8dbf1a37ff08bd76244ffce9f3
mmalerba <mmalerba@google.com>
2021-02-09 16:34:46 +0000

    master - 62d2e37e6 fix(material-experimental/mdc-form-field): allow shrinking in grid/flex layouts (#21820)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.0-sha-6eff9386b');
+	var VERSION = new core.Version('12.0.0-next.0-sha-62d2e37e6');
 
 	/**
 	 * @license
