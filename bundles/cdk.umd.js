9f9ab29c206b65fc101d6aaa38315f96be5a0cc9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-02-09 16:48:19 +0000

    master - c7db803f7 build: set up generic way of passing in environment variables to dev app (#21825)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.0-sha-048431057');
+	var VERSION = new core.Version('12.0.0-next.0-sha-c7db803f7');
 
 	/**
 	 * @license
