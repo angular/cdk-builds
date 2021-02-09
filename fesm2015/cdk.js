dd411214fb150f5832ae8308b982575fe4fffe92
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-02-09 17:36:33 +0000

    master - 0930dc37c build: fix test failures and add missing tests (#21850)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.0-sha-5b62f136f');
+const VERSION = new Version('12.0.0-next.0-sha-0930dc37c');
 
 /**
  * @license
