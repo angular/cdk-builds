fd1f799346475803912e0ec2735a13828a47b10c
Annie Wang <annieyw@google.com>
2021-02-10 14:37:31 +0000

    11.2.x - 14b5c3f89 release: bump version to 11.2.0 w/ changelog (#21864)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.2.0-rc.0-sha-5fcb47a49');
+	var VERSION = new core.Version('11.2.0-sha-14b5c3f89');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.2.0-rc.0-sha-5fcb47a49');
+const VERSION = new Version('11.2.0-sha-14b5c3f89');
 
 /**
  * @license
