93850c0d275dc2b655f62cb43092ce5d971bbf96
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-02-10 21:03:57 +0000

    master - 70ff52878 fix(material/tooltip): clear pending timers on destroy (#16756)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.0-sha-1a1002797');
+	var VERSION = new core.Version('12.0.0-next.0-sha-70ff52878');
 
 	/**
 	 * @license
