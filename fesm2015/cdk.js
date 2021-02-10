b9de1c6605a44b8721580adf75a0ec75c5781b9b
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-02-10 20:53:16 +0000

    master - 1a1002797 feat(material/tooltip): add class to tooltip element based on the current position (#15217)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.0-sha-d0c53ac96');
+const VERSION = new Version('12.0.0-next.0-sha-1a1002797');
 
 /**
  * @license
