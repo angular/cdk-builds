35f298a5323fd1830e0907ee6739cb7f77410edf
mmalerba <mmalerba@google.com>
2021-02-12 21:39:52 +0000

    master - a41f866e3 docs(material/core): show typography as part of the theme (#21810)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.0-sha-cdb2326fd');
+const VERSION = new Version('12.0.0-next.0-sha-a41f866e3');
 
 /**
  * @license
