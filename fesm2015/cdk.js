22a110c15aa5b00cd8db55f3bb187b77f150a0eb
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-02-12 21:36:15 +0000

    master - 6f998527c fix(material/stepper): don't allow focus inside a hidden element (#21842)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.0-sha-e7b604f2d');
+const VERSION = new Version('12.0.0-next.0-sha-6f998527c');
 
 /**
  * @license
