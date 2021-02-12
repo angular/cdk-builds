508c5e4f72623524ecc25978a020be2a361ac560
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-02-12 21:30:58 +0000

    master - 831cbbf68 build: update to Angular 12 (#21885)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.0-sha-70ff52878');
+const VERSION = new Version('12.0.0-next.0-sha-831cbbf68');
 
 /**
  * @license
