e86d6ab27040b9ba0060e0aed1d46058678a866c
Malte <ilmalte@outlook.com>
2021-02-16 17:39:34 +0000

    master - a4331ad99 fix(google-maps): correct minor typo in the code (#21917)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.0-sha-aa7dc0056');
+	var VERSION = new core.Version('12.0.0-next.0-sha-a4331ad99');
 
 	/**
 	 * @license
