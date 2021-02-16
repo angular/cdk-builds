db2ae180c61486e164418f0080170492e5eb0fd2
AlexElin <alexey.elin@list.ru>
2021-02-16 17:42:04 +0000

    master - ddb206e25 docs: use BooleanInput in examples for custom control (#21913)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.0-sha-b9a26a28f');
+const VERSION = new Version('12.0.0-next.0-sha-ddb206e25');
 
 /**
  * @license
