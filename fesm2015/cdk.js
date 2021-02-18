7dbbdeb16f6b2ba8ac8c2bd36051454784c30351
Andrew Seguin <andrewseguin@users.noreply.github.com>
2021-02-18 21:00:14 +0000

    master - ce7b92f96 fix(material-experimental/mdc-core): rename core import file (#21953)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.0-sha-6d413e7d7');
+const VERSION = new Version('12.0.0-next.0-sha-ce7b92f96');
 
 /**
  * @license
