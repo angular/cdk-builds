846a57d7a1881c9aa0e11dc1ea6e126f8f8e8ac2
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-02-18 19:55:45 +0000

    master - 6f3df0160 refactor(material-experimental/mdc-form-field): de-duplicate test harness logic (#21900)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.0-sha-ce9c905ba');
+const VERSION = new Version('12.0.0-next.0-sha-6f3df0160');
 
 /**
  * @license
