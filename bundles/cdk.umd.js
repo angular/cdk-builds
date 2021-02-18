9a30f2734c18223f5b4c1a863ecbc94798f2d6f7
Andrew Seguin <andrewseguin@users.noreply.github.com>
2021-02-18 05:02:27 +0000

    master - 4ae90cc27 build: add .import sass files (#21869)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.0-sha-64a2a0728');
+	var VERSION = new core.Version('12.0.0-next.0-sha-4ae90cc27');
 
 	/**
 	 * @license
