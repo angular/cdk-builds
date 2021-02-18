56071f0678f61cffc4c2cf5a901a8e08f4ae56cb
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-02-18 19:49:31 +0000

    master - 3f1b1f79c fix(google-maps): mapTypeId not being set from options (#21909)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.0-sha-adb31c2d7');
+	var VERSION = new core.Version('12.0.0-next.0-sha-3f1b1f79c');
 
 	/**
 	 * @license
