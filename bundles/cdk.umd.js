24c2f506d54a1629c4c7ee109da13ec8afc6e17c
Annie Wang <annieyw@google.com>
2021-02-19 21:11:26 +0000

    11.2.x - 61deb7129 fix(material-experimental/mdc-button): use mdc mixins to style icons within buttons (#21623)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.2.1-sha-a30f656e2');
+	var VERSION = new core.Version('11.2.1-sha-61deb7129');
 
 	/**
 	 * @license
