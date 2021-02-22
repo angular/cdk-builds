e2eee70507776fb84570e0fdb0ea918b3c7ea18a
Joey Perrott <josephperrott.github@gmail.com>
2021-02-22 21:33:27 +0000

    master - 0e6bbb54f build: update mdc_snapshot_test_cronjob to yarn install before bazel setup (#21980)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.0-sha-496867df3');
+const VERSION = new Version('12.0.0-next.0-sha-0e6bbb54f');
 
 /**
  * @license
