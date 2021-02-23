9e86707a67e1c2939f2200db70652d1a83843b9a
jeripeierSBB <jeremias.peier@sbb.ch>
2021-02-23 18:54:17 +0000

    master - ea008d7ec test(material/select): remove form-field dependency in focus test (#21983)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.0-sha-5a9ff9a5c');
+	var VERSION = new core.Version('12.0.0-next.0-sha-ea008d7ec');
 
 	/**
 	 * @license
