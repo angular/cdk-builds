fcf6f73631c825f29432d1019623bf955cbf8f03
Andrew Seguin <andrewseguin@users.noreply.github.com>
2021-02-24 22:37:55 +0000

    11.2.x - 99f10c530 release: bump version to 11.2.2 w/ changelog (#22010)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.2.1-sha-5b30c4cfc');
+	var VERSION = new core.Version('11.2.2-sha-99f10c530');
 
 	/**
 	 * @license
