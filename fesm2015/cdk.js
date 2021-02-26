180e6d7ab39c0115e3e045b5a9ea2b94950dc163
AlexElin <alexey.elin@list.ru>
2021-02-26 03:59:26 +0000

    11.2.x - 48bd3e461 docs: mention `entryComponents` is no longer necessary (#21972)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.2.2-sha-a99a4d298');
+const VERSION = new Version('11.2.2-sha-48bd3e461');
 
 /**
  * @license
