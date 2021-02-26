ad22f79e133c9210fbb2aebcae3c6743bef737d8
Annie Wang <annieyw@google.com>
2021-02-26 13:11:14 +0000

    master - 088a75694 fix(material-experimental/mdc-button): add mdc ripple class for fab (#21923)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.0-sha-6e054c4af');
+const VERSION = new Version('12.0.0-next.0-sha-088a75694');
 
 /**
  * @license
