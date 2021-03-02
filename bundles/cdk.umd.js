984839745c0a4093671600b84d51b99ca616e8fc
Andrew Seguin <andrewseguin@users.noreply.github.com>
2021-03-02 19:34:52 +0000

    master - 7006b482c fix(material-experimental/mdc-checkbox): remove background state styles (#21748)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.0-sha-c791d6a6d');
+	var VERSION = new core.Version('12.0.0-next.0-sha-7006b482c');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.0-sha-c791d6a6d');
+const VERSION = new Version('12.0.0-next.0-sha-7006b482c');
 
 /**
  * @license
