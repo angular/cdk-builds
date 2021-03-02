c2aede2a25951c5d2bd161896522836f1cbda930
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-02 18:22:12 +0000

    master - efc0f9e70 fix(cdk/overlay): resolve regression when overlay is imported through shorthand path (#22043)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.0-sha-56a9fc36a');
+	var VERSION = new core.Version('12.0.0-next.0-sha-efc0f9e70');
 
 	/**
 	 * @license
