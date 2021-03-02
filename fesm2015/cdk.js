032920b2cecadf074897d9cf1c2a9b7c4ba112b5
mmalerba <mmalerba@google.com>
2021-03-02 16:54:54 +0000

    master - 716158724 fix(material-experimental/mdc-tooltip): change class prefixes to mat-mdc (#22025)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.0-sha-5c6129297');
+const VERSION = new Version('12.0.0-next.0-sha-716158724');
 
 /**
  * @license
