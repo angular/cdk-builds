45a3fba66298f2680081ac4fd9b212edbcbc29a1
Andrew Seguin <andrewseguin@users.noreply.github.com>
2021-03-02 19:32:24 +0000

    master - d8935d17b fix(material-experimental/mdc-form-field): always check hideRequiredMarker (#22068)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.0-sha-e7821185b');
+const VERSION = new Version('12.0.0-next.0-sha-d8935d17b');
 
 /**
  * @license
