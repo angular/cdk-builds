348a37438a0859bf9dcd00f056dc1fcf7b64ac8f
Wagner Maciel <wagnermaciel@google.com>
2021-03-03 21:32:24 +0000

    master - 691a0c86f build: bump angular version to 12 || 13 (#22101)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.0-sha-d7987f0f7');
+	var VERSION = new core.Version('12.0.0-next.0-sha-691a0c86f');
 
 	/**
 	 * @license
