dbfce58c9dbc2568886d39b2f430ecc12529e944
Wagner Maciel <wagnermaciel@google.com>
2021-03-03 22:04:52 +0000

    master - 389e66ace release: bump version to 12.0.0-next.1 w/ changelog (#22104)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.0-sha-128e9374b');
+const VERSION = new Version('12.0.0-next.1-sha-389e66ace');
 
 /**
  * @license
