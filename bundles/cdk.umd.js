2f80ddb06632f307be6510681468906a5b5d26c0
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-05 03:44:57 +0000

    11.2.x - e94b7bd3c fix(material-experimental/mdc-progress-bar): run resize observer outside zone (#21564)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.2.3-sha-322469ab8');
+	var VERSION = new core.Version('11.2.3-sha-e94b7bd3c');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.2.3-sha-322469ab8');
+const VERSION = new Version('11.2.3-sha-e94b7bd3c');
 
 /**
  * @license
