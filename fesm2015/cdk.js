e744a5d0b806441e1c696160fbceae81aec6dab2
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-05 03:47:11 +0000

    master - 40cce7691 fix(material-experimental/mdc-autocomplete): only animate along y axis (#21588)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.1-sha-373af254c');
+const VERSION = new Version('12.0.0-next.1-sha-40cce7691');
 
 /**
  * @license
