1a056ad6dbb6c2ce85be5273ee374f09c10d524c
Michael Prentice <splaktar@gmail.com>
2021-03-05 03:34:43 +0000

    master - 0441ae9e5 fix(material/schematics): generate code that works in strict CLI projects (#22052)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.1-sha-389e66ace');
+	var VERSION = new core.Version('12.0.0-next.1-sha-0441ae9e5');
 
 	/**
 	 * @license
