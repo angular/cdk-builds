9b9d0abaaf2f380e817cb843d3213e6a5d434897
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-05 03:51:52 +0000

    master - 9f879b291 fix(material-experimental/mdc-snack-bar): use MDC-based button (#21442)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.1-sha-d869d79bc');
+const VERSION = new Version('12.0.0-next.1-sha-9f879b291');
 
 /**
  * @license
