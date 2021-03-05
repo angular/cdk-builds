7de4c848671db912d32b6d0585e0f83ce4856d53
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-05 03:44:55 +0000

    master - 99391e793 refactor(material/select): remove deprecated APIs for v12 (#21827)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.1-sha-a405ce6ce');
+const VERSION = new Version('12.0.0-next.1-sha-99391e793');
 
 /**
  * @license
