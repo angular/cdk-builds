6d5cedf34ecfaf04b5a2fabba566f7b05d5d7398
Michael Prentice <splaktar@gmail.com>
2021-03-07 22:43:49 +0000

    11.2.x - a14c6fb24 docs(cdk/a11y): fix overview table of contents (#22136)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.2.3-sha-e94b7bd3c');
+	var VERSION = new core.Version('11.2.3-sha-a14c6fb24');
 
 	/**
 	 * @license
