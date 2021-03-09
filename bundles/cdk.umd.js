bc26f78b01cb95d5bc718dfe82517d49e6b5780d
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-09 16:58:27 +0000

    master - a1e005667 build: clean up sass patch workaround (#22057)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.1-sha-b4fafdad7');
+	var VERSION = new core.Version('12.0.0-next.1-sha-a1e005667');
 
 	/**
 	 * @license
