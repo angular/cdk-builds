7d03c3910dfa2c2cb050977a862e1ec772c866ae
Annie Wang <annieyw@google.com>
2021-03-09 14:45:18 +0000

    11.2.x - 766c9ea0d fix(material-experimental/mdc-form-field): fix height for form field with textarea (#22089)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.2.3-sha-a14c6fb24');
+	var VERSION = new core.Version('11.2.3-sha-766c9ea0d');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.2.3-sha-a14c6fb24');
+const VERSION = new Version('11.2.3-sha-766c9ea0d');
 
 /**
  * @license
