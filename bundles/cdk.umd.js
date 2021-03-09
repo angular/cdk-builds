2a6172971f91148ff618fbbb34a7a0ddba1d5aea
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-09 14:44:38 +0000

    master - 0093105b6 fix(material/table): data source filteredData not updating while disconnected (#22058)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.1-sha-0003d8fde');
+	var VERSION = new core.Version('12.0.0-next.1-sha-0093105b6');
 
 	/**
 	 * @license
