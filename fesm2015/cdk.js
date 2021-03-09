faa4e1a5f9fed9ece1f47d9ed297bc315e51c448
Kristiyan Kostadinov <crisbeto@abv.bg>
2021-03-09 14:49:47 +0000

    master - e6578d95f fix(material-experimental/mdc-checkbox): add focus indication in high contrast mode

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.1-sha-23a0248bc');
+const VERSION = new Version('12.0.0-next.1-sha-e6578d95f');
 
 /**
  * @license
