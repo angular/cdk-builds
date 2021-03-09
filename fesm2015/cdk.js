0b64c704e60c3da9d76f74c6b241778abdfe15d6
Annie Wang <annieyw@google.com>
2021-03-09 14:42:03 +0000

    master - 17f66d693 feat(material-experimental/mdc-button): add default config for FAB (#22102)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.1-sha-4aa48a15f');
+const VERSION = new Version('12.0.0-next.1-sha-17f66d693');
 
 /**
  * @license
