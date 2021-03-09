53eecbf1d17b6d57c96e361d181594792e54330e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-09 14:50:58 +0000

    master - b4fafdad7 fix(material/grid-list): remove internal figure element (#21826)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.1-sha-e6578d95f');
+	var VERSION = new core.Version('12.0.0-next.1-sha-b4fafdad7');
 
 	/**
 	 * @license
