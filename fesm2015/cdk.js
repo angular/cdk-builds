668305a538b9c38417a6a45b8b4e91d85970350a
AlexElin <alexey.elin@list.ru>
2021-03-09 20:57:24 +0000

    11.2.x - 045af6f3b docs: monospace DataSource (table docs) (#22114)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.2.3-sha-766c9ea0d');
+const VERSION = new Version('11.2.3-sha-045af6f3b');
 
 /**
  * @license
