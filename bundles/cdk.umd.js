19bfbbffcdca345255532cf4a5448fbde745bfe9
Andrew Seguin <andrewseguin@users.noreply.github.com>
2021-03-10 22:44:43 +0000

    master - 97f1cccd7 fix(material-experimental/mdc-progress-bar): improve buffer color (#22152)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.2-sha-fe9b81c7e');
+	var VERSION = new core.Version('12.0.0-next.2-sha-97f1cccd7');
 
 	/**
 	 * @license
