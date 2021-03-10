c4d5fb79eaeb5e50e12ce15a7417ab91b7aa3b0f
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-10 22:27:29 +0000

    master - edadf8bcf refactor(material-experimental/mdc-core): clean up remaining @import usages (#22045)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.2-sha-623056083');
+const VERSION = new Version('12.0.0-next.2-sha-edadf8bcf');
 
 /**
  * @license
