5cf46cdaba38a1a7db4c6d0d364d7766cc5841ab
mmalerba <mmalerba@google.com>
2021-03-11 21:56:26 +0000

    master - 85b5df638 fix(material-experimental/mdc-checkbox): fix mdc function name (#22198)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.2-sha-8a10451f9');
+const VERSION = new Version('12.0.0-next.2-sha-85b5df638');
 
 /**
  * @license
