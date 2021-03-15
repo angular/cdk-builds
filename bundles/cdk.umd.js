33e994f936cea596fb46941866175054f5c2ac52
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-15 18:06:42 +0000

    master - 9f560f4fd docs(material/button-toggle): add missing type on public API (#22224)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.2-sha-d66cb83f7');
+	var VERSION = new core.Version('12.0.0-next.2-sha-9f560f4fd');
 
 	/**
 	 * @license
