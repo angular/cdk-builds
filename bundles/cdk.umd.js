49c40add0a0fa2ba6682053fb19b60881f12b2d3
Joey Perrott <josephperrott.github@gmail.com>
2021-03-15 18:05:34 +0000

    master - d66cb83f7 build: lay foundation for migrating to ng-dev release tooling (#22167)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.2-sha-58118ecee');
+	var VERSION = new core.Version('12.0.0-next.2-sha-d66cb83f7');
 
 	/**
 	 * @license
