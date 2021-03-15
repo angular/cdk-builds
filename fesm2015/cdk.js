60d8edeb1f685b3ed04dc5c838f294c5ce6eb0dc
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-15 18:03:13 +0000

    master - b2622b4d1 build: add lint rule to prevent @import usage (#22222)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.2-sha-168611d79');
+const VERSION = new Version('12.0.0-next.2-sha-b2622b4d1');
 
 /**
  * @license
