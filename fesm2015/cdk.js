3326f8fed993f241092b9ecc96311fce46453229
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-16 15:23:29 +0000

    master - 55845baa4 docs: remove node-sass references (#22240)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.2-sha-4d54ff15b');
+const VERSION = new Version('12.0.0-next.2-sha-55845baa4');
 
 /**
  * @license
