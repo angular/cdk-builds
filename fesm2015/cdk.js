fca6c8d0b39d1dc7912ef1e7e15cc5fbf3692eb3
Bruno Kilian <bruno222@users.noreply.github.com>
2021-03-16 15:31:58 +0000

    master - af26fe578 ci: fix the warning on lock-closed gh-actions: Warning: Unexpected input(s) 'lock-bot-key' (#21547)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.2-sha-84d9a3065');
+const VERSION = new Version('12.0.0-next.2-sha-af26fe578');
 
 /**
  * @license
