c4f411153679ddb293f96a4755d883b04079c336
mmalerba <mmalerba@google.com>
2021-03-16 23:04:01 +0000

    master - 397710de1 refactor(material-experimental/mdc-chips): switch imports to (#22239)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.2-sha-d11e2937c');
+const VERSION = new Version('12.0.0-next.2-sha-397710de1');
 
 /**
  * @license
