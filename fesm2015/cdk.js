b75f3ea5dc7bc7695c3dd38a5ba9a2a799c78341
mmalerba <mmalerba@google.com>
2021-03-17 20:57:22 +0000

    master - 30fc69307 release: bump version to 12.0.0-next.3 w/ changelog (#22264)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.2-sha-fef9bf52a');
+const VERSION = new Version('12.0.0-next.3-sha-30fc69307');
 
 /**
  * @license
