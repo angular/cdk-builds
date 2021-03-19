4e9a8bafca2f0e81e69757438bdb70d5116cbffb
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-19 20:16:21 +0000

    master - 1fac4918e fix(google-maps): instantiate geocoder lazily (#22159)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.3-sha-4b11445a9');
+	var VERSION = new core.Version('12.0.0-next.3-sha-1fac4918e');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.3-sha-4b11445a9');
+const VERSION = new Version('12.0.0-next.3-sha-1fac4918e');
 
 /**
  * @license
