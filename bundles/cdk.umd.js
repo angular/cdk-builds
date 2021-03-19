84d1d80080df43e38abde44022ca83667a14055f
Dominik Rabij <dominik.rabij@gmail.com>
2021-03-19 19:02:51 +0000

    master - 5b7b03ea7 fix(material/progress-bar): Support ChromeVox (#22166)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.3-sha-902e946dc');
+	var VERSION = new core.Version('12.0.0-next.3-sha-5b7b03ea7');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.3-sha-902e946dc');
+const VERSION = new Version('12.0.0-next.3-sha-5b7b03ea7');
 
 /**
  * @license
