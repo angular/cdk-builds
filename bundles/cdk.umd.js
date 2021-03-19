787381d4060ed78282732b18afa70f0596bf9ece
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-19 19:13:11 +0000

    master - 9b67d670b fix(material/select): trim aria-labelledby (#22251)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.3-sha-50b3207d1');
+	var VERSION = new core.Version('12.0.0-next.3-sha-9b67d670b');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.3-sha-50b3207d1');
+const VERSION = new Version('12.0.0-next.3-sha-9b67d670b');
 
 /**
  * @license
