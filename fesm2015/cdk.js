da1421682af878b1ebb03a30e2516093be027123
Michael Prentice <splaktar@gmail.com>
2021-03-19 19:08:22 +0000

    master - c0286ea50 fix(cdk/a11y): FocusTrap deprecation docs don't render correctly (#22137)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.3-sha-16b7adb84');
+const VERSION = new Version('12.0.0-next.3-sha-c0286ea50');
 
 /**
  * @license
