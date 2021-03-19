8f0ae4f7dd444133ac8778922d0ee414b1741ce2
mmalerba <mmalerba@google.com>
2021-03-19 19:03:58 +0000

    master - 16b7adb84 fix(material-experimental/mdc-select): fix floating label behavior (#22134)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.3-sha-5b7b03ea7');
+const VERSION = new Version('12.0.0-next.3-sha-16b7adb84');
 
 /**
  * @license
