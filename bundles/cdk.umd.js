1019242270035800fa12c65beb5c7cad1722aa49
Andrew Seguin <andrewseguin@users.noreply.github.com>
2021-03-19 19:36:21 +0000

    master - 08bbd50a6 fix(material-experimental/mdc-button): remove unthemed icon color (#22281)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.3-sha-2da7135e4');
+	var VERSION = new core.Version('12.0.0-next.3-sha-08bbd50a6');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.3-sha-2da7135e4');
+const VERSION = new Version('12.0.0-next.3-sha-08bbd50a6');
 
 /**
  * @license
