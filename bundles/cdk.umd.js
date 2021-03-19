1b36633fd1a9b2d6bdb2c33ca8cf1834d6df899e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-19 18:17:06 +0000

    master - 40ceda28f feat(material/core): expose new @use-based Sass API (#22173)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.3-sha-263c90c38');
+	var VERSION = new core.Version('12.0.0-next.3-sha-40ceda28f');
 
 	/**
 	 * @license
