498a03bebf2441bdd530f60bff96a8cb70c96d0b
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-23 15:53:21 +0000

    master - 588a50650 fix(google-maps): initialize directions service lazily (#22302)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.3-sha-b0272794f');
+	var VERSION = new core.Version('12.0.0-next.3-sha-588a50650');
 
 	/**
 	 * @license
