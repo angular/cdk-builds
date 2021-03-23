1ffe1ef4130527ae78c6bb0d50de29646f1173dd
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-23 19:17:44 +0000

    master - 7c9b83a6f fix(google-maps): ensure that a mapTypeId is always passed in (#22098)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.3-sha-ba6efbf6e');
+const VERSION = new Version('12.0.0-next.3-sha-7c9b83a6f');
 
 /**
  * @license
