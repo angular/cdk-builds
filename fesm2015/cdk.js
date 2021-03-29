fcc814a2217d9cc4e788f7263839d0165a19949a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-29 00:59:08 +0000

    master - e829c225e refactor(material-experimental/mdc-core): expose new @use-based Sass API (#22338)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.4-sha-a2c4b7664');
+const VERSION = new Version('12.0.0-next.4-sha-e829c225e');
 
 /**
  * @license
