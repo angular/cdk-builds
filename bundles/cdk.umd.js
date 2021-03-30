7b32190c690090d96d805e4872b94d81478b61c6
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-30 12:26:19 +0000

    master - 9fb92f044 refactor(cdk/overlay): clean up variable names (#22359)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.4-sha-0c83adc8f');
+	var VERSION = new core.Version('12.0.0-next.4-sha-9fb92f044');
 
 	/**
 	 * @license
