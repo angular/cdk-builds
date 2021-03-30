929bf58760acc53505e64f15a0b65ac6b0165fbc
felhag <62592273+felhag@users.noreply.github.com>
2021-03-30 17:52:52 +0000

    11.2.x - 06877802a feat(radio/checkbox): show background-color in print stylesheet (#22298) (#22299)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.2.6-sha-6277ee266');
+const VERSION = new Version('11.2.6-sha-06877802a');
 
 /**
  * @license
