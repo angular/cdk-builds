02a82ed9a31bb10cb62700485e157ac8a78e3852
Andrew <aslapierre@gmail.com>
2021-03-30 17:38:56 +0000

    master - 4a06d4fd2 docs(material/theming): Fix typo in theming.md (#22342)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.4-sha-9fb92f044');
+const VERSION = new Version('12.0.0-next.4-sha-4a06d4fd2');
 
 /**
  * @license
