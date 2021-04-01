2a3d454f765398c5e22c59925b319a34592d2ed3
Jeremy Elbourn <jelbourn@google.com>
2021-04-01 00:43:56 +0000

    11.2.x - 03947c75b build: don't require all reviewers to approve PRs (#22363)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.2.7-sha-ef408b8c9');
+const VERSION = new Version('11.2.7-sha-03947c75b');
 
 /**
  * @license
