9003fb5a1880252f2d234d2ef340f9054a23b010
Jeremy Elbourn <jelbourn@google.com>
2021-04-01 01:23:24 +0000

    master - 34dba1366 docs(material/core): rewrite typography guide for `@use` (#22366)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.5-sha-48e22f3ce');
+	var VERSION = new core.Version('12.0.0-next.5-sha-34dba1366');
 
 	/**
 	 * @license
