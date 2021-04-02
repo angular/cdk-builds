19195c7d94952d6666d42192e133a1aaefabddff
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-04-02 07:21:11 +0000

    master - ea5e5e04d fix(material/datepicker): toggle icon not visible in high contrast mode on Chromium browsers (#22352)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.5-sha-1d906b64a');
+const VERSION = new Version('12.0.0-next.5-sha-ea5e5e04d');
 
 /**
  * @license
