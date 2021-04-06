97a822e590718ac7c89211aca0507ec62ba9b1fb
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-04-06 03:11:26 +0000

    master - 2fa78c6fe fix(material/sidenav): mark sidenav content as scrollable (#19848)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.5-sha-1b639f5b7');
+const VERSION = new Version('12.0.0-next.5-sha-2fa78c6fe');
 
 /**
  * @license
