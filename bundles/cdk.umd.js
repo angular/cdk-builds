8ebd9945bcd8eda58704b2439fb5fcefb46ac518
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-04-06 03:10:11 +0000

    master - 1b639f5b7 refactor(material/sort): remove deprecated APIs for v12 (#21833)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.5-sha-4f8e87e38');
+	var VERSION = new core.Version('12.0.0-next.5-sha-1b639f5b7');
 
 	/**
 	 * @license
