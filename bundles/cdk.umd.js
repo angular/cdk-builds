e12e3f4361bf5e01752f4293b43c3b1f8d474793
Andrew Seguin <andrewseguin@users.noreply.github.com>
2021-04-06 03:12:36 +0000

    master - 5519131a7 fix(material-experimental/mdc-paginator): rename module file (#22387)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.5-sha-2fa78c6fe');
+	var VERSION = new core.Version('12.0.0-next.5-sha-5519131a7');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.5-sha-2fa78c6fe');
+const VERSION = new Version('12.0.0-next.5-sha-5519131a7');
 
 /**
  * @license
