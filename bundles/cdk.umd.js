0da7a537031cab53c629438f6370f12589e595c7
Annie Wang <annieyw@google.com>
2021-04-07 19:10:44 +0000

    master - 212c02655 release: bump version to 12.0.0-next.6 w/ changelog (#22425)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.5-sha-4a3ca82c7');
+	var VERSION = new core.Version('12.0.0-next.6-sha-212c02655');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.5-sha-4a3ca82c7');
+const VERSION = new Version('12.0.0-next.6-sha-212c02655');
 
 /**
  * @license
