e5a3f040a332acdc9ee0ea589041891f4ec6c498
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-04-07 05:15:24 +0000

    master - 4ad2d9608 fix(material-experimental/mdc-list): align ripple timings with MDC (#22396)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.5-sha-c45cd69f3');
+	var VERSION = new core.Version('12.0.0-next.5-sha-4ad2d9608');
 
 	/**
 	 * @license
