2a985c2e67ad1a019e7aefe7402dd492d6ff0c78
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-04-08 16:52:51 +0000

    11.2.x - 9aa3a26d5 refactor(form-field): remove unnecessary bindings (#16941)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.2.8-sha-84bde9819');
+const VERSION = new Version('11.2.8-sha-9aa3a26d5');
 
 /**
  * @license
