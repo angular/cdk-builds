d38d827b008d85e0b4d8c226d44efedf2e5b4250
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-04-08 16:54:33 +0000

    master - 70564f3b8 build: add commit message scope for multiple components (#22402)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.6-sha-60946cc01');
+	var VERSION = new core.Version('12.0.0-next.6-sha-70564f3b8');
 
 	/**
 	 * @license
