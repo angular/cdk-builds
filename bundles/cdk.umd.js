d9f1971fe080a61a07e284331d62a353b28bf78a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-04-09 17:03:25 +0000

    master - 0cb27f95c fix(material/schematics): switch custom theme schematic to `@use` (#22436)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.6-sha-10cc9cfa5');
+	var VERSION = new core.Version('12.0.0-next.6-sha-0cb27f95c');
 
 	/**
 	 * @license
