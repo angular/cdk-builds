14f9781b74c0247a0da95d6fee8a712cdb2a4b7c
James Wright <jameswr@google.com>
2021-04-09 17:10:31 +0000

    master - ce64f5e29 docs(material/snack-bar): Fix typo in snackbar jsdoc. (#22322)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.6-sha-93d3a9200');
+	var VERSION = new core.Version('12.0.0-next.6-sha-ce64f5e29');
 
 	/**
 	 * @license
