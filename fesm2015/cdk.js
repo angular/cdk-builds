dbc5fb1370659f7ad101317b936358e7c52c8987
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-04-09 17:05:43 +0000

    master - 5f07f2c15 docs(material/stepper): add live example of responsive stepper (#22401)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.6-sha-672040521');
+const VERSION = new Version('12.0.0-next.6-sha-5f07f2c15');
 
 /**
  * @license
