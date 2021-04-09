3617c7ef35c02ffd29b62d439d802f1272546556
Jeremy Elbourn <jelbourn@google.com>
2021-04-09 17:04:35 +0000

    master - 672040521 docs(material/theming): document strong focus indicators (#22374)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.6-sha-0cb27f95c');
+	var VERSION = new core.Version('12.0.0-next.6-sha-672040521');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.6-sha-0cb27f95c');
+const VERSION = new Version('12.0.0-next.6-sha-672040521');
 
 /**
  * @license
