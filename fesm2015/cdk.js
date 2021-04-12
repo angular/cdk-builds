19ad66d5bee3138b6f2ef7faae8359b10e0599cf
Alan Agius <alan.agius4@gmail.com>
2021-04-12 17:22:52 +0000

    master - 55a2a6c39 refactor(material/schematics): update `$schema` and `id` keywords (#22459)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.6-sha-4b06b90a1');
+const VERSION = new Version('12.0.0-next.6-sha-55a2a6c39');
 
 /**
  * @license
