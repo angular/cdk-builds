bd5e8e80becdc759077bea1acf06d32ab58dc5c0
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-04-12 17:24:07 +0000

    master - 3318877b6 build: add lint rule for unused Sass imports (#22454)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.6-sha-55a2a6c39');
+const VERSION = new Version('12.0.0-next.6-sha-3318877b6');
 
 /**
  * @license
