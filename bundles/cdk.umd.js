2828c3d151a29de46ba5a4eacee0a5bc1c566942
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-04-13 17:31:27 +0000

    master - 64087316f feat(material-experimental/mdc-core): set up MDC-based elevation (#22397)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.6-sha-2f177b622');
+	var VERSION = new core.Version('12.0.0-next.6-sha-64087316f');
 
 	/**
 	 * @license
