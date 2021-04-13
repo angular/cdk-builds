674f267c14902cb5a3ff179fd92f9bc0374f9842
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-04-13 17:15:04 +0000

    master - 7039d3e00 feat(material/datepicker): remove dependency on dialog (#22383)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.6-sha-d030a8712');
+const VERSION = new Version('12.0.0-next.6-sha-7039d3e00');
 
 /**
  * @license
