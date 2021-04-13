3735ea44bb00cf6d2f28fa7a40e5b1e96511a98d
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-04-13 17:37:43 +0000

    master - 6626fee81 build: add peer dependency to rxjs (#22471)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.6-sha-58b4ab6d7');
+	var VERSION = new core.Version('12.0.0-next.6-sha-6626fee81');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-next.6-sha-58b4ab6d7');
+const VERSION = new Version('12.0.0-next.6-sha-6626fee81');
 
 /**
  * @license
