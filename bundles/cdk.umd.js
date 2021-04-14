0d56ba723d91da7cbefac44737a1c847cce08d5f
mmalerba <mmalerba@google.com>
2021-04-14 21:34:36 +0000

    master - 119684eb8 release: update version to 12.1.0-next.0 (#22484)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.7-sha-c9b9d8aaf');
+	var VERSION = new core.Version('12.1.0-next.0-sha-119684eb8');
 
 	/**
 	 * @license
