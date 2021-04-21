574592d61e40efc06bf9f0442b0e442abf8b9ce8
Wagner Maciel <wagnermaciel@google.com>
2021-04-21 17:50:52 +0000

    11.2.x - 6c0051689 release: bump version to 11.2.10 w/ changelog (#22528)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.2.9-sha-da9294469');
+	var VERSION = new core.Version('11.2.10-sha-6c0051689');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.2.9-sha-da9294469');
+const VERSION = new Version('11.2.10-sha-6c0051689');
 
 /**
  * @license
