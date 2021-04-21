4469db2e1c7a59a1de45ce3c9ae440362afc658e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-04-21 11:13:26 +0000

    12.0.x - 4f6bf3ae0 fix(material/schematics): replace removed variables with their values in sass api migration (#22505)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.7-sha-c9b9d8aaf');
+	var VERSION = new core.Version('12.0.0-next.7-sha-4f6bf3ae0');
 
 	/**
 	 * @license
