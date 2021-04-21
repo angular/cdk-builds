a2b3536c989c63453a5b18345675280b1ad22231
AlexElin <alexey.elin@list.ru>
2021-04-21 11:13:17 +0000

    master - 45f856659 docs: add indentation to mat-form-field with suffix example (#22502)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.1.0-next.0-sha-16d57aed8');
+	var VERSION = new core.Version('12.1.0-next.0-sha-45f856659');
 
 	/**
 	 * @license
