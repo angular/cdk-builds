7ca5bed0fe7a7fb2e83be275549bb3f577d9c9cc
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-04-21 11:19:41 +0000

    master - 1678df9f2 docs: resolve some docs typos (#22514)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.1.0-next.0-sha-0fbd3e752');
+const VERSION = new Version('12.1.0-next.0-sha-1678df9f2');
 
 /**
  * @license
