dde126af4c6475f0832bfd192665c7f91d22140d
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-04-21 11:19:17 +0000

    11.2.x - da9294469 fix(material/autocomplete): error if keydown is dispatched too early (#22513)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.2.9-sha-1399e3dab');
+	var VERSION = new core.Version('11.2.9-sha-da9294469');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.2.9-sha-1399e3dab');
+const VERSION = new Version('11.2.9-sha-da9294469');
 
 /**
  * @license
