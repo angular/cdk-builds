ab370fa142a94370213b0bdc0b5cdefd02df68ee
Jeremy Elbourn <jelbourn@google.com>
2021-04-21 11:22:20 +0000

    master - 3e4e6d03f docs(material/theming): rewrite elevation guide (#22467)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.1.0-next.0-sha-2a9854ecd');
+	var VERSION = new core.Version('12.1.0-next.0-sha-3e4e6d03f');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.1.0-next.0-sha-2a9854ecd');
+const VERSION = new Version('12.1.0-next.0-sha-3e4e6d03f');
 
 /**
  * @license
