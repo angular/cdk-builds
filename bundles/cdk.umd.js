980f3f1e9713f081dbb7c70ad861f408a4675095
Jeremy Elbourn <jelbourn@google.com>
2021-04-21 11:20:31 +0000

    12.0.x - 6772611b3 docs(material/theming): revise customizing-component-styles (#22466)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-next.7-sha-4f6bf3ae0');
+	var VERSION = new core.Version('12.0.0-next.7-sha-6772611b3');
 
 	/**
 	 * @license
