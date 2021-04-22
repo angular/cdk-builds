d5543af805d93b08819cc887fffb56f8bb1ab405
mmalerba <mmalerba@google.com>
2021-04-22 14:31:00 +0000

    11.2.x - 9bb7b0c5e fix(cdk/a11y): prevent crashing Chrome tabs with hidden text (#22536)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.2.10-sha-6c0051689');
+	var VERSION = new core.Version('11.2.10-sha-9bb7b0c5e');
 
 	/**
 	 * @license
