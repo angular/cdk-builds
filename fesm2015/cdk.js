0ad5dadb6137b96c50141ab1c9a0642d076dd1ea
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-04-27 13:24:36 +0000

    11.2.x - 6349434d1 fix(material/datepicker): support for date range input in MDC-based form field (#22565)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.2.10-sha-90877709d');
+const VERSION = new Version('11.2.10-sha-6349434d1');
 
 /**
  * @license
