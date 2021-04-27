a9ac85ec4def6e1e4e0877f39fac8c3e52626e16
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-04-27 13:04:31 +0000

    11.2.x - 90877709d fix(material-experimental/mdc-card): reset native header margin (#22550)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.2.10-sha-9bb7b0c5e');
+	var VERSION = new core.Version('11.2.10-sha-90877709d');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.2.10-sha-9bb7b0c5e');
+const VERSION = new Version('11.2.10-sha-90877709d');
 
 /**
  * @license
