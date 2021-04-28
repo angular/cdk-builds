6599f178d9fab64503c6124d4b02b71ab0172ba6
Andrew Seguin <andrewjs@google.com>
2021-04-28 17:16:27 +0000

    12.0.x - 08257b1fd release: bump version to 12.0.0-rc.1 w/ changelog

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-rc.0-sha-8bcdea200');
+	var VERSION = new core.Version('12.0.0-rc.1-sha-08257b1fd');
 
 	/**
 	 * @license
