774dc40502ca0bee20b00b45a30fe7cea18b9200
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-05-03 04:47:56 +0000

    master - d92f8f25b fix(material/core): update mixin functions to avoid unknown cast (#22577)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.1.0-next.0-sha-a92ad3d0f');
+const VERSION = new Version('12.1.0-next.0-sha-d92f8f25b');
 
 /**
  * @license
