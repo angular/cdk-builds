25d626b5ba4fedb466a9d4702cfc4d4148fab224
cynthialong0-0 <82900738+cynthialong0-0@users.noreply.github.com>
2021-05-03 04:49:09 +0000

    master - 491d2ec57 fix(material/core): `mat-ripple-element` is not fired on disable (#22537)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.1.0-next.0-sha-d92f8f25b');
+	var VERSION = new core.Version('12.1.0-next.0-sha-491d2ec57');
 
 	/**
 	 * @license
