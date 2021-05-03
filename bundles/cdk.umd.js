15e934d144d0b1aad45be84d69cb1e1fe65a1068
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-05-03 04:51:23 +0000

    master - 8ef4a7761 fix(material-experimental/mdc-menu): implement increasing elevation (#22506)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.1.0-next.0-sha-f9cc56418');
+	var VERSION = new core.Version('12.1.0-next.0-sha-8ef4a7761');
 
 	/**
 	 * @license
