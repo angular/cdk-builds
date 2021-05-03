1ab8f9b13b6cecd895704f8a6b2a95a0df71498f
mcalmus <mike@calmus.org>
2021-05-03 04:52:38 +0000

    master - 979a4fadf fix(material/datepicker): Only update selection when value changed (#21846) (#22369)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.1.0-next.0-sha-8ef4a7761');
+const VERSION = new Version('12.1.0-next.0-sha-979a4fadf');
 
 /**
  * @license
