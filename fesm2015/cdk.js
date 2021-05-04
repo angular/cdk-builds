99e76ea4f9e8d4e71771456b473e914e8aaee222
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-05-04 20:12:40 +0000

    master - 7fe4925f3 build: remove unnecessary dependency in webdriver test (#22621)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.1.0-next.0-sha-b111d4f02');
+const VERSION = new Version('12.1.0-next.0-sha-7fe4925f3');
 
 /**
  * @license
