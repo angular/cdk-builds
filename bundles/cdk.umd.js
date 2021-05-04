22d91db0465c21d6b141a4c1ddedad864eaaf903
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-05-04 20:11:28 +0000

    master - b111d4f02 build: fix size-test integration tooling (#22619)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.1.0-next.0-sha-58a742150');
+	var VERSION = new core.Version('12.1.0-next.0-sha-b111d4f02');
 
 	/**
 	 * @license
