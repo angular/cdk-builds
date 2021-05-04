963d948eac1d69d5e7e5f252c166522b4a8571a8
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-05-04 18:23:55 +0000

    master - cabb9ce49 fix(material-experimental/mdc-form-field): error with latest canary version (#22623)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.1.0-next.0-sha-0f9c1e6ef');
+const VERSION = new Version('12.1.0-next.0-sha-cabb9ce49');
 
 /**
  * @license
