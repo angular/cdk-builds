6a31361115c3afac065c4d57062b821cde864e9e
Kristiyan Kostadinov <crisbeto@abv.bg>
2021-05-07 05:28:42 +0000

    master - cd12aa790 fix(material/schematics): theming migration schematic not inserting use statement at the top of the file in some cases

--- schematics/ng-update/find-stylesheets.d.ts
+++ schematics/ng-update/find-stylesheets.d.ts
@@ -6,5 +6,11 @@
  * found in the LICENSE file at https://angular.io/license
  */
 import { Tree } from '@angular-devkit/schematics';
-/** Finds stylesheets in the given directory from within the specified tree. */
-export declare function findStylesheetFiles(tree: Tree, baseDir: string): string[];
+/**
+ * Finds stylesheets in the given directory from within the specified tree.
+ * @param tree Devkit tree where stylesheet files can be found in.
+ * @param startDirectory Optional start directory where stylesheets should be searched in.
+ *   This can be useful if only stylesheets within a given folder are relevant (to avoid
+ *   unnecessary iterations).
+ */
+export declare function findStylesheetFiles(tree: Tree, startDirectory?: string): string[];
