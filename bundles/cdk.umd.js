f10b8b46e8125dea74011b5ec38f75432b760a26
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-05-07 05:31:26 +0000

    master - be872c06c fix(material-experimental/mdc-list): error with latest MDC list canary version (#22636)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.1.0-next.0-sha-cd12aa790');
+	var VERSION = new core.Version('12.1.0-next.0-sha-be872c06c');
 
 	/**
 	 * @license
