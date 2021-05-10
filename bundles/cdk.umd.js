0a2403a3db4302294bf660ef2ffea0937f4afb51
Michael Prentice <splaktar@gmail.com>
2021-05-10 16:47:36 +0000

    12.0.x - 7636cd83e refactor(material/schematics): fix some typos and add JSDoc (#22610)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-rc.2-sha-2b99e4b60');
+	var VERSION = new core.Version('12.0.0-rc.2-sha-7636cd83e');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-rc.2-sha-2b99e4b60');
+const VERSION = new Version('12.0.0-rc.2-sha-7636cd83e');
 
 /**
  * @license
