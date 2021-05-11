fbb9625a6cb3872bc903b679ffc35cd5005e8ae4
mmalerba <mmalerba@google.com>
2021-05-11 17:03:25 +0000

    12.0.x - 369cdd32a build: update to latest Angular RC (#22664)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-rc.2-sha-7636cd83e');
+	var VERSION = new core.Version('12.0.0-rc.2-sha-369cdd32a');
 
 	/**
 	 * @license
