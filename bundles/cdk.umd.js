3dd962735c5c2e32f98450713af20803b2e021fa
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-05-12 16:46:58 +0000

    master - d11ed521e test(material/slider): clean up harness testing setup (#22670)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.1.0-next.0-sha-287834aec');
+	var VERSION = new core.Version('12.1.0-next.0-sha-d11ed521e');
 
 	/**
 	 * @license
