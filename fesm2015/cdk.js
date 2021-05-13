bbd011dec84c0a34bd86c99700c596887286edda
mmalerba <mmalerba@google.com>
2021-05-13 16:46:58 +0000

    master - c04bd21d6 fix(material-experimental/mdc-slide-toggle): set $deprecated-suffix on import (#22645)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.1.0-next.0-sha-583246301');
+const VERSION = new Version('12.1.0-next.0-sha-c04bd21d6');
 
 /**
  * @license
