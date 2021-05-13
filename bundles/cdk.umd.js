cefded58daf1673697999a06ba8bd99fbf0136b4
Michael Prentice <splaktar@gmail.com>
2021-05-13 16:56:41 +0000

    11.2.x - b5224ea4a fix(material-experimental/mdc-core): update the required MDC version (#22608)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.2.12-sha-9df8ced45');
+	var VERSION = new core.Version('11.2.12-sha-b5224ea4a');
 
 	/**
 	 * @license
