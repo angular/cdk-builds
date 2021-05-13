aae966c5879baf6b52b7b42e2795c45df19ce5d1
Andrew Seguin <andrewseguin@users.noreply.github.com>
2021-05-13 16:50:51 +0000

    12.0.x - 06c8e7903 fix(material/table): Make table data source interface generic (#22673)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-sha-3d820a149');
+	var VERSION = new core.Version('12.0.0-sha-06c8e7903');
 
 	/**
 	 * @license
