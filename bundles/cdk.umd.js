effcbdf5cadf9e26a07444f031fe840bc237e7ff
Andrew Seguin <andrewseguin@users.noreply.github.com>
2021-05-13 18:25:30 +0000

    12.0.x - e89422add fix(material-experimental/mdc-progress-spinner): set line-height (#22668)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-sha-06c8e7903');
+	var VERSION = new core.Version('12.0.0-sha-e89422add');
 
 	/**
 	 * @license
