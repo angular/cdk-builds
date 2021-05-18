74ad961b98f5b9d9ee7b6343817e0a95a48c4a56
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-05-18 19:54:22 +0000

    master - 736782344 fix(material/datepicker): pass correct inject flags to injector (#22665)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.1.0-next.0-sha-3027f8c22');
+const VERSION = new Version('12.1.0-next.0-sha-736782344');
 
 /**
  * @license
