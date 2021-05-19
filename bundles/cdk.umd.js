facab4de22b3c9fa18d34c55f21a97c41ccef3ca
Andrew Seguin <andrewseguin@users.noreply.github.com>
2021-05-19 16:11:50 +0000

    12.0.x - 365ed7c07 Revert "docs(material/table): use declarative idiomatic RxJS in table-http-example (#22053)" (#22725)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.0-sha-54e832d08');
+	var VERSION = new core.Version('12.0.0-sha-365ed7c07');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-sha-54e832d08');
+const VERSION = new Version('12.0.0-sha-365ed7c07');
 
 /**
  * @license
