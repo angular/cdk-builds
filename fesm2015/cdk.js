c7fafa7aaa62a8f2178cd484c00b4e7605005190
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-05-19 18:19:26 +0000

    12.0.x - c73fb1212 build: update to rules_nodejs v3.5.0 (#22693)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.0-sha-365ed7c07');
+const VERSION = new Version('12.0.0-sha-c73fb1212');
 
 /**
  * @license
