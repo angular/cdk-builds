8c361d2200d519ae03292da53ee8d503baf22500
Chris Ha <chunghha@users.noreply.github.com>
2021-05-19 17:53:31 +0000

    11.2.x - d48e6dae3 fix(material/sort): fix undefined object access

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('11.2.12-sha-b5224ea4a');
+const VERSION = new Version('11.2.12-sha-d48e6dae3');
 
 /**
  * @license
