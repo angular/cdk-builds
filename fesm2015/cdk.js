341018c019915bf1a66f434bb3cb7d5128dbeaa4
Jeremy Elbourn <jelbourn@google.com>
2021-05-20 15:13:38 +0000

    master - 284bdaab5 refactor(material/schematics): support extra variables in themingApi migration (#22741)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.1.0-next.0-sha-94076af58');
+const VERSION = new Version('12.1.0-next.0-sha-284bdaab5');
 
 /**
  * @license
