fbfcf72d1a2c2fab71f0d2f741213b0989dce168
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-05-21 15:46:56 +0000

    12.0.x - 5928896b8 fix(material/checkbox): disable ripple animation under noop animations module (#22706)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.1-sha-4bfdeb3a1');
+const VERSION = new Version('12.0.1-sha-5928896b8');
 
 /**
  * @license
