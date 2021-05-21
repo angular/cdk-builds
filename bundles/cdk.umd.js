1fb6aecbe94f35d89668f65827abf28d8a0d4c94
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-05-21 12:30:18 +0000

    master - e7a64d371 fix(material/tabs): projected tabs not being picked up (#21794)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.1.0-next.0-sha-284bdaab5');
+	var VERSION = new core.Version('12.1.0-next.0-sha-e7a64d371');
 
 	/**
 	 * @license
