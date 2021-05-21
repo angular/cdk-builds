876680e5ca7555cfb33cda085a77a7152329532a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-05-21 12:31:21 +0000

    12.0.x - 4bfdeb3a1 fix(material/slide-toggle): disable ripples when animations are disabled (#22713)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.1-sha-96a65d951');
+	var VERSION = new core.Version('12.0.1-sha-4bfdeb3a1');
 
 	/**
 	 * @license
