f26ecc0e5a27e96d45e008434b7ecdacac4e82b0
Aristeidis Bampakos <bampakoa@gmail.com>
2021-05-24 13:50:58 +0000

    12.0.x - c8abcd9ec docs: use icon button in input clear example (#22732)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.1-sha-bd1a4d821');
+	var VERSION = new core.Version('12.0.1-sha-c8abcd9ec');
 
 	/**
 	 * @license
