f3747a7014d6a925827ca0e2f61351ad804d458f
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-05-24 15:31:53 +0000

    master - 70e029eda docs: update documentation for `RippleGlobalOptions.animation` (#22767)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.1.0-next.0-sha-897e739af');
+const VERSION = new Version('12.1.0-next.0-sha-70e029eda');
 
 /**
  * @license
