4b3ac291d2264f45ed7513103f9248e0b39e9b3d
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-05-24 14:02:15 +0000

    master - 54c3ddfd7 docs: add doc string to base signature of `parallel` function (#22753)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.1.0-next.0-sha-04668e062');
+const VERSION = new Version('12.1.0-next.0-sha-54c3ddfd7');
 
 /**
  * @license
