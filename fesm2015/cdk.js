c19cf816fa36d4035c2dbae80ac1ff6bc188a31e
Annie Wang <annieyw@google.com>
2021-05-25 19:09:27 +0000

    12.0.x - 4a7374fac docs(material/form-field): add placeholder to prefix/suffix example for better alignment (#22775)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.1-sha-b05a4ba78');
+const VERSION = new Version('12.0.1-sha-4a7374fac');
 
 /**
  * @license
