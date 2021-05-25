c369e88c2ec53abc56742b16c51b34f74448470e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-05-25 12:57:32 +0000

    master - 89c847441 fix(material-experimental/mdc-slider): disable animations when noop module is included (#22649)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.1.0-next.0-sha-a1d3467b6');
+const VERSION = new Version('12.1.0-next.0-sha-89c847441');
 
 /**
  * @license
