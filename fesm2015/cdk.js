ee7b25c6c0437f2e3f3293535d86c9bc0afee82d
mmalerba <mmalerba@google.com>
2021-05-25 03:20:02 +0000

    master - 5de7a2208 docs(material/form-field): fix handling of error state in the custom form field control docs (#22782)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.1.0-next.0-sha-0a4dbe170');
+const VERSION = new Version('12.1.0-next.0-sha-5de7a2208');
 
 /**
  * @license
