f0c8db8a9aa07ea1e8796562deabe9de78a30194
Annie Wang <annieyw@google.com>
2021-05-25 20:08:34 +0000

    master - 555852e5e docs(material/table): fix table examples (#22792)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.1.0-next.0-sha-0de3f7288');
+const VERSION = new Version('12.1.0-next.0-sha-555852e5e');
 
 /**
  * @license
