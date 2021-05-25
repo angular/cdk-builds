40835c4d2e8f4410631352c7f92bca6285818217
Amy Sorto <8575252+amysorto@users.noreply.github.com>
2021-05-25 20:08:49 +0000

    12.0.x - d1743b97b docs(material/input): Add information about error messages and color to accessibility section (#22772)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.1-sha-4a7374fac');
+const VERSION = new Version('12.0.1-sha-d1743b97b');
 
 /**
  * @license
