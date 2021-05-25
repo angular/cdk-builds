dbe36cc31f400ace5d3408e2df9d3613ef7a4886
ArcaneAj <41121914+ArcaneAj@users.noreply.github.com>
2021-05-25 17:44:21 +0000

    12.0.x - 17a41acd7 docs: Fixed incorrect theme variable used in theme file (#22761)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.1-sha-6e0eab535');
+	var VERSION = new core.Version('12.0.1-sha-17a41acd7');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.1-sha-6e0eab535');
+const VERSION = new Version('12.0.1-sha-17a41acd7');
 
 /**
  * @license
