304294ab2b4684837efcdabd71bdc8c21f42fbbb
Amy Sorto <8575252+amysorto@users.noreply.github.com>
2021-05-25 20:09:38 +0000

    master - c4ff91009 docs(material/input): Add information about error messages and color to accessibility section (#22772)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.1.0-next.0-sha-555852e5e');
+	var VERSION = new core.Version('12.1.0-next.0-sha-c4ff91009');
 
 	/**
 	 * @license
