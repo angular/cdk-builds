487d0c53313e1bc82e2726afe1697bdfd0bbb940
mmalerba <mmalerba@google.com>
2021-05-25 17:31:21 +0000

    master - d239c95d8 build: build the core overview file as part of the docs content (#22790)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.1.0-next.0-sha-89c847441');
+	var VERSION = new core.Version('12.1.0-next.0-sha-d239c95d8');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.1.0-next.0-sha-89c847441');
+const VERSION = new Version('12.1.0-next.0-sha-d239c95d8');
 
 /**
  * @license
