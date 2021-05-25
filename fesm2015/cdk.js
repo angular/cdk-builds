0714777ce1051eb7d6124229fe5f748a1c57a7e2
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-05-25 12:56:28 +0000

    master - a1d3467b6 fix(material/core): typography-hierarchy mixin not exposed (#22752)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.1.0-next.0-sha-f3c30962e');
+const VERSION = new Version('12.1.0-next.0-sha-a1d3467b6');
 
 /**
  * @license
