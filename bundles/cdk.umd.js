644f309f9f10e901365ac185da22f5de29aa7424
mmalerba <mmalerba@google.com>
2021-05-25 03:20:14 +0000

    12.0.x - 8c6028665 docs(material/core): Add a brief overview that refers users to the API docs (#22773)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.1-sha-a8613e477');
+	var VERSION = new core.Version('12.0.1-sha-8c6028665');
 
 	/**
 	 * @license
