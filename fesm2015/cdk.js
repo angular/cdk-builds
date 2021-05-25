84fa32ca1ba6c6b6d9a988713db2c2c19142d60c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-05-25 17:55:22 +0000

    12.0.x - 3a8ce7026 docs: add example for providing custom `MatPaginatorIntl` (#22771)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.1-sha-17a41acd7');
+const VERSION = new Version('12.0.1-sha-3a8ce7026');
 
 /**
  * @license
