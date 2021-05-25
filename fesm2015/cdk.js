a8fbc468a38beddaad9c4ba6643c1eddda6a7fd3
Andrew Seguin <andrewseguin@users.noreply.github.com>
2021-05-25 19:01:43 +0000

    12.0.x - b05a4ba78 docs(material/table): add docs on row/column styles and bindings (#22778)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.1-sha-f8bac36b5');
+const VERSION = new Version('12.0.1-sha-b05a4ba78');
 
 /**
  * @license
