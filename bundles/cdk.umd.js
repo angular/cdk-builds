b37df233f5df55a6cff0cb63c590728a9f6c3ea2
mmalerba <mmalerba@google.com>
2021-05-25 17:31:01 +0000

    12.0.x - 6e0eab535 build: build the core overview file as part of the docs content (#22790)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.1-sha-9e6086778');
+	var VERSION = new core.Version('12.0.1-sha-6e0eab535');
 
 	/**
 	 * @license
