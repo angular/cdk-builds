7bba9c314c8ad64b692adfec1378f272572723b4
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-05-25 18:55:39 +0000

    master - a48f73657 build: expose static methods and properties (#22786)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.1.0-next.0-sha-d239c95d8');
+const VERSION = new Version('12.1.0-next.0-sha-a48f73657');
 
 /**
  * @license
