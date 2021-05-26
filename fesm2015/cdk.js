e73a0b6fd7b0b6170e0e2e46f1d395246ff6af11
mmalerba <mmalerba@google.com>
2021-05-26 21:43:27 +0000

    12.0.x - e92133eb4 docs(material/form-field): update documentation about the form-field appearances (#22817)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.2-sha-f9990fe6f');
+const VERSION = new Version('12.0.2-sha-e92133eb4');
 
 /**
  * @license
