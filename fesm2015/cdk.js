85ec836c5472525848fd9b1285dfa96c9e0304b3
mmalerba <mmalerba@google.com>
2021-05-26 18:51:26 +0000

    master - 763b7ebe7 docs(material/datepicker): show API docs for mat-calendar and add an … (#22794)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.1.0-next.0-sha-97e483613');
+const VERSION = new Version('12.1.0-next.0-sha-763b7ebe7');
 
 /**
  * @license
