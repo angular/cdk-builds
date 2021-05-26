86b8b7fcdd68d833d9c4b843fbe5d2d301f5c7d2
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-05-26 18:49:07 +0000

    master - a59aaa389 docs: add example for using chip-list with form control (#22814)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.1.0-next.0-sha-34dd46815');
+const VERSION = new Version('12.1.0-next.0-sha-a59aaa389');
 
 /**
  * @license
