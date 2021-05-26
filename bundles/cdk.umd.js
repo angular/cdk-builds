e14cf081f9803d635bcb144d159f1af02401c3aa
Andrew Seguin <andrewseguin@users.noreply.github.com>
2021-05-26 19:57:11 +0000

    master - 7a87ef95f docs(material/table): add examples with changing data (#22808)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.1.0-next.0-sha-0b04570a7');
+	var VERSION = new core.Version('12.1.0-next.0-sha-7a87ef95f');
 
 	/**
 	 * @license
