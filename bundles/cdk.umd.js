8330e9d6ec535b83b27a49e168510277329c4318
Jeremy Elbourn <jelbourn@google.com>
2021-05-27 19:42:59 +0000

    master - f68a1dc22 docs(cdk/layout): better explain how layout breakpoints work (#22821)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.1.0-next.0-sha-d198cb5d6');
+	var VERSION = new core.Version('12.1.0-next.0-sha-f68a1dc22');
 
 	/**
 	 * @license
