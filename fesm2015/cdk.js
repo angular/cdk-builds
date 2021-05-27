e75941fbdd3ae889c11d2f162668703b1d5373c1
Jeremy Elbourn <jelbourn@google.com>
2021-05-27 19:42:18 +0000

    12.0.x - fb869dbf3 docs(cdk/layout): better explain how layout breakpoints work (#22821)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.2-sha-9c6c4d414');
+const VERSION = new Version('12.0.2-sha-fb869dbf3');
 
 /**
  * @license
