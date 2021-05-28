51eb22fe3ed285309b8cd8cb2527ac3637c9fb51
Jeremy Elbourn <jelbourn@google.com>
2021-05-28 03:31:27 +0000

    12.0.x - f6d850f27 docs(material/datepicker): clarify moment adapter package (#22820)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.2-sha-fb869dbf3');
+	var VERSION = new core.Version('12.0.2-sha-f6d850f27');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.2-sha-fb869dbf3');
+const VERSION = new Version('12.0.2-sha-f6d850f27');
 
 /**
  * @license
