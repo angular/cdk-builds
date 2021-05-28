f632fa4dacfa7d26027c52440acbab51df230d0b
Billy Lando <billy_lando@yahoo.com>
2021-05-28 06:06:16 +0000

    12.0.x - 3eeb92e6c docs: add missing parenthese to harness guide (#22833)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.2-sha-ed8418cdc');
+	var VERSION = new core.Version('12.0.2-sha-3eeb92e6c');
 
 	/**
 	 * @license
