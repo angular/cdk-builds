098a26e424da566e1c7f31a608ee05df81ce8328
Jeremy Elbourn <jelbourn@google.com>
2021-05-28 05:13:43 +0000

    12.0.x - ed8418cdc docs(cdk/layout): add BreakpointObserver example (#22828)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.2-sha-f6d850f27');
+	var VERSION = new core.Version('12.0.2-sha-ed8418cdc');
 
 	/**
 	 * @license
