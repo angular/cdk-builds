32970180cd12080139a7e35a303ca177a3f1ca75
Nitin Malave <51904027+nitinmalave@users.noreply.github.com>
2021-05-29 18:36:38 +0000

    12.0.x - 7bd02f64b refactor(material/snack-bar): refactor variable type to const in snack-bar component unit tests (#22774)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.2-sha-3eeb92e6c');
+const VERSION = new Version('12.0.2-sha-7bd02f64b');
 
 /**
  * @license
