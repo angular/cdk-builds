557703b495b4a4ff8ac9fce085739cd5e74f5a17
Andrew Seguin <andrewseguin@users.noreply.github.com>
2021-05-29 18:41:39 +0000

    12.0.x - 6b5d8e464 fix(material-experimental/mdc-table): add background inherit to header cells (#22751)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.2-sha-7bd02f64b');
+const VERSION = new Version('12.0.2-sha-6b5d8e464');
 
 /**
  * @license
