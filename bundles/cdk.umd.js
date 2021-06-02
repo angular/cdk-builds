0ba9db7f5bb1e5e23ed7097d19a7efd933af5443
Jeremy Elbourn <jelbourn@google.com>
2021-06-02 23:06:16 +0000

    12.0.x - 48d2df242 fix(material/schematics): don't insert duplicate `@use` statements in themingApi (#22755)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.2-sha-2e1236d58');
+	var VERSION = new core.Version('12.0.2-sha-48d2df242');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.2-sha-2e1236d58');
+const VERSION = new Version('12.0.2-sha-48d2df242');
 
 /**
  * @license
