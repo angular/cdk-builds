79f6098669fa4aee8c22255fcf568241fb4166c8
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-06-02 23:10:10 +0000

    master - 3aa65ba93 refactor(material-experimental/mdc-list): no longer use deprecated styles (#22530)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.1.0-next.0-sha-00290976c');
+	var VERSION = new core.Version('12.1.0-next.0-sha-3aa65ba93');
 
 	/**
 	 * @license
