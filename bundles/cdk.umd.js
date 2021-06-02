ad9f8989299568579886e9103fcb772523f86b0f
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-06-02 23:03:36 +0000

    master - 5ad5c5759 refactor(material/form-field): fix incorrectly named mixin base constant (#22836)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.1.0-next.0-sha-e2633d102');
+	var VERSION = new core.Version('12.1.0-next.0-sha-5ad5c5759');
 
 	/**
 	 * @license
