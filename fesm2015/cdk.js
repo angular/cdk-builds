05b3032c16c0086218efa787a4a5340d5bfaf0fc
Andrew Seguin <andrewseguin@users.noreply.github.com>
2021-06-03 18:11:48 +0000

    12.0.x - c72468430 release: bump version to 12.0.3 w/ changelog (#22874)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.2-sha-48d2df242');
+const VERSION = new Version('12.0.3-sha-c72468430');
 
 /**
  * @license
