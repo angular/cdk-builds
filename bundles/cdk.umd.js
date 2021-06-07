ca21033ce9fb3bfc60b9cb0af8793cb1cf21f599
Nitin Malave <51904027+nitinmalave@users.noreply.github.com>
2021-06-07 21:13:29 +0000

    master - fee243309 test(material/button): refactor variable type to const in button component unit tests (#22893)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.1.0-next.0-sha-68e55669a');
+	var VERSION = new core.Version('12.1.0-next.0-sha-fee243309');
 
 	/**
 	 * @license
