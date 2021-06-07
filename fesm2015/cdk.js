18bd44a8f025d1385f7721ae452676bc601dd454
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-06-07 20:53:56 +0000

    12.0.x - f2c994b49 fix(material-experimental/mdc-button): set proper touch target (#22846)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.3-sha-c72468430');
+const VERSION = new Version('12.0.3-sha-f2c994b49');
 
 /**
  * @license
