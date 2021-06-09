2b484399826fc7c6053fc0a93ee7136f80f2e0fa
Wagner Maciel <wagnermaciel@google.com>
2021-06-09 17:32:38 +0000

    master - 4d2a6f9fc release: update changelog for 12.1.0-next.0 (#22938)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.1.0-next.0-sha-9cc70e25c');
+	var VERSION = new core.Version('12.1.0-next.0-sha-4d2a6f9fc');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.1.0-next.0-sha-9cc70e25c');
+const VERSION = new Version('12.1.0-next.0-sha-4d2a6f9fc');
 
 /**
  * @license
