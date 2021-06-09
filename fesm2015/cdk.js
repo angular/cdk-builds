1f1cb392f87d4899d45fb057764f05ffb01f85cb
jeripeierSBB <jeremias.peier@sbb.ch>
2021-06-09 05:11:09 +0000

    master - 3cd1f52d4 test(material/chips): prevent unintentional test success (#22920)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.1.0-next.0-sha-2f873a8d7');
+const VERSION = new Version('12.1.0-next.0-sha-3cd1f52d4');
 
 /**
  * @license
