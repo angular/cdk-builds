7d531ff3217938601934baa7ee3ce47ffcbf6eb4
Wagner Maciel <wagnermaciel@google.com>
2021-06-10 12:46:04 +0000

    master - 4cee3b707 fix(material-experimental/mdc-slider): delete isRequired function (#22876)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.1.0-next.0-sha-61ce45da1');
+const VERSION = new Version('12.1.0-next.0-sha-4cee3b707');
 
 /**
  * @license
