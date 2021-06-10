872a9cc742d3e5418886bfe340be2be4d8122e7a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-06-10 12:40:44 +0000

    12.0.x - 2af24d86e fix(material-experimental/mdc-tabs): ink bar animation not always disabled (#22848)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.4-sha-046963515');
+	var VERSION = new core.Version('12.0.4-sha-2af24d86e');
 
 	/**
 	 * @license
