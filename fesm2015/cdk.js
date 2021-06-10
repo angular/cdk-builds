cd1779e69a3251fbe1914899afe0647971ce8b2e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-06-10 12:53:10 +0000

    master - fc8a6f9cf fix(multiple): add inject flags to manual injector calls (#22922)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.1.0-next.0-sha-f750db2ad');
+const VERSION = new Version('12.1.0-next.0-sha-fc8a6f9cf');
 
 /**
  * @license
