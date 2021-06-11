f13944ef88afb23fe60d5f0e495cbf426f3d0472
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-06-11 02:41:26 +0000

    master - 10725496d build: update to typescript 4.3 (#22949)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.1.0-next.0-sha-27f5ca91b');
+	var VERSION = new core.Version('12.1.0-next.0-sha-10725496d');
 
 	/**
 	 * @license
