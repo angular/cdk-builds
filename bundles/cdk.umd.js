1d300a54ec00c5ce6b5222adc4119091e4140439
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-06-11 20:44:21 +0000

    master - 5ccd590b7 build: update devkit dependencies to v12.1.0-next.5 (#22957)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.1.0-next.0-sha-e6ae6dd9e');
+	var VERSION = new core.Version('12.1.0-next.0-sha-5ccd590b7');
 
 	/**
 	 * @license
