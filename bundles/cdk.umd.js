d974a51f4fd84d380a7088def8fa951529259c70
AlexElin <alexey.elin@list.ru>
2021-06-11 20:42:14 +0000

    master - e6ae6dd9e docs(multiple): use `includes` instead of `indexOf` to check whether a string contains a given substring (#22956)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.1.0-next.0-sha-aea46e77f');
+	var VERSION = new core.Version('12.1.0-next.0-sha-e6ae6dd9e');
 
 	/**
 	 * @license
