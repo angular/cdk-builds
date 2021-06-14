c66e6928dc12c6d99403a0d6583f41994a60b548
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-06-14 13:24:47 +0000

    12.0.x - 0012409b7 fix(material-experimental/mdc-button): set proper touch target (#22931)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.4-sha-fe4806c23');
+	var VERSION = new core.Version('12.0.4-sha-0012409b7');
 
 	/**
 	 * @license
