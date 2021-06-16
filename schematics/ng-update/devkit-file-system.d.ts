60724a4981d7ce29311760091b8d5d3a1015cd03
Nitin Malave <51904027+nitinmalave@users.noreply.github.com>
2021-06-16 01:54:23 +0000

    12.0.x - 90f33167b test(material/bottom-sheet): refactor variable type to const in bottom-sheet component unit tests (#22895)

--- schematics/ng-update/devkit-file-system.d.ts
+++ schematics/ng-update/devkit-file-system.d.ts
@@ -20,11 +20,11 @@ export declare class DevkitFileSystem extends FileSystem {
     resolve(...segments: string[]): Path;
     edit(filePath: Path): UpdateRecorder;
     commitEdits(): void;
-    exists(fileOrDirPath: Path): boolean;
+    fileExists(filePath: Path): boolean;
+    directoryExists(dirPath: Path): boolean;
     overwrite(filePath: Path, content: string): void;
     create(filePath: Path, content: string): void;
     delete(filePath: Path): void;
     read(filePath: Path): string | null;
     readDirectory(dirPath: Path): DirectoryEntry;
-    private _isExistingDirectory;
 }
