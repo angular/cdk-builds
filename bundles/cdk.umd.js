fb1bea446524c7f735e87d209bb5bd4b8498e0ef
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-06-16 01:50:45 +0000

    master - 805d3aef8 fix(cdk/schematics): avoid runtime errors thrown by devkit tree (#22982)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.1.0-next.0-sha-24d33a85a');
+	var VERSION = new core.Version('12.1.0-next.0-sha-805d3aef8');
 
 	/**
 	 * @license
