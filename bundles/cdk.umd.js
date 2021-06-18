d6d876c6ec8b94bee5cb3b0132ca4faac089c741
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-06-18 17:18:53 +0000

    master - 92d27a8b2 docs: clarify value of the form control in the chips-form-control example (#22995)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.2.0-next.0-sha-fafb24d60');
+	var VERSION = new core.Version('12.2.0-next.0-sha-92d27a8b2');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.2.0-next.0-sha-fafb24d60');
+const VERSION = new Version('12.2.0-next.0-sha-92d27a8b2');
 
 /**
  * @license
