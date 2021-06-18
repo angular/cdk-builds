7fa1ec39db26a8f9c04b5b57d01e5af1adb67b3f
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-06-18 17:19:19 +0000

    12.0.x - 5d17311c0 build: run unit tests with partial compilation and Angular linker (#22979)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.0.5-sha-e3ef642db');
+	var VERSION = new core.Version('12.0.5-sha-5d17311c0');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.5-sha-e3ef642db');
+const VERSION = new Version('12.0.5-sha-5d17311c0');
 
 /**
  * @license
