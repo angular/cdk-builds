52bea026bb4d72cc8eb017758496a1137c8fe62b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-06-18 17:20:00 +0000

    master - 4531748fd docs: improve paginator-intl example (#22988)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.2.0-next.0-sha-92d27a8b2');
+	var VERSION = new core.Version('12.2.0-next.0-sha-4531748fd');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.2.0-next.0-sha-92d27a8b2');
+const VERSION = new Version('12.2.0-next.0-sha-4531748fd');
 
 /**
  * @license
