31c66f7370ccfa0ef314e9928b6d61499823b358
Tobias Schneider <tsndr@users.noreply.github.com>
2021-06-22 23:09:34 +0000

    master - 90479854c fix(material/slide-toggle): Made label non user selectable (#22756)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.2.0-next.0-sha-40a3e1646');
+const VERSION = new Version('12.2.0-next.0-sha-90479854c');
 
 /**
  * @license
