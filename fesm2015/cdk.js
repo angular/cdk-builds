d18d2d51afbdb0f8c530f6ba2fed10555457cee8
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-06-22 23:12:57 +0000

    master - b7db96c10 fix(material/badge): avoid emitting the structural styles more than once (#23011)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.2.0-next.0-sha-136fb5a4a');
+const VERSION = new Version('12.2.0-next.0-sha-b7db96c10');
 
 /**
  * @license
