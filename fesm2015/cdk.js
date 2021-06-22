aa58ba65a7938a32c108e73ce09557eb0dbcc315
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-06-22 22:55:58 +0000

    master - 150d5af0e fix(material-experimental/mdc-radio): add accessible touch targets (#22994)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.2.0-next.0-sha-f0ddd54d4');
+const VERSION = new Version('12.2.0-next.0-sha-150d5af0e');
 
 /**
  * @license
