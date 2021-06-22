370d3f6b7bf4f2bb816eca35c169b70f058bba00
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-06-22 23:17:42 +0000

    master - 2f695c432 test(cdk/a11y): use DI to construct modality checker (#23030)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.2.0-next.0-sha-cfa9a893b');
+const VERSION = new Version('12.2.0-next.0-sha-2f695c432');
 
 /**
  * @license
