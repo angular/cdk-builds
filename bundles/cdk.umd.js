c636171d35f29e3865e19350814dd937096ec4b9
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-06-24 19:05:17 +0000

    11.2.x - 3a4e7d92b build: update to new remote instance name for RBE (#23056) (#23062)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('11.2.13-sha-625063cac');
+	var VERSION = new core.Version('11.2.13-sha-3a4e7d92b');
 
 	/**
 	 * @license
