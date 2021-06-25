441b0f62da15ced04d15297e79d81071c80e135f
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-06-25 21:33:35 +0000

    master - d6b6b5ac9 build: enable public API verification on the MDC slider (#23053)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.2.0-next.0-sha-8a28f1559');
+	var VERSION = new core.Version('12.2.0-next.0-sha-d6b6b5ac9');
 
 	/**
 	 * @license
