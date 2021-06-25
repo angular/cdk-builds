c1cdc87707c0333b47903161f9dbae6066f7d287
mmalerba <mmalerba@google.com>
2021-06-25 06:41:27 +0000

    12.0.x - 2752799e9 release: bump version to 12.0.6 w/ changelog (#23071)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.0.5-sha-4039c1caa');
+const VERSION = new Version('12.0.6-sha-2752799e9');
 
 /**
  * @license
