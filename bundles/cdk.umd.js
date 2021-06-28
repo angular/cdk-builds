86005be638d5dac9470f56d34afee3429978cc32
mmalerba <mmalerba@google.com>
2021-06-28 18:40:33 +0000

    master - 8b2ec8bc3 fix(material-experimental/mdc-dialog): use same container selector as non-mdc version (#23074)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.2.0-next.0-sha-fa3525178');
+	var VERSION = new core.Version('12.2.0-next.0-sha-8b2ec8bc3');
 
 	/**
 	 * @license
