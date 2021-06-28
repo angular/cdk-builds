8e3ee599aa2b4b39a4a3a8a65ce00c8ef10fb41b
Jeremy Elbourn <jelbourn@google.com>
2021-06-28 18:36:56 +0000

    master - da91c1828 fix(cdk/overlay): use interface for test environment globals (#23041)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.2.0-next.0-sha-fe24ce75c');
+	var VERSION = new core.Version('12.2.0-next.0-sha-da91c1828');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.2.0-next.0-sha-fe24ce75c');
+const VERSION = new Version('12.2.0-next.0-sha-da91c1828');
 
 /**
  * @license
