09a39f415fd07abbedd898f1e4305fd398791986
Wagner Maciel <wagnermaciel@google.com>
2021-07-08 17:46:20 +0000

    12.1.x - 140849089 fix(material/core): Focus indicator theme and color mixins should accept a basic color as well. (#23016) (#23134)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.1.1-sha-03203daf9');
+	var VERSION = new core.Version('12.1.1-sha-140849089');
 
 	/**
 	 * @license
