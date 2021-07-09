3a4e1e50d7e716ec2a52af2da768721ec3ad8543
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-07-09 13:13:27 +0000

    master - 43527d6f8 docs: fix typo in drag-drop docs (#23116)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.2.0-next.0-sha-298da1cbc');
+	var VERSION = new core.Version('12.2.0-next.0-sha-43527d6f8');
 
 	/**
 	 * @license
