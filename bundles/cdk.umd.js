182697d415c424158f7566094a473c60e496ea88
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-07-09 13:06:52 +0000

    master - 6b553ec79 feat(material/slide-toggle): add aria-describedby input (#23104)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.2.0-next.0-sha-ad06d7dc1');
+	var VERSION = new core.Version('12.2.0-next.0-sha-6b553ec79');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.2.0-next.0-sha-ad06d7dc1');
+const VERSION = new Version('12.2.0-next.0-sha-6b553ec79');
 
 /**
  * @license
