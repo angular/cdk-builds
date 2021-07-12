e53e556bc21c1667df2a1fddf0ed7174f4bf1159
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-07-12 13:37:02 +0000

    master - ae968ab1b fix(material/radio): hidden circle visible on some zoom levels (#22066)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.2.0-next.0-sha-d733ee279');
+	var VERSION = new core.Version('12.2.0-next.0-sha-ae968ab1b');
 
 	/**
 	 * @license
