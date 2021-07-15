c62fc92aeea02cdfdaea3a799821892569b6abc0
subsetdht <brent.singleton@gmail.com>
2021-07-15 15:25:58 +0000

    12.1.x - 3d27215f1 docs(material/table): fix table formatting in ngFor example (#23158)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.1.2-sha-0884cc9ed');
+	var VERSION = new core.Version('12.1.2-sha-3d27215f1');
 
 	/**
 	 * @license
