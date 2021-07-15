9b65a7e8332aba22f829f7f5b19fd41909d8c6fc
Zach Arend <zarend@users.noreply.github.com>
2021-07-15 17:04:33 +0000

    12.1.x - 9a19f92c9 build: add vim swamp files to .gitignore (#23173)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.1.2-sha-3d27215f1');
+	var VERSION = new core.Version('12.1.2-sha-9a19f92c9');
 
 	/**
 	 * @license
