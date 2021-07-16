6beffd46e337f909fd56ddce716b3ca86c5d6716
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-07-16 19:24:56 +0000

    master - 4404d0ad7 fix(material/input): show focus indication for readonly inputs (#22847)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.2.0-next.0-sha-d09061791');
+	var VERSION = new core.Version('12.2.0-next.0-sha-4404d0ad7');
 
 	/**
 	 * @license
