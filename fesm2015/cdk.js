e9484cb8fc91359801c124753f60a543a2791d36
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-07-20 22:00:31 +0000

    master - 81935ad7c refactor(material/tooltip): use overlay outside click tracking (#23123)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.2.0-next.0-sha-de22048c2');
+const VERSION = new Version('12.2.0-next.0-sha-81935ad7c');
 
 /**
  * @license
