84eb27b361ffd0b0cc982f3fe2b166ee638d17ac
Wagner Maciel <wagnermaciel@google.com>
2021-07-21 20:22:16 +0000

    12.1.x - f7a4a1c03 Revert "fix(material/table): switch back to class-based styling for flex tables (#22350)" (#23212)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.1.2-sha-881b0137c');
+	var VERSION = new core.Version('12.1.2-sha-f7a4a1c03');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.1.2-sha-881b0137c');
+const VERSION = new Version('12.1.2-sha-f7a4a1c03');
 
 /**
  * @license
