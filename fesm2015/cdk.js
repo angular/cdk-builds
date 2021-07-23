dcde8145a456bb755e4d853009ec882425d701c6
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-07-23 20:12:32 +0000

    master - c7baa54a9 build: resolve latest MDC version through yarn (#23220)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.2.0-next.0-sha-26366e759');
+const VERSION = new Version('12.2.0-next.0-sha-c7baa54a9');
 
 /**
  * @license
