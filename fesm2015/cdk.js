d8902e03e30e685b4ca3a68d52b52210d1d0c497
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-07-27 04:50:06 +0000

    12.1.x - fd5f3da52 fix(material-experimental/mdc-list): fix selected indication in high contrast mode (#23242)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.1.3-sha-51b7cbaba');
+const VERSION = new Version('12.1.3-sha-fd5f3da52');
 
 /**
  * @license
