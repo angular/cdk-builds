b67da2df975d8a2663066a82bc276a9cf6839ae1
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-07-27 04:52:13 +0000

    master - 74d08cfa4 fix(material-experimental/mdc-list): fix secondary line accidentally wrapping text (#23236)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.2.0-next.0-sha-f1e57b845');
+const VERSION = new Version('12.2.0-next.0-sha-74d08cfa4');
 
 /**
  * @license
