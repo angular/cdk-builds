3d1e80e8abfb8749344c21d75fd49118dc719f74
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-07-27 06:56:34 +0000

    master - 091305d00 ci: fix saucelabs browser crashing due to idle timeout (#23240)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.2.0-next.0-sha-4af0547b0');
+const VERSION = new Version('12.2.0-next.0-sha-091305d00');
 
 /**
  * @license
