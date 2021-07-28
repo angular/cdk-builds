2d4bbebfec10cb9eb93deff137b6eee18fedab54
mmalerba <mmalerba@google.com>
2021-07-28 18:24:50 +0000

    12.1.x - 5f7e28649 release: bump version to 12.1.4 w/ changelog (#23260)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.1.3-sha-23ec0b411');
+	var VERSION = new core.Version('12.1.4-sha-5f7e28649');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.1.3-sha-23ec0b411');
+const VERSION = new Version('12.1.4-sha-5f7e28649');
 
 /**
  * @license
