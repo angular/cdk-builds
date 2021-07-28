8fafbb2bedda76f729a95f9b00e75142371a2b6a
mmalerba <mmalerba@google.com>
2021-07-28 20:58:53 +0000

    12.2.x - 3f113c465 release: bump version to 12.2.0-rc.0 w/ changelog (#23263)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.2.0-next.0-sha-0fdc82911');
+	var VERSION = new core.Version('12.2.0-rc.0-sha-3f113c465');
 
 	/**
 	 * @license
