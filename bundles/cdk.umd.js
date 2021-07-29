adb9186a704577bda4710d40a5e7e3dd3934ab5a
Andrew Seguin <andrewseguin@users.noreply.github.com>
2021-07-29 15:38:09 +0000

    12.1.x - 6c8785274 fix(material-experimental/mdc-table): flex tables should stretch cells (#23259)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.1.4-sha-5f7e28649');
+	var VERSION = new core.Version('12.1.4-sha-6c8785274');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.1.4-sha-5f7e28649');
+const VERSION = new Version('12.1.4-sha-6c8785274');
 
 /**
  * @license
