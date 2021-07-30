932e884fc6e799770573311fbbd1050949961ff7
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-07-30 22:28:42 +0000

    master - c660dac44 build: fix deprecation warning from highlightjs (#23280)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('13.0.0-next.0-sha-51796e160');
+	var VERSION = new core.Version('13.0.0-next.0-sha-c660dac44');
 
 	/**
 	 * @license
