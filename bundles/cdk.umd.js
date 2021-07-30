20ec91a380530bd0b11c2f4243d8a7d8d546c166
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-07-30 22:11:11 +0000

    12.2.x - 28b589acc fix(material/menu): not interrupting keyboard events to other overlays (#22928)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.2.0-rc.0-sha-47a9c532d');
+	var VERSION = new core.Version('12.2.0-rc.0-sha-28b589acc');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.2.0-rc.0-sha-47a9c532d');
+const VERSION = new Version('12.2.0-rc.0-sha-28b589acc');
 
 /**
  * @license
