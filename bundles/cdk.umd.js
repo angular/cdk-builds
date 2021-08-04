8b3119d64542ce7f63ccaf3e7ed1a1fde30ec85b
Zach Arend <zarend@users.noreply.github.com>
2021-08-04 18:29:43 +0000

    12.1.x - 9a61fb326 Revert "fix(material/menu): not interrupting keyboard events to other overlays (#22928)" (#23306)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.1.4-sha-dbb7226eb');
+	var VERSION = new core.Version('12.1.4-sha-9a61fb326');
 
 	/**
 	 * @license
