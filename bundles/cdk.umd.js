16dec5e6586f2baa7168141f27be672e9b728d73
Amy Sorto <8575252+amysorto@users.noreply.github.com>
2021-08-04 02:35:10 +0000

    master - b3a6a0a29 fix(material-experimental/mdc-paginator): add screen reader announcement for current range when navigating between pages (#23285)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('13.0.0-next.0-sha-0980b6eeb');
+	var VERSION = new core.Version('13.0.0-next.0-sha-b3a6a0a29');
 
 	/**
 	 * @license
--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('13.0.0-next.0-sha-0980b6eeb');
+const VERSION = new Version('13.0.0-next.0-sha-b3a6a0a29');
 
 /**
  * @license
