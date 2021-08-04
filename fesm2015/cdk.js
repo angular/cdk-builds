5f7c06cc3254d590f038dd7e4735fa3275e6ac3d
Zach Arend <zarend@users.noreply.github.com>
2021-08-04 18:29:22 +0000

    12.2.x - 6aff589ee Revert "fix(material/menu): not interrupting keyboard events to other overlays (#22928)" (#23306)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.2.0-rc.0-sha-a752641ff');
+const VERSION = new Version('12.2.0-rc.0-sha-6aff589ee');
 
 /**
  * @license
