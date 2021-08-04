1e31320410b7247de342fab9768630e1de215850
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-08-04 02:36:10 +0000

    master - 45f3bf9cc refactor(material/progress-spinner): clean up IE workarounds (#23283)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('13.0.0-next.0-sha-b3a6a0a29');
+const VERSION = new Version('13.0.0-next.0-sha-45f3bf9cc');
 
 /**
  * @license
