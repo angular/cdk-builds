83023995724674d4a0ad0702580a4c8032f84935
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-08-04 02:37:07 +0000

    master - 0baca1878 perf(cdk/drag-drop): avoid excessive change detections with zone-patch-rxjs (#23272)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('13.0.0-next.0-sha-45f3bf9cc');
+	var VERSION = new core.Version('13.0.0-next.0-sha-0baca1878');
 
 	/**
 	 * @license
