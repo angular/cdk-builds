f4968cabc604fdfa1e5029efde0f2ab420bfedf0
Zach Arend <zarend@users.noreply.github.com>
2021-08-06 16:29:46 +0000

    12.2.x - 1ea4aa6fb release: add luxon adapter back to the changelog. (#23324)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.2.0-sha-b922846ed');
+const VERSION = new Version('12.2.0-sha-1ea4aa6fb');
 
 /**
  * @license
