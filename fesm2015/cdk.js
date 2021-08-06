e427358c65726a84001e668653979627f2e3b659
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-08-06 18:52:12 +0000

    master - 73bef2bec fix(material/tabs): pagination not working inside flex container (#23160)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('13.0.0-next.0-sha-f8778b584');
+const VERSION = new Version('13.0.0-next.0-sha-73bef2bec');
 
 /**
  * @license
