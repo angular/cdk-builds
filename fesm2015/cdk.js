a012d30f9fcf91ad7ad22cd5917ba79a3a84c5f6
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-08-06 18:57:57 +0000

    master - a4ae3b88c feat(material-experimental/mdc-slide-toggle): switch to non-deprecated styles (#23143)

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('13.0.0-next.0-sha-8d3938477');
+const VERSION = new Version('13.0.0-next.0-sha-a4ae3b88c');
 
 /**
  * @license
