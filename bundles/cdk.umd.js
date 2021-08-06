880aa998bfffaaf7ea0108890c0b9abffab15886
Zach Arend <zarend@users.noreply.github.com>
2021-08-06 16:34:05 +0000

    master - 2bd5fab0d release: add luxon adapter back to the changelog. (#23324) (#23325)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('13.0.0-next.0-sha-57d8e78dd');
+	var VERSION = new core.Version('13.0.0-next.0-sha-2bd5fab0d');
 
 	/**
 	 * @license
