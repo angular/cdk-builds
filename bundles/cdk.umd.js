1c16922d6356a45fba1606801d250b3264b0c792
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-08-09 15:54:20 +0000

    master - 0114ccdb9 build: update peerDependencies to allow rxjs 7 (#23316)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('13.0.0-next.0-sha-ec7d16468');
+	var VERSION = new core.Version('13.0.0-next.0-sha-0114ccdb9');
 
 	/**
 	 * @license
