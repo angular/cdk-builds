64afba92d952da793c4432df004c2c199565ea69
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-08-11 15:34:58 +0000

    12.2.x - 8a4ee2582 test: fix syntax error for slide-toggle mdc test

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.2.0-sha-7a97590ae');
+const VERSION = new Version('12.2.0');
 
 /**
  * @license
