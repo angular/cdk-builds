7eab3210460740d83c06bb6ec942b959c0535c71
Andrew Seguin <andrewjs@google.com>
2021-10-15 12:10:21 +0000

    12.2.x - d6d6106a4 release: cut the v12.2.10 release

--- fesm2015/cdk.js
+++ fesm2015/cdk.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('12.2.9');
+const VERSION = new Version('12.2.10');
 
 /**
  * @license
