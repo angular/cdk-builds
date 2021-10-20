2cdfe45c15548f83a45600009836b97e95c4bde4
Andrew Seguin <andrewjs@google.com>
2021-10-20 12:43:06 +0000

    12.2.x - 8e24b8168 release: cut the v12.2.11 release

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Angular Component Development Kit. */
-	var VERSION = new core.Version('12.2.10');
+	var VERSION = new core.Version('12.2.11');
 
 	/**
 	 * @license
