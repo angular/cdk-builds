bca54076b60252c8983a15b6552beb0f9f27cf64
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-11-09 00:23:09 +0000

    12.2.x - a0cf5220f fix(material/core): test environment check not picking up jest (#23722) (#23924)

--- fesm2015/platform.js
+++ fesm2015/platform.js
@@ -329,24 +329,21 @@ function _getEventTarget(event) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-let testGlobals;
-// We check the Node-specific `global` first, because tools tend to add a fake
-// `window` in Node environments which won't actually receive global variables.
-if (typeof global !== 'undefined') {
-    testGlobals = global;
-}
-else if (typeof window !== 'undefined') {
-    testGlobals = window;
-}
-else {
-    testGlobals = {};
-}
 /** Gets whether the code is currently running in a test environment. */
 function _isTestEnvironment() {
-    return (typeof testGlobals.__karma__ !== 'undefined' && !!testGlobals.__karma__) ||
-        (typeof testGlobals.jasmine !== 'undefined' && !!testGlobals.jasmine) ||
-        (typeof testGlobals.jest !== 'undefined' && !!testGlobals.jest) ||
-        (typeof testGlobals.Mocha !== 'undefined' && !!testGlobals.Mocha);
+    // We can't use `declare const` because it causes conflicts inside Google with the real typings
+    // for these symbols and we can't read them off the global object, because they don't appear to
+    // be attached there for some runners like Jest.
+    // (see: https://github.com/angular/components/issues/23365#issuecomment-938146643)
+    return (
+    // @ts-ignore
+    (typeof __karma__ !== 'undefined' && !!__karma__) ||
+        // @ts-ignore
+        (typeof jasmine !== 'undefined' && !!jasmine) ||
+        // @ts-ignore
+        (typeof jest !== 'undefined' && !!jest) ||
+        // @ts-ignore
+        (typeof Mocha !== 'undefined' && !!Mocha));
 }
 
 /**
