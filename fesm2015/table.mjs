57a7babbc94fd612ec1f9957b28ac66d97d03127
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2022-01-19 11:55:42 +0000

    13.1.x - cc8c49bbf build: fix unformatted files (#24231)

--- fesm2015/table.mjs
+++ fesm2015/table.mjs
@@ -1435,7 +1435,7 @@ class CdkTable {
             this._customRowDefs,
             this._customHeaderRowDefs,
             this._customFooterRowDefs,
-            this._columnDefsByName
+            this._columnDefsByName,
         ].forEach(def => {
             def.clear();
         });
