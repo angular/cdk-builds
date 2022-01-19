8180057d63f3e7af1c699424371f672fad3ee133
Andrew Seguin <andrewjs@google.com>
2022-01-19 21:02:54 +0000

    master - 9b1fd6159 docs: release notes for the v13.2.0-rc.0 release

--- fesm2020/cdk.mjs
+++ fesm2020/cdk.mjs
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('13.2.0-next.1');
+const VERSION = new Version('13.3.0-next.0');
 
 /**
  * @license
