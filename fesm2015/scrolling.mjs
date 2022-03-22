09fafd23119cc3f47d8f4d924af9635509a9de09
Wagner Maciel <wagnermaciel@google.com>
2022-03-22 18:11:10 +0000

    13.3.x - d87ba370a Revert "perf(cdk/scrolling): do not run change detection if there are no `viewChange` listeners (#23987)"

--- fesm2015/scrolling.mjs
+++ fesm2015/scrolling.mjs
@@ -1114,9 +1114,7 @@ class CdkVirtualForOf {
         });
         this._viewport.renderedRangeStream.pipe(takeUntil(this._destroyed)).subscribe(range => {
             this._renderedRange = range;
-            if (this.viewChange.observers.length) {
-                ngZone.run(() => this.viewChange.next(this._renderedRange));
-            }
+            ngZone.run(() => this.viewChange.next(this._renderedRange));
             this._onRenderedDataChange();
         });
         this._viewport.attach(this);
