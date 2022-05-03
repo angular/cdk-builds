0ffe1fa78f4cd30a420b4641fc38d2faccc0c78b
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2022-05-03 18:15:46 +0000

    main - e31343f8f build: resolve type bundling issues (#24865)

--- menu/menu-group.d.ts
+++
@@ -1,8 +0,0 @@
-import * as i0 from "@angular/core";
-/**
- * A grouping container for `CdkMenuItemRadio` instances, similar to a `role="radiogroup"` element.
- */
-export declare class CdkMenuGroup {
-    static ɵfac: i0.ɵɵFactoryDeclaration<CdkMenuGroup, never>;
-    static ɵdir: i0.ɵɵDirectiveDeclaration<CdkMenuGroup, "[cdkMenuGroup]", ["cdkMenuGroup"], {}, {}, never, never, false>;
-}
