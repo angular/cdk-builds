91366a09c71c932894383dd0b7202d1a96b5b495
Amy Sorto <8575252+amysorto@users.noreply.github.com>
2022-09-22 21:08:39 +0000

    main - 15288a9aa fix(material/schematics): update and handle import statements for the MDC migration

--- schematics/update-tool/public-api.d.ts
+++ schematics/update-tool/public-api.d.ts
@@ -12,4 +12,5 @@ export * from './migration';
 export * from './target-version';
 export * from './utils/decorators';
 export * from './utils/imports';
+export * from './utils/property-name';
 export * from './version-changes';
