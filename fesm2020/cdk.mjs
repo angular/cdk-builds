3f3416f4d2da8a9851434f9f2097e4f97f8957f9
Miles Malerba <mmalerba@google.com>
2022-11-09 23:10:14 +0000

    14.2.x - 441742713 release: cut the v14.2.7 release

--- fesm2020/cdk.mjs
+++ fesm2020/cdk.mjs
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('14.2.6');
+const VERSION = new Version('14.2.7');
 
 /**
  * @license
