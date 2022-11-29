d93519b56fac38d34f3c4a2bf7be27c92dc7cd51
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2022-11-29 05:28:28 +0000

    main - bbdadefb6 build: update to latest version of parse5 (#26086)

--- schematics/utils/parse5-element.d.ts
+++ schematics/utils/parse5-element.d.ts
@@ -5,6 +5,8 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import { Element } from 'parse5';
+import { DefaultTreeAdapterMap } from 'parse5';
+export declare type Element = DefaultTreeAdapterMap['element'];
+export declare type ChildNode = DefaultTreeAdapterMap['childNode'];
 /** Determines the indentation of child elements for the given Parse5 element. */
 export declare function getChildElementIndentation(element: Element): number;
