bcd8ff06120e3153d3799411a6ad58d04c553a51
Miles Malerba <mmalerba@google.com>
2023-01-05 18:22:10 +0000

    15.0.x - 2a37f3a55 release: cut the v15.0.4 release

--- fesm2020/cdk.mjs
+++ fesm2020/cdk.mjs
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('15.0.3');
+const VERSION = new Version('15.0.4');
 
 /**
  * @license
