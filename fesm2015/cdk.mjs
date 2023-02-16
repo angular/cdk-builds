fe8c3380ae6eb241cb05902adc75f84f6bc04569
Zach Arend <zachzach@google.com>
2023-02-16 19:16:18 +0000

    main - 796638b62 docs: release notes for the v15.2.0-rc.0 release

--- fesm2015/cdk.mjs
+++ fesm2015/cdk.mjs
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('15.2.0-next.4');
+const VERSION = new Version('15.3.0-next.0');
 
 /**
  * @license
