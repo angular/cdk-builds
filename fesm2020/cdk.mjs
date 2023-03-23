3c3224f5755e26a1c9aef55f0426267a3cef90b0
Wagner Maciel <wagnermaciel@google.com>
2023-03-23 19:38:57 +0000

    main - 46b985287 release: cut the v16.0.0-next.3 release

--- fesm2020/cdk.mjs
+++ fesm2020/cdk.mjs
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('16.0.0-next.2');
+const VERSION = new Version('16.0.0-next.3');
 
 /**
  * @license
