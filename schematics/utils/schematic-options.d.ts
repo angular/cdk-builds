27ecbcc69fb8dbb107769fdb31e56be8907092b1
Kristiyan Kostadinov <crisbeto@abv.bg>
2023-04-25 05:13:49 +0000

    16.0.x - 360e19be4 fix(material/schematics): support standalone projects in tree schematic

--- schematics/utils/schematic-options.d.ts
+++ schematics/utils/schematic-options.d.ts
@@ -7,6 +7,7 @@
  */
 import { ProjectDefinition } from '@angular-devkit/core/src/workspace';
 import { Schema } from '@schematics/angular/component/schema';
+import { Tree } from '@angular-devkit/schematics';
 /**
  * Returns the default options for the `@schematics/angular:component` schematic which would
  * have been specified at project initialization (ng new or ng init).
@@ -15,3 +16,5 @@ import { Schema } from '@schematics/angular/component/schema';
  * "--inlineStyle", "--skipTests" and "--inlineTemplate" options to the "component" schematic.
  */
 export declare function getDefaultComponentOptions(project: ProjectDefinition): Partial<Schema>;
+/** Determines whether the schematic is configured to be standalone. */
+export declare function isStandaloneSchematic(host: Tree, options: Schema): Promise<boolean>;
