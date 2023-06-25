b0d48fcc554067ac7c23a7ee98ecaf8102434390
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2023-06-25 13:40:20 +0000

    16.1.x - 419528977 fix(cdk/schematics): clean up deep imports of devkit APIs (#27363)

--- schematics/utils/project-main-file.d.ts
+++ schematics/utils/project-main-file.d.ts
@@ -5,7 +5,6 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import { Path } from '@angular-devkit/core';
-import { ProjectDefinition } from '@angular-devkit/core/src/workspace';
+import { Path, workspaces } from '@angular-devkit/core';
 /** Looks for the main TypeScript file in the given project and returns its path. */
-export declare function getProjectMainFile(project: ProjectDefinition): Path;
+export declare function getProjectMainFile(project: workspaces.ProjectDefinition): Path;
--- schematics/utils/project-targets.d.ts
+++ schematics/utils/project-targets.d.ts
@@ -5,14 +5,13 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import { ProjectDefinition, TargetDefinition } from '@angular-devkit/core/src/workspace';
-import { JsonValue } from '@angular-devkit/core';
+import { JsonValue, workspaces } from '@angular-devkit/core';
 /** Object that maps a CLI target to its default builder name. */
 export declare const defaultTargetBuilders: {
     build: string;
     test: string;
 };
 /** Resolves the architect options for the build target of the given project. */
-export declare function getProjectTargetOptions(project: ProjectDefinition, buildTarget: string): Record<string, JsonValue | undefined>;
+export declare function getProjectTargetOptions(project: workspaces.ProjectDefinition, buildTarget: string): Record<string, JsonValue | undefined>;
 /** Gets all targets from the given project that match the specified builder name. */
-export declare function getTargetsByBuilderName(project: ProjectDefinition, builderName: string): TargetDefinition[];
+export declare function getTargetsByBuilderName(project: workspaces.ProjectDefinition, builderName: string): workspaces.TargetDefinition[];
