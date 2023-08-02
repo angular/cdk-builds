985a1115c2b74b62d68b5af155a4fb3675eb5062
Andrew Seguin <andrewjs@google.com>
2023-08-02 20:43:53 +0000

    main - a162afdc7 docs: release notes for the v16.2.0-rc.0 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('16.2.0-next.5');
+const VERSION = new Version('16.3.0-next.0');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
