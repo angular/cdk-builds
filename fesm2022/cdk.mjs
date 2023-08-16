99e85b349dd5c4b17ba4ff683700e7114bc6c0d6
Zach Arend <zachzach@google.com>
2023-08-16 21:18:11 +0000

    16.2.x - ebe36eb19 release: cut the v16.2.1 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('16.2.0');
+const VERSION = new Version('16.2.1');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
