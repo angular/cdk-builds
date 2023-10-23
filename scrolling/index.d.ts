f0c3b27bb14f0f4abd636a53ed12b78b95a5143f
Andrew Seguin <andrewseguin@users.noreply.github.com>
2023-10-23 17:18:43 +0000

    main - 2b5a7faea fix(material/checkbox): add aria-checked (#27970)

--- scrolling/index.d.ts
+++ scrolling/index.d.ts
@@ -1,3 +1,4 @@
+import { BooleanInput } from '@angular/cdk/coercion';
 import { ChangeDetectorRef } from '@angular/core';
 import { CollectionViewer } from '@angular/cdk/collections';
 import { DataSource } from '@angular/cdk/collections';
@@ -277,7 +278,9 @@ export declare class CdkVirtualScrollViewport extends CdkVirtualScrollable imple
      * Whether rendered items should persist in the DOM after scrolling out of view. By default, items
      * will be removed.
      */
-    appendOnly: boolean;
+    get appendOnly(): boolean;
+    set appendOnly(value: BooleanInput);
+    private _appendOnly;
     /** Emits when the index of the first element visible in the viewport changes. */
     readonly scrolledIndexChange: Observable<number>;
     /** The element that wraps the rendered content. */
@@ -392,7 +395,6 @@ export declare class CdkVirtualScrollViewport extends CdkVirtualScrollable imple
     private _calculateSpacerSize;
     static ɵfac: i0.ɵɵFactoryDeclaration<CdkVirtualScrollViewport, [null, null, null, { optional: true; }, { optional: true; }, null, null, { optional: true; }]>;
     static ɵcmp: i0.ɵɵComponentDeclaration<CdkVirtualScrollViewport, "cdk-virtual-scroll-viewport", never, { "orientation": { "alias": "orientation"; "required": false; }; "appendOnly": { "alias": "appendOnly"; "required": false; }; }, { "scrolledIndexChange": "scrolledIndexChange"; }, never, ["*"], true, never>;
-    static ngAcceptInputType_appendOnly: unknown;
 }
 
 /** Time in ms to throttle the resize events by default. */
