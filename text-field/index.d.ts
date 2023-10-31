20e7a38b178b5ed9c6e5ae3c63415e2a1ac930db
Alfinity <its.alfinity@gmail.com>
2023-10-31 17:58:57 +0000

    main - 2f468f160 fix(cdk/accordion): add `CDK_ACCORDION` export (#27981)

--- text-field/index.d.ts
+++ text-field/index.d.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit } from '@angular/core';
+import { BooleanInput } from '@angular/cdk/coercion';
 import { DoCheck } from '@angular/core';
 import { ElementRef } from '@angular/core';
 import { EventEmitter } from '@angular/core';
@@ -95,7 +96,7 @@ export declare class CdkTextareaAutosize implements AfterViewInit, DoCheck, OnDe
     set maxRows(value: NumberInput);
     /** Whether autosizing is enabled or not */
     get enabled(): boolean;
-    set enabled(value: boolean);
+    set enabled(value: BooleanInput);
     get placeholder(): string;
     set placeholder(value: string);
     /** Cached height of a textarea with a single row. */
@@ -151,7 +152,6 @@ export declare class CdkTextareaAutosize implements AfterViewInit, DoCheck, OnDe
     private _scrollToCaretPosition;
     static ɵfac: i0.ɵɵFactoryDeclaration<CdkTextareaAutosize, [null, null, null, { optional: true; }]>;
     static ɵdir: i0.ɵɵDirectiveDeclaration<CdkTextareaAutosize, "textarea[cdkTextareaAutosize]", ["cdkTextareaAutosize"], { "minRows": { "alias": "cdkAutosizeMinRows"; "required": false; }; "maxRows": { "alias": "cdkAutosizeMaxRows"; "required": false; }; "enabled": { "alias": "cdkTextareaAutosize"; "required": false; }; "placeholder": { "alias": "placeholder"; "required": false; }; }, {}, never, never, false, never>;
-    static ngAcceptInputType_enabled: unknown;
 }
 
 declare namespace i1 {
