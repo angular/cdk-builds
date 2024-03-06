6238ea52ab8daa02879ac0141165c792036428ec
Amy Sorto <8575252+amysorto@users.noreply.github.com>
2024-03-06 20:18:29 +0000

    17.3.x - 4e0e120 release: cut the v17.3.0-rc.0 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('17.3.0-next.0');
+const VERSION = new Version('17.3.0-rc.0');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
