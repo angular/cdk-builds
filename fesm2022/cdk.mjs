e2d44e11a8ecfa138f461bc83f718205701356fa
Miles Malerba <mmalerba@google.com>
2024-03-20 21:13:55 +0000

    17.3.x - 1623d66 release: cut the v17.3.1 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('17.3.0');
+const VERSION = new Version('17.3.1');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
