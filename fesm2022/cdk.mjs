93332c2322c213b8c04074eafd173d15c449c072
Andrew Seguin <andrewjs@google.com>
2024-05-02 12:35:08 +0000

    17.3.x - 9a0a467 release: cut the v17.3.7 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('17.3.6');
+const VERSION = new Version('17.3.7');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
