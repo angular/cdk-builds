1c8c4f81931ec73111aedf6caf73378fde462f1c
Miles Malerba <mmalerba@google.com>
2024-08-07 20:41:41 +0000

    main - 9938d24 docs: release notes for the v18.2.0-rc.0 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('18.2.0-next.3');
+const VERSION = new Version('18.3.0-next.0');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
