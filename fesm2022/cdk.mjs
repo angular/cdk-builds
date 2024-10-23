aec74d536507854bb4b27a91c959e886bc01e2ef
Andrew Seguin <andrewjs@google.com>
2024-10-23 20:53:34 +0000

    19.0.x - a2c52c1 release: cut the v19.0.0-next.10 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('19.0.0-next.9');
+const VERSION = new Version('19.0.0-next.10');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
