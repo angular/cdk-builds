a1340c65cf0ab7d7b31bcbe9a210ada55f4c6a84
Karl Seamon <bannanafiend@gmail.com>
2024-10-28 07:42:02 +0000

    main - 8c52b6d perf(cdk/table): Use ResizeObservers instead of dom measurement to reduce layout thrashing (where possible) (#29814)

--- table/index.d.ts
+++ table/index.d.ts
@@ -1063,6 +1063,10 @@ export declare class StickyStyler {
     private _isBrowser;
     private readonly _needsPositionStickyOnElement;
     private readonly _positionListener?;
+    private _elemSizeCache;
+    private _resizeObserver;
+    private _updatedStickyColumnsParamsToReplay;
+    private _stickyColumnsReplayTimeout;
     private _cachedCellWidths;
     private readonly _borderCellCss;
     /**
@@ -1097,8 +1101,9 @@ export declare class StickyStyler {
      *     in this index position should be stuck to the end of the row.
      * @param recalculateCellWidths Whether the sticky styler should recalculate the width of each
      *     column cell. If `false` cached widths will be used instead.
+     * @param replay Whether to enqueue this call for replay after a ResizeObserver update.
      */
-    updateStickyColumns(rows: HTMLElement[], stickyStartStates: boolean[], stickyEndStates: boolean[], recalculateCellWidths?: boolean): void;
+    updateStickyColumns(rows: HTMLElement[], stickyStartStates: boolean[], stickyEndStates: boolean[], recalculateCellWidths?: boolean, replay?: boolean): void;
     /**
      * Applies sticky positioning to the row's cells if using the native table layout, and to the
      * row itself otherwise.
@@ -1156,6 +1161,20 @@ export declare class StickyStyler {
      * Non-sticky cells do not need to have a value set since their positions will not be applied.
      */
     _getStickyEndColumnPositions(widths: number[], stickyStates: boolean[]): number[];
+    /**
+     * Retreives the most recently observed size of the specified element from the cache, or
+     * meaures it directly if not yet cached.
+     */
+    private _retrieveElementSize;
+    /**
+     * Conditionally enqueue the requested sticky update and clear previously queued updates
+     * for the same rows.
+     */
+    private _updateStickyColumnReplayQueue;
+    /** Remove updates for the specified rows from the queue. */
+    private _removeFromStickyColumnReplayQueue;
+    /** Update _elemSizeCache with the observed sizes. */
+    private _updateCachedSizes;
 }
 
 export declare interface StickyUpdate {
