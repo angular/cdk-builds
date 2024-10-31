10a7fbada8651d4142a6b26eab6939941e428620
Amy Sorto <8575252+amysorto@users.noreply.github.com>
2024-10-31 14:13:47 +0000

    18.2.x - 3565b50 release: cut the v18.2.11 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('18.2.10');
+const VERSION = new Version('18.2.11');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
