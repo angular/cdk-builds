ed7af92acfc0fff55761720c8e9cfc0d08c2fb84
Amy Sorto <8575252+amysorto@users.noreply.github.com>
2024-10-31 14:25:23 +0000

    19.0.x - a5d9004 release: cut the v19.0.0-rc.0 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('19.0.0-next.10');
+const VERSION = new Version('19.0.0-rc.0');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
