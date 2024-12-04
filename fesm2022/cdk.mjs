eae06ef65465abfce861c4a0a23c62bd2b9051fb
Andrew Seguin <andrewjs@google.com>
2024-12-04 18:53:21 +0000

    19.0.x - 8f2b8f3 release: cut the v19.0.2 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('19.0.1');
+const VERSION = new Version('19.0.2');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
