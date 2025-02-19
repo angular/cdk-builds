0732c410a31908a729b60f50a7aa0178ecbc6593
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-02-19 18:28:24 +0000

    main - abcbf51 docs: release notes for the v19.2.0-rc.0 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('19.2.0-next.4');
+const VERSION = new Version('19.3.0-next.0');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
