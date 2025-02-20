f044b5ddca1770d7e89d9419a5db27d523e661ff
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-02-20 06:22:27 +0000

    main - d7150a4 build: update main branch to v20 (#30524)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('19.3.0-next.0');
+const VERSION = new Version('20.0.0-next.0');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
