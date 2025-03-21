925daaa90893ab79d91ed93ad9eaa49aa53faa67
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-03-21 09:48:38 +0000

    19.2.x - 4a2bd9f release: cut the v19.2.6 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('19.2.5');
+const VERSION = new Version('19.2.6');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
