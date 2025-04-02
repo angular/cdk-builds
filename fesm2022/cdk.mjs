5ca0511ca29a8e2e41479ddfe7703b4c33cff721
Miles Malerba <mmalerba@google.com>
2025-04-02 20:21:56 +0000

    19.2.x - c5c5d13 release: cut the v19.2.8 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('19.2.7');
+const VERSION = new Version('19.2.8');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
