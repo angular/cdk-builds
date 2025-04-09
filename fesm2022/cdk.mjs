151161f86af01334046a3da9c6c6eb3e3dd4efb2
Wagner Maciel <wagnermaciel@google.com>
2025-04-09 18:10:38 +0000

    19.2.x - 94c2c0a release: cut the v19.2.9 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('19.2.8');
+const VERSION = new Version('19.2.9');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
