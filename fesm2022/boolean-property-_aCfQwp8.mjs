bc3de143b045ba9c431ababeaf5defa6881bcd50
Joey Perrott <josephperrott@gmail.com>
2025-04-23 18:20:31 +0000

    19.2.x - 54cc2cf [PATCH] Migrate to use rules_js based ng_package (#30940)

--- fesm2022/boolean-property-_aCfQwp8.mjs
+++
@@ -1,7 +0,0 @@
-/** Coerces a data-bound value (typically a string) to a boolean. */
-function coerceBooleanProperty(value) {
-    return value != null && `${value}` !== 'false';
-}
-
-export { coerceBooleanProperty };
-//# sourceMappingURL=boolean-property-_aCfQwp8.mjs.map
---
+++ npm_package/fesm2022/boolean-property-DaaVhX5A.mjs
@@ -0,0 +1,7 @@
+/** Coerces a data-bound value (typically a string) to a boolean. */
+function coerceBooleanProperty(value) {
+    return value != null && `${value}` !== 'false';
+}
+
+export { coerceBooleanProperty as c };
+//# sourceMappingURL=boolean-property-DaaVhX5A.mjs.map
