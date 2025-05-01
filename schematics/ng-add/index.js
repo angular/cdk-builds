840fed9a927bf214b5f153361370a27944246f00
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-01 12:40:37 +0000

    20.0.x - 12f7de8 release: cut the v20.0.0-next.10 release

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -26,6 +26,6 @@ function default_1() {
     // In order to align the CDK version with other Angular dependencies that are setup by
     // `@schematics/angular`, we use tilde instead of caret. This is default for Angular
     // dependencies in new CLI projects.
-    return (0, utility_1.addDependency)('@angular/cdk', `~20.0.0-next.9`, { existing: utility_1.ExistingBehavior.Skip });
+    return (0, utility_1.addDependency)('@angular/cdk', `~20.0.0-next.10`, { existing: utility_1.ExistingBehavior.Skip });
 }
 //# sourceMappingURL=index.js.map
