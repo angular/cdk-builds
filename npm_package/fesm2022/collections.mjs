ec435c3903214cfc3ef530b611619f2e20b6b370
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-05-01 08:26:07 +0000

    19.2.x - 6aff47c build: release script producing duplicate results (#30999)

--- npm_package/fesm2022/collections.mjs
+++
@@ -1,8 +0,0 @@
-export { U as UniqueSelectionDispatcher } from './unique-selection-dispatcher-DtHZDqyJ.mjs';
-export { A as ArrayDataSource, _ as _RecycleViewRepeaterStrategy, b as _VIEW_REPEATER_STRATEGY, a as _ViewRepeaterOperation } from './recycle-view-repeater-strategy-DoWdPqVw.mjs';
-export { D as DataSource, i as isDataSource } from './data-source-D34wiQZj.mjs';
-export { _ as _DisposeViewRepeaterStrategy } from './dispose-view-repeater-strategy-D_JReLI1.mjs';
-export { S as SelectionModel, g as getMultipleValuesInSingleSelectionError } from './selection-model-CeeHVIcP.mjs';
-import '@angular/core';
-import 'rxjs';
-//# sourceMappingURL=collections.mjs.map
