e6748c76a3719017eb4731b1da3d142e10ff6e22
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-01 12:34:31 +0000

    main - 68f8d14 release: cut the v20.0.0-next.9 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('20.0.0-next.8');
+const VERSION = new Version('20.0.0-next.9');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
