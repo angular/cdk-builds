70c6ddbd45766b9ab42586de338e97175ea05563
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-20 12:33:38 +0000

    20.1.x - 4c73d82 release: cut the v20.1.7 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('20.1.6');
+const VERSION = new Version('20.1.7');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
