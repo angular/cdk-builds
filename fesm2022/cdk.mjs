8e81f5b4ec5cb0db9847c4c69ded2fe884ce2ccc
Andrew Seguin <andrewseguin@google.com>
2025-09-03 14:06:07 +0000

    main - ca753cd release: cut the v21.0.0-next.2 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('21.0.0-next.1');
+const VERSION = new Version('21.0.0-next.2');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
