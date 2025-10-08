fba20d297bdce0ab04d6fbba1b06159807b47798
Angular Robot <angular-robot@google.com>
2025-10-08 15:06:46 +0000

    20.2.x - 4e1f2d1 build: update github/codeql-action action to v3.30.7 (#32021)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('20.2.7');
+const VERSION = new Version('20.2.8');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
