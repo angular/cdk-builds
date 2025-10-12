f6ac1da38b8d3f1f52df5c78162b3303bf82c413
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-12 23:30:37 +0000

    main - 544438c fix(cdk/testing): make setContenteditableValue required (#32058)

--- types/_harness-environment-chunk.d.ts
+++ types/_harness-environment-chunk.d.ts
@@ -117,9 +117,8 @@ interface TestElement {
     /**
      * Sets the value of a `contenteditable` element.
      * @param value Value to be set on the element.
-     * @breaking-change 16.0.0 Will become a required method.
      */
-    setContenteditableValue?(value: string): Promise<void>;
+    setContenteditableValue(value: string): Promise<void>;
     /** Gets the value for the given attribute from the element. */
     getAttribute(name: string): Promise<string | null>;
     /** Checks whether the element has the given class. */
