ae6eeb3322e72bd36868b937ba053aaf83da124f
tjshiu <35056071+tjshiu@users.noreply.github.com>
2025-10-31 16:54:30 +0000

    20.2.x - a257c72 release: cut the v20.2.11 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Angular Component Development Kit. */
-const VERSION = new Version('20.2.10');
+const VERSION = new Version('20.2.11');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
