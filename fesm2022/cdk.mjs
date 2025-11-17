9ce902416a8c70733fc3f2891896ca09db775190
Miles Malerba <mmalerba@google.com>
2025-11-17 22:46:31 +0000

    21.0.x - e7dd78a release: cut the v21.0.0-rc.3 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.2');
+const VERSION = new Version('21.0.0-rc.3');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
