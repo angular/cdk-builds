31492d1c3d3c4f3dc66c5fbc1aee580aa2fb7f9e
Wagner Maciel <wagnermaciel@google.com>
2025-12-03 21:09:57 +0000

    main - 11cee77 release: cut the v21.1.0-next.1 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.0');
+const VERSION = new Version('21.1.0-next.1');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
