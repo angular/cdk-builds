0f465cd702d2c2342030ddc1c9bdad1df221f615
Wagner Maciel <wagnermaciel@google.com>
2025-12-04 19:43:54 +0000

    main - 76f2603 docs(aria/menu): separate menubar examples (#32476)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.1+sha-98d6693');
+const VERSION = new Version('21.1.0-next.1+sha-76f2603');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
