f223ca91296e0b41573929c796d91c77625f3441
Alan Agius <alanagius@google.com>
2025-12-04 15:25:31 +0000

    21.0.x - be533f1 ci: update snapshots CI workflow to use the new `build-snapshots` script (#32474)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.2');
+const VERSION = new Version('21.0.2+sha-be533f1');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
