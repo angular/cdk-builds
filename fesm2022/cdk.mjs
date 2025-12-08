98ebc09871077cf7de9e2df8ae0e286fc7ec0d07
Joey Perrott <josephperrott@gmail.com>
2025-12-08 18:13:56 +0000

    main - fa73b35 build: update to bazel version 8.4.2 (#32490)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.1+sha-c4fa201');
+const VERSION = new Version('21.1.0-next.1+sha-fa73b35');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
