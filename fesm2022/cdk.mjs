bb112e2b09e64ac0c17b35e57b659909cc1a1b24
Andrew Seguin <andrewseguin@users.noreply.github.com>
2025-12-08 20:19:06 +0000

    21.0.x - 151322f fix(material/core): default secondary color for focus indicator (#32491)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.2+sha-875b7fc');
+const VERSION = new Version('21.0.2+sha-151322f');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
