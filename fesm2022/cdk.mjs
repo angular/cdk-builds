289f8416e5e22b8e0993d6b8e3fbfc2d07425fce
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-12-08 17:09:18 +0000

    main - c4fa201 build: link directly to drag&drop on adev (#32473)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.1+sha-52720a3');
+const VERSION = new Version('21.1.0-next.1+sha-c4fa201');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
