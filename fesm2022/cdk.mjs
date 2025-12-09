b38904bc8c0d49c20f04929e90196c1caf7b9bfb
Joey Perrott <josephperrott@gmail.com>
2025-12-09 21:34:10 +0000

    main - 472d382 [main] build: update cross-repo angular dependencies  (#32478)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.1+sha-eccaecd');
+const VERSION = new Version('21.1.0-next.1+sha-472d382');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
