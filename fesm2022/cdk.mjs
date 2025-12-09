d21ef92e0f537f6b34741fa22b556c8142786bc6
Joey Perrott <josephperrott@gmail.com>
2025-12-09 22:13:52 +0000

    main - 43fddbc build: update formatting ignore controls to be done via prettierignore (#32506)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.1+sha-ad3e7d2');
+const VERSION = new Version('21.1.0-next.1+sha-43fddbc');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
