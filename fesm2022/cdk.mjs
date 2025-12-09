b145a89cf436627f5e862d9e652fc9fc09f7d054
Wagner Maciel <wagnermaciel@google.com>
2025-12-09 21:42:52 +0000

    main - ad3e7d2 docs: create aria autocomplete docs (#32499)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.1+sha-472d382');
+const VERSION = new Version('21.1.0-next.1+sha-ad3e7d2');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
