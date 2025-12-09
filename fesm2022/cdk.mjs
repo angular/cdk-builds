2213a8b136799702cba4001b116cea5763f7212b
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-12-09 08:59:16 +0000

    main - b7b4baa refactor(aria/listbox): avoid circular references at runtime (#32488)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.1+sha-ed7e1da');
+const VERSION = new Version('21.1.0-next.1+sha-b7b4baa');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
