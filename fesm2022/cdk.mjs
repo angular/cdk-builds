85978f3110686f82c3f9d85ea3aba776ff9cfb4d
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-12-09 17:36:03 +0000

    main - 6b6c4ff build: reformat files after dependency update (#32501)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.1+sha-1fab386');
+const VERSION = new Version('21.1.0-next.1+sha-6b6c4ff');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
