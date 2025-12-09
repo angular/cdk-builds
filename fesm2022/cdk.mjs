5345f2cba7f68ae916ed88e6dda912328424213e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-12-09 17:25:26 +0000

    main - 1fab386 fix(material/table): fixed layout not working (#32494)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.1+sha-b7b4baa');
+const VERSION = new Version('21.1.0-next.1+sha-1fab386');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
