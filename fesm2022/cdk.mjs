876c7c5af380e85ce390dfc704a9ed4d001ef1fc
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-12-10 12:56:52 +0000

    21.0.x - 79603d6 refactor(aria/tree): split directives and resolve circular dependencies

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.2+sha-a0489f2');
+const VERSION = new Version('21.0.2+sha-79603d6');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
