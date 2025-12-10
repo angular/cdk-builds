619a0af524106d894871b15be3058a16640ebd17
Andrew Seguin <andrewjs@google.com>
2025-12-10 20:40:15 +0000

    main - df49bca docs: release notes for the v21.0.3 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.1+sha-806cfa4');
+const VERSION = new Version('21.1.0-next.1+sha-df49bca');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
