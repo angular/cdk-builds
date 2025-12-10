f9ce78e47a01087ae67a2d444e0723f1828a4897
Andrew Seguin <andrewjs@google.com>
2025-12-10 20:45:38 +0000

    main - eab53ab release: cut the v21.1.0-next.2 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.1+sha-df49bca');
+const VERSION = new Version('21.1.0-next.2+sha-eab53ab');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
