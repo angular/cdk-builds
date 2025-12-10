55dfcd98540a43ae8e82e869307a201519415075
Andrew Seguin <andrewseguin@users.noreply.github.com>
2025-12-10 21:03:16 +0000

    21.0.x - 1620cab fix(multiple): use focus-visible for focus indicator (#32514)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.3+sha-9748ab5');
+const VERSION = new Version('21.0.3+sha-1620cab');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
