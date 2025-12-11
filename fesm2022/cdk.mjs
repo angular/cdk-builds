4b1a243bfa0b042cbb610adc75e4a0f3d269776c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-12-11 09:24:54 +0000

    21.0.x - 05e7724 refactor(multiple): use relative imports within aria package (#32522)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.3+sha-1620cab');
+const VERSION = new Version('21.0.3+sha-05e7724');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
