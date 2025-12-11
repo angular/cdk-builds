9d012aa40c33b9c27a8aac39af557a713d4710a0
Dominik Rabij <dominikrabij@google.com>
2025-12-11 13:34:08 +0000

    21.0.x - 4256fcb refactor(multiple): eliminate usages of any type batch #4 (#32394)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.3+sha-05e7724');
+const VERSION = new Version('21.0.3+sha-4256fcb');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
