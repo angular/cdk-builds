e3937921da3240b83f1b4b4dc21f930fa802a892
Wagner Maciel <wagnermaciel@google.com>
2025-12-11 19:45:08 +0000

    main - 450641c docs: add aria select page (#32508)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.2+sha-aff31f1');
+const VERSION = new Version('21.1.0-next.2+sha-450641c');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
