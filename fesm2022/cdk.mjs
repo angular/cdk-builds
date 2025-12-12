1573f383af541e894aa95551df5011837184ea86
Jaime Burgos <73321943+SkyZeroZx@users.noreply.github.com>
2025-12-12 16:03:24 +0000

    main - c2c7669 docs: add reference links to ARIA (#32498)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.2+sha-ebb1c17');
+const VERSION = new Version('21.1.0-next.2+sha-c2c7669');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
