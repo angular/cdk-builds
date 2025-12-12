8b3873f116373e889a2593b2a20112fcd2209b38
Andrew Seguin <andrewseguin@users.noreply.github.com>
2025-12-12 23:05:10 +0000

    21.0.x - 8649802 refactor(multiple): use signal primitives (#32532)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.3+sha-2ba60b0');
+const VERSION = new Version('21.0.3+sha-8649802');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
