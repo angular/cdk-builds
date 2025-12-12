12ab5eb7c47400e22fecec56976b0f1109ff1e2a
Angular Robot <angular-robot@google.com>
2025-12-12 18:36:23 +0000

    main - e619880 build: lock file maintenance (#32500)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.2+sha-4574ae5');
+const VERSION = new Version('21.1.0-next.2+sha-e619880');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
