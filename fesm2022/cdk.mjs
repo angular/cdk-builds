80b66f37e5f749e052c66a3266649bdf826f4b5c
Wagner Maciel <wagnermaciel@google.com>
2025-12-12 18:19:23 +0000

    21.0.x - 87208a1 docs(aria/combobox): autocomplete clear buttons (#32531)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.3+sha-15ce9b4');
+const VERSION = new Version('21.0.3+sha-87208a1');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
