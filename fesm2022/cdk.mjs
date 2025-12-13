ecdf8b78e83d10613f086b0968f1e4aa4c68e2ad
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-12-13 19:18:20 +0000

    main - f95fb0e refactor(multiple): avoid rxjs interop (#32534)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.2+sha-023f845');
+const VERSION = new Version('21.1.0-next.2+sha-f95fb0e');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
