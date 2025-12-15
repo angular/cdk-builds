98a4102ed6f5d049c18d7d36be28dfaad219f186
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-12-15 13:16:28 +0000

    main - d4be9ad build: set up public-api files for aria (#32535)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.2+sha-749654e');
+const VERSION = new Version('21.1.0-next.2+sha-d4be9ad');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
