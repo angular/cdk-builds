43b25fe207ffa93e492e911e14aaf42c0d384238
Angular Robot <angular-robot@google.com>
2025-12-15 13:05:42 +0000

    main - 749654e build: update actions/upload-artifact action to v6 (#32539)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.2+sha-f95fb0e');
+const VERSION = new Version('21.1.0-next.2+sha-749654e');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
