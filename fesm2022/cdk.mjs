e79cb776c2004c8e3ccf7dfbd930dad8a6645f10
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-12-16 15:42:43 +0000

    main - 045f126 refactor(material/tree): fix strict property initialization errors

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.2+sha-e1f9a7d');
+const VERSION = new Version('21.1.0-next.2+sha-045f126');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
