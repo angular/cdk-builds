922f54e208b05e89e84eb6b39dca5f0251fad2e5
Andrew Seguin <andrewseguin@users.noreply.github.com>
2025-12-16 13:45:18 +0000

    main - e1f9a7d refactor: cleanup remaining signal primitive usages (#32549)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.2+sha-8b96a84');
+const VERSION = new Version('21.1.0-next.2+sha-e1f9a7d');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
