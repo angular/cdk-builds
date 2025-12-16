dec30f6fe396e673996f4d03729ae8efc6cc14fd
Wagner Maciel <wagnermaciel@google.com>
2025-12-16 17:29:43 +0000

    21.0.x - 92260e6 docs(aria/tree): improve disabled ui (#32548)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.3+sha-e45fb73');
+const VERSION = new Version('21.0.3+sha-92260e6');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
