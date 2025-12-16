79acdde952fe154c60d2ec39d9a879084c615de3
Andrey Dolgachev <adolgachev@users.noreply.github.com>
2025-12-16 21:51:15 +0000

    main - 204788b fix(material/slide-toggle): Fix a11y issues by hiding label when it has no content. (#32480)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.2+sha-a947f68');
+const VERSION = new Version('21.1.0-next.2+sha-204788b');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
