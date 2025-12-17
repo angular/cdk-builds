da491bc1dd0f0e9f8577b12cd5dc9e0a6530fe9a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-12-17 14:01:54 +0000

    main - e63c564 fix(multiple): type issues when importing Aria directives (#32561)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.2+sha-7b076c1');
+const VERSION = new Version('21.1.0-next.2+sha-e63c564');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
