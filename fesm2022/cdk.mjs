54f0caa5fba8793ef9eb5670f9b4f92c6b61cc07
tjshiu <35056071+tjshiu@users.noreply.github.com>
2025-12-17 23:28:40 +0000

    21.0.x - 2f35755 release: cut the v21.0.4 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.3+sha-c929692');
+const VERSION = new Version('21.0.4+sha-2f35755');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -26,6 +26,6 @@ function default_1() {
     // In order to align the CDK version with other Angular dependencies that are setup by
     // `@schematics/angular`, we use tilde instead of caret. This is default for Angular
     // dependencies in new CLI projects.
-    return (0, utility_1.addDependency)('@angular/cdk', `~21.0.3+sha-c929692`, { existing: utility_1.ExistingBehavior.Skip });
+    return (0, utility_1.addDependency)('@angular/cdk', `~21.0.4+sha-2f35755`, { existing: utility_1.ExistingBehavior.Skip });
 }
 //# sourceMappingURL=index.js.map
