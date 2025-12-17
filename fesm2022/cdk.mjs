5eead643aa88dcd10c308171314b4e72add114ce
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-12-17 14:03:09 +0000

    21.0.x - ecfe310 fix(multiple): type issues when importing Aria directives (#32561)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.3+sha-d313920');
+const VERSION = new Version('21.0.3+sha-ecfe310');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
