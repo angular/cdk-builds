1dc52beb720a991bb670835bb125d7294101af87
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-12-17 12:57:31 +0000

    main - 7b076c1 refactor(google-maps): fix strict property initialization errors

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.2+sha-204788b');
+const VERSION = new Version('21.1.0-next.2+sha-7b076c1');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
