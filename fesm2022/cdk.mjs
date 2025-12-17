940f56ec5b0bd9c0357623f4425a8521c485e971
Alan Agius <alanagius@google.com>
2025-12-17 21:33:00 +0000

    main - 91fcb13 build: migrate Bazel ignore rules from .bazelignore to REPO.bazel (#32562)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.2+sha-dd40f25');
+const VERSION = new Version('21.1.0-next.2+sha-91fcb13');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
