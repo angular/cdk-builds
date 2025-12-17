d8d27781c91a17e16b0ca35800f4e82cf531bea1
Andrew Scott <atscott01@gmail.com>
2025-12-17 20:41:30 +0000

    21.0.x - 4d2a716 fix(cdk/testing): Skip task tracking if zone patches aren't present (#32544)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.3+sha-ecfe310');
+const VERSION = new Version('21.0.3+sha-4d2a716');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
