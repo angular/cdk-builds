45735507b4eef5107d4f3415348bf153325e0dbd
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-12-18 13:47:44 +0000

    main - 7b1df64 build: set up harness integration test for vitest

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.3+sha-a9be1aa');
+const VERSION = new Version('21.1.0-next.3+sha-7b1df64');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
