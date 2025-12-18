b199fa90c9f09e45a697c7d019cd871ed77dfb30
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-12-18 14:10:03 +0000

    main - c16f559 build: enable strict property initialization for entire repo (#32567)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.3+sha-7b1df64');
+const VERSION = new Version('21.1.0-next.3+sha-c16f559');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
