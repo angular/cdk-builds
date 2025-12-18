04b4e1c9ed5b1083aa08c91e23ac399e63dc6b06
Andrew Seguin <andrewjs@google.com>
2025-12-18 18:15:53 +0000

    main - da68c8b docs: release notes for the v21.0.5 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.3+sha-c16f559');
+const VERSION = new Version('21.1.0-next.3+sha-da68c8b');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
