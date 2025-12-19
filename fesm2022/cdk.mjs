475fd8d473ba20045b3393423d8a14d93a2da938
Alan Agius <alanagius@google.com>
2025-12-19 17:56:33 +0000

    main - ef53e73 build: switch Node.js toolchain to derive version from .nvmrc. [main] (#32575)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.3+sha-d77de86');
+const VERSION = new Version('21.1.0-next.3+sha-ef53e73');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
