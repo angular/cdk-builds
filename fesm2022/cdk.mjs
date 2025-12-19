19ee60b596d7b4ef70b019194ac6e936007ca6bc
Andrew Seguin <andrewseguin@users.noreply.github.com>
2025-12-19 15:11:17 +0000

    21.0.x - 7e70532 docs: disallow docs-content for search indexer (#32577)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.5+sha-8ff6ffc');
+const VERSION = new Version('21.0.5+sha-7e70532');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
