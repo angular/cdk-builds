70d555318f845db9832443839db5bad88a04f4ee
Alan Agius <alanagius@google.com>
2025-12-19 17:56:11 +0000

    21.0.x - 6080def build: switch Node.js toolchain to derive version from .nvmrc [patch] (#32576)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.5+sha-7e70532');
+const VERSION = new Version('21.0.5+sha-6080def');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
