9021c56aa6a052d67062609fe8244d9212c0031e
Karl Seamon <bannanafiend@gmail.com>
2025-12-27 05:27:40 +0000

    main - 8cff9c5 perf(material/checkbox): Use > combinator for hover selector (#32586)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.3+sha-ef53e73');
+const VERSION = new Version('21.1.0-next.3+sha-8cff9c5');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
