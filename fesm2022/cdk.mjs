e02f13790f35758b672cc3f559d021e7738a982c
Wagner Maciel <wagnermaciel@google.com>
2025-12-29 17:47:14 +0000

    main - 21650a6 refactor(multiple): create tree behavior (#32569)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.3+sha-8cff9c5');
+const VERSION = new Version('21.1.0-next.3+sha-21650a6');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
