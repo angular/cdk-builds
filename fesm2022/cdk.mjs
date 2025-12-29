97ae5702f96b3bcdf512af68b0c064fc3fa41bd6
Wagner Maciel <wagnermaciel@google.com>
2025-12-29 19:17:05 +0000

    main - c27b1b4 refactor(multiple): improve aria ListTypeahead._getItem (#32579)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.3+sha-21650a6');
+const VERSION = new Version('21.1.0-next.3+sha-c27b1b4');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
