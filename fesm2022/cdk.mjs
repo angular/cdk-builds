05acb8363292d9406e050de38d42e3df8b24660e
Ruslan Lekhman <lekhman112@gmail.com>
2026-01-02 08:15:26 +0000

    21.0.x - f22e7fa docs: improve properties and methods template (#32071)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.5+sha-8275576');
+const VERSION = new Version('21.0.5+sha-f22e7fa');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
