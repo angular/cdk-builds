7e710882310639e0921508da1f006cb8923c6bb4
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-01-05 18:20:40 +0000

    main - eba6ee8 fix(material/radio): allow aria attributes to be reset (#32613)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.3+sha-1f34b94');
+const VERSION = new Version('21.1.0-next.3+sha-eba6ee8');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
