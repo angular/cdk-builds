e25ffe4019232b9fadca1dc198268cdbe0e29200
Karl Seamon <bannanafiend@gmail.com>
2026-01-05 21:17:30 +0000

    21.0.x - ca4a7a4 perf(material/table): Use text-align: start instead of descendent dir selector. (#32585)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.5+sha-66e2a08');
+const VERSION = new Version('21.0.5+sha-ca4a7a4');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -26,6 +26,6 @@ function default_1() {
     // In order to align the CDK version with other Angular dependencies that are setup by
     // `@schematics/angular`, we use tilde instead of caret. This is default for Angular
     // dependencies in new CLI projects.
-    return (0, utility_1.addDependency)('@angular/cdk', `~21.0.5+sha-66e2a08`, { existing: utility_1.ExistingBehavior.Skip });
+    return (0, utility_1.addDependency)('@angular/cdk', `~21.0.5+sha-ca4a7a4`, { existing: utility_1.ExistingBehavior.Skip });
 }
 //# sourceMappingURL=index.js.map
