1d00da20ec8300d289d74c4826b0b7168aaa9220
Karl Seamon <bannanafiend@gmail.com>
2026-01-05 21:18:07 +0000

    main - 89bdf48 perf(material/table): Use text-align: start instead of descendent dir selector. (#32585)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.3+sha-eba6ee8');
+const VERSION = new Version('21.1.0-next.3+sha-89bdf48');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
