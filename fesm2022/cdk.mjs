23618a9ea4001ec4cd5d39ebed2db7d9bbaba274
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-01-05 08:54:44 +0000

    main - 83e30e2 build: disable formatting on styles and templates (#32614)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.3+sha-869d4f8');
+const VERSION = new Version('21.1.0-next.3+sha-83e30e2');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
