43899694456ff1477780339cbae77e95d10aa651
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-01-05 11:23:18 +0000

    21.0.x - e4ce169 fix(material/select): global option not disabling popover (#32610)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.5+sha-1396042');
+const VERSION = new Version('21.0.5+sha-e4ce169');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
