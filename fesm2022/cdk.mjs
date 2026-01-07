c003ee832effdad80b17c80de44df638e6d1d045
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-01-07 17:21:51 +0000

    21.0.x - 00c8d29 build: bump year (#32608)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.5+sha-ca4a7a4');
+const VERSION = new Version('21.0.5+sha-00c8d29');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
