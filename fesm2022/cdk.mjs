8e62c7e6adbb1ea61ce1dae7bdd92f795f75c859
Miles Malerba <mmalerba@google.com>
2026-01-08 21:41:18 +0000

    main - 7abceee docs: release notes for the v21.0.6 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.3+sha-14479d4');
+const VERSION = new Version('21.1.0-next.3+sha-7abceee');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
