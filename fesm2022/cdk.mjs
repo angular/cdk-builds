ba106366ba38e32d0f63d7a64b8ca3c79c63933a
Miles Malerba <mmalerba@google.com>
2026-01-08 22:59:46 +0000

    main - 8875da1 release: cut the v21.1.0-next.4 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.3+sha-7abceee');
+const VERSION = new Version('21.1.0-next.4+sha-8875da1');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
