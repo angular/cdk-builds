831afc3476ce9465275e63295164c8211d599368
Jeremy Mowery <jeremy@mowery.dev>
2026-01-08 13:09:04 +0000

    21.0.x - 96a20fd build(multiple): Recommend installing the angular language service extension and bazel extension (#32599)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.5+sha-00c8d29');
+const VERSION = new Version('21.0.5+sha-96a20fd');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -26,6 +26,6 @@ function default_1() {
     // In order to align the CDK version with other Angular dependencies that are setup by
     // `@schematics/angular`, we use tilde instead of caret. This is default for Angular
     // dependencies in new CLI projects.
-    return (0, utility_1.addDependency)('@angular/cdk', `~21.0.5+sha-00c8d29`, { existing: utility_1.ExistingBehavior.Skip });
+    return (0, utility_1.addDependency)('@angular/cdk', `~21.0.5+sha-96a20fd`, { existing: utility_1.ExistingBehavior.Skip });
 }
 //# sourceMappingURL=index.js.map
