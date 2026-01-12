0539dd4d010c119630e6bcf597393489e1313bb4
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-01-12 08:24:54 +0000

    main - 02542de refactor(cdk/overlay): revert internal workaround again (#32626)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.4+sha-8875da1');
+const VERSION = new Version('21.1.0-next.4+sha-02542de');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
