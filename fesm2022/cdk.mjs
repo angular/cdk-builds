1ab9962498d6903432ccc38546c01cf09bb1a366
Md.Asif Parvez Sarker <AsifParvezsarker26@gmail.com>
2026-01-13 11:53:35 +0000

    main - fb1b66b fix(material/slider): add missing closing parenthesis in tick mark transform (#32580)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.4+sha-5a39e79');
+const VERSION = new Version('21.1.0-next.4+sha-fb1b66b');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
