2b10c0920987edca0998665df5ce57cd9af08836
Angular Robot <angular-robot@google.com>
2026-01-13 11:41:29 +0000

    main - 5a39e79 build: lock file maintenance (#32550)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.4+sha-17c910e');
+const VERSION = new Version('21.1.0-next.4+sha-5a39e79');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -26,6 +26,6 @@ function default_1() {
     // In order to align the CDK version with other Angular dependencies that are setup by
     // `@schematics/angular`, we use tilde instead of caret. This is default for Angular
     // dependencies in new CLI projects.
-    return (0, utility_1.addDependency)('@angular/cdk', `~21.1.0-next.4+sha-17c910e`, { existing: utility_1.ExistingBehavior.Skip });
+    return (0, utility_1.addDependency)('@angular/cdk', `~21.1.0-next.4+sha-5a39e79`, { existing: utility_1.ExistingBehavior.Skip });
 }
 //# sourceMappingURL=index.js.map
