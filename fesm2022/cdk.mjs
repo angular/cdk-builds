16119783eaa0913bb50ea55e4016b67525f25351
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-01-13 10:50:53 +0000

    main - 17c910e docs(cdk/drag-drop): remove live examples and docs (#32629)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.4+sha-fa13870');
+const VERSION = new Version('21.1.0-next.4+sha-17c910e');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
