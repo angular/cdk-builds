b8d1e7709bee14e57f398f7bc3832995da4f55e1
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-01-14 18:46:15 +0000

    main - d6a834e refactor(material/sidenav): run listener through renderer

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0-next.0+sha-008aa7a');
+const VERSION = new Version('21.2.0-next.0+sha-d6a834e');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
