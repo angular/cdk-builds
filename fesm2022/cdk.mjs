da0e1bb27fa3b452d729326d5dc148319bd4b605
Angular Robot <angular-robot@google.com>
2026-01-14 13:55:22 +0000

    main - fdb22b6 build: update cross-repo angular dependencies (#32638)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.4+sha-c40f428');
+const VERSION = new Version('21.1.0-next.4+sha-fdb22b6');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
