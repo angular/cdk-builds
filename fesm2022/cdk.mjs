4985d2f91daaeb426ce11105262dc5531bbf4d1b
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-01-19 08:15:08 +0000

    main - 454ff0f refactor(material/tooltip): drop NgClass dependency (#32666)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0-next.0+sha-71cdb84');
+const VERSION = new Version('21.2.0-next.0+sha-454ff0f');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
