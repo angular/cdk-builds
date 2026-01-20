655e1b2b43855f36e41a029d77caa3e4bde46548
Dominik Rabij <dominikrabij@google.com>
2026-01-20 18:06:54 +0000

    main - 019b902 refactor(material/table): add filterPredicate non-object warning (#32578)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0-next.0+sha-1f8b4b5');
+const VERSION = new Version('21.2.0-next.0+sha-019b902');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
