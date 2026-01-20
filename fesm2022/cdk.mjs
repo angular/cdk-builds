eda2f7aca20d726016fdc0954ffe3de81c158e38
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-01-20 08:03:58 +0000

    21.1.x - 6b5466a fix(multiple): incorrect paths in declaration files for aria

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0+sha-028f89b');
+const VERSION = new Version('21.1.0+sha-6b5466a');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
