421f2e8b881d976cb184a201467e84f7da8a41f6
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-01-20 06:41:42 +0000

    21.1.x - 028f89b fix(multiple): resolve sass if function deprecation warnings (#32674)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0+sha-3de1770');
+const VERSION = new Version('21.1.0+sha-028f89b');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
