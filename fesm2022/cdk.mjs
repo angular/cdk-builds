47a531348478a04e881814e969e01946540248c6
Cheng-Hsuan Tsai <webcat@google.com>
2026-01-20 19:13:16 +0000

    main - ec384a3 fix(aria/grid): unit tests for grid pattern (#32633)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0-next.0+sha-019b902');
+const VERSION = new Version('21.2.0-next.0+sha-ec384a3');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
