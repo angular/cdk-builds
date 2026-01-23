1d3bd9d8a44c185f396f6dc25c9da1f0818a9b2d
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-01-23 06:57:14 +0000

    main - e78587f fix(cdk/table): throw when multiple row templates are used with virtual scrolling (#32682)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0-next.1+sha-7cb78db');
+const VERSION = new Version('21.2.0-next.1+sha-e78587f');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
