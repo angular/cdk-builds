dfe63176edb09c71703d7905db415760de623b55
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-01-26 08:50:05 +0000

    21.1.x - 9b54cb2 fix(cdk/listbox): incorrect wording in error message (#32697)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.1+sha-4b8fc71');
+const VERSION = new Version('21.1.1+sha-9b54cb2');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
