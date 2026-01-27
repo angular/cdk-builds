9fd9e6c7303676ac1a23d629e40dbb2efb38ea42
Andrey Dolgachev <adolgachev@users.noreply.github.com>
2026-01-27 23:45:51 +0000

    21.1.x - 1d9918a fix(aria/combobox): Add announcement for empty results to autocomplete demo (#32707)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.1+sha-7824225');
+const VERSION = new Version('21.1.1+sha-1d9918a');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
