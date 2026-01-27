83af3521babdf14b80a8026b437d531d9dc8814b
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-01-27 17:21:23 +0000

    main - 34bcffb fix(material/timepicker): make it easier to style timepicker panel (#32711)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0-next.1+sha-0395ca2');
+const VERSION = new Version('21.2.0-next.1+sha-34bcffb');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
