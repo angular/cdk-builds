c79cd74c9e0ddcb088936f425c9150b85666a0fa
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-01-27 08:26:56 +0000

    main - 0395ca2 perf(aria/grid): avoid excessive change detections (#32705)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0-next.1+sha-c71256d');
+const VERSION = new Version('21.2.0-next.1+sha-0395ca2');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
