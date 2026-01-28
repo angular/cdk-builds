4c2a579b8ef9bbbdac388cf8624a3ce3f94f4f7b
Andrey Dolgachev <adolgachev@users.noreply.github.com>
2026-01-28 21:55:09 +0000

    main - dbb94dc [aria/multiple] Add to and clean up input validation across aria components (#32713)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0-next.1+sha-82a51f3');
+const VERSION = new Version('21.2.0-next.1+sha-dbb94dc');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
