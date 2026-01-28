6e951bdb84cf605431239d73fe9e7e923623974c
Mathieu <72860893+MathieuAbbal@users.noreply.github.com>
2026-01-28 23:03:07 +0000

    main - d5925b6 docs(material/sidenav): fix typo in drawer comment (#32702)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0-next.1+sha-ed448e6');
+const VERSION = new Version('21.2.0-next.1+sha-d5925b6');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
