e3ca448a376a6d17c730fe6816e08caf4788d531
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-01-29 07:39:41 +0000

    main - d67ac1f build: update to jasmine 6 (#32694)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0-next.1+sha-cce8b51');
+const VERSION = new Version('21.2.0-next.1+sha-d67ac1f');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
