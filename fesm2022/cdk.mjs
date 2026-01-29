fbd63bce672519957ef3a2f81f9603ab5301acc1
tjshiu <35056071+tjshiu@users.noreply.github.com>
2026-01-29 00:54:55 +0000

    main - cce8b51 docs: release notes for the v21.1.2 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0-next.1+sha-d5925b6');
+const VERSION = new Version('21.2.0-next.1+sha-cce8b51');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
