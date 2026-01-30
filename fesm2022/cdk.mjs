4fe9a12f665bda5eb0e65420782f6b1f43aaad6b
Michael Small <85510853+msmallest@users.noreply.github.com>
2026-01-30 18:13:09 +0000

    main - 80be4e6 docs(cdk/menu): add required `'value'` to `ngMenuItem` jsdocs (#32440)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0-next.2+sha-727a11d');
+const VERSION = new Version('21.2.0-next.2+sha-80be4e6');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
