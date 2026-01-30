f936b5673ef8d50456fdc9c70c50748335f4623a
Andrey Dolgachev <adolgachev@users.noreply.github.com>
2026-01-30 20:04:21 +0000

    21.1.x - 4186f74 fix(aria/menu): Add label property for proper aria-label (#32710)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.2+sha-16e5dce');
+const VERSION = new Version('21.1.2+sha-4186f74');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -26,6 +26,6 @@ function default_1() {
     // In order to align the CDK version with other Angular dependencies that are setup by
     // `@schematics/angular`, we use tilde instead of caret. This is default for Angular
     // dependencies in new CLI projects.
-    return (0, utility_1.addDependency)('@angular/cdk', `~21.1.2+sha-16e5dce`, { existing: utility_1.ExistingBehavior.Skip });
+    return (0, utility_1.addDependency)('@angular/cdk', `~21.1.2+sha-4186f74`, { existing: utility_1.ExistingBehavior.Skip });
 }
 //# sourceMappingURL=index.js.map
