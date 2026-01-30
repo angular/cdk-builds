9b0d94612c1bd7cb5687b67887155790ba3f76db
Caleb Eggensperger <calebegg@gmail.com>
2026-01-30 21:53:05 +0000

    main - 865e3d5 fix(material/chips): Example violates axe accessibility tests (#32679)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0-next.2+sha-f1157b1');
+const VERSION = new Version('21.2.0-next.2+sha-865e3d5');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
