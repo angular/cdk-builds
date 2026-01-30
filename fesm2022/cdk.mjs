5c12a2adfa4364d3979b403e2fbf75e5516e7121
Andrey Dolgachev <adolgachev@users.noreply.github.com>
2026-01-30 20:04:09 +0000

    main - f1157b1 fix(aria/menu): Add label property for proper aria-label (#32710)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0-next.2+sha-80be4e6');
+const VERSION = new Version('21.2.0-next.2+sha-f1157b1');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
