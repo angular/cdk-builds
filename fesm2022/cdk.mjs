ec4357393f1e55f87e5f7e7bc358cc40e2789650
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-01-30 06:42:27 +0000

    21.1.x - c4f745c fix(material/badge): move warning check later (#32724)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.2+sha-d726935');
+const VERSION = new Version('21.1.2+sha-c4f745c');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
