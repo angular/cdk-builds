2b401f72e3c32bad55e2793a09462c5dacd8bbbb
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-01-30 06:42:21 +0000

    main - a3f2ec8 fix(material/badge): move warning check later (#32724)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0-next.2+sha-06e3d75');
+const VERSION = new Version('21.2.0-next.2+sha-a3f2ec8');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
