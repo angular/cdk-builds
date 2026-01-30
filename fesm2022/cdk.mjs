197f55e8ac429fffa339543a16c3756e7aff3d11
Andrey Dolgachev <adolgachev@users.noreply.github.com>
2026-01-30 04:32:46 +0000

    main - 06e3d75 fix(aria/toolbar): show outline on selected items when forced colors active (#32708)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0-next.2+sha-6fd833d');
+const VERSION = new Version('21.2.0-next.2+sha-06e3d75');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
