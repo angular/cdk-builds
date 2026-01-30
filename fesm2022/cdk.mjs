52f742f6d91e071d36ec080d72be4d4e12a221da
danielolaru91 <danielolaru1991@gmail.com>
2026-01-30 03:48:30 +0000

    main - 6fd833d perf(cdk-experimental/column-resize): add debounce to column header hover to prevent excessive handler rendering (#30709)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0-next.2+sha-f27562c');
+const VERSION = new Version('21.2.0-next.2+sha-6fd833d');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
