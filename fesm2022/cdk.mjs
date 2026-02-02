e1e61caf0ada3b84497d044bb9c2be12f65b4015
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-02-02 17:55:14 +0000

    main - d030725 fix(aria/menu): align output naming with styleguide

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0-next.2+sha-bfd792f');
+const VERSION = new Version('21.2.0-next.2+sha-d030725');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
