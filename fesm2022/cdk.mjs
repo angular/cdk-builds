ab26467f24b8072baeead09fb94f1a8ddd61beb8
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-02-02 05:17:56 +0000

    21.1.x - 3211b5a build: remove deprecated compiler option (#32692)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.2+sha-910aed3');
+const VERSION = new Version('21.1.2+sha-3211b5a');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
