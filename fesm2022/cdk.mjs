6ef5e14c96f770d5e76290de07c7266d311aac22
Andrey Dolgachev <adolgachev@users.noreply.github.com>
2026-02-04 21:15:16 +0000

    main - 94f65dc docs: release notes for the v21.1.3 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0-next.2+sha-d030725');
+const VERSION = new Version('21.2.0-next.2+sha-94f65dc');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
