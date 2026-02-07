54376e9bab4cc22d2867b3ddb105fa8d0440e7d4
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-02-07 01:35:51 +0000

    main - bee5511 fix(material/slide-toggle): overly broad selector (#32740)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0-next.3+sha-977004b');
+const VERSION = new Version('21.2.0-next.3+sha-bee5511');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
