5f9199d0ade3ed10079c6afa647c13ec4a2c5f34
Andrey Dolgachev <adolgachev@users.noreply.github.com>
2026-02-10 22:28:05 +0000

    21.1.x - 2a26959 fix(multiple): change aria keyboard manager to only handle repeated events in correct places (#32728)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.3+sha-d175b01');
+const VERSION = new Version('21.1.3+sha-2a26959');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
