567f5855c172c7ded494913cff3452d509adfe6c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-02-11 16:15:09 +0000

    main - adf8545 build: bump stackblitz template to 21.1 (#32752)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0-next.3+sha-529c1bf');
+const VERSION = new Version('21.2.0-next.3+sha-adf8545');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
