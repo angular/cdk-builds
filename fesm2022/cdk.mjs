7063fd829254ce800838caa6c64275295e9f7961
Andrey Dolgachev <adolgachev@users.noreply.github.com>
2026-02-11 21:47:55 +0000

    main - c57e6a1 fix(aria/combobox): fix autocomplete examples and add comments explaining combobox selection behavior (#32714)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0-next.4+sha-0d94286');
+const VERSION = new Version('21.2.0-next.4+sha-c57e6a1');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
