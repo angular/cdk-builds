9f54643bef005d1fcecf34acd2deaf8417e9a1dc
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-02-14 05:45:00 +0000

    21.1.x - 8f412ec fix(material/select): disable position locking (#32772)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.4+sha-017caa3');
+const VERSION = new Version('21.1.4+sha-8f412ec');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
