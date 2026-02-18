666ce99a77f0cd73baaf103ebc1094845b33c692
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-02-18 11:49:35 +0000

    main - c485374 release: cut the v21.2.0-next.5 release

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -26,6 +26,6 @@ function default_1() {
     // In order to align the CDK version with other Angular dependencies that are setup by
     // `@schematics/angular`, we use tilde instead of caret. This is default for Angular
     // dependencies in new CLI projects.
-    return (0, utility_1.addDependency)('@angular/cdk', `~21.2.0-next.4+sha-de35190`, { existing: utility_1.ExistingBehavior.Skip });
+    return (0, utility_1.addDependency)('@angular/cdk', `~21.2.0-next.5+sha-c485374`, { existing: utility_1.ExistingBehavior.Skip });
 }
 //# sourceMappingURL=index.js.map
