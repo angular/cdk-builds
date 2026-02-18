44b492893139decc967bf33fbae5fe3eaf9bcd3b
Angular Robot <angular-robot@google.com>
2026-02-18 11:17:44 +0000

    21.1.x - da5c79e build: update pnpm to v10.29.3 (#32756)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.4+sha-c825fa2');
+const VERSION = new Version('21.1.4+sha-da5c79e');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
