737d61804c3b2905c8437b99599a094e4dca8b94
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-02-18 11:53:26 +0000

    21.2.x - dc8675d release: cut the v21.2.0-rc.0 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0-next.5+sha-c485374');
+const VERSION = new Version('21.2.0-rc.0+sha-dc8675d');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
