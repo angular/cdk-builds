7fccfe5836b091461ee023cbc1331f17aeb3fe68
Andrey Dolgachev <adolgachev@users.noreply.github.com>
2026-02-19 16:45:03 +0000

    21.1.x - 50c34e6 fix(aria/combobox): Adjust AutoComplete examples to properly announce no results text (#32786)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.5+sha-37fa94d');
+const VERSION = new Version('21.1.5+sha-50c34e6');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
