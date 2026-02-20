1869f393b8b9295b493287bbcc180b7fe284ecec
Angular Robot <angular-robot@google.com>
2026-02-20 13:56:26 +0000

    21.2.x - f9e7149 build: update cross-repo angular dependencies (#32802)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0-rc.0+sha-1c14bfb');
+const VERSION = new Version('21.2.0-rc.0+sha-f9e7149');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -26,6 +26,6 @@ function default_1() {
     // In order to align the CDK version with other Angular dependencies that are setup by
     // `@schematics/angular`, we use tilde instead of caret. This is default for Angular
     // dependencies in new CLI projects.
-    return (0, utility_1.addDependency)('@angular/cdk', `~21.2.0-rc.0+sha-1c14bfb`, { existing: utility_1.ExistingBehavior.Skip });
+    return (0, utility_1.addDependency)('@angular/cdk', `~21.2.0-rc.0+sha-f9e7149`, { existing: utility_1.ExistingBehavior.Skip });
 }
 //# sourceMappingURL=index.js.map
