9d47532b9e2627e5a68d83abcc1acfd1026cde6e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-02-22 07:39:55 +0000

    21.2.x - ef2f1cb build: set up schematics for v22 (#32806)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0-rc.0+sha-f9e7149');
+const VERSION = new Version('21.2.0-rc.0+sha-ef2f1cb');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
