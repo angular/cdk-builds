ac522622062a68c8f77681cb3c67742e33f71748
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-02-23 18:12:39 +0000

    main - b095048 fix(cdk/portal): allow bindings to be passed to component portal (#32814)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.3.0-next.0+sha-0d36e86');
+const VERSION = new Version('21.3.0-next.0+sha-b095048');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
