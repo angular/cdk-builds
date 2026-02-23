83d76d680d7be529868fccb5d5962f3c6edfdca8
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-02-23 17:29:42 +0000

    21.2.x - 7fe3081 fix(material/datepicker): drop dependency on NgClass (#32810)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0-rc.0+sha-ef2f1cb');
+const VERSION = new Version('21.2.0-rc.0+sha-7fe3081');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
