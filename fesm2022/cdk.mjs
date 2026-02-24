ff29080435cfdf5b8baa09821e6b65dcf468905e
Miles Malerba <mmalerba@google.com>
2026-02-24 19:16:06 +0000

    main - e896823 ci: remove mmalerba from code reviewers (#32824)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.3.0-next.0+sha-b095048');
+const VERSION = new Version('21.3.0-next.0+sha-e896823');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
