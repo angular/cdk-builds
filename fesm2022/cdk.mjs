e7c746bf167534cd81c994c450d2ce2d38560c85
Andrew Seguin <andrewjs@google.com>
2026-02-25 18:35:25 +0000

    21.2.x - 37cffba release: cut the v21.2.0 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0-rc.0+sha-eeacb0e');
+const VERSION = new Version('21.2.0+sha-37cffba');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
