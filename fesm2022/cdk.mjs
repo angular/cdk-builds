ead57e33d6817efcfc13eef0555d81bde091a7be
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-02-25 19:09:27 +0000

    21.2.x - cebf303 fix(material/checkbox): hide decorative elements from assistive technology

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0+sha-37cffba');
+const VERSION = new Version('21.2.0+sha-cebf303');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
