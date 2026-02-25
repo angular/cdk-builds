17eaab310457f1b7d09e42fbaf8f575b77240a25
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-02-25 20:03:17 +0000

    21.2.x - 2abe8e6 fix(material/menu): render backdrop for context menu if explicitly opted in (#32845)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0+sha-cebf303');
+const VERSION = new Version('21.2.0+sha-2abe8e6');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
