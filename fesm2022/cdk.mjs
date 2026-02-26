8edceea9e6c048b8360a593d2e38ab7b4aa5f6a4
Jeremy Mowery <jermowery@google.com>
2026-02-26 23:07:24 +0000

    main - b4a89d5 feat(material/button): Add support for showing a progress indicator inside the button (#32698)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.3.0-next.0+sha-936f114');
+const VERSION = new Version('21.3.0-next.0+sha-b4a89d5');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
