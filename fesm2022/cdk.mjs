133f6ae53beb6ccaa1b8431aaf735e14b357f255
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-03-02 08:16:19 +0000

    21.2.x - 1f66af8 refactor: remove remaining NgClass usages (#32873)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0+sha-2abe8e6');
+const VERSION = new Version('21.2.0+sha-1f66af8');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
