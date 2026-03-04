95bc306c356fd8b79655900e09b3e9a974a355c3
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-03-04 18:34:44 +0000

    main - e69549c fix(aria/menu): itemSelected not emitted when menu is attached to a trigger (#32880)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.3.0-next.0+sha-f6b7739');
+const VERSION = new Version('21.3.0-next.0+sha-e69549c');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
