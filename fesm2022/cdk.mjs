0d40ef4cfe8c15d6a996389f4528b1eb1b933f77
tjshiu <35056071+tjshiu@users.noreply.github.com>
2026-03-05 01:43:43 +0000

    main - b627aef release: cut the v21.3.0-next.0 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.3.0-next.0+sha-00c4cf5');
+const VERSION = new Version('21.3.0-next.0+sha-b627aef');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
