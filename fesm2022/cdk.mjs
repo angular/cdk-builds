da3e7dd0b56d122d6084e2213a12f320776c7e61
Angular Robot <angular-robot@google.com>
2026-03-05 01:07:58 +0000

    main - 88a9365 build: update all non-major dependencies (#32690)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.3.0-next.0+sha-00844db');
+const VERSION = new Version('21.3.0-next.0+sha-88a9365');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
