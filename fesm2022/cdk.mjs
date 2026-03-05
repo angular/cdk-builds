886ab233c17f71fd88cc80d9a3f2d5b02f226f67
tjshiu <35056071+tjshiu@users.noreply.github.com>
2026-03-05 01:29:23 +0000

    21.2.x - 902a1f6 release: cut the v21.2.1 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0+sha-6a8da08');
+const VERSION = new Version('21.2.1+sha-902a1f6');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
