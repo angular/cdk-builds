70ad60b3d050951b17e3ca2fb1c42a59b1daeaca
Angular Robot <angular-robot@google.com>
2026-03-05 00:34:09 +0000

    21.2.x - 6a8da08 build: lock file maintenance (#32844)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.0+sha-c1295c5');
+const VERSION = new Version('21.2.0+sha-6a8da08');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
