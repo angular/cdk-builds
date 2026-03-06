cd7b8ca8184525573470bb632a71ada505227aa6
Alan Agius <alanagius@google.com>
2026-03-06 06:57:45 +0000

    21.2.x - ef607a6 ci: add issue labeling and refactor jobs (#32890)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.1+sha-345c543');
+const VERSION = new Version('21.2.1+sha-ef607a6');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
