0d46d8085b301b4e44a98a281a9b6dc80fd00037
Alan Agius <alanagius@google.com>
2026-03-06 06:59:05 +0000

    main - 8912b00 ci: add issue labeling and refactor jobs (#32890)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.0+sha-72498b4');
+const VERSION = new Version('22.0.0-next.0+sha-8912b00');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
