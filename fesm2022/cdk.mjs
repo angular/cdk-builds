ea8c3981d2ee2a8c531a81c4b6d3ddbe489be770
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-03-09 16:33:30 +0000

    main - c2a178c refactor(cdk/drag-drop): move tests away from fakeAsync (#32898)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.0+sha-23be683');
+const VERSION = new Version('22.0.0-next.0+sha-c2a178c');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
