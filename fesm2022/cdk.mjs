d0ee357aa5c0d2f32ecebaf6af0863ff697cde13
Alan Agius <alanagius@google.com>
2026-03-09 16:38:17 +0000

    21.2.x - 439cd88 build: update `aspect_rules_js` to `3.0.2` (#32910)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.1+sha-91a31a7');
+const VERSION = new Version('21.2.1+sha-439cd88');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
