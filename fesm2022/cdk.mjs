244f81a739f287e3241ed1d691336aa10f1e1b1d
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-03-13 05:03:47 +0000

    21.2.x - 9175265 fix(material/datepicker): add harness for datepicker actions (#32925)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.2+sha-6049dbb');
+const VERSION = new Version('21.2.2+sha-9175265');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
