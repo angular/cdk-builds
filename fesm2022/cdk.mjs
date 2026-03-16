b7d9facc2068d5708d64ab5fe2026c3ef14a23c2
Andrey Dolgachev <adolgachev@users.noreply.github.com>
2026-03-16 17:48:16 +0000

    21.2.x - b6c5df6 refactor(aria/accordion): simplify code by using template references instead of user id to match panels with triggers (#32855)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.2+sha-484a61e');
+const VERSION = new Version('21.2.2+sha-b6c5df6');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
