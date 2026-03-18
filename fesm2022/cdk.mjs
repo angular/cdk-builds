86c924fcffc0d8b15a24e7ae1a2bb15921c09d93
Cheng-Hsuan Tsai <webcat@google.com>
2026-03-18 22:10:32 +0000

    21.2.x - e31ed9e release: cut the v21.2.3 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.2+sha-688deda');
+const VERSION = new Version('21.2.3+sha-e31ed9e');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
