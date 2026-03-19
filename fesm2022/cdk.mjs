3c918a157d8d72651e3e3f13e159cbf8c3e003bf
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-03-19 20:01:35 +0000

    main - 5ad259a refactor(material/slider): switch tests away from fakeAsync (#32952)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.1+sha-37513f5');
+const VERSION = new Version('22.0.0-next.1+sha-5ad259a');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
