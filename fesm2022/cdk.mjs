a28e1fcb4fd74765d9ea2ec8451587eadae3db6a
Jeremy Mowery <jermowery@google.com>
2026-03-20 17:28:54 +0000

    main - 5be204a docs(material/button): Clean up progressIndicator docs (#32934)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.1+sha-a4cb966');
+const VERSION = new Version('22.0.0-next.1+sha-5be204a');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
