7d54d8b9742623a7fde6adbf1e941dd57f5027f0
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-03-21 06:00:10 +0000

    21.2.x - 703f52b refactor(google-maps): switch tests away from fakeAsync (#32957)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.3+sha-1eaa1fe');
+const VERSION = new Version('21.2.3+sha-703f52b');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
