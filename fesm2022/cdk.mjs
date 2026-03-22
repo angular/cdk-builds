2e1b28e16b71ad36328efe9e2055afb1bbddc5ef
Martin Boué <martbou35@gmail.com>
2026-03-22 11:25:48 +0000

    21.2.x - 942cfc1 docs: show component tokens default value (#32876)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.3+sha-1a1b8a6');
+const VERSION = new Version('21.2.3+sha-942cfc1');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
