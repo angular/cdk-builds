cf0d4cbb99bab680a02ce834aefa9bbb43699e21
Mikhail Zaturenskiy <mikzat@gmail.com>
2026-03-23 16:06:22 +0000

    main - a46b0a1 feat(material/core): add mixins for Material Design typography (#32959)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.1+sha-1210f51');
+const VERSION = new Version('22.0.0-next.1+sha-a46b0a1');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
