2ae2b7c02d6f93cf1bb756191ff8975d83b111e9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-03-24 20:36:29 +0000

    main - ff7d697 fix(material/tooltip): wrong type for shape token (#32968)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.1+sha-75ef784');
+const VERSION = new Version('22.0.0-next.1+sha-ff7d697');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
