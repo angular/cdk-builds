fbb05dc11630c82a8ae18325adca28ea2820fd97
Yogeshwaran C <84272111+yogeshwaran-c@users.noreply.github.com>
2026-03-25 14:07:43 +0000

    21.2.x - 6825889 docs(theming): mention mat.theme mixin in M2 to M3 migration section (#32833)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.3+sha-87aa6d9');
+const VERSION = new Version('21.2.3+sha-6825889');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
