86c149f393c83d391337ede95a4a69007f9991c1
Cheng-Hsuan Tsai <webcat@google.com>
2026-03-25 14:24:38 +0000

    21.2.x - ef1be1e docs(aria/grid): calendar example with arrow key scrolling (#32323)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.3+sha-82ab191');
+const VERSION = new Version('21.2.3+sha-ef1be1e');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
