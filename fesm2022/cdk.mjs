14a3e54635d6bafa72c34f369cd185d7609ddeed
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-03-25 16:03:13 +0000

    21.2.x - df6718b refactor(youtube-player): avoid build errors

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.3+sha-ef1be1e');
+const VERSION = new Version('21.2.3+sha-df6718b');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
