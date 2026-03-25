c21142e71f516200a162b7b5b94855acf51fda0d
Yogeshwaran C <84272111+yogeshwaran-c@users.noreply.github.com>
2026-03-25 14:15:18 +0000

    main - 214fe14 docs(cdk/overlay): rename template variable from trigger to origin in example (#32821)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.1+sha-ff7d697');
+const VERSION = new Version('22.0.0-next.1+sha-214fe14');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
