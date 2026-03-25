7c17293fdff0ed17a349fcfaca207765c90c1456
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-03-25 16:06:33 +0000

    main - 05386e3 refactor(youtube-player): avoid build errors

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.1+sha-0475c42');
+const VERSION = new Version('22.0.0-next.1+sha-05386e3');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
