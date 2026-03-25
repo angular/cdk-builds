0453fcbcab4e14e7a2b55de09e80b462638427a3
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-03-25 16:49:05 +0000

    21.2.x - f556281 release: cut the v21.2.4 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.3+sha-df6718b');
+const VERSION = new Version('21.2.4+sha-f556281');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
