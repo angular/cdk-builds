474e33489b00c0523f4e8704d019ede3558e3fc7
Olawuni Olagoke <33896026+olagokemills@users.noreply.github.com>
2026-03-26 13:46:51 +0000

    main - 9055ac5 docs(material/tree): update examples to use modern childrenAccessor API (#32751)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.2+sha-3dda44f');
+const VERSION = new Version('22.0.0-next.2+sha-9055ac5');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
