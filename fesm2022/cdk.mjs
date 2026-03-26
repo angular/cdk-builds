647ea5205c06a14e19cfe737a35e86459c498ae5
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-03-26 16:50:54 +0000

    main - d6cc784 refactor(multiple): remove declare global usages (#32991)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.2+sha-9055ac5');
+const VERSION = new Version('22.0.0-next.2+sha-d6cc784');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
