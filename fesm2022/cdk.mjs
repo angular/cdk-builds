3ef3bbfb50c2793cd41db094f2dbe163d4820c03
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-03-26 17:40:58 +0000

    main - a55b197 build: test failure in example (#32996)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.2+sha-d6cc784');
+const VERSION = new Version('22.0.0-next.2+sha-a55b197');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
