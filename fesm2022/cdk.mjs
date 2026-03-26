6cf91c6dd1c56619c0d1df392029684e14103d7d
Alan Agius <alanagius@google.com>
2026-03-26 12:47:49 +0000

    main - 3dda44f build: update `@nginfra/angular-linking` to latests (#32993)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.2+sha-9d5a162');
+const VERSION = new Version('22.0.0-next.2+sha-3dda44f');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
