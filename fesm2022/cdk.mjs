2200299cdc1929a0948614220bcced1e8817cd63
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-03-31 17:21:54 +0000

    main - b35a52a build: add explicit change detection to docs site

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.2+sha-ac39110');
+const VERSION = new Version('22.0.0-next.2+sha-b35a52a');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
