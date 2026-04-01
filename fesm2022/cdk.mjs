fa0eb52cd409bd09ac30ffeae3e7ed8539c184fa
Yogeshwaran C <84272111+yogeshwaran-c@users.noreply.github.com>
2026-04-01 22:58:20 +0000

    21.2.x - e433a66 docs(material/dialog): clarify that default options replace rather than merge (#32819)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.5+sha-c9d40aa');
+const VERSION = new Version('21.2.5+sha-e433a66');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
