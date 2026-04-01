7a6d98fb59065e7616fd14574bae50e95ecdfe0c
Andrey Dolgachev <adolgachev@users.noreply.github.com>
2026-04-01 21:31:23 +0000

    main - 8473a27 release: cut the v22.0.0-next.3 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.2+sha-69cb129');
+const VERSION = new Version('22.0.0-next.3+sha-8473a27');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
