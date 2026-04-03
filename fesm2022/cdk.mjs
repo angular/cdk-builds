d969e9e85e2afec91aa8c7257cf0b77af0dddef6
Angular Robot <angular-robot@google.com>
2026-04-03 23:53:15 +0000

    21.2.x - 424b282 build: update dependency aspect_rules_ts to v3.8.8 (#33025)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.5+sha-e433a66');
+const VERSION = new Version('21.2.5+sha-424b282');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
