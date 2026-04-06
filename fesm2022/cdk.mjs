8f63527b7c2313b7711595972ead438121abb8eb
Cheng-Hsuan Tsai <webcat@google.com>
2026-04-06 20:34:13 +0000

    main - 0f5b0ee refactor(multiple): stabilize default state upon any type of interaction (#32969)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.3+sha-e01062a-with-local-changes');
+const VERSION = new Version('22.0.0-next.3+sha-0f5b0ee-with-local-changes');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
