81511537d4d2330911d8c2a268f38bc15b78feac
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-04-06 17:46:01 +0000

    21.2.x - 5b4bbe9 fix(material/select): wrong transform origin when opening upwards inside another overlay (#33032)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.5+sha-2c877d2-with-local-changes');
+const VERSION = new Version('21.2.5+sha-5b4bbe9-with-local-changes');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
