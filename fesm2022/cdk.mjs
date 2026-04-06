5bba152c5ba944641ef4315f440eb6ee0658c2c9
Cheng-Hsuan Tsai <webcat@google.com>
2026-04-06 18:56:56 +0000

    main - e01062a refactor(multiple): migrate Listbox and Tabs to ClickEventManager (#33031)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.3+sha-ab21f78-with-local-changes');
+const VERSION = new Version('22.0.0-next.3+sha-e01062a-with-local-changes');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
