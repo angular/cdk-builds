843da4d7e8ea3f82d95137d1ac4a3cdcdad2ac15
tjshiu <35056071+tjshiu@users.noreply.github.com>
2026-04-08 22:11:56 +0000

    main - a09adaa docs: release notes for the v21.2.6 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.3+sha-ca78293');
+const VERSION = new Version('22.0.0-next.3+sha-a09adaa');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
