bb8f0bdda2da72a883361916ad9d9d64e077ba46
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-04-10 16:08:45 +0000

    main - 348c3c8 fix(material/select): remove modal workaround

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.4+sha-a49508b');
+const VERSION = new Version('22.0.0-next.4+sha-348c3c8');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
