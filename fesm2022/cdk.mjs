7c2f4314793af53ba171a20213013eba3e99fb37
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-04-10 10:37:29 +0000

    main - a49508b feat(aria/toolbar): add test harnesses (#33068)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.4+sha-30f2239');
+const VERSION = new Version('22.0.0-next.4+sha-a49508b');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
