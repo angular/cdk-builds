a80180f20422a54f183004aaf1b78fecdac3f6d8
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-04-12 11:12:43 +0000

    main - 6cb6b5e fix(multiple): make more public APIs readonly (#33071)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.4+sha-c1a8b93');
+const VERSION = new Version('22.0.0-next.4+sha-6cb6b5e');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
