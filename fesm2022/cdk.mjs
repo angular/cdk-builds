2643cf0245fb66177e52a70fcd90dda05e50ea1b
Cheng-Hsuan Tsai <webcat@google.com>
2026-04-13 16:43:21 +0000

    main - 75fae52 feat(aria/menu): introduce menu harness (#33067)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.4+sha-ad5fba3');
+const VERSION = new Version('22.0.0-next.4+sha-75fae52');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
