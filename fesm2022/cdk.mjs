e110d293daaf27f09762998635c786103109781c
Cheng-Hsuan Tsai <webcat@google.com>
2026-04-14 17:58:10 +0000

    main - bddfc39 refactor(aria/grid): simplify Grid cell to only support single widget (#33075)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.4+sha-61f9ac0');
+const VERSION = new Version('22.0.0-next.4+sha-bddfc39');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
