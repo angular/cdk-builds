d2d11819a393b48dc2f5e4bea2ddfbf62594bbb1
Cheng-Hsuan Tsai <webcat@google.com>
2026-04-14 20:39:24 +0000

    main - c25e625 feat(aria/tabs): add test harnesses (#33079)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.4+sha-bddfc39');
+const VERSION = new Version('22.0.0-next.4+sha-c25e625');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
