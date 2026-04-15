c2d29508f1910d851fb2c98f74dea762fec7bc2b
Cheng-Hsuan Tsai <webcat@google.com>
2026-04-15 00:16:15 +0000

    main - 2a13f69 refactor(aria/grid): remove range selection to reduce computational overhead (#33055)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.4+sha-c25e625');
+const VERSION = new Version('22.0.0-next.4+sha-2a13f69');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
