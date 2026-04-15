418f7de7c571bc315e005716a386b031a37f24b3
Andrey Dolgachev <adolgachev@users.noreply.github.com>
2026-04-15 22:35:25 +0000

    main - 100dc2b refactor(aria/accordion): Replace ContentChildren with manual registration of triggers (#33027)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.4+sha-2a13f69');
+const VERSION = new Version('22.0.0-next.4+sha-100dc2b');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
