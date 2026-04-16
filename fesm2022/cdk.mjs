8d3ee094c9fa918c00183d60936ec6f2d2502859
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-04-16 07:38:48 +0000

    main - 1b6052f fix(material/sort): deprecate MatSortHeaderIntl and hide from docs (#33089)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.4+sha-100dc2b');
+const VERSION = new Version('22.0.0-next.4+sha-1b6052f');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
