8d5cb84b50dd3a934b784e21db3592bce24c3853
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-04-16 07:38:49 +0000

    21.2.x - 5954c45 fix(material/sort): deprecate MatSortHeaderIntl and hide from docs (#33089)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.6+sha-a236a40');
+const VERSION = new Version('21.2.6+sha-5954c45');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -26,6 +26,6 @@ function default_1() {
     // In order to align the CDK version with other Angular dependencies that are setup by
     // `@schematics/angular`, we use tilde instead of caret. This is default for Angular
     // dependencies in new CLI projects.
-    return (0, utility_1.addDependency)('@angular/cdk', `~21.2.6+sha-a236a40`, { existing: utility_1.ExistingBehavior.Skip });
+    return (0, utility_1.addDependency)('@angular/cdk', `~21.2.6+sha-5954c45`, { existing: utility_1.ExistingBehavior.Skip });
 }
 //# sourceMappingURL=index.js.map
