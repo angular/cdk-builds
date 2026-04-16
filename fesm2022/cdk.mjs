9e778292581bcdfcf16fa97cbe8a030e8675eeba
Andrew Seguin <andrewjs@google.com>
2026-04-16 16:15:31 +0000

    21.2.x - 2a6cff6 release: cut the v21.2.7 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.6+sha-5954c45');
+const VERSION = new Version('21.2.7+sha-2a6cff6');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
