ef9f3684945125d3132a6b3972e28624fd45ec6a
Cheng-Hsuan Tsai <webcat@google.com>
2026-04-22 23:08:12 +0000

    main - 5499cd2 docs: release notes for the v21.2.8 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.5+sha-91a4932');
+const VERSION = new Version('22.0.0-next.5+sha-5499cd2');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
