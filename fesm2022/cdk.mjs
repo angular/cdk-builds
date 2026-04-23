3008451118fb65253b06852682e9e01c813ad42c
Cheng-Hsuan Tsai <webcat@google.com>
2026-04-23 11:24:42 +0000

    21.2.x - e7053b0 build: update cross-repo angular dependencies (21.2.x) (#33129)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.8+sha-e2ef493');
+const VERSION = new Version('21.2.8+sha-e7053b0');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
