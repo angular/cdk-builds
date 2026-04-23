7a9330b9e87e4a0bdf123763bbd53766a35785f6
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-04-23 09:31:23 +0000

    main - e7533d4 build: align icons in docs (#33127)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.6+sha-39b80fe');
+const VERSION = new Version('22.0.0-next.6+sha-e7533d4');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
