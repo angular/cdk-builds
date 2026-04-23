2772d1183c2463bf7199505c25606476cc05d24c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-04-23 09:31:17 +0000

    21.2.x - e2ef493 build: align icons in docs (#33127)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.8+sha-c51c38c');
+const VERSION = new Version('21.2.8+sha-e2ef493');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
