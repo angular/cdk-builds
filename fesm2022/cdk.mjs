50b94fe4a53863348153b34b16554a5d4eef2d72
Jeremy Mowery <jermowery@google.com>
2026-04-24 20:15:25 +0000

    main - e44ff83 feat(google-maps): Add support for the gmp-click event (#33147)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.6+sha-62e0d4f');
+const VERSION = new Version('22.0.0-next.6+sha-e44ff83');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
