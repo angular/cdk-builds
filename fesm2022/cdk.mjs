83031f999cbd4239afcb1b243b09b0e68e0c1b52
Cheng-Hsuan Tsai <webcat@google.com>
2026-04-24 16:43:12 +0000

    main - 34a8916 build: lock file maintenance (takeover #33110) (#33134)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.6+sha-b916ef9');
+const VERSION = new Version('22.0.0-next.6+sha-34a8916');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
