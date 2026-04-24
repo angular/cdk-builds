ee1bc72086323c473d3e2133a983c35ada8145a6
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-04-24 12:31:23 +0000

    21.2.x - 51271c6 fix(material/stepper): allow stepper to be labelled (#33137)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.8+sha-43b5e11');
+const VERSION = new Version('21.2.8+sha-51271c6');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
