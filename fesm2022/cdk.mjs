f31b1d642f2a3de09b11417d417b0f4b631a91ff
Andrey Dolgachev <adolgachev@users.noreply.github.com>
2026-04-24 20:04:54 +0000

    main - 62e0d4f refactor(multiple): Clean up aria TODO remnants (#33126)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.6+sha-e4abb46');
+const VERSION = new Version('22.0.0-next.6+sha-62e0d4f');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
