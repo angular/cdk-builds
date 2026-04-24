ae34e226b536b66ba0d01eb150f3b24ff6f9c173
Cheng-Hsuan Tsai <webcat@google.com>
2026-04-24 16:50:49 +0000

    main - a7b2c51 refactor(aria/grid): support deferred focus target (#33072)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.6+sha-34a8916');
+const VERSION = new Version('22.0.0-next.6+sha-a7b2c51');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
