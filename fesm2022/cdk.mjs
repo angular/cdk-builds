08419822983881b51bdf09ceaf76c7bd3ec870b8
Angular Robot <angular-robot@google.com>
2026-04-24 17:00:18 +0000

    main - dd12062 build: update devinfra digest to c4d0c37 (#33143)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.6+sha-6ec07bc');
+const VERSION = new Version('22.0.0-next.6+sha-dd12062');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
