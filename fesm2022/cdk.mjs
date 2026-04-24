2d43b384238bd4dec892213f1a5f3308169972e8
Andrey Dolgachev <adolgachev@users.noreply.github.com>
2026-04-24 17:19:49 +0000

    main - e4abb46 refactor(multiple): Clean up afterRenderEffect usage across angular aria (#33054)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.6+sha-dd12062');
+const VERSION = new Version('22.0.0-next.6+sha-e4abb46');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
