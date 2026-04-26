4b70217bf873964c979c33208f611e36ac0ba619
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-04-26 07:46:09 +0000

    main - fccc2ef refactor(cdk/scrolling): switch tests away from fakeAsync (#33150)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.6+sha-13c92a3');
+const VERSION = new Version('22.0.0-next.6+sha-fccc2ef');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
