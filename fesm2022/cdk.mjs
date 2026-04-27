593a5e1e9dbb7234ab277141677c3ae157265947
tjshiu <35056071+tjshiu@users.noreply.github.com>
2026-04-27 19:15:34 +0000

    main - a2519f7 refactor(aria/combobox) Add examples  (#32857)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.6+sha-fccc2ef');
+const VERSION = new Version('22.0.0-next.6+sha-a2519f7');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
