c2d20f36438de8c10caeea62e0c2edb17ef626ac
swseverance <swseverance@gmail.com>
2026-04-28 12:29:32 +0000

    main - af49eee docs(material/tooltip): update tooltip-delay example layout styles (#33098)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.6+sha-cd8b81c');
+const VERSION = new Version('22.0.0-next.6+sha-af49eee');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
