5607b4bb91ab615c50601b776a500eebf463b56e
Joy Serquiña <44146839+essjay05@users.noreply.github.com>
2026-04-29 22:07:22 +0000

    main - 218a77c fix(aria/combobox): separates placeholder prefixes (#33163)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.7+sha-dc6a9a3');
+const VERSION = new Version('22.0.0-next.7+sha-218a77c');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
