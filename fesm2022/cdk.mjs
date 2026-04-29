f3ae87698052239f7066ee5bfab2974979d32e65
tjshiu <35056071+tjshiu@users.noreply.github.com>
2026-04-29 18:24:25 +0000

    main - 8c3f9b0 Autocomplete - Uses simple-combobox example (#33155)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.7+sha-75718e4');
+const VERSION = new Version('22.0.0-next.7+sha-8c3f9b0');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
