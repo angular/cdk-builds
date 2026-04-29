76f50799f485930f189126dc1ef9ad0e82c1b3fb
Cheng-Hsuan Tsai <webcat@google.com>
2026-04-29 22:40:47 +0000

    main - bb4f8ec fix(multiple): re-export collection util (#33171)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.7+sha-218a77c');
+const VERSION = new Version('22.0.0-next.7+sha-bb4f8ec');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
