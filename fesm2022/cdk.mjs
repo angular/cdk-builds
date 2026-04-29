7a28578f3eade2cd9de67a99c9b0e72f37005b21
Cheng-Hsuan Tsai <webcat@google.com>
2026-04-29 21:42:40 +0000

    main - dc6a9a3 refactor(multiple): implement generic child discovery with SortedCollection (#33151)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.7+sha-8c3f9b0');
+const VERSION = new Version('22.0.0-next.7+sha-dc6a9a3');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
