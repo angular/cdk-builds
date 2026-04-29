a2003a90b4766e24e71208893e046ccac184da83
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-04-29 08:00:00 +0000

    main - e1481e4 test: attempt to resolve flaky tests (#33166)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.6+sha-af49eee');
+const VERSION = new Version('22.0.0-next.6+sha-e1481e4');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
