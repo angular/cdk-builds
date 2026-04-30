fcbf4b28a1c8cb62d4fa9ccbbe1f0fb85012c981
tjshiu <35056071+tjshiu@users.noreply.github.com>
2026-04-30 16:58:45 +0000

    main - d1b075b refactor(aria/toolbar): Add another example for simple-combobox and toolbar (#33173)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.7+sha-c4ee339');
+const VERSION = new Version('22.0.0-next.7+sha-d1b075b');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
