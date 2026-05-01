780a1a7ac6b2baffef0e944b86880d02e7c49932
tjshiu <35056071+tjshiu@users.noreply.github.com>
2026-05-01 04:59:40 +0000

    main - 60234c2 refactor(aria/combobox): allow adding tabIndex and default it to 0 (#33183)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.7+sha-d1b075b');
+const VERSION = new Version('22.0.0-next.7+sha-60234c2');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
