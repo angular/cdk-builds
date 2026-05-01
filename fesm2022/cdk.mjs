3158c2f74cf7b478a973bc44f0c8f861f34ec4e7
tjshiu <35056071+tjshiu@users.noreply.github.com>
2026-05-01 05:37:41 +0000

    main - 3c6f299 docs(aria/combobox): update the simple-combobox examples to open on click (#33184)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.7+sha-60234c2');
+const VERSION = new Version('22.0.0-next.7+sha-3c6f299');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -26,6 +26,6 @@ function default_1() {
     // In order to align the CDK version with other Angular dependencies that are setup by
     // `@schematics/angular`, we use tilde instead of caret. This is default for Angular
     // dependencies in new CLI projects.
-    return (0, utility_1.addDependency)('@angular/cdk', `~22.0.0-next.7+sha-60234c2`, { existing: utility_1.ExistingBehavior.Skip });
+    return (0, utility_1.addDependency)('@angular/cdk', `~22.0.0-next.7+sha-3c6f299`, { existing: utility_1.ExistingBehavior.Skip });
 }
 //# sourceMappingURL=index.js.map
