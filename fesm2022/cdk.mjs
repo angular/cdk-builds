e0f8026216c4b4335b52a9401f9be3a8c656e5cd
tjshiu <35056071+tjshiu@users.noreply.github.com>
2026-05-02 00:29:34 +0000

    main - 306fe00 refactor(aria/combobox): make input readonly when disabled (#33187)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.7+sha-619a53d');
+const VERSION = new Version('22.0.0-next.7+sha-306fe00');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
