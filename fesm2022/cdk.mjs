4d9beb6de1d21ee2c018cc5b2dd84ea03bd152e4
Cheng-Hsuan Tsai <webcat@google.com>
2026-05-03 09:40:22 +0000

    main - 9d73c98 fix(material/menu): missing panelClass getter (#33191)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.7+sha-a3479f7');
+const VERSION = new Version('22.0.0-next.7+sha-9d73c98');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
