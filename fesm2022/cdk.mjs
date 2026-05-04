b72642b4c9c10f5cb4fb379894c672abe0d1976d
tjshiu <35056071+tjshiu@users.noreply.github.com>
2026-05-04 19:18:35 +0000

    main - d396fa8 docs(aria/combobox): bind cdkConnectedOverlayOpen to popupExpanded in simple-combobox examples (#33197)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.7+sha-e3d84f2');
+const VERSION = new Version('22.0.0-next.7+sha-d396fa8');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
