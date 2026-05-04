7047fe0e514d9cd721e810b1302ae2130dbf91f2
Cheng-Hsuan Tsai <webcat@google.com>
2026-05-04 18:04:08 +0000

    main - c6e99f1 refactor(aria/menu): use SortedCollection (#33186)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.7+sha-9d73c98');
+const VERSION = new Version('22.0.0-next.7+sha-c6e99f1');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
