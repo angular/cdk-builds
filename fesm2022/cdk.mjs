c55e8d853ac9af9b3544862bb427f6d3083272fe
tjshiu <35056071+tjshiu@users.noreply.github.com>
2026-05-04 19:42:59 +0000

    main - 3f631c8 refactor(aria/combobox): Updates the combobox-tree-examples and resets isDeleting to false with keypress (#33193)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.7+sha-d396fa8');
+const VERSION = new Version('22.0.0-next.7+sha-3f631c8');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
