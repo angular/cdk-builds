860ce195c8ac3c434c666b976bf6658bcc95bf77
Cheng-Hsuan Tsai <webcat@google.com>
2026-05-05 21:49:17 +0000

    main - f4e9a87 refactor(aria/grid): consolidate widget focus logic and activation handling (#33203)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.7+sha-4d045b8');
+const VERSION = new Version('22.0.0-next.7+sha-f4e9a87');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
