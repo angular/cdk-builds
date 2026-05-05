18a1ab60d93d9c626332d79b0afb053b65d709ab
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-05-05 12:24:33 +0000

    main - 4d045b8 fix(aria/menu): do not set default aria-label (#33202)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.7+sha-a08f1cb');
+const VERSION = new Version('22.0.0-next.7+sha-4d045b8');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
