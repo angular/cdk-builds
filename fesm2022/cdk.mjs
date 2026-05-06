f3f0c4eff849040bd9edb558b2c46cb651494744
tjshiu <35056071+tjshiu@users.noreply.github.com>
2026-05-06 15:55:44 +0000

    main - 0ca47b4 feat(aria/combobox): migrate simple-combobox directly into primary entrypoints (#33206)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.7+sha-b8201ed');
+const VERSION = new Version('22.0.0-next.7+sha-0ca47b4');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
