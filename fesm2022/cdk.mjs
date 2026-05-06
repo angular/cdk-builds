92c1daa2c75b1129a1a69a3ba4be73c726a0ce66
tjshiu <35056071+tjshiu@users.noreply.github.com>
2026-05-06 22:18:59 +0000

    22.0.x - 0aee554 refactor(multiple): stabilize focus state when active item is deleted (#33199)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.8+sha-9813e31');
+const VERSION = new Version('22.0.0-next.8+sha-0aee554');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
