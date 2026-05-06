269082833f649028b40fa3f3675c00727c5e31d4
Andrey Dolgachev <adolgachev@users.noreply.github.com>
2026-05-06 20:05:51 +0000

    22.0.x - 9813e31 release: cut the v22.0.0-next.8 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.7+sha-46763fb');
+const VERSION = new Version('22.0.0-next.8+sha-9813e31');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -26,6 +26,6 @@ function default_1() {
     // In order to align the CDK version with other Angular dependencies that are setup by
     // `@schematics/angular`, we use tilde instead of caret. This is default for Angular
     // dependencies in new CLI projects.
-    return (0, utility_1.addDependency)('@angular/cdk', `~22.0.0-next.7+sha-46763fb`, { existing: utility_1.ExistingBehavior.Skip });
+    return (0, utility_1.addDependency)('@angular/cdk', `~22.0.0-next.8+sha-9813e31`, { existing: utility_1.ExistingBehavior.Skip });
 }
 //# sourceMappingURL=index.js.map
