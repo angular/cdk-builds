dcc8e1ad692afd3f5b34330d5c5cb8de670a95b6
Andrey Dolgachev <adolgachev@users.noreply.github.com>
2026-05-06 20:06:54 +0000

    main - 2b668f2 docs: release notes for the v22.0.0-next.8 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.7+sha-46763fb');
+const VERSION = new Version('22.1.0-next.0+sha-2b668f2');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
