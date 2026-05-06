b6df1c45914114ced20182ef4d8214a5cafcd0d4
Andrey Dolgachev <adolgachev@users.noreply.github.com>
2026-05-06 18:49:11 +0000

    21.2.x - 583da8a release: cut the v21.2.10 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.9+sha-babfbb7');
+const VERSION = new Version('21.2.10+sha-583da8a');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
