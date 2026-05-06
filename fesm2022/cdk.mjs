77af50fc52a99bf22cb1a1d4923786bfb26a4e57
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-05-06 17:23:28 +0000

    21.2.x - e08fdf2 build: fix failing test (#33212)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.9+sha-09e89fa');
+const VERSION = new Version('21.2.9+sha-e08fdf2');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
