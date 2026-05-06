0b24ede34b59321c422b98c5f01dfe15debc74c5
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-05-06 09:42:09 +0000

    main - b8201ed fix(google-maps): deprecate heatmap layer (#33208)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.7+sha-05cc247');
+const VERSION = new Version('22.0.0-next.7+sha-b8201ed');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
