5eceef6a5c70c74eb99a6ac4e7bc6450c6e40063
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-05-09 09:44:07 +0000

    main - 0070780 fix(multiple): remove developer preview tag from aria (#33232)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.0+sha-ebbea92');
+const VERSION = new Version('22.1.0-next.0+sha-0070780');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
