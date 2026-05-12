ccd25e3be61ff0923d48dea49c75ba5da1233176
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-05-12 18:41:46 +0000

    main - 930fe9b fix(material/sidenav): mark content as inert while open

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.0+sha-463311c');
+const VERSION = new Version('22.1.0-next.0+sha-930fe9b');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
