23ff1153a4e42000df7c77a18f4a880e6fc5c27c
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-05-12 18:41:33 +0000

    22.0.x - c316198 fix(material/sidenav): mark content as inert while open

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.8+sha-516aa8d');
+const VERSION = new Version('22.0.0-next.8+sha-c316198');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
