3a1bc44a5eb214f476db54171903bfa04eae0dcc
Andrey Dolgachev <adolgachev@users.noreply.github.com>
2026-05-12 20:46:40 +0000

    main - 33593e7 test(multiple): check for incorrect usage of Angular Aria directives and log violations (#33195)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.0+sha-930fe9b');
+const VERSION = new Version('22.1.0-next.0+sha-33593e7');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
