94c07de99085b46ce39b88e30a39073b944dbeb0
Andrey Dolgachev <adolgachev@users.noreply.github.com>
2026-05-12 20:46:49 +0000

    22.0.x - fcc87c2 test(multiple): check for incorrect usage of Angular Aria directives and log violations (#33195)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-next.8+sha-c316198');
+const VERSION = new Version('22.0.0-next.8+sha-fcc87c2');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
