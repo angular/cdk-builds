f87db35deb430a098c37479a2353d54b50698a97
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-05-14 12:32:49 +0000

    22.0.x - a4d92c5 fix(material/sidenav): more robust reset logic for inert attribute (#33257)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-rc.0+sha-d9f78d1');
+const VERSION = new Version('22.0.0-rc.0+sha-a4d92c5');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
