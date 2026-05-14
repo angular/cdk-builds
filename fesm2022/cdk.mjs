03519ecc086fe78096d05858af891f47f62b8457
Angular Robot <angular-robot@google.com>
2026-05-14 18:08:16 +0000

    main - 95d7290 build: update dependency rollup-plugin-sourcemaps2 to v0.5.7 (#33254)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.0+sha-3b79a13');
+const VERSION = new Version('22.1.0-next.0+sha-95d7290');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
