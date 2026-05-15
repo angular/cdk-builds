a5e73aba4612cc9bf349b2a1237abda1b983cd98
swseverance <swseverance@gmail.com>
2026-05-15 05:57:14 +0000

    22.0.x - a3a710d docs(material/tooltip): fix tooltip-position-at-origin example overflow (#33218)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-rc.0+sha-2117518');
+const VERSION = new Version('22.0.0-rc.0+sha-a3a710d');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
