e7c0b62216a3ebe90141f4218e24b509e5737773
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-05-15 10:33:29 +0000

    22.0.x - c8dbb5f refactor(material/snack-bar): switch tests away from fakeAsync (#33261)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-rc.0+sha-a3a710d');
+const VERSION = new Version('22.0.0-rc.0+sha-c8dbb5f');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
