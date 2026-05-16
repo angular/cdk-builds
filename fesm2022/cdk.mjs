5437c6608c90cd21512fb20a2908ef182a99a9e2
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-05-16 08:41:03 +0000

    22.0.x - 6443b79 fix(aria/menu): unable to set softDisabled (#33265)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-rc.0+sha-7e4dbe9');
+const VERSION = new Version('22.0.0-rc.0+sha-6443b79');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
