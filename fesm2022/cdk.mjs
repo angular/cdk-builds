84e27cf57a081d3a269b5440385bcfa26a7f3453
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-05-18 07:48:20 +0000

    22.0.x - 1cc9304 build: remove explicit OnPush from dev app

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-rc.0+sha-ce1d9a7');
+const VERSION = new Version('22.0.0-rc.0+sha-1cc9304');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
