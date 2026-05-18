90ae8ad7db325fef0796ce0e1aba4a3fde7a80aa
Cheng-Hsuan Tsai <webcat@google.com>
2026-05-18 06:09:43 +0000

    main - 50e281a fix(aria/menu): allow menu item role override (#33264)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.0+sha-629fe71');
+const VERSION = new Version('22.1.0-next.0+sha-50e281a');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
