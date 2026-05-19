5c7ea21525c446736f952895fbed6b5d105d0bf1
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-05-19 10:37:28 +0000

    22.0.x - f1a4355 fix(material/sidenav): handle mixed sidenav and drawer (#33274)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-rc.0+sha-4c29897');
+const VERSION = new Version('22.0.0-rc.0+sha-f1a4355');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
