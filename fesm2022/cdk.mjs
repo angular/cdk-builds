06cf92dbcf67219855d8277624985c6e0fd8a023
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-05-20 10:40:12 +0000

    22.0.x - f2f6267 fix(material/datepicker): ensure dates don't overflow on a small screen (#33281)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-rc.0+sha-26f55b6');
+const VERSION = new Version('22.0.0-rc.0+sha-f2f6267');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
