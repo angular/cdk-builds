08fbd7235c43346544772c52412ddae3912dacf3
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-05-21 11:24:49 +0000

    main - 11e2eb3 fix(multiple): prevent form submissions in aria directives (#33297)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.0+sha-5d73083');
+const VERSION = new Version('22.1.0-next.0+sha-11e2eb3');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
