2ddf7673c6b5e8a2133bb97116d7632e58bf0e80
Cheng-Hsuan Tsai <webcat@google.com>
2026-05-27 19:42:11 +0000

    main - 548bc20 docs: release notes for the v21.2.13 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.0+sha-e973652');
+const VERSION = new Version('22.1.0-next.0+sha-548bc20');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
