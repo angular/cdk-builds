7fbc0014bd60f993dffd9135f6768b9d73c2c239
Cheng-Hsuan Tsai <webcat@google.com>
2026-05-27 19:55:35 +0000

    22.0.x - f3a3a7c release: cut the v22.0.0-rc.2 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-rc.1+sha-50f673e');
+const VERSION = new Version('22.0.0-rc.2+sha-f3a3a7c');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
