1ee9f49409f95920015b8b214f90d8c876c540d1
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-05-27 08:02:39 +0000

    main - e973652 refactor(cdk-experimental/popover-edit): switch tests away from fakeAsync

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.0+sha-59b8f43');
+const VERSION = new Version('22.1.0-next.0+sha-e973652');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
