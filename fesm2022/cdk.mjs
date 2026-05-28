f2f3a2ba1da3ebe8044b08544eb6418dd0fd3956
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-05-28 07:29:51 +0000

    22.0.x - 3190451 fix(material/menu): close menu when cleared from trigger (#33306)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-rc.2+sha-f3a3a7c');
+const VERSION = new Version('22.0.0-rc.2+sha-3190451');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
