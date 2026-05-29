f409388d3117cdafced6c9f2cb51b58c2124cf50
Alan Agius <alanagius@google.com>
2026-05-29 19:16:43 +0000

    main - 83f2891 build: add explanation comment to confirmModulesPurge setting (#33298)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.0+sha-5b2018f');
+const VERSION = new Version('22.1.0-next.0+sha-83f2891');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
