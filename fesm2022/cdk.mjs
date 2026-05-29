a3233c175a62179bf501c938b65edfae78b84bfe
Joey Perrott <josephperrott@gmail.com>
2026-05-29 19:05:20 +0000

    main - 5b2018f build: update dev-infra actions to 649c3afeaa46674507b9625537e49de54a695e2b (#33307)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.0+sha-62547b3');
+const VERSION = new Version('22.1.0-next.0+sha-5b2018f');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
