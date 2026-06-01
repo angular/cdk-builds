fbdc2daed3f040f72b01c1bf71a1d3dd5794a225
Wagner Maciel <wagnermaciel@google.com>
2026-06-01 17:38:10 +0000

    main - 5c6ff84 docs: release notes for the v22.0.0-rc.3 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.0+sha-639c29f');
+const VERSION = new Version('22.1.0-next.0+sha-5c6ff84');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
