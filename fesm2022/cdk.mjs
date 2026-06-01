87433016dc05fc6c64d035c45853cff63c64cd61
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-06-01 07:22:05 +0000

    main - 639c29f build: prevent docs site from rendering Aria examples under Material (#33324)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.0+sha-83f2891');
+const VERSION = new Version('22.1.0-next.0+sha-639c29f');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
