e2fcf3ef897888e3ce6560bc4132fef03adbe11a
Joey Perrott <josephperrott@gmail.com>
2026-06-03 13:56:38 +0000

    main - 7e8a0ff build: update to latest @angular/ng-dev (#33341)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.0+sha-ec4f1d0');
+const VERSION = new Version('22.1.0-next.0+sha-7e8a0ff');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
