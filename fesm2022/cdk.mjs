6fbb2e2ce63b48d483f91dc502dd2980132f0358
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-06-03 17:58:44 +0000

    22.0.x - a2329a6 build: ensure deployment key is deleted (#33338)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0+sha-85e1545');
+const VERSION = new Version('22.0.0+sha-a2329a6');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
