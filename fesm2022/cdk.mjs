ca075f6fda46afb7a8b262cd588857ec9b568e78
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-06-03 18:01:10 +0000

    main - 7d09f91 build: remove unused dependencies (#33333)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.0+sha-ee370a0');
+const VERSION = new Version('22.1.0-next.0+sha-7d09f91');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
