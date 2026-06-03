919f232a00cc22bea996d591de9ac04e7fe4c71b
Wagner Maciel <wagnermaciel@google.com>
2026-06-03 14:08:34 +0000

    22.0.x - 85e1545 release: cut the v22.0.0 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0-rc.3+sha-da5c575');
+const VERSION = new Version('22.0.0+sha-85e1545');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
