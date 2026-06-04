e9f4d9cccbbb97453d691345a5b84551babaae72
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-06-04 07:41:51 +0000

    main - 1a869b3 fix(youtube-player): avoid errors with clobbered variables

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.0+sha-7d09f91');
+const VERSION = new Version('22.1.0-next.0+sha-1a869b3');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
