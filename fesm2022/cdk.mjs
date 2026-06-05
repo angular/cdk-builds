a9bb28b9ff39143a7338d9fe4fb524e8a301520c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-06-05 16:01:01 +0000

    main - 5a8f7df fix(material/timepicker): do not allow intervals less than a second (#33354)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.0+sha-1a869b3');
+const VERSION = new Version('22.1.0-next.0+sha-5a8f7df');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
