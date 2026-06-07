14db0063bd1cb26a0e13857a5fc1f9863b9281a3
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-06-07 06:13:37 +0000

    22.0.x - 629aea4 fix(cdk/a11y): avoid prototype conflicts in id generator (#33356)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0+sha-bde3c76');
+const VERSION = new Version('22.0.0+sha-629aea4');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
