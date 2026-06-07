ba33f42c65156cb380cac6943516a6dc0b0175bb
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-06-07 11:05:37 +0000

    main - 03685a9 fix(aria/combobox): avoid error for synthetic events (#33360)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.0+sha-330b496');
+const VERSION = new Version('22.1.0-next.0+sha-03685a9');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
