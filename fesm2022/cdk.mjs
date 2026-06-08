15ce04718b588d3948f59052038d65e72893a3e0
Karim Daher <daher.kareem@hotmail.com>
2026-06-08 18:21:30 +0000

    main - f5a4195 docs(aria/menu): fix menubar example highlight styling (#33290)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.0+sha-40b63f3');
+const VERSION = new Version('22.1.0-next.0+sha-f5a4195');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
