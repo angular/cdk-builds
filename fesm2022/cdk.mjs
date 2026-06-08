27009f0fa305d1767313d5af1d3dbafc020d6fb7
Cheng-Hsuan Tsai <webcat@google.com>
2026-06-08 18:15:00 +0000

    22.0.x - 1c47061 fix(aria/combobox): prevent re-dispatching keyboard event on control target change (#33362)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0+sha-7581b05');
+const VERSION = new Version('22.0.0+sha-1c47061');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
