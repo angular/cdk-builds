27249b9bff0233315ed847adf7b4c8c149636499
Karim Daher <daher.kareem@hotmail.com>
2026-06-08 18:21:29 +0000

    22.0.x - 7a8b3f7 docs(aria/menu): fix menubar example highlight styling (#33290)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0+sha-1c47061');
+const VERSION = new Version('22.0.0+sha-7a8b3f7');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -26,6 +26,6 @@ function default_1() {
     // In order to align the CDK version with other Angular dependencies that are setup by
     // `@schematics/angular`, we use tilde instead of caret. This is default for Angular
     // dependencies in new CLI projects.
-    return (0, utility_1.addDependency)('@angular/cdk', `~22.0.0+sha-1c47061`, { existing: utility_1.ExistingBehavior.Skip });
+    return (0, utility_1.addDependency)('@angular/cdk', `~22.0.0+sha-7a8b3f7`, { existing: utility_1.ExistingBehavior.Skip });
 }
 //# sourceMappingURL=index.js.map
