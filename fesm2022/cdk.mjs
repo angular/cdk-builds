ca261893d8f32eed28cc78d80ae6f10a26b1dbe9
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-06-10 21:17:38 +0000

    22.0.x - 75f6542 release: cut the v22.0.1 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0+sha-d7a8cb9');
+const VERSION = new Version('22.0.1+sha-75f6542');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
