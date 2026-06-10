c76143bf6c441962efb9a5b5b8e88f88f22abcb2
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-06-10 17:10:06 +0000

    22.0.x - d7a8cb9 fix(material/dialog): ignore clicks on aria-disabled close buttons (#33373)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.0+sha-49aeb67');
+const VERSION = new Version('22.0.0+sha-d7a8cb9');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
