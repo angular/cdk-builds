00972ba0cb0f40befa212c2fc4751e9541d762f7
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-06-11 11:10:41 +0000

    main - cd10a97 fix(material/bottom-sheet): ensure animation event comes from container

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.0+sha-82725e5');
+const VERSION = new Version('22.1.0-next.0+sha-cd10a97');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
