7b3d2b6c96ec3ff63cde1d75a27af42d20bea821
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-06-13 10:47:20 +0000

    main - 2430e6e fix(youtube-player): update to latest typings and fix error (#33387)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.0+sha-eb2c033');
+const VERSION = new Version('22.1.0-next.0+sha-2430e6e');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -26,6 +26,6 @@ function default_1() {
     // In order to align the CDK version with other Angular dependencies that are setup by
     // `@schematics/angular`, we use tilde instead of caret. This is default for Angular
     // dependencies in new CLI projects.
-    return (0, utility_1.addDependency)('@angular/cdk', `~22.1.0-next.0+sha-eb2c033`, { existing: utility_1.ExistingBehavior.Skip });
+    return (0, utility_1.addDependency)('@angular/cdk', `~22.1.0-next.0+sha-2430e6e`, { existing: utility_1.ExistingBehavior.Skip });
 }
 //# sourceMappingURL=index.js.map
