a890ac5fd534b517ff222cff80d45e1e0c409642
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-06-14 10:25:22 +0000

    22.0.x - 58d694a test(multiple): fix aot compilation errors in tests (#33389)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.1+sha-2995797');
+const VERSION = new Version('22.0.1+sha-58d694a');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
