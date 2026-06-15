fef298a20fcc28a9a34eafdd0c12888f24101aa2
Jeri Peier <jeremias.peier@sbb.ch>
2026-06-15 11:13:36 +0000

    22.0.x - d5fc56d build: update stackBlitz examples to v22 (#33351)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.1+sha-58d694a');
+const VERSION = new Version('22.0.1+sha-d5fc56d');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
