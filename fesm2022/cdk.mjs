0d5389dc4437bc9d9e5ceffa3be6610454f8f46b
Cheng-Hsuan Tsai <webcat@google.com>
2026-06-15 17:12:42 +0000

    main - 31781a0 fix(aria/combobox): allow setting readonly (#33364)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.0+sha-5b06da7');
+const VERSION = new Version('22.1.0-next.0+sha-31781a0');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
