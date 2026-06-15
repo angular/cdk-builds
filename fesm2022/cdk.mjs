716021003bbb3f99903eec223ea09358ed1a3b3e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-06-15 12:42:22 +0000

    22.0.x - 9ad7964 build: update review config (#33394)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.1+sha-d5fc56d');
+const VERSION = new Version('22.0.1+sha-9ad7964');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
