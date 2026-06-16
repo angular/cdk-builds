a6ab0aaa2966e4e959f942cfcc77805d47a78995
zsheikh-eng <zaksheikh@google.com>
2026-06-16 09:19:42 +0000

    main - e49f127 fix(material/chips): correct focus management on chip destruction (#33329)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.0+sha-31781a0');
+const VERSION = new Version('22.1.0-next.0+sha-e49f127');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
