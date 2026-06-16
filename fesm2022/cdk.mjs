9ebe86f6c45152f82b1a3b84cdcf8e54c0ff3462
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-06-16 18:25:55 +0000

    main - e31d3fd fix(material/chips): wrong padding when chip only has edit icon (#33407)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.0+sha-e49f127');
+const VERSION = new Version('22.1.0-next.0+sha-e31d3fd');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
