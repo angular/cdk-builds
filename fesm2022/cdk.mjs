6df6e4e6e749e91de5ff6cfbd4c543ebbb3ca372
tjshiu <35056071+tjshiu@users.noreply.github.com>
2026-06-17 22:09:51 +0000

    main - 0981e58 docs: release notes for the v22.0.2 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.0+sha-9d8d6c0');
+const VERSION = new Version('22.1.0-next.0+sha-0981e58');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
