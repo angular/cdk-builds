544866938984765cc8e4f3f50e6508785b221cc6
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-06-19 09:39:49 +0000

    main - 73939a8 feat(material/slide-toggle): add full-width support (#33424)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.1+sha-d829d1c');
+const VERSION = new Version('22.1.0-next.1+sha-73939a8');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
