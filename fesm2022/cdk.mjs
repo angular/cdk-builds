5328195c9e7835cfed0bf36f42028f21b86fcc6c
Silva Dev BR <116388885+Will-thom@users.noreply.github.com>
2026-06-19 05:55:53 +0000

    main - d829d1c docs(material/form-field): fix incorrect chip component reference (#33161)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.1+sha-d81320f');
+const VERSION = new Version('22.1.0-next.1+sha-d829d1c');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
