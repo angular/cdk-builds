c4773149707cf4b0164be1c36b3a27a5f44c6cfd
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-06-20 07:26:29 +0000

    main - 88b24e0 test(material/chips): fix flakes and re-enable test (#33426)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.1+sha-8925fc9');
+const VERSION = new Version('22.1.0-next.1+sha-88b24e0');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
