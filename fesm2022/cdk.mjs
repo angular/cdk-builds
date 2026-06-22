cd192ad47bbf06aaf54f986f64cdfdcdb61e26eb
Joey Perrott <josephperrott@gmail.com>
2026-06-22 19:40:36 +0000

    main - 6accbf3 Revert "build: update dependency node to v22.23.0 (#33421)" (#33431)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.1+sha-b6cb812');
+const VERSION = new Version('22.1.0-next.1+sha-6accbf3');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
