a2c890ac5d041c559ce51564c8d678538b0addfb
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-06-23 10:19:02 +0000

    22.0.x - f4686e9 docs(cdk/overlay): add popover to docs (#33434)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.2+sha-576d883');
+const VERSION = new Version('22.0.2+sha-f4686e9');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
