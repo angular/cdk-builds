3268a88d31bbf43660cca72f14b5aa4f61e6bbd2
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-06-25 14:28:44 +0000

    22.0.x - 9e552a7 fix(cdk/scrolling): reset more properties to avoid jumping during virtual scroll (#33439)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.2+sha-f4686e9');
+const VERSION = new Version('22.0.2+sha-9e552a7');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
