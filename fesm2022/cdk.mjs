e34de4b8b4f3b347c75a6db2d53ebaad17dcdbc8
Cheng-Hsuan Tsai <webcat@google.com>
2026-06-26 19:13:01 +0000

    main - 8002bce fix(aria/combobox): zonejs compatibility (#33453)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.1+sha-9184787');
+const VERSION = new Version('22.1.0-next.1+sha-8002bce');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
