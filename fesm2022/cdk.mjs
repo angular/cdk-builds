8ec3b0ea7ff8a619ffdd66610f4e029ff70d19b7
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-06-27 07:11:42 +0000

    22.0.x - a436cd4 fix(material/tabs): validate animation duration (#33452)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.2+sha-e87216c');
+const VERSION = new Version('22.0.2+sha-a436cd4');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
