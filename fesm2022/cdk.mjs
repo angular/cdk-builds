a19f6e762adf131a66e63803e458bfe0a91a6b16
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-06-29 10:38:47 +0000

    main - ce47109 fix(material/sidenav): update inert value if mode changes (#33461)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.1+sha-a1ce1ad');
+const VERSION = new Version('22.1.0-next.1+sha-ce47109');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
