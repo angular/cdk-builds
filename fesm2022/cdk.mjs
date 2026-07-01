c2e6c4e5f4b82d8305c995b2816036ec055d1d9d
Cheng-Hsuan Tsai <webcat@google.com>
2026-07-01 16:39:54 +0000

    main - def323f docs: release notes for the v22.0.3 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.1+sha-e2cab48');
+const VERSION = new Version('22.1.0-next.1+sha-def323f');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
