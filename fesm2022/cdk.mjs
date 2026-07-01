e6c1c9e3ee12d8068e366767249dbd811d138298
Joy Serquiña <44146839+essjay05@users.noreply.github.com>
2026-07-01 16:13:09 +0000

    22.0.x - de9dd59 fix(aria/combobox): improve autocomplete placeholder text color contrast (#33465)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.2+sha-3b74812');
+const VERSION = new Version('22.0.2+sha-de9dd59');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
