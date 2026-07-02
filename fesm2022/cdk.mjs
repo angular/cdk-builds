00114ce35d011e9c1dfcb278ae67120f58de94a3
Angular Robot <angular-robot@google.com>
2026-07-02 08:52:32 +0000

    main - 4738e8b build: update dependency firebase-tools to v15.22.4 (#33476)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.2+sha-bc5bda0');
+const VERSION = new Version('22.1.0-next.2+sha-4738e8b');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
