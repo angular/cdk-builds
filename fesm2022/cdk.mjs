ceb076cdd58d0633c68fd656b4fc820f87a7dbbd
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-07-03 14:45:49 +0000

    22.0.x - 918090a fix(aria/listbox): allow navigation to specific index (#33485)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.3+sha-21668c7');
+const VERSION = new Version('22.0.3+sha-918090a');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
