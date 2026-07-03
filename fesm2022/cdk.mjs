025a98906da29a208fbbfa71badcc84ef0ca279e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-07-03 14:45:34 +0000

    main - ee20441 fix(aria/listbox): allow navigation to specific index (#33485)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.2+sha-4738e8b');
+const VERSION = new Version('22.1.0-next.2+sha-ee20441');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
