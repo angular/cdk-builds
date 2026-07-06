807f5cc0bed57afeaeaa7fda6e8e67dad79bad8b
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-07-06 08:14:19 +0000

    22.0.x - fe8dd7d docs: use proper shadow classes in examples (#33494)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.3+sha-f2e55ea');
+const VERSION = new Version('22.0.3+sha-fe8dd7d');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
