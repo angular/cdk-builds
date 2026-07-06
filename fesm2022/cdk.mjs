f7f2f5fc2df74e5a8ce1caf4af560a4eadec9a5d
Michael Small <85510853+msmallest@users.noreply.github.com>
2026-07-06 07:30:54 +0000

    22.0.x - f2e55ea docs: convert auto/badge/btn/sheet/btntoggle/input to signals (#33492)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.3+sha-514d6f0');
+const VERSION = new Version('22.0.3+sha-f2e55ea');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
