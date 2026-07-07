7a7b7509aa6b14bcbc4d3e79f857770d43520ee7
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-07-07 16:06:17 +0000

    22.0.x - c86b024 fix(material/core): rework error state matcher to handle signal forms (#33500)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.3+sha-7535cbe');
+const VERSION = new Version('22.0.3+sha-c86b024');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
