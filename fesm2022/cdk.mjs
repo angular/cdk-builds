5e1f7a06dba893d67e84e0d42ce49d586cd4b746
Michael Small <85510853+msmallest@users.noreply.github.com>
2026-07-07 11:46:55 +0000

    main - 8205e24 docs: convert list/pag/progb/progs/rad/rip/elev to signals (#33497)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.2+sha-76f5be0');
+const VERSION = new Version('22.1.0-next.2+sha-8205e24');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
