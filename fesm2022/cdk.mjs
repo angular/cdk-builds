7c4bdf50f5c8cdabc9d4a34bc42cb62916338d30
Bhuvansh <bhuvanshkataria@gmail.com>
2026-07-08 13:02:17 +0000

    22.0.x - af94674 docs(material/toolbar): add navigation bar example (#33420)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.4+sha-adbf935');
+const VERSION = new Version('22.0.4+sha-af94674');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
