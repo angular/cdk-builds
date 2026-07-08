e2977f2e55c0870dae5febd2fc4f9370f4f5cf60
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-07-08 05:18:50 +0000

    main - bf2e02e build: update babel imports (#33488)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.2+sha-c5aa548');
+const VERSION = new Version('22.1.0-next.2+sha-bf2e02e');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
