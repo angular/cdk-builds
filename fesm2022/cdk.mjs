42332f4a5a67b79b22f786ebe6596b61b0fa3b1f
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-07-08 08:50:43 +0000

    main - 6e95d70 release: cut the v22.1.0-next.3 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.2+sha-25f7070');
+const VERSION = new Version('22.1.0-next.3+sha-6e95d70');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
