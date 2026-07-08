a234ed229c55f29e66c1c2852ed6959bff22129b
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-07-08 08:36:49 +0000

    22.0.x - bef34ec release: cut the v22.0.4 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.3+sha-a58e552');
+const VERSION = new Version('22.0.4+sha-bef34ec');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
