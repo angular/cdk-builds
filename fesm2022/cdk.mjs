53340934bfcdc822eb3ee81aa41ecff45d98267a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-07-08 05:28:22 +0000

    22.0.x - a58e552 docs(material/table): switch http example to resource (#33480)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.3+sha-b4559b0');
+const VERSION = new Version('22.0.3+sha-a58e552');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
