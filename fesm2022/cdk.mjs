94ab50a800c30d874bc3c4e1e897a4f5856df010
Angular Robot <angular-robot@google.com>
2026-07-09 09:23:20 +0000

    main - 425dcbb build: update dependency protobufjs to v8.7.0 (#33508)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.3+sha-dfa23c0');
+const VERSION = new Version('22.1.0-next.3+sha-425dcbb');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
