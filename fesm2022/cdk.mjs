7538cf31f32dec1a736cc7171a2406f5bec43497
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-07-09 12:41:16 +0000

    22.0.x - b2620ab fix(multiple): pass form field to error state tracker (#33509)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.4+sha-7e39e29-with-local-changes');
+const VERSION = new Version('22.0.4+sha-b2620ab-with-local-changes');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
