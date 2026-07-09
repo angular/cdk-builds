88ed6b8afc430c045cce3295fdb6126efa5bbd98
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-07-09 11:19:02 +0000

    main - dde1165 build: update to Babel 8 and latest Angular version (#33512)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.3+sha-425dcbb');
+const VERSION = new Version('22.1.0-next.3+sha-dde1165');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
