8ec0435af095110666121b4fcd3825931f87d79e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-07-09 12:41:21 +0000

    main - ebd64a1 fix(multiple): pass form field to error state tracker (#33509)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.3+sha-dde1165');
+const VERSION = new Version('22.1.0-next.3+sha-ebd64a1');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
