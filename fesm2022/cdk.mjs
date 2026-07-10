e723f366368609768bb3f19803b1c496b1db6d91
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-07-10 12:27:01 +0000

    22.0.x - e74c69b fix(aria/combobox): closing immediately when opening programmatically with zone.js (#33518)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.4+sha-b2620ab-with-local-changes');
+const VERSION = new Version('22.0.4+sha-e74c69b-with-local-changes');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
