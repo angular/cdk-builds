aab138cf401217d8abe89fbc535b2da73afed10b
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-07-12 12:13:13 +0000

    main - c895595 test(google-maps): move more tests away from fakeAsync (#33521)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.3+sha-def7bc2');
+const VERSION = new Version('22.1.0-next.3+sha-c895595');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
