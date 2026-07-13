9e85c675f0bf1545139d80648937c6abe61923cf
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-07-13 14:04:31 +0000

    main - 9a247a8 fix(material/datepicker): use rounded shape for calendar focus indicators (#33524)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.3+sha-14d896a');
+const VERSION = new Version('22.1.0-next.3+sha-9a247a8');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
