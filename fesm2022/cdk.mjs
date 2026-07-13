28fa04ecf9e926ee21cf44fd5b31707b9978724d
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-07-13 05:12:42 +0000

    22.0.x - 092b55a fix(material/sort): double focus indicators when strong focus is enabled (#33520)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.4+sha-9f54a41-with-local-changes');
+const VERSION = new Version('22.0.4+sha-092b55a-with-local-changes');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
