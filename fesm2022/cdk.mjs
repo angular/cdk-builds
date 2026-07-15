d2e5040c663d23d995e99bc2e93984e30c4ed8f5
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-07-15 03:07:15 +0000

    22.0.x - d26e892 fix(material/button): match focus indicator shape to FAB (#33527)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.4+sha-1741b22');
+const VERSION = new Version('22.0.4+sha-d26e892');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
