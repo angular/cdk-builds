87969db99b0903c7764c5980c4ee8b6461ee40cd
Karim Daher <daher.kareem@hotmail.com>
2026-07-15 20:16:13 +0000

    main - 5fad07b docs(aria/menu): disable overlay close-on-escape in menubar examples (#33344)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.3+sha-c729db7');
+const VERSION = new Version('22.1.0-next.3+sha-5fad07b');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
