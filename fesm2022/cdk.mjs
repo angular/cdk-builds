95498e51ceeacfd4a0fe1e4767d4ecbee060f93e
Angular Robot <angular-robot@google.com>
2026-07-15 10:35:53 +0000

    main - e8548de build: update pnpm to v11.13.0 (#33528)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.3+sha-d324696');
+const VERSION = new Version('22.1.0-next.3+sha-e8548de');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
