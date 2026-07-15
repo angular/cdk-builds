9e885dc9a8f6fae55d08ae4126c8cdae12407272
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-07-15 16:06:56 +0000

    22.0.x - e93bc9e fix(material/stepper): add border radius to header hover state in m2 (#33529)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.4+sha-d26e892');
+const VERSION = new Version('22.0.4+sha-e93bc9e');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -26,6 +26,6 @@ function default_1() {
     // In order to align the CDK version with other Angular dependencies that are setup by
     // `@schematics/angular`, we use tilde instead of caret. This is default for Angular
     // dependencies in new CLI projects.
-    return (0, utility_1.addDependency)('@angular/cdk', `~22.0.4+sha-d26e892`, { existing: utility_1.ExistingBehavior.Skip });
+    return (0, utility_1.addDependency)('@angular/cdk', `~22.0.4+sha-e93bc9e`, { existing: utility_1.ExistingBehavior.Skip });
 }
 //# sourceMappingURL=index.js.map
