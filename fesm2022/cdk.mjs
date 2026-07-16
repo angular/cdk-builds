ec67721443a82db4098e040ca70f40a03e564c20
tjshiu <35056071+tjshiu@users.noreply.github.com>
2026-07-16 00:24:43 +0000

    22.0.x - eac6975 release: cut the v22.0.5 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.4+sha-2dd67df');
+const VERSION = new Version('22.0.5+sha-eac6975');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
