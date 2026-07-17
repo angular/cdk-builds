fd4e3d3b17786a5a7fb3883f27e7a4e404d4e1ad
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-07-17 08:09:22 +0000

    main - 3d21c1b fix(material/expansion): always reset outline (#33542)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.4+sha-d7f9057');
+const VERSION = new Version('22.1.0-next.4+sha-3d21c1b');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
