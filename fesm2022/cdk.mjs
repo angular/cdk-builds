bc79f80ba2bf0f779944e824152e439f952376e4
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-07-17 08:09:58 +0000

    22.0.x - 842bc94 fix(material/expansion): always reset outline (#33542)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.5+sha-98cc317');
+const VERSION = new Version('22.0.5+sha-842bc94');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
