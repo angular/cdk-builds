c8bee3602db32a749e7b5cc6c60c0224795e0cfd
Angular Robot <angular-robot@google.com>
2026-07-17 18:05:29 +0000

    main - a15b754 build: update pnpm to v11.13.1 (#33546)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.4+sha-3d21c1b');
+const VERSION = new Version('22.1.0-next.4+sha-a15b754');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
