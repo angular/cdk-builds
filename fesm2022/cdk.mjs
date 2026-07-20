dda1826fa1e309b080585a95be4161c78cbd8032
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-07-20 17:43:56 +0000

    22.0.x - e493c98 refactor(material-experimental/column-resize): switch tests away from fakeAsync (#33549)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.5+sha-8bce120');
+const VERSION = new Version('22.0.5+sha-e493c98');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
