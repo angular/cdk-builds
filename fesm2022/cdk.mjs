64af0a4a8532e2924d5e0bba56dd08ef0fe2f716
Joy Serquiña <44146839+essjay05@users.noreply.github.com>
2026-07-20 22:55:51 +0000

    22.0.x - b5224c6 fix(material/list): make disabled selection list keyboard navigable (#33544)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.5+sha-e493c98');
+const VERSION = new Version('22.0.5+sha-b5224c6');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
