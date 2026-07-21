0a2e7a0786f5426930a3851fda892b2d7588104d
Michael Small <85510853+msmallest@users.noreply.github.com>
2026-07-21 07:28:48 +0000

    main - 43a87a4 docs: convert Stepper/Tabs/Timepicker example to signals (#33536)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.4+sha-ce6875e');
+const VERSION = new Version('22.1.0-next.4+sha-43a87a4');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
