abbaca5be857278916ca81439017a5989da3bdd2
Joy Serquiña <44146839+essjay05@users.noreply.github.com>
2026-07-21 17:11:02 +0000

    main - 56c7ff6 fix(material/slide-toggle): prevent icon label text-spacing cutoff (#33559)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.4+sha-43a87a4');
+const VERSION = new Version('22.1.0-next.4+sha-56c7ff6');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
