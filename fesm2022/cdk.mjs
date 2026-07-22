7eea1c75ad46705159549b6510d27527bf63d851
Andrew Seguin <andrewjs@google.com>
2026-07-22 18:46:06 +0000

    main - def30ba docs: release notes for the v22.0.6 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.4+sha-659b562');
+const VERSION = new Version('22.1.0-next.4+sha-def30ba');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
