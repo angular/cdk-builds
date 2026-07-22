efa5488558075bb501b31425ad00a2cc47f63c53
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-07-22 16:23:08 +0000

    main - 659b562 build: update dependencies and account for breaking changes (#33565)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.4+sha-452d3cc');
+const VERSION = new Version('22.1.0-next.4+sha-659b562');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
