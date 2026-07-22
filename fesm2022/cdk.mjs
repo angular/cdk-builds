243c96d1f12e237d01cdc09dc242bbcd73238662
Lajos Szoke <63732287+laliconfigcat@users.noreply.github.com>
2026-07-22 08:47:11 +0000

    22.0.x - 6e5acc1 fix(material-luxon-adapter): preserve timezone on .clone() (#33564)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.5+sha-9354833');
+const VERSION = new Version('22.0.5+sha-6e5acc1');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
