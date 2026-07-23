40b78541523144de64ea58c80dd2df21a109595c
Cheng-Hsuan Tsai <webcat@google.com>
2026-07-23 16:04:08 +0000

    main - b211bf1 fix(aria/grid): account for disabled cell widgets and in-place disablement in roving tabindex (#33571)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.4+sha-501d674');
+const VERSION = new Version('22.1.0-next.4+sha-b211bf1');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
