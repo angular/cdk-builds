c23655a0835a7de1b55a0d0dfbc38f7ba3affe7e
swseverance <swseverance@gmail.com>
2026-07-23 07:04:48 +0000

    22.0.x - 0b8d015 build: skip branch manager job on forked repositories (#33233)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.0.6+sha-f473227');
+const VERSION = new Version('22.0.6+sha-0b8d015');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
