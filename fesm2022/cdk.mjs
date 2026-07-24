7d0c8ddf33af1f269558f055976d028d8f737b32
Andrew Seguin <andrewjs@google.com>
2026-07-24 14:56:16 +0000

    main - 464891b docs: release notes for the v22.1.0-rc.0 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.4+sha-b211bf1');
+const VERSION = new Version('22.2.0-next.0+sha-464891b');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
