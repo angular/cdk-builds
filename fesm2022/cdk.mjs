a5b1acfe9dd9dd2e24b5a1299cf8ea0a4c2726bb
Andrew Seguin <andrewjs@google.com>
2026-07-24 14:53:02 +0000

    22.1.x - 9f78268 release: cut the v22.1.0-rc.0 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-next.4+sha-b211bf1');
+const VERSION = new Version('22.1.0-rc.0+sha-9f78268');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
