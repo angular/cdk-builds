705b32789a67476e80ac853595b826fea4ada4ec
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-07-27 05:43:17 +0000

    22.1.x - ba02c5f fix(multiple): avoid instanceof checks in aria directives (#33587)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-rc.0+sha-9f78268');
+const VERSION = new Version('22.1.0-rc.0+sha-ba02c5f');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
