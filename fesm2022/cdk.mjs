d7dd47e132c4f0207561f253bf80beefa37dc0e5
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-07-27 18:51:17 +0000

    22.1.x - d89d07a refactor(cdk/clipboard): switch tests away from fakeAsync (#33585)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0-rc.0+sha-ba02c5f');
+const VERSION = new Version('22.1.0-rc.0+sha-d89d07a');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
