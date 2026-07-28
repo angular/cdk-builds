c6018bae0080d8631a9a1b57f52f6700e2d9378b
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-07-28 21:27:47 +0000

    main - 7f8a729 fix(material/schematics): resolve issues in navigation schematic (#33590)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.0+sha-d26a0be');
+const VERSION = new Version('22.2.0-next.0+sha-7f8a729');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
