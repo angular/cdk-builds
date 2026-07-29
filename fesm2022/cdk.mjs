ba999290e7c1ed7d8a440e41ac78c7484b81fc5e
Michael Small <85510853+msmallest@users.noreply.github.com>
2026-07-29 09:47:42 +0000

    main - af2b626 docs: convert CDK overlay to signals (#33591)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.0+sha-7f8a729');
+const VERSION = new Version('22.2.0-next.0+sha-af2b626');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
