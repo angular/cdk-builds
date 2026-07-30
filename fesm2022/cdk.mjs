2a841ae95fbe2e20c633cd76d80dad9cc3b1c0e7
Alan Agius <alanagius@google.com>
2026-07-30 14:44:47 +0000

    main - 017a4fc ci: add branch tracking comments to dev-infra GitHub actions (#33605)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.0+sha-b8e88a0');
+const VERSION = new Version('22.2.0-next.0+sha-017a4fc');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
