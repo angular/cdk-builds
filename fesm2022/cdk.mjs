875fb6327bffa851f05e6023e1b7bce5ae5fe6f8
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-07-30 15:53:15 +0000

    main - 3fb737c fix(material/checkbox): avoid making the touch target a focus stop (#33584)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.0+sha-017a4fc');
+const VERSION = new Version('22.2.0-next.0+sha-3fb737c');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
