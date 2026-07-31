bf03e8bf632bd56c131df78b1c9a61893837fb18
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-07-31 11:32:14 +0000

    main - b2f3a08 fix(material/chips): add presentation role to container elements (#33611)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.0+sha-aa40684');
+const VERSION = new Version('22.2.0-next.0+sha-b2f3a08');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
