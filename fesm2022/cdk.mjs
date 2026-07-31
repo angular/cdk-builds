5548adc13695f7a976e884a32635070a4e9a563d
Michael Small <85510853+msmallest@users.noreply.github.com>
2026-07-31 07:18:05 +0000

    main - aa40684 docs: add signal form example for button toggle (#33592)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.0+sha-1a3d972');
+const VERSION = new Version('22.2.0-next.0+sha-aa40684');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
