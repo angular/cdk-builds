572fc7e5f05d51f9ad765d8cd7975bd452629e7f
Cheng-Hsuan Tsai <webcat@google.com>
2026-07-31 21:45:00 +0000

    main - 8d3da9e feat(multiple): export aria injection tokens to support custom subclasses (#33607)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.0+sha-b2f3a08');
+const VERSION = new Version('22.2.0-next.0+sha-8d3da9e');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
