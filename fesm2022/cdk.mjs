67bba57ae9897dde12f51ccd02b2de3e083532e6
Angular Robot <angular-robot@google.com>
2026-07-31 22:35:35 +0000

    22.1.x - d906245 build: update dependency node to v22.23.2 (#33609)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0+sha-8e31636');
+const VERSION = new Version('22.1.0+sha-d906245');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
