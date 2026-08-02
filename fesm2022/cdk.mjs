0b04a6a144a2d257819c78e063874e2b8db89c82
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-08-02 08:44:12 +0000

    22.1.x - 8b9c54a test(material/expansion): switch tests away from fakeAsync (#33617)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.0+sha-d906245');
+const VERSION = new Version('22.1.0+sha-8b9c54a');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
