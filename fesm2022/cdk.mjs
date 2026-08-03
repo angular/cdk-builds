939dbcc5c235cc11f89d9e8fdc5c41cca722c077
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-08-03 18:31:39 +0000

    main - 7935938 build: bump stackblitz dependency to 22.1 (#33616)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.0+sha-c6505d8');
+const VERSION = new Version('22.2.0-next.0+sha-7935938');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
