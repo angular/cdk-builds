318c54859a534fcba8c27661d229688a7e97d0b1
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-08-04 15:29:03 +0000

    main - 495bdaf fix(material/core): ensure all forward statements are relative (#33627)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.0+sha-7935938');
+const VERSION = new Version('22.2.0-next.0+sha-495bdaf');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
