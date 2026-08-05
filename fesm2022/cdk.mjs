e7f319a55e434aa0d47fb4f9fdbf12cba2046a18
Lazizbek Ergashev <lazerg2@gmail.com>
2026-08-05 16:52:24 +0000

    22.1.x - b42eea5 fix(material/form-field): update view when the control is swapped out (#33636)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.1+sha-0b67c3c');
+const VERSION = new Version('22.1.1+sha-b42eea5');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
