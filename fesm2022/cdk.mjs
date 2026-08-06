44ac77d3fd68695fa020ef57c2ce97a5be027f92
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-08-06 10:34:39 +0000

    22.1.x - 8d67957 fix(material/datepicker): actions overflowing at some screen sizes with touch UI (#33645)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.1+sha-b42eea5');
+const VERSION = new Version('22.1.1+sha-8d67957');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
