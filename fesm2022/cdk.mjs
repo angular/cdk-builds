af568fc888915b4b79b357c2d2c5f76e25815e56
Lazizbek Ergashev <lazerg2@gmail.com>
2026-08-07 14:16:36 +0000

    main - c87e3a5 fix(aria/combobox): empty aria-controls when the popup widget has no id (#33635)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.0+sha-426a69a');
+const VERSION = new Version('22.2.0-next.0+sha-c87e3a5');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
