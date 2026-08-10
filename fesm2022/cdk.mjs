cd9b1387b890e1e032ca9511f643a360a6292919
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-08-10 18:40:58 +0000

    main - f3e6276 fix(cdk/a11y): move focus to root if there are no focusable elements in focus trap (#33654)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.0+sha-d7f6c84');
+const VERSION = new Version('22.2.0-next.0+sha-f3e6276');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
