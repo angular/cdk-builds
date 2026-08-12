28a0291b2f317993952e7fecc1bf1a8d92cd6269
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-08-12 09:05:11 +0000

    main - 9582c5f fix(multiple): tokens specified in wrong category (#33660)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.0+sha-f3e6276');
+const VERSION = new Version('22.2.0-next.0+sha-9582c5f');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
