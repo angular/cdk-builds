3f8d458375141f44e128482aa4cf6e296d88e18e
tjshiu <35056071+tjshiu@users.noreply.github.com>
2026-08-12 21:15:46 +0000

    22.1.x - aa598a0 release: cut the v22.1.2 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.1+sha-b9b11cb');
+const VERSION = new Version('22.1.2+sha-aa598a0');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
