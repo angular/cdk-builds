e601f4fb43b0522397fbb44149251d467e74f4fc
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-08-12 09:24:43 +0000

    22.1.x - 025ec7d build: switch tests away from protractor (#33664)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.1+sha-ef99839');
+const VERSION = new Version('22.1.1+sha-025ec7d');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
