772ee8a9717aa64ac5dec800606e8f3a22d1c1d8
tjshiu <35056071+tjshiu@users.noreply.github.com>
2026-08-12 21:29:05 +0000

    main - 5fcd952 release: cut the v22.2.0-next.1 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.0+sha-50a6faf');
+const VERSION = new Version('22.2.0-next.1+sha-5fcd952');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
