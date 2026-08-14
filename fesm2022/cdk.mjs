2dc4392a8137816c91fd335145b240ecb2592253
Joy Serquiña <44146839+essjay05@users.noreply.github.com>
2026-08-14 08:25:29 +0000

    main - 1f13d60 docs(material/form-field): update form-field prefix suffix a11y docs (#33680)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.1+sha-5fcd952');
+const VERSION = new Version('22.2.0-next.1+sha-1f13d60');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
