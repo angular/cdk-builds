5bd66ab15f98bacde062f356827452c8210cfeaa
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-08-15 09:46:57 +0000

    main - af7d3d7 build: switch docs e2e tests away from protractor and run them (#33675)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.1+sha-1f13d60');
+const VERSION = new Version('22.2.0-next.1+sha-af7d3d7');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
