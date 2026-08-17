445a5a7b0b463a3bfda71c36f3631c60d8789791
Amit <145347884+amit0-01@users.noreply.github.com>
2026-08-17 12:56:24 +0000

    main - 8134529 docs(material/table): remove unnecessary template literals (#33543)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.1+sha-aa57b8f-with-local-changes');
+const VERSION = new Version('22.2.0-next.1+sha-8134529-with-local-changes');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
