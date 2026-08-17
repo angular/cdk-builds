a6dac71c9651fcfde68275ecadf620caf3f507dd
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-08-17 05:00:28 +0000

    main - 6a92017 docs(aria/menu): fix doc string examples (#33686)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.1+sha-4d2ff40-with-local-changes');
+const VERSION = new Version('22.2.0-next.1+sha-6a92017-with-local-changes');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
