d8fb609de525043bc3b4ce67325f080b8f24d7f4
Cheng-Hsuan Tsai <webcat@google.com>
2026-08-19 18:11:26 +0000

    22.1.x - 7aa08a1 build: enable stableTypeOrdering (#33699)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.3+sha-4e75d4d-with-local-changes');
+const VERSION = new Version('22.1.3+sha-7aa08a1-with-local-changes');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
