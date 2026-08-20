512a4fc82c032cd85d314302c21d398fec3201a8
Jeremy Mowery <jermowery@google.com>
2026-08-20 16:10:44 +0000

    main - cacab55 feat(material/menu): add disabledInteractive input to MatMenuItem (#33693)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.2+sha-ec2dbff-with-local-changes');
+const VERSION = new Version('22.2.0-next.2+sha-cacab55-with-local-changes');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
