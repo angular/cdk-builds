9bbeafd78afec00063a8c03890a47cc12e0fdb86
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-08-21 19:57:41 +0000

    main - cd5d811 build: fix broken lock file (#33714)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.2+sha-b91d6da');
+const VERSION = new Version('22.2.0-next.2+sha-cd5d811');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
