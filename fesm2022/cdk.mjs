c0e13ca62d3d187671b71fd13c876253a3d1d0b6
Angular Robot <angular-robot@google.com>
2026-08-21 09:27:22 +0000

    main - d5fa9e8 build: update all non-major dependencies (#33707)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.2+sha-5cccb18-with-local-changes');
+const VERSION = new Version('22.2.0-next.2+sha-d5fa9e8');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
