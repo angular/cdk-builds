fbc745f77a4e7860d90d160e598a746043bb3767
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-08-21 07:55:06 +0000

    main - 5cccb18 build: remove scorecard workflow (#33711)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.2+sha-cacab55-with-local-changes');
+const VERSION = new Version('22.2.0-next.2+sha-5cccb18-with-local-changes');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
