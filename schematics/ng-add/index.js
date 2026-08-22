3986923907521c483de5a130d4858e4ca5931963
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-08-22 08:17:25 +0000

    22.1.x - 6be9b4c test(material/sort): switch tests away from fakeAsync (#33716)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -26,6 +26,6 @@ function default_1() {
     // In order to align the CDK version with other Angular dependencies that are setup by
     // `@schematics/angular`, we use tilde instead of caret. This is default for Angular
     // dependencies in new CLI projects.
-    return (0, utility_1.addDependency)('@angular/cdk', `~22.1.3+sha-98842bf`, { existing: utility_1.ExistingBehavior.Skip });
+    return (0, utility_1.addDependency)('@angular/cdk', `~22.1.3+sha-6be9b4c`, { existing: utility_1.ExistingBehavior.Skip });
 }
 //# sourceMappingURL=index.js.map
