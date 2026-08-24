6537175d9d3505151900fee8e9a0e30a58c51071
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-08-24 07:29:52 +0000

    22.1.x - 7c71cdf test(multiple): address remaining fakeAsync usages in CDK (#33721)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.3+sha-6be9b4c');
+const VERSION = new Version('22.1.3+sha-7c71cdf');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
