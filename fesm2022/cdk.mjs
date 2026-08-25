7e9e3a08b985ff5f6b1c28c84732f67043c750cc
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-08-25 17:35:53 +0000

    main - e0fd08c fix(material/sidenav): prevent drawer from getting stuck when toggled rapidly (#33726)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.2+sha-5006b41');
+const VERSION = new Version('22.2.0-next.2+sha-e0fd08c');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
