b02d1537c30f7d27cdf4e60cc261a41d9ae157e3
Lazizbek Ergashev <lazerg2@gmail.com>
2026-08-26 22:38:29 +0000

    main - de579cc fix(aria/combobox): popup not closing when focus leaves it (#33697)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.3+sha-cd9c7da');
+const VERSION = new Version('22.2.0-next.3+sha-de579cc');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
