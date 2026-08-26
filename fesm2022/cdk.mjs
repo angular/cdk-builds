e219fe1e93a380e01afe7972c929f9286ef43056
Cheng-Hsuan Tsai <webcat@google.com>
2026-08-26 17:49:33 +0000

    main - cd9c7da fix(aria/menu): make MenuItem value optional (#33715)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.3+sha-3cc2d3b');
+const VERSION = new Version('22.2.0-next.3+sha-cd9c7da');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
