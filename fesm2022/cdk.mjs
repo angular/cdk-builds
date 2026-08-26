0a938c23b4759689d460de1909e351be6de943a7
Artur <arthurandrosovich@gmail.com>
2026-08-26 10:48:03 +0000

    main - 6956699 fix(cdk/table): show a real error message instead of a crash when a table column is missing (#33698)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.2+sha-4358fb0');
+const VERSION = new Version('22.2.0-next.2+sha-6956699');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
