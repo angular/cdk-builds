c13279c37b6064fe3f86bf46fcbef5fce1b73941
Andrei <andrei.caleniuc@uipath.com>
2026-08-27 09:06:56 +0000

    22.1.x - bef29a2 fix(multiple): add forms to cdk peer dependencies (#33666)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.4+sha-9a63c20');
+const VERSION = new Version('22.1.4+sha-bef29a2');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
