ff454c9d328ffe1451c16e77cf2d7eb1ed2c9bc4
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-08-28 18:25:36 +0000

    main - 46b918e fix(aria/menu): focus not moved into menu if items are delayed (#33743)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.3+sha-bcb3e79');
+const VERSION = new Version('22.2.0-next.3+sha-46b918e');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
