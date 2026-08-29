06c4ebba67b90a4cce655775c00365b50880c035
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-08-29 08:31:18 +0000

    22.1.x - 96b87fe test(multiple): remove most remaining fakeAsync usages (#33748)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.4+sha-9ff8a7c');
+const VERSION = new Version('22.1.4+sha-96b87fe');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
