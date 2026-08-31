2e29d4c34e893b486a713f2cf6a08076f4cedcea
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-08-31 15:33:46 +0000

    main - 5d64e39 feat(material/icon): add material symbol classes automatically (#33750)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.3+sha-d33df31');
+const VERSION = new Version('22.2.0-next.3+sha-5d64e39');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -26,6 +26,6 @@ function default_1() {
     // In order to align the CDK version with other Angular dependencies that are setup by
     // `@schematics/angular`, we use tilde instead of caret. This is default for Angular
     // dependencies in new CLI projects.
-    return (0, utility_1.addDependency)('@angular/cdk', `~22.2.0-next.3+sha-d33df31`, { existing: utility_1.ExistingBehavior.Skip });
+    return (0, utility_1.addDependency)('@angular/cdk', `~22.2.0-next.3+sha-5d64e39`, { existing: utility_1.ExistingBehavior.Skip });
 }
 //# sourceMappingURL=index.js.map
