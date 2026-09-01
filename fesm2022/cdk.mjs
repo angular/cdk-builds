6838b38bc85fba6d80d869339817eeac024e6d1c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-09-01 15:56:53 +0000

    main - ab16b1f fix(multiple): include material symbols in selectors (#33754)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.3+sha-5d64e39');
+const VERSION = new Version('22.2.0-next.3+sha-ab16b1f');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
