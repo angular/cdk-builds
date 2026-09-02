8f2898142c97e674e178f46f90fae981eab1e5d0
Cheng-Hsuan Tsai <webcat@google.com>
2026-09-02 06:09:46 +0000

    main - 0b81ad7 refactor(aria/toolbar): remove selection behavior (#33747)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.3+sha-ab16b1f');
+const VERSION = new Version('22.2.0-next.3+sha-0b81ad7');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
