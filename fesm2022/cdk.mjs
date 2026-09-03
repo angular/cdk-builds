3d7c065854864c7c2e1540dfdbde2e0b76c6382e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-09-03 17:50:50 +0000

    22.1.x - 690ddb4 fix(cdk/overlay): expose currently open overlays (#33768)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.5+sha-8257c4d');
+const VERSION = new Version('22.1.5+sha-690ddb4');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
