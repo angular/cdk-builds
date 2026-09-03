2148469564e73f8331fad51d938d4758d895f922
Angular Robot <angular-robot@google.com>
2026-09-03 09:05:51 +0000

    22.1.x - b36d693 build: update bazel dependencies (#33766)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.5+sha-caf0973');
+const VERSION = new Version('22.1.5+sha-b36d693');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
