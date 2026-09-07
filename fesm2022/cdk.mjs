92f9c6f181ce884f1c0534add817dfc8a9285e08
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-09-07 05:22:18 +0000

    22.1.x - 3ae909d refactor(google-maps): move types reference in maps package (#33773)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.5+sha-690ddb4');
+const VERSION = new Version('22.1.5+sha-3ae909d');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
