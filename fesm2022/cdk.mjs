b6f7299d675a099337065a3d27e6d42405765c52
Michael Small <85510853+msmallest@users.noreply.github.com>
2026-09-09 08:54:16 +0000

    main - cbc5e96 docs: add signal form example + copy for `matInput` (#33779)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.4+sha-40ee191');
+const VERSION = new Version('22.2.0-next.4+sha-cbc5e96');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
