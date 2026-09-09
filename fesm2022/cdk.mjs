d5176c31244f69540002c792aa953c378c0df309
Michael Small <85510853+msmallest@users.noreply.github.com>
2026-09-09 09:03:09 +0000

    main - 87357a1 docs: add signal forms examples to Checkbox and Chips (#33623)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.4+sha-cbc5e96');
+const VERSION = new Version('22.2.0-next.4+sha-87357a1');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
