8044c14bcbe72ba4e65a70f946111deb65f38810
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-09-12 08:00:36 +0000

    main - 7e73582 docs: update documentation to use Material Symbols (#33796)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.5+sha-aad74d3');
+const VERSION = new Version('22.2.0-next.5+sha-7e73582');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
