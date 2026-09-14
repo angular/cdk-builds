ef91ea96906356f9b4a0337fb9268909030492ef
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-09-14 17:52:44 +0000

    main - b0019f8 fix(material/core): generate Material Symbols in schematic (#33797)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.5+sha-caf8df8');
+const VERSION = new Version('22.2.0-next.5+sha-b0019f8');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
