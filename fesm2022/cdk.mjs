922e13a99cf7f5b28b8e23e5613c79a2b9267e75
Lajos Szoke <63732287+laliconfigcat@users.noreply.github.com>
2026-09-14 15:05:34 +0000

    main - d3acf18 feat(material-luxon-adapter): add option to set zone (#33579)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.5+sha-7e73582');
+const VERSION = new Version('22.2.0-next.5+sha-d3acf18');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
