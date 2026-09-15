1f8732b89d0b32e133f6a6350f1eff10798767d7
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-09-15 17:07:01 +0000

    main - fd3f028 fix(material/icon): false positive in font detection (#33805)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.5+sha-84336cf');
+const VERSION = new Version('22.2.0-next.5+sha-fd3f028');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
