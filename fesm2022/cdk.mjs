9f0869cfb8da4d19a87b25c8853cb1f5da9937f8
solt_onigiri_ <kano.nests@gmail.com>
2026-09-15 07:41:29 +0000

    main - a3936ea fix(cdk/table): update rows when row definitions change (#33670)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.5+sha-b0019f8');
+const VERSION = new Version('22.2.0-next.5+sha-a3936ea');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
