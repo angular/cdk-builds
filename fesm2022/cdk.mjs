3b7ec83d0e6291a10f27341471a7550f6146ba5d
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-09-16 09:07:09 +0000

    22.1.x - 0cf603b build: update to pnpm 12 (#33802) (#33815)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.6+sha-510b393');
+const VERSION = new Version('22.1.6+sha-0cf603b');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
