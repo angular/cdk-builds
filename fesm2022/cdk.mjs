a4fafbfdf0d2861dd97c2c9d158d25b9641dded8
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-09-16 09:33:56 +0000

    main - ce87196 docs: release notes for the v22.1.7 release

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.5+sha-fd3f028');
+const VERSION = new Version('22.2.0-next.5+sha-ce87196');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
