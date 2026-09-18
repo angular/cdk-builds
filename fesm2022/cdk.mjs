2f4fbf8ccef60aebdabdfcfb1c8aeee861f8a28f
Michael Small <85510853+msmallest@users.noreply.github.com>
2026-09-18 13:59:34 +0000

    22.1.x - d8a86db docs: mention forms support in outstanding controls (#33823)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.7+sha-e950f29');
+const VERSION = new Version('22.1.7+sha-d8a86db');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
