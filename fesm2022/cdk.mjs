97a30293f0cdaa6a7e939b22c5f7353bb44f529e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-09-18 18:39:10 +0000

    21.2.x - d63c34b docs: use correct version when redirecting externally (#33808) (#33841)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.2.14+sha-8fe4d6a');
+const VERSION = new Version('21.2.14+sha-d63c34b');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
