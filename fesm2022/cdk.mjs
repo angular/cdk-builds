e286473926c59495761ef5b2bd3db411ed5eaa16
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-09-18 15:21:08 +0000

    22.1.x - 377256d fix(aria/accordion): ensure right attribute is set when used as a host directive (#33839)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.1.7+sha-d8a86db');
+const VERSION = new Version('22.1.7+sha-377256d');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
