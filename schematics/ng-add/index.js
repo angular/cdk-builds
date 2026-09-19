b7265e8ee7da615cc11d8ae6955f3da84d4e9619
Angular Robot <angular-robot@google.com>
2026-09-19 07:31:37 +0000

    main - 008280b build: update all non-major dependencies (#33824)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -26,6 +26,6 @@ function default_1() {
     // In order to align the CDK version with other Angular dependencies that are setup by
     // `@schematics/angular`, we use tilde instead of caret. This is default for Angular
     // dependencies in new CLI projects.
-    return (0, utility_1.addDependency)('@angular/cdk', `~22.3.0-next.0+sha-6a9f2d0`, { existing: utility_1.ExistingBehavior.Skip });
+    return (0, utility_1.addDependency)('@angular/cdk', `~22.3.0-next.0+sha-008280b`, { existing: utility_1.ExistingBehavior.Skip });
 }
 //# sourceMappingURL=index.js.map
