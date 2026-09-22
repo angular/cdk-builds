a9a514dc7014cf3d4748248782d974044e8546d2
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-09-22 19:05:47 +0000

    22.2.x - 7e06f05 docs: add .gitignore to stackblitz template (#33847)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-rc.0+sha-67c4cfb');
+const VERSION = new Version('22.2.0-rc.0+sha-7e06f05');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
