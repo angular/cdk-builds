f42493469b9c5fba7054a201ebf001f6c6766b43
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-09-22 19:59:31 +0000

    22.2.x - ddbb73a docs: redo custom form field guide for signal forms

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-rc.0+sha-7e06f05');
+const VERSION = new Version('22.2.0-rc.0+sha-ddbb73a');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
