12f891e714281104aa991aa04566087fe3ebe9ba
Angular Robot <angular-robot@google.com>
2026-09-24 09:49:08 +0000

    22.2.x - d98f3ff build: update all github actions (#33826)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0+sha-68d65c2');
+const VERSION = new Version('22.2.0+sha-d98f3ff');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
