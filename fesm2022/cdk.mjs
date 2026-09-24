2d281bf4a19f9d7288d049bb25e5c3dda33b9e29
Michael Small <85510853+msmallest@users.noreply.github.com>
2026-09-24 09:45:32 +0000

    main - aa51ba5 docs: add signal form example for autocomplete (#33852)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.3.0-next.0+sha-0c41bc7');
+const VERSION = new Version('22.3.0-next.0+sha-aa51ba5');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
