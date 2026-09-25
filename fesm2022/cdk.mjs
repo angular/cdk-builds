132cb9c48c5660117872e4ba1cf75826d32eead8
Shuaib Hasan Akib <48770016+MeAkib@users.noreply.github.com>
2026-09-25 09:26:50 +0000

    main - 8d9f52c docs(material/tree): show testing API docs (#33862)

--- fesm2022/cdk.mjs
+++ fesm2022/cdk.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.3.0-next.0+sha-aa51ba5');
+const VERSION = new Version('22.3.0-next.0+sha-8d9f52c');
 
 export { VERSION };
 //# sourceMappingURL=cdk.mjs.map
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -26,6 +26,6 @@ function default_1() {
     // In order to align the CDK version with other Angular dependencies that are setup by
     // `@schematics/angular`, we use tilde instead of caret. This is default for Angular
     // dependencies in new CLI projects.
-    return (0, utility_1.addDependency)('@angular/cdk', `~22.3.0-next.0+sha-aa51ba5`, { existing: utility_1.ExistingBehavior.Skip });
+    return (0, utility_1.addDependency)('@angular/cdk', `~22.3.0-next.0+sha-8d9f52c`, { existing: utility_1.ExistingBehavior.Skip });
 }
 //# sourceMappingURL=index.js.map
